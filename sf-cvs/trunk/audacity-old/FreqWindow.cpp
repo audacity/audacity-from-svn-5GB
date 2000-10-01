6917fe2af34f12c0cb429d97b46ef721f82cd781
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2000-10-01 23:50:02 +0000

    FreqWindow, for displaying spectrum plots

---
+++ sf-cvs/trunk/audacity-old/FreqWindow.cpp
@@ -0,0 +1,169 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  FreqWindow.cpp
+
+  Dominic Mazzoni
+
+**********************************************************************/
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include <wx/wxprec.h>
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#include <wx/brush.h>
+#include <wx/image.h>
+#endif
+
+#include "FreqWindow.h"
+
+enum {
+  FirstID = 7000,
+  
+  FreqCloseButtonID,
+  FreqAlgChoiceID,
+  FreqSizeChoiceID,
+  FreqFuncChoiceID
+};
+
+FreqWindow *gFreqWindow = NULL;
+
+#ifdef __WXMAC__
+#define FREQ_WINDOW_WIDTH 400
+#define FREQ_WINDOW_HEIGHT 300
+#endif
+
+#ifdef __WXGTK__
+#define FREQ_WINDOW_WIDTH 410
+#define FREQ_WINDOW_HEIGHT 320
+#endif
+
+#ifdef __WXMSW__
+#define FREQ_WINDOW_WIDTH 400
+#define FREQ_WINDOW_HEIGHT 300
+#endif
+
+void InitFreqWindow(wxFrame *parent)
+{
+  wxPoint where;
+
+  where.x = 150;
+  where.y = 150;
+
+  gFreqWindow = new FreqWindow(parent, -1, "Frequency Analysis",
+					 where);
+  gFreqWindow->Show(TRUE);
+}
+
+// FreqWindow
+
+BEGIN_EVENT_TABLE(FreqWindow, wxMiniFrame)
+  EVT_CLOSE  (                      FreqWindow::OnCloseWindow)
+  EVT_PAINT  (                      FreqWindow::OnPaint)
+  EVT_BUTTON (FreqCloseButtonID,    FreqWindow::OnCloseWindow)
+  EVT_CHOICE (FreqAlgChoiceID,      FreqWindow::OnAlgChoice)
+  EVT_CHOICE (FreqSizeChoiceID,     FreqWindow::OnSizeChoice)
+  EVT_CHOICE (FreqFuncChoiceID,     FreqWindow::OnFuncChoice)
+END_EVENT_TABLE()
+
+FreqWindow::FreqWindow(wxFrame* parent, wxWindowID id, const wxString& title,
+				   const wxPoint& pos) :
+  wxMiniFrame(parent, id, title, pos,
+			  wxSize(FREQ_WINDOW_WIDTH, FREQ_WINDOW_HEIGHT),
+              wxTINY_CAPTION_HORIZ | wxSTAY_ON_TOP),
+  mData(NULL)
+{
+  mPlotRect.x = 10;
+  mPlotRect.y = 10;
+  mPlotRect.width = 380;
+  mPlotRect.height = 240;
+
+  mCloseButton = new wxButton(this, FreqCloseButtonID,
+							  "Close",
+							  wxPoint(320, 260),
+							  wxSize(60, 30));
+
+  wxString algChoiceStrings[4] = {"Spectrum",
+								  "Log Spectrum",
+								  "Cepstrum",
+								  "Log Cepstrum"};
+  
+  mAlgChoice = new wxChoice(this, FreqAlgChoiceID,
+							wxPoint(10, 260),
+							wxSize(90, 30),
+							4, algChoiceStrings);
+
+  wxString sizeChoiceStrings[7] = {"256",
+								   "512",
+								   "1024",
+								   "2048",
+								   "4096",
+								   "8192",
+								   "16384"};
+  
+  mSizeChoice = new wxChoice(this, FreqAlgChoiceID,
+							 wxPoint(110, 260),
+							 wxSize(90, 30),
+							 7, sizeChoiceStrings);  
+
+  wxString funcChoiceStrings[3] = {"Rectangular",
+								   "Hamming",
+								   "Hanning"};
+  
+  mFuncChoice = new wxChoice(this, FreqFuncChoiceID,
+							 wxPoint(220, 260),
+							 wxSize(90, 30),
+							 3, funcChoiceStrings);  
+  
+  mBackgroundBrush.SetColour(wxColour(204, 204, 204));
+  mBackgroundPen.SetColour(wxColour(204, 204, 204));
+}
+
+FreqWindow::~FreqWindow()
+{
+  delete mCloseButton;
+  delete mAlgChoice;
+  delete mSizeChoice;
+  delete mFuncChoice;
+  if (mData)
+	delete[] mData;
+}
+
+void FreqWindow::OnAlgChoice(wxCommandEvent &event)
+{
+}
+
+void FreqWindow::OnSizeChoice(wxCommandEvent &event)
+{
+}
+
+void FreqWindow::OnFuncChoice(wxCommandEvent &event)
+{
+}
+
+void FreqWindow::OnPaint(wxPaintEvent &evt)
+{
+  wxPaintDC dc(this);
+
+  int width, height;
+  GetSize(&width, &height);
+  
+  dc.SetBrush(mBackgroundBrush);
+  dc.SetPen(mBackgroundPen);
+  dc.DrawRectangle(0, 0, width, height);
+
+  dc.SetPen(*wxBLACK_PEN);
+  dc.SetBrush(*wxWHITE_BRUSH);
+  dc.DrawRectangle(mPlotRect);
+}
+
+void FreqWindow::OnCloseWindow(wxCloseEvent& WXUNUSED(event))
+{
+  this->Show(FALSE);
+}
---
+++ sf-cvs/trunk/audacity-old/FreqWindow.h
@@ -0,0 +1,66 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  FreqWindow.h
+
+  Dominic Mazzoni
+
+**********************************************************************/
+
+#ifndef __AUDACITY_FREQ_WINDOW__
+#define __AUDACITY_FREQ_WINDOW__
+
+#include <wx/brush.h>
+#include <wx/button.h>
+#include <wx/choice.h>
+#include <wx/gdicmn.h>
+#include <wx/pen.h>
+#include <wx/minifram.h>
+
+class FreqWindow;
+
+class TrackList;
+
+extern FreqWindow *gFreqWindow;
+
+void InitFreqWindow(wxFrame *parent);
+
+class FreqWindow: public wxMiniFrame
+{
+public:
+  FreqWindow(wxFrame *parent, wxWindowID id,
+			 const wxString& title,
+			 const wxPoint& pos);
+
+  virtual ~FreqWindow();
+
+  //void Plot(TrackList *list);
+  
+  void OnCloseWindow(wxCloseEvent& event);
+  void OnPaint(wxPaintEvent& event);
+  void OnAlgChoice(wxCommandEvent& event);
+  void OnSizeChoice(wxCommandEvent& event);
+  void OnFuncChoice(wxCommandEvent& event);
+
+  //void Recalc();
+  
+private:
+  
+  wxBrush   mBackgroundBrush;
+  wxPen     mBackgroundPen;
+
+  wxButton  *mCloseButton;
+  wxChoice  *mAlgChoice;
+  wxChoice  *mSizeChoice;
+  wxChoice  *mFuncChoice;
+
+  wxRect    mPlotRect;
+
+  int       mDataLen;
+  float     *mData;
+    
+DECLARE_EVENT_TABLE()
+};
+
+#endif
