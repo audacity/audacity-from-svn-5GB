82a0eb5297307267927f74cb8898c78c6c22155d
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2000-11-26 22:06:00 +0000

    Preferences/Config file support (includes Mac code to find the Preferences folder)

---
+++ sf-cvs/trunk/audacity-old/Prefs.cpp
@@ -0,0 +1,93 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  Prefs.cpp
+
+  Dominic Mazzoni
+
+**********************************************************************/
+
+#ifdef __WXMAC__
+#include <Files.h>
+#include <Folders.h>
+
+/* prototype of MoreFiles fn, included in wxMac already */
+pascal	OSErr	FSpGetFullPath(const FSSpec *spec,
+							               short *fullPathLength,
+							               Handle *fullPath);
+
+#endif
+
+#include "AudacityApp.h"
+#include "Prefs.h"
+
+wxConfig *gPrefs;
+
+void InitPreferences()
+{
+  wxString vendorName = "Audacity";
+  wxString appName = "Audacity";
+
+  wxTheApp->SetVendorName(vendorName);
+  wxTheApp->SetAppName(appName);
+
+  #ifndef __WXMAC__
+  
+  gPrefs = wxConfigBase::Get();
+  
+  #else
+  
+  wxString prefsFileName = "Audacity Preferences";
+  wxString prefsPath;
+  FSSpec theSpec;
+  OSErr err;
+  
+  err = FindFolder(kOnAppropriateDisk,
+                   'pref',
+                   true,
+                   &theSpec.vRefNum,
+                   &theSpec.parID);
+  
+  if (err) {
+    gPrefs = (wxConfig *)wxConfigBase::Get();
+    return;
+  }
+
+  Handle nameh;
+  short namelen;
+  theSpec.name[0] = 0;
+  err = FSpGetFullPath(&theSpec,
+                       &namelen,
+                       &nameh);
+  
+  if (err) {
+    gPrefs = (wxConfig *)wxConfigBase::Get();
+    return;
+  }
+  
+  namelen--; // FSpGetFullPath returns an extra ':' we don't want
+  HLock(nameh);
+  char *str = new char[namelen+1];
+  memcpy(str, (char *)*nameh, namelen);
+  str[namelen] = 0;
+  HUnlock(nameh);
+  DisposeHandle(nameh);
+  
+  prefsPath = ::wxMac2UnixFilename(str);
+  prefsPath += prefsFileName;
+  
+  delete[] str;
+
+  gPrefs = (wxConfig *) new wxFileConfig(appName,
+                                         vendorName,
+                                         prefsPath,
+                                         prefsPath,
+                                         wxCONFIG_USE_LOCAL_FILE);
+  
+  wxConfigBase::Set(gPrefs);
+  
+  #endif
+
+  gPrefs->Write("/Version", AUDACITY_VERSION_STRING);
+}
---
+++ sf-cvs/trunk/audacity-old/Prefs.h
@@ -0,0 +1,16 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  Prefs.h
+
+  Dominic Mazzoni
+
+**********************************************************************/
+
+#include <wx/config.h>
+
+void InitPreferences();
+
+extern wxConfig* gPrefs;
+
