baf475a856770a5fbbcd7191eeee786d72154696
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2001-03-24 18:31:45 +0000

    Changed version number to 0.95 and fixed MS Windows resource file

--- sf-cvs/trunk/audacity-old/AudacityApp.h
+++ sf-cvs/trunk/audacity-old/AudacityApp.h
@@ -12,7 +12,7 @@
 **********************************************************************/
 
 // Increment this every time you release a new version
-#define AUDACITY_VERSION_STRING "0.94"
+#define AUDACITY_VERSION_STRING "0.95"
 
 // Don't change this unless the file format changes
 // in an irrevocable way
