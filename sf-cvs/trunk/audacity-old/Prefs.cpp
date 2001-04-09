fb0e26f6dfcbcc86635cc43817aed3ee177678eb
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2001-04-09 07:01:42 +0000

    wxMac now implements Preferences, so we don't need to special-case the Mac side anymore

--- sf-cvs/trunk/audacity-old/Prefs.cpp
+++ sf-cvs/trunk/audacity-old/Prefs.cpp
@@ -65,64 +65,9 @@ void InitPreferences()
 
   wxTheApp->SetVendorName(vendorName);
   wxTheApp->SetAppName(appName);
-
-  #ifndef __WXMAC__
   
   gPrefs = new wxConfig(appName);
   wxConfigBase::Set(gPrefs);
-
-  #else
-  
-  wxString prefsFileName = "Audacity Preferences";
-  wxString prefsPath;
-  FSSpec theSpec;
-  OSErr err;
-  
-  err = FindFolder(kOnAppropriateDisk,
-                   'pref',
-                   true,
-                   &theSpec.vRefNum,
-                   &theSpec.parID);
-  
-  if (err) {
-    gPrefs = (wxConfig *)wxConfigBase::Get();
-    return;
-  }
-
-  Handle nameh;
-  short namelen;
-  theSpec.name[0] = 0;
-  err = FSpGetFullPath(&theSpec,
-                       &namelen,
-                       &nameh);
-  
-  if (err) {
-    gPrefs = (wxConfig *)wxConfigBase::Get();
-    return;
-  }
-  
-  namelen--; // FSpGetFullPath returns an extra ':' we don't want
-  HLock(nameh);
-  char *str = new char[namelen+1];
-  memcpy(str, (char *)*nameh, namelen);
-  str[namelen] = 0;
-  HUnlock(nameh);
-  DisposeHandle(nameh);
-  
-  prefsPath = ::wxMac2UnixFilename(str);
-  prefsPath += prefsFileName;
-  
-  delete[] str;
-
-  gPrefs = (wxConfig *) new wxFileConfig(appName,
-                                         vendorName,
-                                         "",
-                                         prefsPath,
-                                         wxCONFIG_USE_GLOBAL_FILE);
-  
-  wxConfigBase::Set(gPrefs);
-  
-  #endif
 
   gPrefs->Write("/Version", AUDACITY_VERSION_STRING);
 }
@@ -130,8 +75,8 @@ void InitPreferences()
 void FinishPreferences()
 {
   if (gPrefs) {
-	wxConfigBase::Set(NULL);
-	delete gPrefs;
-	gPrefs = NULL;
+		wxConfigBase::Set(NULL);
+		delete gPrefs;
+		gPrefs = NULL;
   }
 }
