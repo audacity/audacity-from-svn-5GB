0c08100e35bf768751bda301df1fd4caaaf4798b
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2001-09-02 05:22:23 +0000

    *** empty log message ***

--- sf-cvs/trunk/audacity-old/configmac.h
+++ sf-cvs/trunk/audacity-old/configmac.h
@@ -4,12 +4,11 @@
 #define AUDACITY_CREATOR      'auDy'
 #define AUDACITY_PROJECT_TYPE 'auDp'
 
-// Use xaudio for importing MP3 on the mac
-#define USE_XAUDIO
-#define MP3SUPPORT
+// Use libmad for importing MP3 on the mac
+//#define MP3SUPPORT
 
 // No Ogg Vorbis support (yet)
-// #define USE_LIBVORBIS
+#define USE_LIBVORBIS
 
 // ID3 support
 #define USE_ID3LIB
