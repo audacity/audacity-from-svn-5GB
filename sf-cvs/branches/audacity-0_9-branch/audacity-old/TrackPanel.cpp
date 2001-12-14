036005acfe76468dedc27c5855366e3c3efee241
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2001-12-14 08:00:24 +0000

    Checked in Jonathan Ryshpan's shift-select code

--- sf-cvs/branches/audacity-0_9-branch/audacity-old/TrackPanel.cpp
+++ sf-cvs/branches/audacity-0_9-branch/audacity-old/TrackPanel.cpp
@@ -490,15 +490,21 @@ void TrackPanel::HandleSelect(wxMouseEvent & event)
             double selend =
                 mViewInfo->h + ((event.m_x - r.x) / mViewInfo->zoom);
 
-            if (selend > mViewInfo->sel1) {
+            // Thanks to Jonathan Ryshpan for this new logic which
+            // grabs either the left or right side, depending on
+            // which is closer:
+            if (selend >= mViewInfo->sel1) {
                mViewInfo->sel1 = selend;
                mSelStart = mViewInfo->sel0;
-            } else if (selend < mViewInfo->sel0) {
+            } else if (selend <= mViewInfo->sel0) {
                mViewInfo->sel0 = selend;
                mSelStart = mViewInfo->sel1;
             } else {
-               mViewInfo->sel1 = selend;
-               mSelStart = mViewInfo->sel0;
+               if ((mViewInfo->sel1 - selend) <= (selend - mViewInfo->sel0)) {
+                  mViewInfo->sel1 = selend;
+               } else {
+                  mViewInfo->sel0 = selend;
+               }
             }
 
             mListener->
