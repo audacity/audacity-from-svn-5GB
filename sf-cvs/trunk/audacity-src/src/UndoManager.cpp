8a819e7a657fbf9d2959ff3dd9b7f9bc7188651b
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2002-01-22 10:57:38 +0000

    Make 'Undo' set the last saved selection, not the previous one.

--- sf-cvs/trunk/audacity-src/src/UndoManager.cpp
+++ sf-cvs/trunk/audacity-src/src/UndoManager.cpp
@@ -145,10 +145,10 @@ TrackList *UndoManager::Undo(double *sel0, double *sel1)
 {
    wxASSERT(UndoAvailable());
 
-   current--;
-
    *sel0 = stack[current]->sel0;
    *sel1 = stack[current]->sel1;
+
+   current--;
 
    return stack[current]->tracks;
 }
