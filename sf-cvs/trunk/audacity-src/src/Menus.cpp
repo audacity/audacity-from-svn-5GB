a7c849fe395c75adb62e99ee553a259047291e3c
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2002-01-31 18:03:32 +0000

    Fix a thinko in new Save Changes code.

--- sf-cvs/trunk/audacity-src/src/Menus.cpp
+++ sf-cvs/trunk/audacity-src/src/Menus.cpp
@@ -215,7 +215,7 @@ void AudacityProject::OnUpdateMenus(wxUpdateUIEvent & event)
    // Note that the titles of the menus here are dependent on the
    // titles above.
 
-   mFileMenu->Enable(SaveID, mUndoManager.UndoAvailable());
+   mFileMenu->Enable(SaveID, mUndoManager.UnsavedChanges());
    mEditMenu->Enable(mEditMenu->FindItem(_("Undo")),
                      mUndoManager.UndoAvailable());
    mEditMenu->Enable(mEditMenu->FindItem(_("Redo")),
