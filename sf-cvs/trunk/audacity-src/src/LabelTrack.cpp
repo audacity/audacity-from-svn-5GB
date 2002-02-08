830779b820ec014222398af59c2402509b1caea2
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2002-02-08 05:51:45 +0000

    Fix a segfault in Cut() for label tracks.

--- sf-cvs/trunk/audacity-src/src/LabelTrack.cpp
+++ sf-cvs/trunk/audacity-src/src/LabelTrack.cpp
@@ -367,7 +367,7 @@ void LabelTrack::Cut(double t0, double t1, VTrack ** dest)
          len--;
          i--;
       }
-      if (mLabels[i]->t > t1)
+      else if (mLabels[i]->t > t1)
          mLabels[i]->t -= (t1 - t0);
    }
    ((LabelTrack *) (*dest))->mClipLen = (t1 - t0);
@@ -425,7 +425,7 @@ void LabelTrack::Clear(double t0, double t1)
          len--;
          i--;
       }
-      if (mLabels[i]->t > t1)
+      else if (mLabels[i]->t > t1)
          mLabels[i]->t -= (t1 - t0);
    }
 }
