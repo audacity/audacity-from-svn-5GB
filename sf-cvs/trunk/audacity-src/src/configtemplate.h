842e5bc3f7f9e01214db76bd5965c7c97fba5e1c
habes <habes@47890b92-0858-11df-a26f-8b716316a5bc>
2002-04-21 07:15:40 +0000

    the off_t check was causing weird compilation errors, and was unneccesary anyway

--- sf-cvs/trunk/audacity-src/src/configtemplate.h
+++ sf-cvs/trunk/audacity-src/src/configtemplate.h
@@ -1,59 +1,70 @@
-/* configtemplate.h.  Generated automatically from configure.in by autoheader.  */
+/* src/configtemplate.h.  Generated from configure.in by autoheader.  */
 
-/* Define if you have the <inttypes.h> header file. */
+/* Define to 1 if you have the <inttypes.h> header file. */
 #undef HAVE_INTTYPES_H
 
-/* Define if you have the <memory.h> header file. */
+/* Define to 1 if you have the <memory.h> header file. */
 #undef HAVE_MEMORY_H
 
-/* Define if you have the <stdint.h> header file. */
+/* Define to 1 if you have the <stdint.h> header file. */
 #undef HAVE_STDINT_H
 
-/* Define if you have the <stdlib.h> header file. */
+/* Define to 1 if you have the <stdlib.h> header file. */
 #undef HAVE_STDLIB_H
 
-/* Define if you have the <strings.h> header file. */
+/* Define to 1 if you have the <strings.h> header file. */
 #undef HAVE_STRINGS_H
 
-/* Define if you have the <string.h> header file. */
+/* Define to 1 if you have the <string.h> header file. */
 #undef HAVE_STRING_H
 
-/* Define if you have the <sys/stat.h> header file. */
+/* Define to 1 if you have the <sys/stat.h> header file. */
 #undef HAVE_SYS_STAT_H
 
-/* Define if you have the <sys/types.h> header file. */
+/* Define to 1 if you have the <sys/types.h> header file. */
 #undef HAVE_SYS_TYPES_H
 
-/* Define if you have the <unistd.h> header file. */
+/* Define to 1 if you have the <unistd.h> header file. */
 #undef HAVE_UNISTD_H
 
-/* Define if you have the ANSI C header files. */
+/* Define if mp3 support is implemented */
+#undef MP3SUPPORT
+
+/* Define to the address where bug reports for this package should be sent. */
+#undef PACKAGE_BUGREPORT
+
+/* Define to the full name of this package. */
+#undef PACKAGE_NAME
+
+/* Define to the full name and version of this package. */
+#undef PACKAGE_STRING
+
+/* Define to the one symbol short name of this package. */
+#undef PACKAGE_TARNAME
+
+/* Define to the version of this package. */
+#undef PACKAGE_VERSION
+
+/* Define to 1 if you have the ANSI C header files. */
 #undef STDC_HEADERS
 
-/* Define to empty if `const' does not conform to ANSI C. */
-#undef const
-
-/* Define to `long' if <sys/types.h> does not define. */
-#undef off_t
-
-/* Define to `unsigned' if <sys/types.h> does not define. */
-#undef size_t
+/* Define if LADSPA plug-ins are enabled */
+#undef USE_LADSPA
 
 /* Define if libid3tag is present */
 #undef USE_LIBID3TAG
 
-/* Define if the ogg vorbis decoding library is present */
-#undef USE_LIBVORBIS
-
-/* Define if at least one supported library for mp3 functions is present */
-#undef MP3SUPPORT
-
 /* Define if mp3 support is implemented with the libmad library */
 #undef USE_LIBMAD
 
-/* Define if wavelet support is implemented with the wave++ library */
+/* Define if the ogg vorbis decoding library is present */
+#undef USE_LIBVORBIS
+
+/* Define if wave++ is present */
 #undef USE_WAVELET
 
-/* Define if LADSPA plug-in support is enabled */
-#undef USE_LADSPA
+/* Define to empty if `const' does not conform to ANSI C. */
+#undef const
 
+/* Define to `unsigned' if <sys/types.h> does not define. */
+#undef size_t
