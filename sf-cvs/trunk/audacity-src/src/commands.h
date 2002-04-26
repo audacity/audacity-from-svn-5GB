960564ac1e40a0c391273cea26a9f43d351763e6
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2002-04-26 21:53:50 +0000

    Adding some menu accelerators and marking strings for translation.

--- sf-cvs/trunk/audacity-src/src/commands.h
+++ sf-cvs/trunk/audacity-src/src/commands.h
@@ -139,98 +139,98 @@ enum {
    wxString pcmFormat = sf_header_name(format & SF_FORMAT_TYPEMASK);
 
    mExportString.Printf(_("&Export as %s..."), pcmFormat.c_str());
-   mExportSelectionString.Printf(_("Export &Selection as %s..."),
+   mExportSelectionString.Printf(_("Export Selection as %s..."),
                                  pcmFormat.c_str());
-   wxString lossyFormat = gPrefs->Read("/FileFormats/LossyExportFormat", "MP3");
+   wxString lossyFormat = gPrefs->Read(_("/FileFormats/LossyExportFormat"), "MP3");
    mExportLossyString.Printf(_("Export as %s..."), lossyFormat.c_str());
    mExportSelectionLossyString.Printf(_("Export Selection as %s..."),
                                       lossyFormat.c_str());
 
    // File menu
-   CMD_ADDMENU("New\tCtrl+N", "New", OnNew, fileMenu, enabledMenu);
-   CMD_ADDMENU("Open...\tCtrl+O", "Open", OnOpen, fileMenu, enabledMenu);
-   CMD_ADDMENU("Close\tCtrl+W", "Close", OnClose, fileMenu, enabledMenu);
-   CMD_ADDMENU("Save Project\tCtrl+S", "Save Project", OnSave, fileMenu, enabledMenu);
-   CMD_ADDMENU("Save Project &As...", "Save Project As", OnSaveAs, fileMenu, enabledMenu);
+   CMD_ADDMENU(_("&New\tCtrl+N"), _("New"), OnNew, fileMenu, enabledMenu);
+   CMD_ADDMENU(_("&Open...\tCtrl+O"), _("Open"), OnOpen, fileMenu, enabledMenu);
+   CMD_ADDMENU(_("&Close\tCtrl+W"), _("Close"), OnClose, fileMenu, enabledMenu);
+   CMD_ADDMENU(_("&Save Project\tCtrl+S"), _("Save Project"), OnSave, fileMenu, enabledMenu);
+   CMD_ADDMENU(_("Save Project &As..."), _("Save Project As"), OnSaveAs, fileMenu, enabledMenu);
 
-   CMD_ADDMENUSEP(mExportString, "Export As", OnExportMix, fileMenu, enabledMenu);
-   CMD_ADDMENU(mExportSelectionString, "Export Selection As", OnExportSelection, fileMenu, enabledMenu);
+   CMD_ADDMENUSEP(mExportString, _("Export As"), OnExportMix, fileMenu, enabledMenu);
+   CMD_ADDMENU(mExportSelectionString, _("Export Selection As"), OnExportSelection, fileMenu, enabledMenu);
 
-   CMD_ADDMENUSEP(mExportLossyString, "Export As (lossy)", OnExportLossyMix, fileMenu, enabledMenu);
-   CMD_ADDMENU(mExportSelectionLossyString, "Export Selection As (lossy)", OnExportLossySelection, fileMenu, enabledMenu);
+   CMD_ADDMENUSEP(mExportLossyString, _("Export As (lossy)"), OnExportLossyMix, fileMenu, enabledMenu);
+   CMD_ADDMENU(mExportSelectionLossyString, _("Export Selection As (lossy)"), OnExportLossySelection, fileMenu, enabledMenu);
 
-   CMD_ADDMENUSEP("Export &Labels...", "Export Labels", OnExportLabels, fileMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("Export &Labels..."), _("Export Labels"), OnExportLabels, fileMenu, enabledMenu);
 
-   CMD_ADDMENUSEP("Preferences...\tCtrl+P", "Preferences", OnPreferences, fileMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("&Preferences...\tCtrl+P"), _("Preferences"), OnPreferences, fileMenu, enabledMenu);
 
-   CMD_ADDMENUSEP("E&xit", "Exit", OnExit, fileMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("E&xit"), _("Exit"), OnExit, fileMenu, enabledMenu);
 
    // Edit menu
-   CMD_ADDMENU("Undo\tCtrl+Z", "Undo", Undo, editMenu, enabledMenu);
-   CMD_ADDMENU("Redo\tCtrl+R", "Redo", Redo, editMenu, enabledMenu);
+   CMD_ADDMENU(_("Undo\tCtrl+Z"), _("Undo"), Undo, editMenu, enabledMenu);
+   CMD_ADDMENU(_("&Redo\tCtrl+R"), _("Redo"), Redo, editMenu, enabledMenu);
 
-   CMD_ADDMENUSEP("Cut\tCtrl+X", "Cut", Cut, editMenu, enabledMenu);
-   CMD_ADDMENU("Copy\tCtrl+C", "Copy", Copy, editMenu, enabledMenu);
-   CMD_ADDMENU("Paste\tCtrl+V", "Paste", Paste, editMenu, enabledMenu);
-   CMD_ADDMENU("Trim\tCtrl+T", "Trim", Trim, editMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("Cut\tCtrl+X"), _("Cut"), Cut, editMenu, enabledMenu);
+   CMD_ADDMENU(_("Copy\tCtrl+C"), _("Copy"), Copy, editMenu, enabledMenu);
+   CMD_ADDMENU(_("Paste\tCtrl+V"), _("Paste"), Paste, editMenu, enabledMenu);
+   CMD_ADDMENU(_("&Trim\tCtrl+T"), _("Trim"), Trim, editMenu, enabledMenu);
 
-   CMD_ADDMENUSEP("Delete\tCtrl+K", "Delete", OnDelete, editMenu, enabledMenu);
-   CMD_ADDMENU("Silence\tCtrl+L", "Silence", OnSilence, editMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("&Delete\tCtrl+K"), _("Delete"), OnDelete, editMenu, enabledMenu);
+   CMD_ADDMENU(_("&Silence\tCtrl+L"), _("Silence"), OnSilence, editMenu, enabledMenu);
 
-   CMD_ADDMENUSEP("Insert Silence...", "Insert Silence", OnInsertSilence, editMenu, enabledMenu);
-   CMD_ADDMENU("Split\tCtrl+Y", "New", OnSplit, editMenu, enabledMenu);
-   CMD_ADDMENU("Duplicate\tCtrl+D", "New", OnDuplicate, editMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("&Insert Silence..."), _("Insert Silence"), OnInsertSilence, editMenu, enabledMenu);
+   CMD_ADDMENU(_("Split\tCtrl+Y"), _("New"), OnSplit, editMenu, enabledMenu);
+   CMD_ADDMENU(_("D&uplicate\tCtrl+D"), _("New"), OnDuplicate, editMenu, enabledMenu);
 
-   CMD_ADDMENUSEP("Select All\tCtrl+A", "Select All", OnSelectAll, editMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("Select All\tCtrl+A"), _("Select All"), OnSelectAll, editMenu, enabledMenu);
 
    // View menu
-   CMD_ADDMENU("Zoom In\tCtrl+1", "Zoom In", OnZoomIn, viewMenu, enabledMenu);
-   CMD_ADDMENU("Zoom Normal\tCtrl+2", "Zoom Normal", OnZoomNormal, viewMenu, enabledMenu);
-   CMD_ADDMENU("Zoom Out\tCtrl+3", "Zoom Out", OnZoomOut, viewMenu, enabledMenu);
-   CMD_ADDMENU("Fit in Window\tCtrl+F", "Fit in Window", OnZoomFit, viewMenu, enabledMenu);
-   CMD_ADDMENU("Zoom to Selection\tCtrl+E", "Zoom to Selection", OnZoomSel, viewMenu, enabledMenu);
+   CMD_ADDMENU(_("Zoom &In\tCtrl+1"), _("Zoom In"), OnZoomIn, viewMenu, enabledMenu);
+   CMD_ADDMENU(_("Zoom &Normal\tCtrl+2"), _("Zoom Normal"), OnZoomNormal, viewMenu, enabledMenu);
+   CMD_ADDMENU(_("Zoom &Out\tCtrl+3"), _("Zoom Out"), OnZoomOut, viewMenu, enabledMenu);
+   CMD_ADDMENU(_("&Fit in Window\tCtrl+F"), _("Fit in Window"), OnZoomFit, viewMenu, enabledMenu);
+   CMD_ADDMENU(_("Zoom to &Selection\tCtrl+E"), _("Zoom to Selection"), OnZoomSel, viewMenu, enabledMenu);
 
-   CMD_ADDMENUSEP("History", "Undo History", UndoHistory, viewMenu, enabledMenu);
-   CMD_ADDMENU("Plot Spectrum\tCtrl+U", "Plot Spectrum", OnPlotSpectrum, viewMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("History"), _("Undo History"), UndoHistory, viewMenu, enabledMenu);
+   CMD_ADDMENU(_("Plot Spectrum\tCtrl+U"), _("Plot Spectrum"), OnPlotSpectrum, viewMenu, enabledMenu);
 
 #ifndef __WXMAC__
-   CMD_ADDMENUSEP("Float Control Toolbar", "Float Control Toolbar", OnFloatControlToolBar, viewMenu, enabledMenu);
-   CMD_ADDMENU("Unload Edit Toolbar", "Load Edit Toolbar", OnLoadEditToolBar, viewMenu, enabledMenu);
-   CMD_ADDMENU("Float Edit Toolbar", "Float Edit Toolbar", OnFloatEditToolBar, viewMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("Float Control Toolbar"), _("Float Control Toolbar"), OnFloatControlToolBar, viewMenu, enabledMenu);
+   CMD_ADDMENU(_("Unload Edit Toolbar"), _("Load Edit Toolbar"), OnLoadEditToolBar, viewMenu, enabledMenu);
+   CMD_ADDMENU(_("Float Edit Toolbar"), _("Float Edit Toolbar"), OnFloatEditToolBar, viewMenu, enabledMenu);
 #endif
 
    // Project menu
-   CMD_ADDMENU("Import Audio...\tCtrl+I", "Import Audio", OnImport, projectMenu, enabledMenu);
-   CMD_ADDMENU("Import Labels...", "Import Labels...", OnImportLabels, projectMenu, enabledMenu);
-   CMD_ADDMENU("Import &MIDI...", "Import MIDI", OnImportMIDI, projectMenu, enabledMenu);
-   CMD_ADDMENU("Import Raw Data...", "Import Raw Data", OnImportRaw, projectMenu, enabledMenu);
+   CMD_ADDMENU(_("&Import Audio...\tCtrl+I"), _("Import Audio"), OnImport, projectMenu, enabledMenu);
+   CMD_ADDMENU(_("Import Labels..."), _("Import Labels..."), OnImportLabels, projectMenu, enabledMenu);
+   CMD_ADDMENU(_("Import &MIDI..."), _("Import MIDI"), OnImportMIDI, projectMenu, enabledMenu);
+   CMD_ADDMENU(_("Import Raw &Data..."), _("Import Raw Data"), OnImportRaw, projectMenu, enabledMenu);
 
 #ifdef USE_LIBID3TAG
-   CMD_ADDMENUSEP("Edit ID3 Tags...", "Edit ID3 Tags", OnEditID3, projectMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("Edit ID3 Tags..."), _("Edit ID3 Tags"), OnEditID3, projectMenu, enabledMenu);
 #endif
 
-   CMD_ADDMENUSEP("&Quick Mix", "Quick Mix", OnQuickMix, projectMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("&Quick Mix"), _("Quick Mix"), OnQuickMix, projectMenu, enabledMenu);
 
-   CMD_ADDMENUSEP("Align Tracks Together", "Align Tracks Together", OnAlign, projectMenu, enabledMenu);
-   CMD_ADDMENU("Align with Zero", "Align with Zero", OnAlignZero, projectMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("Align Tracks &Together"), _("Align Tracks Together"), OnAlign, projectMenu, enabledMenu);
+   CMD_ADDMENU(_("Align with &Zero"), _("Align with Zero"), OnAlignZero, projectMenu, enabledMenu);
 
-   CMD_ADDMENUSEP("New &Audio Track", "New Audio Track", OnNewWaveTrack, projectMenu, enabledMenu);
-   CMD_ADDMENU("New &Label Track", "New Label Track", OnNewLabelTrack, projectMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("New &Audio Track"), _("New Audio Track"), OnNewWaveTrack, projectMenu, enabledMenu);
+   CMD_ADDMENU(_("New &Label Track"), _("New Label Track"), OnNewLabelTrack, projectMenu, enabledMenu);
 
-   CMD_ADDMENUSEP("&Remove Track(s)", "Remove Track(s)", OnRemoveTracks, projectMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("&Remove Track(s)"), _("Remove Track(s)"), OnRemoveTracks, projectMenu, enabledMenu);
 
    // Help menu
-   CMD_ADDMENU("About Audacity...", "About Audacity", OnAbout, helpMenu, enabledMenu);
+   CMD_ADDMENU(_("About Audacity..."), _("About Audacity"), OnAbout, helpMenu, enabledMenu);
 
 #ifndef __WXMAC__
-   CMD_ADDMENUSEP("Online Help...", "Online Help", OnHelp, helpMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("Online Help..."), _("Online Help"), OnHelp, helpMenu, enabledMenu);
 #else
-   CMD_ADDMENU("Online Help...", "Online Help", OnHelp, helpMenu, enabledMenu);
+   CMD_ADDMENU(_("Online Help..."), _("Online Help"), OnHelp, helpMenu, enabledMenu);
 #endif
-   CMD_ADDMENU("Online Help Index...", "Online Help Index", OnHelpIndex, helpMenu, enabledMenu);
-   CMD_ADDMENU("Search Online Help...", "Search Online Help", OnHelpSearch, helpMenu, enabledMenu);
+   CMD_ADDMENU(_("Online Help Index..."), _("Online Help Index"), OnHelpIndex, helpMenu, enabledMenu);
+   CMD_ADDMENU(_("Search Online Help..."), _("Search Online Help"), OnHelpSearch, helpMenu, enabledMenu);
 
-   CMD_ADDMENUSEP("Run Benchmark...", "Run Benchmark", OnBenchmark, helpMenu, enabledMenu);
+   CMD_ADDMENUSEP(_("Run Benchmark..."), _("Run Benchmark"), OnBenchmark, helpMenu, enabledMenu);
 }
 
 #endif
