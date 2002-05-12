cc874f58d0f5a874c9a199c31aa4789102e1a2e1
nestify <nestify@47890b92-0858-11df-a26f-8b716316a5bc>
2002-05-12 21:24:40 +0000

    Fix submenu enabling/disabling brought about by previous commit.

--- sf-cvs/trunk/audacity-src/src/Menus.cpp
+++ sf-cvs/trunk/audacity-src/src/Menus.cpp
@@ -64,7 +64,6 @@
 
 #define MAX_NUMBER_OF_PLUGINS_IN_MENU 10
 
-
 void AudacityProject::CreateMenuBar()
 {
    mMenusDirtyCheck = gMenusDirty;
@@ -230,11 +229,11 @@ void AudacityProject::BuildMenuBar()
       wxMenu * tmp;
       wxString label;
       tmp = new wxMenu;
-      int lower,upper;
+      int lower,upper, submenu;
       
       lower = 1;
       upper = MAX_NUMBER_OF_PLUGINS_IN_MENU;
-
+      submenu=0;
       for (fi = 0; fi < numPlugins; fi++){
   
          tmp->Append(FirstPluginID + fi + numPluginSubMenus,
@@ -247,7 +246,7 @@ void AudacityProject::BuildMenuBar()
             
             upper = fi +1;   //upper limit should be the current plugin number (+1)
             label =  wxString::Format(_("Plugins %d to %d"), lower, upper);  //make submenu label
-            mPluginMenu->Append(FirstPluginID + fi, label, tmp++ , label);   //Add submenu
+            mPluginMenu->Append(FirstPluginID + submenu++, label, tmp++ , label);   //Add submenu
             tmp= new wxMenu;                                                 //reset temp. menu
             lower += MAX_NUMBER_OF_PLUGINS_IN_MENU;                          //reset lower for next time.
          }
@@ -490,6 +489,8 @@ void AudacityProject::OnUpdateMenus(wxUpdateUIEvent & event)
    SetMenuState(mProjectMenu, AlignZeroID, numTracksSelected > 0);
    SetMenuState(mProjectMenu, RemoveTracksID, numTracksSelected > 0);
 
+
+   //Enable the effects menu
    int e;
    for (e = 0; e < Effect::GetNumEffects(false); e++) {
       mEffectMenu->Enable(FirstEffectID + e,
@@ -497,14 +498,19 @@ void AudacityProject::OnUpdateMenus(wxUpdateUIEvent & event)
                           && nonZeroRegionSelected);
    }
    
+
+   //Enable/disable the Plugins menu options.
    int numPlugins = Effect::GetNumEffects(true);
    int numPluginSubMenus = (numPlugins) / MAX_NUMBER_OF_PLUGINS_IN_MENU + 1;
 
+   //enable or disable each menu item based on whether there is a selection.
    for (e = 0; e < numPlugins; e++) {
       mPluginMenu->Enable(FirstPluginID + e + numPluginSubMenus,
                           numWaveTracksSelected > 0
                           && nonZeroRegionSelected);
    }
+
+
 
    //Now, go through each toolbar, and and call EnableDisableButtons()
    unsigned int i;
