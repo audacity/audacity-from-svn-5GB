8bf1e95957e9d285235519edf61ca1eb018da747
nestify <nestify@47890b92-0858-11df-a26f-8b716316a5bc>
2002-07-19 17:39:09 +0000

    Adding some comments that helped me figure out what was happening.

--- sf-cvs/trunk/audacity-src/src/effects/SimpleMono.cpp
+++ sf-cvs/trunk/audacity-src/src/effects/SimpleMono.cpp
@@ -21,28 +21,41 @@
 
 bool EffectSimpleMono::Process()
 {
+   //Iterate over each track
    TrackListIterator iter(mWaveTracks);
    WaveTrack *track = (WaveTrack *) iter.First();
    mCurTrackNum = 0;
    while (track) {
+      //Get start and end times from track
       double trackStart = track->GetStartTime();
       double trackEnd = track->GetEndTime();
+
+      //Set the current bounds to whichever left marker is
+      //greater and whichever right marker is less:
       mCurT0 = mT0 < trackStart? trackStart: mT0;
       mCurT1 = mT1 > trackEnd? trackEnd: mT1;
 
+      // Process only if the right marker is to the right of the left marker
       if (mCurT1 > mCurT0) {
+
+         //Transform the marker timepoints to samples
          longSampleCount start = track->TimeToLongSamples(mCurT0);
          longSampleCount end = track->TimeToLongSamples(mCurT1);
-
+         
+         //Get the track rate and samples
          mCurRate = track->GetRate();
          mCurChannel = track->GetChannel();
 
+         //NewTrackSimpleMono() will returns true by default
          if (!NewTrackSimpleMono())
             return false;
+
+         //ProcessOne() (implemented below) processes a single track
          if (!ProcessOne(track, start, end))
             return false;
       }
-
+      
+      //Iterate to the next track
       track = (WaveTrack *) iter.Next();
       mCurTrackNum++;
    }
@@ -50,34 +63,60 @@ bool EffectSimpleMono::Process()
    return true;
 }
 
+
+//ProcessOne() takes a track, transforms it to bunch of buffer-blocks,
+//and executes ProcessSimpleMono on these blocks
 bool EffectSimpleMono::ProcessOne(WaveTrack * track,
                                   longSampleCount start, longSampleCount end)
 {
+   
    longSampleCount s;
+   //Get the length of the buffer (as double). len is
+   //used simple to calculate a progress meter, so it is easier
+   //to make it a double now than it is to do it later 
    double len = (double)(start - end);
 
+   //Initiate a processing buffer.  This buffer will (most likely)
+   //be shorter than the length of the track being processed.
    float *buffer = new float[track->GetMaxBlockSize()];
 
+   //Go through the track one buffer at a time. s counts which
+   //sample the current buffer starts at.
    s = start;
    while (s < end) {
+      //Get a block of samples (smaller than the size of the buffer)
       sampleCount block = track->GetBestBlockSize(s);
+
+      //Adjust the block size if it is the final block in the track
       if (s + block > end)
          block = end - s;
 
+      //Get the samples from the track and put them in the buffer
       track->Get((samplePtr) buffer, floatSample, s, block);
+
+      //Process the buffer.  If it fails, clean up and exit.
       if (!ProcessSimpleMono(buffer, block)) {
          delete[]buffer;
+
+         //Return false because the effect failed.
          return false;
       }
+
+      //Processing succeeded. copy the newly-changed samples back 
+      //onto the track.
       track->Set((samplePtr) buffer, floatSample, s, block);
 
+      //Increment s one blockfull of samples
       s += block;
 
+      //Update the Progress meter
       TrackProgress(mCurTrackNum, s / len);
    }
 
+   //Clean up the buffer
    delete[]buffer;
 
+   //Return true because the effect processing succeeded.
    return true;
 }
 
