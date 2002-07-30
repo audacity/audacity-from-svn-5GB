e92178084e22f6a4a83987aa1a428c6203732d4c
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2002-07-30 05:18:11 +0000

    Added a language choice dialog which appears the first time you run Audacity.

---
+++ sf-cvs/trunk/audacity-src/src/Languages.cpp
@@ -0,0 +1,31 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  Languages.cpp
+
+  Dominic Mazzoni
+
+**********************************************************************/
+
+#include "Languages.h"
+
+void GetLanguages(wxArrayString &langCodes, wxArrayString &langNames)
+{
+   //
+   // TODO:
+   //
+   // This is just a placeholder until somebody writes code
+   // that actually searches for locale files...
+   //
+
+   langCodes.Add("bg"); langNames.Add("Balgarski");
+   langCodes.Add("da"); langNames.Add("Dansk");
+   langCodes.Add("de"); langNames.Add("Deutch");
+   langCodes.Add("en"); langNames.Add("English");
+   langCodes.Add("es"); langNames.Add("Español");
+   langCodes.Add("fr"); langNames.Add("Français");
+   langCodes.Add("it"); langNames.Add("Italiano");
+   langCodes.Add("nl"); langNames.Add("Nederlands");
+   langCodes.Add("ru"); langNames.Add("Russky");
+}
