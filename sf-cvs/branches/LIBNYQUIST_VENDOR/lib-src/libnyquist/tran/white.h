c333b2267e8d8784043005c421e8a3e896f7abaf
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2002-09-16 05:05:26 +0000

    importing libnyquist

---
+++ sf-cvs/branches/LIBNYQUIST_VENDOR/lib-src/libnyquist/tran/white.h
@@ -0,0 +1,32 @@
+sound_type snd_make_white(time_type t0, rate_type sr, time_type d);
+sound_type snd_white(time_type t0, rate_type sr, time_type d);
+    /* LISP: (snd-white ANYNUM ANYNUM ANYNUM) */
+#include <math.h>
+#include "switches.h" /* to define (or not) MACINTOSH */
+
+#if defined(MACINTOSH) || defined(UNIX)
+
+/* rand returns from 0 to RAND_MAX. Scale and offset
+ * to get range from -1 to +1
+ */
+#define random rand
+#define random_scale (2.0/RAND_MAX)
+
+#else
+#ifdef UNIX
+
+/* random is already defined */
+#define random rand
+#define random_scale (2.0/RAND_MAX)
+
+#else
+
+/* random returns from 0 to 0x7FFFFFF.  Scale and offset
+ * to get range from -1 to +1
+ */
+#define random_scale (2.0/0x7FFFFFFF)
+
+long random(void);
+
+#endif
+#endif
