f4209953ca8a5eae272f52883abe3368c9f2a80a
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2002-09-16 05:05:26 +0000

    importing libnyquist

---
+++ sf-cvs/branches/LIBNYQUIST_VENDOR/lib-src/libnyquist/sys/switches.h
@@ -0,0 +1,17 @@
+/* switches.h is copied from uswitches.h
+ * makefile will copy changes back to switches.h but you
+ * lose changes made to uswitches.h */
+
+/* Copyright 1989 Carnegie Mellon University */
+
+#define UNIX
+#define BUFFERED_SYNCHRONOUS_INPUT
+
+/*------------------------------------------*/
+
+/* #define MAX_CHANNELS 16 -- this is the default */
+#define SPACE_FOR_PLAY 10000
+#define SWITCHES
+
+#include "swlogic.h"
+
