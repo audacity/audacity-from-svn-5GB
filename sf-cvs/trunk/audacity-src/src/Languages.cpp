7e32704d5408c44d64674b77fb2dbb96e0d079d7
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2002-09-17 06:58:30 +0000

    Added some unfinished code to Languages.cpp to search for installed languages, and improved directory searching in AudacityApp.cpp.

--- sf-cvs/trunk/audacity-src/src/Languages.cpp
+++ sf-cvs/trunk/audacity-src/src/Languages.cpp
@@ -8,15 +8,21 @@
 
 **********************************************************************/
 
+#include <wx/intl.h>
+
 #include "Languages.h"
+
+#include "Audacity.h"
+#include "AudacityApp.h"
 
 void GetLanguages(wxArrayString &langCodes, wxArrayString &langNames)
 {
    //
    // TODO:
    //
-   // This is just a placeholder until somebody writes code
-   // that actually searches for locale files...
+   // This is a placeholder until the code which
+   // actually searches for locale files (below)
+   // is finished...
    //
 
    langCodes.Add("bg"); langNames.Add("Balgarski");
@@ -32,3 +38,37 @@ void GetLanguages(wxArrayString &langCodes, wxArrayString &langNames)
    langCodes.Add("ru"); langNames.Add("Russky");
    langCodes.Add("sl"); langNames.Add("Slovenscina");
 }
+
+#if 0
+
+   This code doesn't work yet
+
+   wxArrayString audacityPathList = wxGetApp().audacityPathList;
+   wxArrayString results;
+
+   langCodes.Add("en"); langNames.Add("English");
+
+   wxGetApp().AddUniquePathToPathList(wxString::Format("%s/share/locale",
+                                                       INSTALL_PREFIX),
+                                      audacityPathList);
+
+   wxGetApp().FindFilesInPathList("*/audacity.mo",
+                                  audacityPathList,
+                                  wxFILE,
+                                  results);
+
+   wxGetApp().FindFilesInPathList("*/LC_MESSAGES/audacity.mo",
+                                  audacityPathList,
+                                  wxFILE,
+                                  results);
+
+   for(unsigned int i=0; i<results.GetCount(); i++) {
+      wxString path = ::wxPathOnly(results[i]);
+      if (path.Length() > 12 && path.Right(11)=="LC_MESSAGES")
+         path = path.Left(path.Length()-12);
+      wxString langCode = path.AfterLast(wxFILE_SEP_PATH);
+      //wxLanguageInfo *info = wxLocale::GetLanguageInfo(langCode);
+      printf("Found %s\n", (const char *)langCode);
+   }
+#endif
+
