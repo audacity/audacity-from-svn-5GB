1bb7ccaa164649796765d9335486f4ea5e855781
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2002-09-17 07:13:17 +0000

    use GetCwd() instead of "." so that it's sure to use the current directory at the time of program startup, not change it later...

--- sf-cvs/trunk/audacity-src/src/AudacityApp.cpp
+++ sf-cvs/trunk/audacity-src/src/AudacityApp.cpp
@@ -248,7 +248,7 @@ bool AudacityApp::OnInit()
    wxString pathVar = wxGetenv("AUDACITY_PATH");
    if (pathVar != "")
       AddMultiPathsToPathList(pathVar, audacityPathList);
-   AddUniquePathToPathList(".", audacityPathList);
+   AddUniquePathToPathList(::wxGetCwd(), audacityPathList);
    AddUniquePathToPathList(wxString::Format("%s/.audacity-files",
                                             (const char *)home),
                            audacityPathList);
