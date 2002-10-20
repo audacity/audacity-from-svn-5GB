802aa1acd1f1392dc1199fbe84a5acac26ffddde
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2002-10-20 20:58:27 +0000

    Added separate Aqua buttons

--- sf-cvs/trunk/audacity-src/images/EditButtons.h
+++ sf-cvs/trunk/audacity-src/images/EditButtons.h
@@ -1,7 +1,16 @@
+#ifdef __WXMAC__
+#include "Aqua/Up.xpm"
+#include "Aqua/Down.xpm"    
+#include "Aqua/Hilite.xpm"
+#else
+#include "EditButtons/Up.xpm"
+#include "EditButtons/Down.xpm"      
+#include "EditButtons/Hilite.xpm"       
+#endif
+
 #include "EditButtons/CopyAlpha.xpm"
 #include "EditButtons/CutAlpha.xpm"
 #include "EditButtons/Disabled.xpm"  
-#include "EditButtons/Hilite.xpm"       
 #include "EditButtons/Paste.xpm"       
 #include "EditButtons/Redo.xpm"           
 #include "EditButtons/Silence.xpm"     
@@ -9,13 +18,11 @@
 #include "EditButtons/Undo.xpm"
 #include "EditButtons/Copy.xpm"        
 #include "EditButtons/Cut.xpm"        
-#include "EditButtons/Down.xpm"      
 #include "EditButtons/PasteAlpha.xpm"  
 #include "EditButtons/RedoAlpha.xpm"  
 #include "EditButtons/SilenceAlpha.xpm"  
 #include "EditButtons/TrimAlpha.xpm"  
 #include "EditButtons/UndoAlpha.xpm" 
-#include "EditButtons/Up.xpm"
 #include "EditButtons/ZoomIn.xpm"
 #include "EditButtons/ZoomInAlpha.xpm"
 #include "EditButtons/ZoomOut.xpm"
