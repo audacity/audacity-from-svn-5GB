9a3b07b8a1555a361f373fea2d4dc4a5039e6225
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2002-10-22 05:16:52 +0000

    New effects for the Generate menu

---
+++ sf-cvs/trunk/audacity-src/src/effects/Silence.h
@@ -0,0 +1,42 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  Silence.h
+
+  Dominic Mazzoni
+  
+  An effect for the "Generator" menu to add silence.
+
+**********************************************************************/
+
+#ifndef __AUDACITY_EFFECT_SILENCE__
+#define __AUDACITY_EFFECT_SILENCE__
+
+#include <wx/defs.h>
+#include <wx/intl.h>
+
+#include "Effect.h"
+
+class EffectSilence:public Effect {
+
+ public:
+   EffectSilence() {}
+
+   virtual wxString GetEffectName() {
+      return wxString(_("Silence"));
+   }
+
+   virtual wxString GetEffectAction() {
+      return wxString(_("Generating Silence"));
+   }
+
+   virtual int GetEffectFlags() {
+      return BUILTIN_EFFECT | INSERT_EFFECT;
+   }
+
+   virtual bool Process();
+
+};
+
+#endif
