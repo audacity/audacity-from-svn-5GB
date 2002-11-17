44259a134032071c13380f42a00608fe50596a45
bmg300 <bmg300@47890b92-0858-11df-a26f-8b716316a5bc>
2002-11-17 07:01:43 +0000

    Add support for almost all leagal keyboard shortcuts

--- sf-cvs/trunk/audacity-src/src/AudacityApp.cpp
+++ sf-cvs/trunk/audacity-src/src/AudacityApp.cpp
@@ -188,8 +188,9 @@ pascal OSErr AEOpenFiles(const AppleEvent * theAppleEvent, AppleEvent * theReply
 }
 #endif
 
+typedef int (AudacityApp::*SPECIALKEYEVENT)(wxEvent&);
+
 BEGIN_EVENT_TABLE(AudacityApp, wxApp)
-  EVT_CHAR(AudacityApp::OnKey)
 
    //These appear to cause trouble on non-mac platforms:
 #ifdef __WXMAC__
@@ -529,13 +530,27 @@ void AudacityApp::FindFilesInPathList(wxString pattern,
    }
 }
 
-void AudacityApp::OnKey(wxKeyEvent& event)
+//BG: Filters all events before they are processed
+int AudacityApp::FilterEvent(wxEvent& event)
+{
+   if(event.GetEventType() == wxEVT_KEY_DOWN)
+      return (this->*((SPECIALKEYEVENT) (AudacityApp::OnAllKeys)))(event);
+
+   return -1;
+}
+
+//BG: THIS IS NOT A NORMAL KEY CALLBACK
+//BG: it gets called before everything else
+//BG: return -1 to allow other things to process keydown events
+//BG: return TRUE to signify that the event has been processed, but do not allow anything else to process this event
+//BG: return FALSE to signigy that the event has not been processed and should not be processed
+//BG: do not call event.skip, I do not know what would happen
+int AudacityApp::OnAllKeys(wxKeyEvent& event)
 {
    AudacityProject *audacityPrj = GetActiveProject();
 
    if (!audacityPrj) {
-      event.Skip();
-      return;
+      return -1;
    }
 
    wxString newStr = "";
@@ -555,16 +570,244 @@ void AudacityApp::OnKey(wxKeyEvent& event)
       newStr += (char)(64 + key);
    else if (key >= 33 && key <= 126)
       newStr += (char)key;
-   else if (key == WXK_BACK)
-      newStr = "Backspace";
-   else if (key == WXK_DELETE)
-      newStr = "Delete";
-   else if (key == WXK_SPACE)
-      newStr = "Spacebar";
    else
    {
-      event.Skip();
-      return; // Don't change it if we don't recognize the key
+      switch(key)
+      {
+      case WXK_BACK:
+         newStr += "Backspace";
+         break;
+      case WXK_DELETE:
+         newStr += "Delete";
+         break;
+      case WXK_SPACE:
+         newStr += "Spacebar";
+         break;
+      case WXK_PRIOR:
+         newStr += "PageUp";
+         break;
+      case WXK_NEXT:
+         newStr += "PageDown";
+         break;
+      case WXK_END:
+         newStr += "End";
+         break;
+      case WXK_HOME:
+         newStr += "Home";
+         break;
+      case WXK_LEFT:
+         newStr += "Left";
+         break;
+      case WXK_UP:
+         newStr += "Up";
+         break;
+      case WXK_RIGHT:
+         newStr += "Right";
+         break;
+      case WXK_DOWN:
+         newStr += "Down";
+         break;
+      case WXK_INSERT:
+         newStr += "Insert";
+         break;
+      case WXK_NUMPAD0:
+         newStr += "NUMPAD0";
+         break;
+      case WXK_NUMPAD1:
+         newStr += "NUMPAD1";
+         break;
+      case WXK_NUMPAD2:
+         newStr += "NUMPAD2";
+         break;
+      case WXK_NUMPAD3:
+         newStr += "NUMPAD3";
+         break;
+      case WXK_NUMPAD4:
+         newStr += "NUMPAD4";
+         break;
+      case WXK_NUMPAD5:
+         newStr += "NUMPAD5";
+         break;
+      case WXK_NUMPAD6:
+         newStr += "NUMPAD6";
+         break;
+      case WXK_NUMPAD7:
+         newStr += "NUMPAD7";
+         break;
+      case WXK_NUMPAD8:
+         newStr += "NUMPAD8";
+         break;
+      case WXK_NUMPAD9:
+         newStr += "NUMPAD9";
+         break;
+      case WXK_MULTIPLY:
+         newStr += "*";
+         break;
+      case WXK_ADD:
+         newStr += "+";
+         break;
+      case WXK_SUBTRACT:
+         newStr += "-";
+         break;
+      case WXK_DECIMAL:
+         newStr += ".";
+         break;
+      case WXK_DIVIDE:
+         newStr += "/";
+         break;
+      case WXK_F1:
+         newStr += "F1";
+         break;
+      case WXK_F2:
+         newStr += "F2";
+         break;
+      case WXK_F3:
+         newStr += "F3";
+         break;
+      case WXK_F4:
+         newStr += "F4";
+         break;
+      case WXK_F5:
+         newStr += "F5";
+         break;
+      case WXK_F6:
+         newStr += "F6";
+         break;
+      case WXK_F7:
+         newStr += "F7";
+         break;
+      case WXK_F8:
+         newStr += "F8";
+         break;
+      case WXK_F9:
+         newStr += "F9";
+         break;
+      case WXK_F10:
+         newStr += "F10";
+         break;
+      case WXK_F11:
+         newStr += "F11";
+         break;
+      case WXK_F12:
+         newStr += "F12";
+         break;
+      case WXK_F13:
+         newStr += "F13";
+         break;
+      case WXK_F14:
+         newStr += "F14";
+         break;
+      case WXK_F15:
+         newStr += "F15";
+         break;
+      case WXK_F16:
+         newStr += "F16";
+         break;
+      case WXK_F17:
+         newStr += "F17";
+         break;
+      case WXK_F18:
+         newStr += "F18";
+         break;
+      case WXK_F19:
+         newStr += "F19";
+         break;
+      case WXK_F20:
+         newStr += "F20";
+         break;
+      case WXK_F21:
+         newStr += "F21";
+         break;
+      case WXK_F22:
+         newStr += "F22";
+         break;
+      case WXK_F23:
+         newStr += "F23";
+         break;
+      case WXK_F24:
+         newStr += "F24";
+         break;
+      case WXK_PAGEUP:
+         newStr += "PageUp";
+         break;
+      case WXK_PAGEDOWN:
+         newStr += "PageDown";
+         break;
+      case WXK_NUMPAD_ENTER:
+         newStr += "NUMPAD_ENTER";
+         break;
+      case WXK_NUMPAD_F1:
+         newStr += "NUMPAD_F1";
+         break;
+      case WXK_NUMPAD_F2:
+         newStr += "NUMPAD_F2";
+         break;
+      case WXK_NUMPAD_F3:
+         newStr += "NUMPAD_F3";
+         break;
+      case WXK_NUMPAD_F4:
+         newStr += "NUMPAD_F4";
+         break;
+      case WXK_NUMPAD_HOME:
+         newStr += "NUMPAD_HOME";
+         break;
+      case WXK_NUMPAD_LEFT:
+         newStr += "NUMPAD_LEFT";
+         break;
+      case WXK_NUMPAD_UP:
+         newStr += "NUMPAD_UP";
+         break;
+      case WXK_NUMPAD_RIGHT:
+         newStr += "NUMPAD_RIGHT";
+         break;
+      case WXK_NUMPAD_DOWN:
+         newStr += "NUMPAD_DOWN";
+         break;
+      case WXK_NUMPAD_PRIOR:
+         newStr += "NUMPAD_PAGEUP";
+         break;
+      case WXK_NUMPAD_PAGEUP:
+         newStr += "NUMPAD_PAGEUP";
+         break;
+      case WXK_NUMPAD_NEXT:
+         newStr += "NUMPAD_PAGEDOWN";
+         break;
+      case WXK_NUMPAD_PAGEDOWN:
+         newStr += "NUMPAD_PAGEDOWN";
+         break;
+      case WXK_NUMPAD_END:
+         newStr += "NUMPAD_END";
+         break;
+      case WXK_NUMPAD_BEGIN:
+         newStr += "NUMPAD_HOME";
+         break;
+      case WXK_NUMPAD_INSERT:
+         newStr += "NUMPAD_INSERT";
+         break;
+      case WXK_NUMPAD_DELETE:
+         newStr += "NUMPAD_DELETE";
+         break;
+      case WXK_NUMPAD_EQUAL:
+         newStr += "NUMPAD_EQUAL";
+         break;
+      case WXK_NUMPAD_MULTIPLY:
+         newStr += "NUMPAD_MULTIPLY";
+         break;
+      case WXK_NUMPAD_ADD:
+         newStr += "NUMPAD_ADD";
+         break;
+      case WXK_NUMPAD_SUBTRACT:
+         newStr += "NUMPAD_SUBTRACT";
+         break;
+      case WXK_NUMPAD_DECIMAL:
+         newStr += "NUMPAD_DECIMAL";
+         break;
+      case WXK_NUMPAD_DIVIDE:
+         newStr += "NUMPAD_DIVIDE";
+         break;
+      default:
+         return -1; // Don't do anything if we don't recognize the key
+      }
    }
 
    if(audacityPrj->IsActive())
@@ -578,11 +821,11 @@ void AudacityApp::OnKey(wxKeyEvent& event)
          if(audFunc)
             (audacityPrj->*((wxEventFunction) audFunc))(event);
 
-         return;
+         return TRUE;
       }
    }
 
-   event.Skip();
+   return -1;
 }
 
 int AudacityApp::OnExit()
