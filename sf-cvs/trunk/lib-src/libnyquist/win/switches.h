ece82785202f3cf18b69c77dc688359484c1fbfc
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2002-11-18 04:53:27 +0000

    Checking in Windows port of libnyquist

---
+++ sf-cvs/trunk/lib-src/libnyquist/win/switches.h
@@ -0,0 +1,24 @@
+/* switches.h is copied from uswitches.h
+ * makefile will copy changes back to switches.h but you
+ * lose changes made to uswitches.h */
+
+/* Copyright 1989 Carnegie Mellon University */
+
+#define XLISP_SOUND
+
+#define BUFFERED_SYNCHRONOUS_INPUT
+
+/*------------------------------------------*/
+
+#define MAX_CHANNELS 16
+#define SPACE_FOR_PLAY 10000
+#define SWITCHES
+
+#define MICROSOFT
+#define DOS
+#define WINDOWS
+/*#define USE_DSOUND*/
+
+#include "swlogic.h"
+
+
