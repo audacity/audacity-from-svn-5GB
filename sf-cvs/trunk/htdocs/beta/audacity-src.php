0685121023ae85d112fdaad1583b7b29c011de45
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-01-01 23:58:04 +0000

    Adding latest version links

---
+++ sf-cvs/trunk/htdocs/beta/audacity-src.php
@@ -0,0 +1,9 @@
+<?
+
+# perform a redirect to the latest version, so links to it can remain static
+
+include 'mirror.php.inc';
+include 'versions.php.inc';
+header('Location: '.mirror.'audacity-src-'.src.'.tgz');
+
+?>
