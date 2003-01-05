a90fbec22ed15ba132baf3a17746b3363c13a2e4
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-01-05 01:31:27 +0000

    Getting rid of duplicates

--- sf-cvs/trunk/htdocs/ru/tutorials.php
+++
@@ -1,9 +0,0 @@
-<?php BoxTop($tutorialsStr); ?>
-
-<p>
-<a href="http://www.daniel.uklinux.net/tutorial/">An Audacity Tutorial
-by Daniel James</a> - comes with sample sound files for you to mix
-together.
-</p>
-
-<?php BoxBottom(); ?>
