75563be8684af0b7707860481abfc7ecd31d5fb0
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-01-05 01:15:32 +0000

    Adding lost translations

---
+++ sf-cvs/trunk/htdocs/de/news2.php
@@ -0,0 +1,31 @@
+<?php BoxTop('11. November 2002: Version 1.1.1 erschienen!'); ?>
+
+<p>
+Version 1.1.1 ist ab sofort verfügbar!
+</p>
+
+<p>Für neue und/oder unerfahrene Anwender empfehlen wir immer noch Audacity
+1.0. Es ist einfach, stabil und hat eine komplette Dokumentation.</p>
+
+<p>
+Fortgeschrittene Anwender werden sicherlich daran interessiert sein, das neue Audacity 1.1.1
+zu testen. Es hat viele neue Funktionen, ist aber noch in Entwicklung und
+hat lediglich Beta - Qualität. Die Dokumentation ist noch nicht fertig.</p>
+
+<p>
+Audacity 1.1.1
+hat die Unterstützung für Mac OS X und für weitere Fremdsprachen stark verbessert.
+Anwender, die das Programm unter Mac OS X oder in Sprachen wie Bulgarisch,
+Dänisch, Dutch, Französisch, Deutsch, Ungarisch, Italienisch, Polnisch, Russisch,
+Slovenisch oder Spanisch nutzen wollen, sollten diese Version probieren.</p>
+
+<p>
+Du kannst Audacity 1.1.1 auf unserer 
+<a href="http://audacity.sourceforge.net/beta.php?lang=de">Beta</a> - Seite finden.
+</p>
+
+<p>
+<a href="http://audacity.sourceforge.net/betanotes.php?lang=de">Neue Funktionen in 1.1.1.</a>
+</p>
+
+<?php BoxBottom(); ?>
