0e41ffa1c0be05c40177dda60cf48174f8a60caf
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2003-02-03 22:28:16 +0000

    new "Change Speed" effect UI

---
+++ sf-cvs/trunk/audacity-src/src/effects/ChangeSpeed.cpp
@@ -0,0 +1,409 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  ChangeSpeed.cpp
+
+  Vaughan Johnson, Dominic Mazzoni
+  
+  Change Speed effect, that affects both pitch & tempo.
+
+**********************************************************************/
+
+#include <math.h>
+
+#include <wx/intl.h>
+#include <wx/msgdlg.h> //v for "not yet implemented"
+#include <wx/valtext.h>
+
+#include "ChangeSpeed.h"
+#include "../WaveTrack.h"
+
+//
+// EffectChangeSpeed
+//
+
+EffectChangeSpeed::EffectChangeSpeed()
+{
+	m_PercentChange = 0.0;
+	m_FromVinyl = 0; 
+	m_ToVinyl = 0; 
+}
+
+wxString EffectChangeSpeed::GetEffectDescription() { 
+   // Note: This is useful only after change amount has been set. 
+   return wxString::Format(_("Applied effect: %s %.1f%%"), 
+                           (const char *)(this->GetEffectName()), 
+									m_PercentChange); 
+} 
+
+bool EffectChangeSpeed::Init()
+{
+	//v
+	return true;
+}
+
+bool EffectChangeSpeed::PromptUser()
+{
+   ChangeSpeedDialog dlog(mParent, -1, _("Change Speed"));
+   dlog.m_PercentChange = m_PercentChange;
+   dlog.m_FromVinyl = m_FromVinyl;
+   dlog.m_ToVinyl = m_ToVinyl;
+   dlog.TransferDataToWindow();
+   dlog.CentreOnParent();
+   dlog.ShowModal();
+
+   if (!dlog.GetReturnCode())
+      return false;
+
+   m_PercentChange = dlog.m_PercentChange;
+   m_FromVinyl = dlog.m_FromVinyl;
+   m_ToVinyl = dlog.m_ToVinyl;
+
+   return true;
+}
+
+bool EffectChangeSpeed::ProcessSimpleMono(float * buffer, sampleCount len)
+{
+	//v 
+   return true;
+}
+
+//----------------------------------------------------------------------------
+// ChangeSpeedDialog
+//----------------------------------------------------------------------------
+
+#define PERCENTCHANGE_MIN -99
+#define PERCENTCHANGE_MAX 100 // warped above zero to actually go up to 400%
+#define PERCENTCHANGE_SLIDER_WARP 1.30105 // warp power takes max from 100 to 400.
+
+// the standard vinyl RPM choices
+// If the percent change is not one of these ratios, the choice control gets "n/a".
+#define CHOICE_33ANDATHIRD 0
+#define CHOICE_45 1
+#define CHOICE_78 2
+#define CHOICE_NA 3 
+
+#define RATIO_33ANDATHIRD_45 //v
+
+// event table for ChangeSpeedDialog
+
+BEGIN_EVENT_TABLE(ChangeSpeedDialog, wxDialog)
+    EVT_BUTTON(wxID_OK, ChangeSpeedDialog::OnOk)
+    EVT_BUTTON(wxID_CANCEL, ChangeSpeedDialog::OnCancel)
+
+    EVT_TEXT(ID_TEXT_PERCENTCHANGE, ChangeSpeedDialog::OnText_PercentChange)
+    EVT_SLIDER(ID_SLIDER_PERCENTCHANGE, ChangeSpeedDialog::OnSlider_PercentChange)
+    EVT_CHOICE(ID_CHOICE_FROMVINYL, ChangeSpeedDialog::OnChoice_FromVinyl)
+    EVT_CHOICE(ID_CHOICE_TOVINYL, ChangeSpeedDialog::OnChoice_ToVinyl)
+END_EVENT_TABLE()
+
+ChangeSpeedDialog::ChangeSpeedDialog(wxWindow * parent, 
+												 wxWindowID id, const wxString & title, 
+												 const wxPoint & position, const wxSize & size, 
+												 long style)
+: wxDialog(parent, id, title, position, size, style)
+{
+   m_bLoopDetect = false;
+
+	m_PercentChange = 0.0;
+	m_FromVinyl = 0; 
+	m_ToVinyl = 0; 
+
+	
+	// CREATE THE CONTROLS PROGRAMMATICALLY.
+
+   wxBoxSizer * pBoxSizer_Dialog = new wxBoxSizer(wxVERTICAL);
+
+	// heading
+   wxStaticText * pStaticText =
+		new wxStaticText(this, ID_TEXT, _("Change Speed, affecting both Tempo and Pitch"),
+								wxDefaultPosition, wxDefaultSize, 0);
+   pBoxSizer_Dialog->Add(pStaticText, 0, wxALIGN_CENTER | wxALL, 8);
+
+
+	// percent change controls
+   wxBoxSizer * pBoxSizer_PercentChange = new wxBoxSizer(wxHORIZONTAL);
+   
+   pStaticText =
+		new wxStaticText(this, ID_TEXT, _("Percent Change:"),
+								wxDefaultPosition, wxDefaultSize, 0);
+   pBoxSizer_PercentChange->Add(pStaticText, 0, 
+											wxALIGN_CENTER_VERTICAL | wxALIGN_RIGHT | wxALL, 5);
+
+	//v Override wxTextValidator to disallow negative values <= -100.0?
+   wxTextCtrl * pTextCtrl_PercentChange =
+       new wxTextCtrl(this, ID_TEXT_PERCENTCHANGE, _("0.0"), 
+								wxDefaultPosition, wxSize(40, -1), 0,
+								wxTextValidator(wxFILTER_NUMERIC));
+   pBoxSizer_PercentChange->Add(pTextCtrl_PercentChange, 0, 
+											wxALIGN_CENTER_VERTICAL | wxALIGN_LEFT | wxALL, 5);
+
+   pBoxSizer_Dialog->Add(pBoxSizer_PercentChange, 0, wxALIGN_CENTER | wxALL, 5);
+
+   wxSlider * pSlider_PercentChange =
+       new wxSlider(this, ID_SLIDER_PERCENTCHANGE, 
+							0, PERCENTCHANGE_MIN, PERCENTCHANGE_MAX,
+							wxDefaultPosition, wxSize(100, -1), wxSL_HORIZONTAL);
+   pBoxSizer_Dialog->Add(pSlider_PercentChange, 1, 
+									wxGROW | wxALIGN_CENTER | wxLEFT | wxRIGHT, 5);
+
+
+	// from/to Vinyl controls
+   wxBoxSizer * pBoxSizer_Vinyl = new wxBoxSizer(wxHORIZONTAL);
+
+	const wxString strArray_VinylRPM[] = {"33 1/3", "45", "78", "n/a"};
+	const int numChoices = 4;
+
+   pStaticText =
+       new wxStaticText(this, ID_TEXT, _("Standard Vinyl RPM:     From"),
+                        wxDefaultPosition, wxDefaultSize, 0);
+   pBoxSizer_Vinyl->Add(pStaticText, 0, 
+								wxALIGN_CENTER_VERTICAL | wxALIGN_RIGHT | wxALL, 5);
+
+   wxChoice * pChoice_FromVinyl = 
+		new wxChoice(this, ID_CHOICE_FROMVINYL, wxDefaultPosition, wxSize(64, -1), 
+							numChoices, strArray_VinylRPM);
+   pBoxSizer_Vinyl->Add(pChoice_FromVinyl, 0, wxALIGN_CENTER | wxALL, 5);
+
+   pStaticText =
+       new wxStaticText(this, ID_TEXT, _("To"),
+                        wxDefaultPosition, wxDefaultSize, 0);
+   pBoxSizer_Vinyl->Add(pStaticText, 0, 
+								wxALIGN_CENTER_VERTICAL | wxALIGN_RIGHT | wxALL, 5);
+
+   wxChoice * pChoice_ToVinyl = 
+		new wxChoice(this, ID_CHOICE_TOVINYL, wxDefaultPosition, wxSize(64, -1), 
+							numChoices, strArray_VinylRPM);
+   pBoxSizer_Vinyl->Add(pChoice_ToVinyl, 0, wxALIGN_CENTER | wxALL, 5);
+
+   pBoxSizer_Dialog->Add(pBoxSizer_Vinyl, 0, wxALIGN_CENTER | wxALL, 5);
+
+
+	// OK & Cancel buttons
+   wxBoxSizer * pBoxSizer_OK = new wxBoxSizer(wxHORIZONTAL);
+
+   wxButton * pButton_OK =
+       new wxButton(this, wxID_OK, _("OK"), wxDefaultPosition,
+                    wxDefaultSize, 0);
+   pButton_OK->SetDefault();
+   pButton_OK->SetFocus();
+   pBoxSizer_OK->Add(pButton_OK, 0, wxALIGN_CENTER | wxALL, 5);
+
+   wxButton * pButton_Cancel =
+       new wxButton(this, wxID_CANCEL, _("Cancel"), wxDefaultPosition,
+                    wxDefaultSize, 0);
+   pBoxSizer_OK->Add(pButton_Cancel, 0, wxALIGN_CENTER | wxALL, 5);
+
+   pBoxSizer_Dialog->Add(pBoxSizer_OK, 0, wxALIGN_CENTER | wxALL, 8);
+
+
+   this->SetAutoLayout(true);
+   this->SetSizer(pBoxSizer_Dialog);
+   pBoxSizer_Dialog->Fit(this);
+   pBoxSizer_Dialog->SetSizeHints(this);
+}
+
+bool ChangeSpeedDialog::Validate()
+{
+   return true; 
+}
+
+bool ChangeSpeedDialog::TransferDataToWindow()
+{
+   m_bLoopDetect = true;
+
+	// percent change controls
+	this->Update_Text_PercentChange();
+	this->Update_Slider_PercentChange();
+
+	// from/to Vinyl controls
+	wxChoice * pChoice = this->GetChoice_FromVinyl();
+	if (pChoice) 
+		pChoice->SetSelection(m_FromVinyl);
+
+	pChoice = this->GetChoice_ToVinyl();
+	if (pChoice) 
+		pChoice->SetSelection(m_ToVinyl);
+
+	m_bLoopDetect = false;
+
+	return true;
+}
+
+bool ChangeSpeedDialog::TransferDataFromWindow()
+{
+	wxString str;
+
+	// percent change controls
+   wxTextCtrl * pTextCtrl = this->GetTextCtrl_PercentChange();
+   if (pTextCtrl) {
+      str = pTextCtrl->GetValue();
+      double newValue;
+      str.ToDouble(&newValue);
+		m_PercentChange = newValue;
+	}
+
+	// Ignore Slider_PercentChange because TextCtrl_PercentChange 
+	// always tracks it & is more precise (decimal points).
+
+
+	// from/to Vinyl controls
+	wxChoice * pChoice = this->GetChoice_FromVinyl();
+	if (pChoice) 
+		m_FromVinyl = pChoice->GetSelection();
+
+   pChoice = this->GetChoice_FromVinyl();
+	if (pChoice) 
+		m_ToVinyl = pChoice->GetSelection();
+
+   return true;
+}
+
+// handler implementations for ChangeSpeedDialog
+
+void ChangeSpeedDialog::OnText_PercentChange(wxCommandEvent & event)
+{
+   if (m_bLoopDetect)
+      return;
+
+   wxTextCtrl * pTextCtrl_PercentChange = this->GetTextCtrl_PercentChange();
+   if (pTextCtrl_PercentChange) {
+      wxString str = pTextCtrl_PercentChange->GetValue();
+      double newValue;
+      str.ToDouble(&newValue);
+		m_PercentChange = newValue;
+
+      m_bLoopDetect = true;
+		this->Update_Slider_PercentChange();
+		this->Update_Vinyl();
+      m_bLoopDetect = false;
+   }
+}
+
+void ChangeSpeedDialog::OnSlider_PercentChange(wxCommandEvent & event)
+{
+   if (m_bLoopDetect)
+      return;
+
+	wxSlider * slider = this->GetSlider_PercentChange();
+	if (slider) {
+		m_PercentChange = (double)(slider->GetValue()); 
+		// Warp positive values to actually go up faster & further than negatives.
+		if (m_PercentChange > 0.0)
+			m_PercentChange = pow(m_PercentChange, PERCENTCHANGE_SLIDER_WARP);
+
+	   m_bLoopDetect = true;
+		this->Update_Text_PercentChange();
+		this->Update_Vinyl();
+	   m_bLoopDetect = false;
+	}
+}
+
+void ChangeSpeedDialog::OnChoice_FromVinyl(wxCommandEvent & event)
+{
+   if (m_bLoopDetect)
+      return;
+
+	wxChoice * pChoice = this->GetChoice_FromVinyl();
+	if (pChoice) {
+		m_FromVinyl = pChoice->GetSelection();
+
+      m_bLoopDetect = true;
+		this->Update_PercentChange();
+      m_bLoopDetect = false;
+   }
+}
+
+void ChangeSpeedDialog::OnChoice_ToVinyl(wxCommandEvent & event)
+{
+   if (m_bLoopDetect)
+      return;
+
+	wxChoice * pChoice = this->GetChoice_ToVinyl();
+	if (pChoice) {
+		m_ToVinyl = pChoice->GetSelection();
+
+      m_bLoopDetect = true;
+		this->Update_PercentChange();
+      m_bLoopDetect = false;
+   }
+}
+
+
+void ChangeSpeedDialog::OnOk(wxCommandEvent & event)
+{
+   TransferDataFromWindow();
+   
+	wxMessageBox(_("Change Speed not yet implemented."),
+                _("Not Yet Implemented"));
+
+   if (Validate()) 
+      EndModal(true);
+   else 
+      event.Skip();
+}
+
+void ChangeSpeedDialog::OnCancel(wxCommandEvent & event)
+{
+   EndModal(false);
+}
+
+
+// helper fns
+
+void ChangeSpeedDialog::Update_Text_PercentChange()
+{
+	wxTextCtrl * pTextCtrl = this->GetTextCtrl_PercentChange();
+	if (pTextCtrl) {
+		wxString str;
+		str.Printf(_("%.1f"), m_PercentChange);
+		pTextCtrl->SetValue(str);
+	}
+}
+
+void ChangeSpeedDialog::Update_Slider_PercentChange()
+{
+   wxSlider * slider = this->GetSlider_PercentChange();
+   if (slider) {
+		double unwarped = m_PercentChange;
+		if (unwarped > 0.0)
+			// Un-warp values above zero to actually go up to PERCENTCHANGE_MAX.
+			unwarped = pow(m_PercentChange, (1.0 / PERCENTCHANGE_SLIDER_WARP));
+		slider->SetValue((int)(unwarped + 0.5)); // Add 0.5 so trunc -> round.
+	}
+}
+
+void ChangeSpeedDialog::Update_Vinyl() 
+// Update Vinyl controls for new percent change.
+{
+	wxChoice * pChoice = this->GetChoice_ToVinyl();
+	if (pChoice) 
+		// Chances are so low that the slider will exactly match a 
+		// standard ratio, just turn it "n/a".
+		pChoice->SetSelection(CHOICE_NA);
+}
+
+void ChangeSpeedDialog::Update_PercentChange() 
+// Update percent change controls for new Vinyl values.
+{
+	// If m_FromVinyl & m_ToVinyl are set, then there's a new percent change.
+	if ((m_FromVinyl != CHOICE_NA) && (m_ToVinyl != CHOICE_NA)) {
+		double fromRPM;
+		double toRPM;
+		switch (m_FromVinyl) {
+		case CHOICE_33ANDATHIRD:	fromRPM = 33.0 + (1.0 / 3.0); break;
+		case CHOICE_45:				fromRPM = 45.0; break;
+		case CHOICE_78:				fromRPM = 78; break;
+		}
+		switch (m_ToVinyl) {
+		case CHOICE_33ANDATHIRD:	toRPM = 33.0 + (1.0 / 3.0); break;
+		case CHOICE_45:				toRPM = 45.0; break;
+		case CHOICE_78:				toRPM = 78; break;
+		}
+		m_PercentChange = ((toRPM * 100.0) / fromRPM) - 100.0;
+
+		this->Update_Text_PercentChange();
+		this->Update_Slider_PercentChange();
+	}
+}
