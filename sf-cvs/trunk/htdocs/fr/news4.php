e8d0dca952e5709385f4c3ade4c7025a2b2b1ab6
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-02-10 08:11:38 +0000

    manual

---
+++ sf-cvs/trunk/htdocs/fr/news4.php
@@ -0,0 +1,11 @@
+<?php BoxTop('22 décembre 2002: la version 1.1.2 pour Windows est maintenant disponible'); ?>
+
+<p>
+Cette nouvelle version est téléchargeable à partir de la page <?php print "<a href=beta.php?$langLinkStr>"; ?>Versions Bêta</a>.
+Nous recommandons aux utilisateurs de la version Beta pour Windows d'obtenir cette nouvelle version pour corriger des problèmes importants. Cette mise à jour n'est pas nécessaire pour les utilisateurs travaillant sur d'autres plateformes.
+</p>
+<p>
+Pour plus de renseignements, veuillez vous référer aux annonces ci-dessous.
+</p>
+
+<?php BoxBottom(); ?>
