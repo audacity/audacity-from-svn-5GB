0e4983e48999c52cb4f6134d982431d0acd6b553
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-02-17 20:48:50 +0000

    Added single-instance checker for Windows only

--- sf-cvs/trunk/audacity-src/src/AudacityApp.cpp
+++ sf-cvs/trunk/audacity-src/src/AudacityApp.cpp
@@ -249,7 +249,27 @@ END_EVENT_TABLE()
 // main frame
 bool AudacityApp::OnInit()
 {
-   // mChecker = new wxSingleInstanceChecker(GetAppName());
+#ifdef __WXMSW__
+   // This is our own replacement for wxSingleInstanceChecker for Windows.
+   mSingleInstanceMutex = (void *)::CreateMutex(NULL, FALSE, "AudacitySingleInstanceMutex");
+   bool alreadyRunning = (GetLastError() == ERROR_ALREADY_EXISTS);
+   if (alreadyRunning) {
+      wxString prompt =
+         "The system has detected that another copy of Audacity may be running.\n"
+         "Running two copies of Audacity simultaneously may lead to data loss or\n"
+         "cause your system to crash.\n\n"
+         "Are you sure you want to launch Audacity now?";
+         int action = wxMessageBox(prompt,
+                                   "Audacity is already running",
+                                   wxYES_NO | wxICON_EXCLAMATION,
+                                   NULL);
+         if (action == wxNO) {
+            CloseHandle((HANDLE)mSingleInstanceMutex);
+            return false;
+         }
+   }
+#endif
+
    ::wxInitAllImageHandlers();
 
    wxFileSystem::AddHandler(new wxZipFSHandler);
