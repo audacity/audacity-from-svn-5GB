0a2b16fd1b9d420859c46749fdf4ed932cfa2e12
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-02-27 07:50:42 +0000

    *** empty log message ***

---
+++ sf-cvs/trunk/htdocs/ru/sisyphus.php
@@ -0,0 +1,25 @@
+<?php BoxTop("Sisyphus"); ?>
+
+<p>
+Audacity также присутствует в банке пакетов (репозитории) 
+Sisyphus компании ALT Linux. Для установки Audacity из 
+Sisyphus используйте следующую команду:
+<xmp>$ apt-get update && apt-get install audacity</xmp>
+</p>
+
+<p>
+Для установки вы также можете воспользоваться удобной 
+графической надстройкой над apt &mdash; Synaptic.
+<p>
+
+<p>
+<a>Более подробная информация о пакете Audacity на www.altlinux.com</a>
+</p>
+
+<p>
+Мэйнтейнер пакета: 
+Андрей Астафьев <br>
+E-mail: <tt>andrei <b>@</b> tvcell.ru</tt>
+</p>
+
+<?php BoxBottom(); ?>
