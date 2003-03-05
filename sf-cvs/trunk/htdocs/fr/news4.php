8ef155d57c8e2e04d32a63a1d3b725e9b794486e
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-03-05 00:32:13 +0000

    automatic

--- sf-cvs/trunk/htdocs/fr/news4.php
+++ sf-cvs/trunk/htdocs/fr/news4.php
@@ -2,7 +2,7 @@
 
 <p>
 Cette nouvelle version est téléchargeable à partir de la page <?php print "<a href=beta.php?$langLinkStr>"; ?>Versions Bêta</a>.
-Nous recommandons aux utilisateurs de la version Beta pour Windows d'obtenir cette nouvelle version pour corriger des problèmes importants. Cette mise à jour n'est pas nécessaire pour les utilisateurs travaillant sur d'autres plateformes.
+Nous recommandons aux utilisateurs de la version Bêta pour Windows d'obtenir cette nouvelle version pour corriger des problèmes importants. Cette mise à jour n'est pas nécessaire aux utilisateurs travaillant sur d'autres plateformes.
 </p>
 <p>
 Pour plus de renseignements, veuillez vous référer aux annonces ci-dessous.
