cf00c1ec649e80fb585d06530bb8efc553b40f4b
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-03-20 06:38:50 +0000

    Noted that Mac OS 9 is out of date

---
+++ sf-cvs/trunk/htdocs/en/macos9note.php
@@ -0,0 +1,6 @@
+<p>
+Note: Unless someone is willing to contribute financially or
+technically, we are not planning to release any more Mac OS 9
+versions of Audacity.  Please email us if you would like to
+help with the Mac OS 9 effort in any way.
+</p>
