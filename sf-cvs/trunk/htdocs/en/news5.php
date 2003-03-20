211a18d1cfd9280818697ad5dc6c76ea5fdab81f
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-03-20 06:33:43 +0000

    Releasing version 1.1.3

---
+++ sf-cvs/trunk/htdocs/en/news5.php
@@ -0,0 +1,25 @@
+<?php BoxTop('March 19, 2003: Version 1.1.3 released'); ?>
+
+<p>
+Go to the <?php print "<a href=beta.php?$langLinkStr>"; ?>Beta Versions</a> page
+to download.
+This version adds a gain and pan control to every track,
+supports high-quality resampling, and has many other new
+features and bug fixes.
+<?php print "<a href=betanotes.php?$langLinkStr>"; ?>List of new features in 1.1.3.</a>
+</p>
+
+<p>
+Note that Audacity 1.1.3 will not read project files saved by
+any other version of Audacity, so you may not want to try this
+version if you have project files you are still working on.
+Export your audio as WAV or AIFF first if you want to open it in this
+new version.
+</p>
+
+<p>
+The plan is for our next release to be a stable version of Audacity
+intended for all users.
+</p>
+
+<?php BoxBottom(); ?>
--- sf-cvs/trunk/htdocs/versions.inc.php
+++ sf-cvs/trunk/htdocs/versions.inc.php
@@ -6,14 +6,14 @@
   // beta/versions.inc.php...
 
   $stableVersion =   "1.0.0";
-  $betaVersion =     "1.1.2";
+  $betaVersion =     "1.1.3";
 
   $winStableVersion = "1.0.0";
   $winStableSize =    "1.6 MB";
   $winStableZipSize = "1.3 MB";
-  $winBetaVersion =   "1.1.2";
-  $winBetaSize =      "2.2 MB";
-  $winBetaZipSize =   "1.9 MB";
+  $winBetaVersion =   "1.1.3";
+  $winBetaSize =      "2.7 MB";
+  $winBetaZipSize =   "2.4 MB";
 
   $macOS9StableVersion = "1.0.0";
   $macOS9StableSize =    "1.5 MB";
@@ -22,12 +22,12 @@
 
   $macOSXStableVersion = "1.0.0";
   $macOSXStableSize =    "1.9 MB";
-  $macOSXBetaVersion =   "1.1.1";
-  $macOSXBetaSize =      "2.4 MB";
+  $macOSXBetaVersion =   "1.1.3";
+  $macOSXBetaSize =      "2.8 MB";
 
   $srcStableVersion = "1.0.0-2";
   $srcStableSize =    "1.6 MB";
-  $srcBetaVersion =   "1.1.1-3";
+  $srcBetaVersion =   "1.1.3";
   $srcBetaSize =      "2.3 MB";
 
   $rpmStableVersion = "1.0.0-2";
