458e71da84627e38ab8e79f12f51eba2a2bffc9a
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-03-20 06:42:05 +0000

    Updating beta notes

--- sf-cvs/trunk/htdocs/en/beta.php
+++ sf-cvs/trunk/htdocs/en/beta.php
@@ -1,8 +1,10 @@
 <p>
 Warning: the following version of Audacity is beta quality!
 It contains dozens of new features, including a new toolbar,
-automatic resampling, and support for 32-bit samples, but it probably
-also contains new bugs.
+automatic resampling, support for 32-bit samples, and pan and
+gain sliders in every track.  It supports many languages other
+than English.  But it is unfinished, the documentation is
+incomplete, and it may contain serious bugs.
 </p>
 <p>
 If you are interested in experimenting with the new features, we
@@ -10,10 +12,12 @@ encourage you to download this version and give it a try.
 If you do, please send us feedback.  Let us know what works,
 what doesn't, and what new features you would like to see.
 You can have this version and Audacity 1.0 on your computer at
-the same time.  
+the same time.
 </p>
 <p>
 Note:
-This version cannot read or write project files saved by Audacity 1.0
-or older.  It uses a new XML-based project file format.
+Audacity 1.1.3 cannot read or write project files saved by any other
+version of Audacity, including 1.1.1 or 1.1.2.  You must export all
+of your audio to standard formats like WAV, AIFF, or MP3 before using
+version 1.1.3.
 </p>
