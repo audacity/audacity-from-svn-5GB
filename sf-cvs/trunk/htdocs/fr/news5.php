0d85ce97e3cca805a050f17cd48e30b9843ed845
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-03-25 00:32:06 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/fr/news5.php
@@ -0,0 +1,16 @@
+<?php BoxTop('19 mars 2003:  la version 1.1.3 est maintenant disponible'); ?>
+
+<p>
+Cette version est téléchargeable à partir de la page <?php print "<a href=beta.php?$langLinkStr>"; ?> Versions Bêta </a>.
+Elle permet de régler le gain et l'effet panoramique pour chaque piste,
+permet le rééchantillonage à haute-définition, offre plusieurs caractéristiques additionnelles et corrige de nombreux bogues.
+<?php print "<a href=betanotes.php?$langLinkStr>"; ?>Nouveautés de la version 1.1.3.</a>
+</p>
+
+<p>
+Prenez note que la version 1.1.3 d'Audacity ne peut ouvrir les fichiers de projet créés par les versions antérieures. Il est donc déconseillé d'essayer cette version si vous travaillez déjà sur des projets. Pour ouvrir ces fichiers avec la version 1.1.3, exportez-les d'abord en format WAV ou AIFF.
+</p>
+
+<p>
+Notre objectif est de faire de la prochaine mise à jour d'Audacity une version stable pour tous les utilisateurs.
+</p>
