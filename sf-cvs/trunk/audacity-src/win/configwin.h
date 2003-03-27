4b686c0989eae859536a567901ee3f2aaba10ed9
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2003-03-27 03:35:11 +0000

    soundtouch -> Windows

--- sf-cvs/trunk/audacity-src/win/configwin.h
+++ sf-cvs/trunk/audacity-src/win/configwin.h
@@ -1,12 +1,14 @@
 // Microsoft Windows specific include file
 
-#define USE_LIBMAD 1
 #define MP3SUPPORT 1
-#define USE_LIBVORBIS 1
 #define USE_LIBID3TAG 1
+#define USE_LIBMAD 1
+#define USE_LIBSAMPLERATE 1
+#define USE_LIBVORBIS 1
 #define USE_NYQUIST 1
 #define USE_PORTMIXER 1
-#define USE_LIBSAMPLERATE 1
+#define USE_SOUNDTOUCH 1
+
 
 #define INSTALL_PREFIX "."
 
