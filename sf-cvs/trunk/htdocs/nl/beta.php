3ee6ba9470185f85bfb7ed8fa0673393f75d79dc
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-03-29 08:32:05 +0000

    automatic

--- sf-cvs/trunk/htdocs/nl/beta.php
+++ sf-cvs/trunk/htdocs/nl/beta.php
@@ -1,14 +1,15 @@
 <p>
 Waarschuwing: de navolgende versie van Audacity is van beta kwaliteit!
 Het bevat vele nieuwe mogelijkheden, zoals nieuwe werkbalken,
-automatische resampling, en ondersteuning voor 32-bit samples, maar waarschijnlijk bevat het ook nieuwe fouten.
+automatische resampling, ondersteuning voor 32-bit samples en "pan en gain" schuifregelaars voor elke muziektrack. Het programma ondersteunt niet alleen Engels maar ook meerdere andere talen. Het programma is echter nog niet geheel af, de documentatie is nog niet gereed en waarschijnlijk bevat het ook nog ernstige fouten.
 </p>
 <p>
-Als je geinteresseerd bent in het experimenteren met de nieuwe mogelijkheden, nodigen wij je uit om deze versie te downloaden en gewoon eens uit te proberen.
+Als je geïnteresseerd bent in het experimenteren met de nieuwe mogelijkheden, nodigen wij je uit om deze versie te downloaden en gewoon eens uit te proberen.
 Als je dit doet, stuur ons dan a.u.b. je ervaring met de werking hiervan. Laat ons weten wat werkt en wat niet en welke nieuwe mogelijkheden je zou willen hebben.
 Je kunt zowel deze versie als Audacity 1.0 tegelijkertijd op je computer geinstalleerd hebben.  
 </p>
 <p>
 Opmerking:
-Deze versie kan geen bestanden die via Audacity 1.0 of ouder zijn opgeslagen, lezen of wegschrijven. Het gebruikt een nieuwe, op XML gebaseerde, bestandsformaat.
+Audacity 1.1.3 kan geen bestanden die via andere Audacity versies (zoals 1.1.1 of 1.1.2 of ouder) zijn opgeslagen, lezen of wegschrijven. Je moet al je audiobestanden exporteren in standaard formaten zoals WAV, AIFF of MP3 voordat je Audacity 1.1.3 gaat gebruiken.
 </p>
+
