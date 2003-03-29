296d66fc0ecf13e23e7c204eecf275f6cb26378a
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-03-29 08:32:05 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/nl/news5.php
@@ -0,0 +1,17 @@
+<?php BoxTop('19 maart 2003: Versie 1.1.3 vrijgegeven'); ?>
+
+<p>
+Ga naar de <?php print "<a href=beta.php?$langLinkStr>"; ?>Beta Versies</a> pagina voor de download.
+Deze versie voegt een "gain en pan" regelaar toe aan elke muziektrack,
+ondersteunt hoge kwaliteits resampling, en bevat vele nieuwe mogelijkheden en herstelde fouten.
+<?php print "<a href=betanotes.php?$langLinkStr>"; ?>Lijst van nieuwe mogelijkheden in 1.1.3.</a>
+</p>
+
+<p>
+Merk wel op dat Audacity 1.1.3 geen via andere Audacity versies opgeslagen projecten kan openen en bewerken, dus kan het zijn dat je deze versie beter niet kunt gebruiken als je nog projecten aan het bewerken bent.
+Exporteer eerst je audiobestanden als WAV of AIFF als je ze in deze nieuwe versie wilt openen.
+</p>
+
+<p>
+Onze eerstvolgende stap zal een vrijgave zijn van een door iedereen te gebruiken stabiele versie van Audacity.
+</p>
