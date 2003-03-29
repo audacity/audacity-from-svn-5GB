8ebd2f55b179fac70864de87d5413e67a7cdacec
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-03-29 20:32:03 +0000

    automatic

--- sf-cvs/trunk/htdocs/nl/gentoo.php
+++ sf-cvs/trunk/htdocs/nl/gentoo.php
@@ -1,18 +1,16 @@
 <?php BoxTop("Gentoo Linux"); ?>
 
 <p>
-Een ebuild voor Audacity is bijgedragen aan het
-<a href="http://www.gentoo.org/">Gentoo Linux</a>
-portage systeem.  Om de laatste versie van Audacity te installeren vanaf de
-portage, voer uit
+<a href="http://www.gentoo.org/">Gentoo Linux</a> heeft momenteel Audacity versie 1.1.1-3 in het "portage-systeem" als de 'stabiele' versie.
+Voor installatie van Audacity...
 <p>
 <xmp>$ emerge audacity</xmp>
 </p>
 
 <p>
 Beheerder: 
-Will Glynn <br>
-E-mail: <tt>delta407 <b>@</b> lerfjhax.com</tt>
+Nick Hadaway <br>
+E-mail: <tt>raker <b>@</b> gentoo.org</tt>
 </p>
 
 <?php BoxBottom(); ?>
