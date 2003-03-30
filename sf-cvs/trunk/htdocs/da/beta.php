6c7a4db65bbd9529c7222fb82b88e4626322b198
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-03-30 12:32:04 +0000

    automatic

--- sf-cvs/trunk/htdocs/da/beta.php
+++ sf-cvs/trunk/htdocs/da/beta.php
@@ -1,8 +1,7 @@
 <p>
 Advarsel: denne udgave af Audacity er en beta-udgave!
 Den har masser af nye faciliteter, blandt andeten ny værktøjslinje
-automatisk resampling, brug af 32-bit sampling, men har sikkert også 
-mange nye fejl.
+automatisk resampling, brug af 32-bit sampling, Gain og Pan kontroller på hvert spor. Programmet er oversat til mange sprog. Programmet betragtes stadig som ufærdigt, dokumentationen er ukomplet, og der kan være alvorlige fejl endnu.
 </p>
 <p>
 Hvis du har lyst til at eksperimentere med de nye faciliteter,
@@ -15,7 +14,5 @@ installeret samtidig på din computer.
 </p>
 <p>
 Bemærk:
-Denne udgave kan ikke læse eller skrive projekter, der kan bruges
-af Audacity 1.0 eller tidligere, da den bruger et nyt XML-baseret
-filformat.
+Denne udgave kan ikke læse eller skrive projektfiler fra andre udgaver af Audacity, hverken 1.0, 1.1.1 eller 1.1.2. For at flytte eksisterende projekter til denne version må du eksportere alle lydsporene til et standardformat (WAV, AIFF) og derefter importere filerne til 1.1.3.
 </p>
