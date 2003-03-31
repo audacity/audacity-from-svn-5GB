72475ec4bf04df030c42daa557c78707aee69c23
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-03-31 09:32:15 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/pt/news5.php
@@ -0,0 +1,16 @@
+<?php BoxTop('19 de Março de 2003: Lançada a versão 1.1.3'); ?>
+
+<p>
+Vá à página <?php print "<a href=beta.php?$langLinkStr>"; ?>Versões Beta </a> para copiar.</p><p>
+Esta nova versão dispõe de controles de ganho (volume) e "pan" (balanço) para cada faixa, re-amostragens de alta qualidade e a correção de diversas falhas das versões anteriores.</p><p>
+Veja a lista completa de novos recursos na <?php print "<a href=betanotes.php?$langLinkStr>"; ?>lista de novos recursos da versão 1.1.3.</a>
+</p>
+
+<p>
+Atenção! Esta versão do Audacity não é capaz de ler ou salvar projetos de versões anteriores do programa. Exporte suas faixas de áudio em formato WAV ou AIFF para abri-las nesta nova versão.
+</p>
+
+<p>
+Esperamos que este seja o último release beta antes da liberação da versão final estável ao público.</p>
+
+<?php BoxBottom(); ?>
