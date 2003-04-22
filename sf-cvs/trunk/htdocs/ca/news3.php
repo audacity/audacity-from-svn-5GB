01c10fa6285dd8d0b87eabf8193b6dc4cd50145e
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-04-22 20:32:19 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/ca/news3.php
@@ -0,0 +1,28 @@
+<?php BoxTop('3 de desembre del 2002: actualització de la versió 1.1.1'); ?>
+
+<p>
+La documentació encara no acabada per a la versió 1.1.1 es troba ara disponible per navegar en línia. Es farà disponible com a una descàrrega independent quan estigui acabada.
+</p>
+
+<p>
+<center>
+<a href="docs1.1/">Documentació de l'Audacity 1.1.1</a>
+</center>
+</p>
+
+<p>
+We are aware that there is a serious problem in version 1.1.1 (beta)
+for Windows.  Selecting anything from a track pop-up menu will crash
+the program.  We will be releasing version 1.1.2 for Windows only
+within a few days to address this problem.  (This only affects the pop-up
+menu in the track's label - all other menus function correctly - and
+this only affects the Microsoft Windows version of Audacity 1.1.1.)
+</p>
+
+<p>
+For Unix users, a new source tarball has been released, 1.1.1-3, which
+fixes problems compiling with wxGTK 2.2.9.  We hope to have a version
+which compiles on RedHat 8 with no modifications soon.
+</p>
+
+<?php BoxBottom(); ?>
