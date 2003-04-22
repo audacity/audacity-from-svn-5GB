874cb4489727abb9d4c1483e77562beb979e30c0
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-04-22 19:32:06 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/ca/news1.php
@@ -0,0 +1,25 @@
+<?php BoxTop('5 de juny del 2002: Versions 1.0.0 i 1.1.0 realitzades!'); ?>
+
+<p>
+L'equip de desenvolupament de l'Audacity es complau d'anunciar del llançament simultani de les versions 1.0.0 i 1.1.0.
+</p>
+
+<p>
+La versió 1.0.0 és la final versió estable de l'Audacity original.
+És un bon i simple editor d'àudio apropiat per gravar barrejar i editar peces ordinàries de qualitat de CD d'àudio, en diverses plataformes incloent-hi el Windows, el Mac OS, i el Linux.
+</p>
+
+<p>
+Version 1.1.0 is the first beta release of the next version of Audacity,
+which includes support for higher-quality audio, more audio file formats,
+more effects, more editing functions, an improved user interface, and
+support for foreign languages.  Though this version has many new and useful
+features, some of these features are still under development and they have
+not been tested as much, and there may have problems.
+</p>
+
+<p>
+És possible tenir les dues versions de l'Audacity alhora al mateix ordinador.
+</p>
+
+<?php BoxBottom(); ?>
