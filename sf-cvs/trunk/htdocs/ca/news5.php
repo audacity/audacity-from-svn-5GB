5152d6e81ec28c970ce1fc2d345612be44752b8e
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-04-22 06:32:06 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/ca/news5.php
@@ -0,0 +1,19 @@
+<?php BoxTop('19 de març del 2003: Versió 1.1.3 realitzada'); ?>
+
+<p>
+Aneu a la pàgina de descàrrega <?php print "<a href=beta.php?$langLinkStr>"; ?>de les verions beta</a>.
+Aquesta versió afegeix control d'augment i resultats de cada peça,
+permet l'ús de remostreig d'alta qualitat, i té altres característiques noves i fixacions d'errors diferents.
+<?php print "<a href=betanotes.php?$langLinkStr>"; ?>Llistat de característiques noves en el 1.1.3.</a>
+</p>
+
+<p>
+Tingueu present que l'Audacity 1.1.3 no llegirà fitxers de projecte desats en el format de qualsevol altra versió anterior de l'Audacity i que per tant no el voldreu provar si teniu projectes en els que encara hi voleu treballar.
+Exporteu primer el vostre àudio com a WAV o AIFF si el voleu obrir en aquesta nova versió.
+</p>
+
+<p>
+El pla és que la nostra propera versió sigui estable independentment del grau de l'usuari.
+</p>
+
+<?php BoxBottom(); ?>
