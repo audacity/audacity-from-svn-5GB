a85316bc22dca54e1691c7102e72459fe8eeb5c4
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2003-05-07 05:00:47 +0000

    Comments and default parameter values.

--- sf-cvs/trunk/audacity-src/src/PitchName.h
+++ sf-cvs/trunk/audacity-src/src/PitchName.h
@@ -17,20 +17,25 @@
 
 // Freq2Pitch takes a frequency in Hz (exponential scale relative to 
 // alphabetic pitch names) and returns a pitch ID number (linear 
-// scale), such that A440 (A4) is 57, middle C is 48, etc.
+// scale), such that A440 (A4) is 57, middle C (C4) is 48, etc.
 // The offset to 57 is used to determine the register. 
 // Each register starts with C (e.g., for middle C and A440, 
 // it's register 4).
 float Freq2Pitch(float freq);
 
-// PitchName takes an integer version of a pitch ID (result from 
+// PitchIndex returns the [0,11] index for a float pitchNum, 
+// as per result from Freq2Pitch, corresponding to modulo 12 
+// of the integer part of (pitchNum + 0.5), so 0=C, 1=C#, etc.
+unsigned int PitchIndex(float pitchNum);
+
+// PitchName takes pitchNum (as per result from 
 // Freq2Pitch) and returns a standard pitch/note name [C, C#, etc.). 
 // Sharps are the default, unless, b_Wantb_WantFlats is true.
-char * PitchName(int pitchID, bool b_WantFlats = false);
+char * PitchName(float pitchNum, bool b_WantFlats = false);
 
 // PitchName_Absolute does the same thing as PitchName, but appends 
 // the register number, e.g., instead of "C" it will return "C4" 
-// if the pitchID corresonds to middle C.
-char * PitchName_Absolute(int pitchID, bool b_WantFlats = false);
+// if the pitchNum corresonds to middle C.
+char * PitchName_Absolute(float pitchNum, bool b_WantFlats = false);
 
 #endif	// __AUDACITY_PITCHNAME__
