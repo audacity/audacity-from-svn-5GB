fb2996ac3971e0bf60b1b7c68738c9e887f261ad
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2003-07-08 20:45:30 +0000

    Added dialog instead of 2 prompts, and Preview.

--- sf-cvs/trunk/audacity-src/src/effects/Echo.cpp
+++ sf-cvs/trunk/audacity-src/src/effects/Echo.cpp
@@ -5,6 +5,7 @@
   Echo.cpp
 
   Dominic Mazzoni
+  Vaughan Johnson (dialog)
 
 **********************************************************************/
 
@@ -29,36 +30,17 @@ wxString EffectEcho::GetEffectDescription() {
 
 bool EffectEcho::PromptUser()
 {
-   wxString temp;
-   wxString title = _("Echo");
-   wxString caption = _("Delay time (seconds): ");
-   wxString default_value = wxString::Format("%f", delay);
+   EchoDialog dlog(this, mParent, -1, _("Echo"));
+   dlog.delay = delay;
+   dlog.decay = decay;
+   dlog.CentreOnParent();
+   dlog.ShowModal();
 
-   temp = wxGetTextFromUser(caption, title,
-                            default_value, mParent, -1, -1, TRUE);
-   if (temp == "")
+   if (!dlog.GetReturnCode())
       return false;
-   while (sscanf((const char *) temp, "%f", &delay) < 0) {
-      caption = _("Please enter a positive number for the delay time: ");
-      temp = wxGetTextFromUser(caption, title,
-                               default_value, mParent, -1, -1, TRUE);
-      if (temp == "")
-         return false;
-   }
 
-   caption = _("Enter the decay factor: ");
-   default_value = wxString::Format("%f", decay);
-   temp = wxGetTextFromUser(caption, title,
-                            default_value, mParent, -1, -1, TRUE);
-   if (temp == "")
-      return false;
-   while (sscanf((const char *) temp, "%f", &decay) < 0) {
-      caption = _("Please enter a positive number for the decay factor: ");
-      temp = wxGetTextFromUser(caption, title,
-                               default_value, mParent, -1, -1, TRUE);
-      if (temp == "")
-         return false;
-   }
+   delay = dlog.delay;
+   decay = dlog.decay;
 
    return true;
 }
@@ -138,3 +120,209 @@ bool EffectEcho::ProcessOne(int count, WaveTrack * track,
 
    return true;
 }
+
+//----------------------------------------------------------------------------
+// EchoDialog
+//----------------------------------------------------------------------------
+
+enum {
+   ID_TEXT_DELAY = 10001,
+   ID_TEXT_DECAY,
+	ID_BUTTON_PREVIEW
+};
+
+
+// event table for EchoDialog
+
+BEGIN_EVENT_TABLE(EchoDialog, wxDialog)
+    EVT_BUTTON(wxID_OK, EchoDialog::OnOk)
+    EVT_BUTTON(wxID_CANCEL, EchoDialog::OnCancel)
+
+    //vvv EVT_TEXT(ID_TEXT_DELAY, EchoDialog::OnText_Delay)
+    //vvv EVT_SLIDER(ID_TEXT_DECAY, EchoDialog::OnText_Decay)
+
+    EVT_BUTTON(ID_BUTTON_PREVIEW, EchoDialog::OnPreview)
+END_EVENT_TABLE()
+
+EchoDialog::EchoDialog(EffectEcho * effect,
+								wxWindow * parent, wxWindowID id,
+								const wxString & title, 
+								const wxPoint & position, 
+								const wxSize & size, 
+								long style)
+: wxDialog(parent, id, title, position, size, style)
+{
+   m_bLoopDetect = false;
+	m_pEffect = effect;
+
+	// NULL out these control members because there are some cases where the 
+	// event table handlers get called during this method, and those handlers that 
+	// can cause trouble check for NULL.
+   m_pTextCtrl_Delay = NULL;
+   m_pTextCtrl_Decay = NULL;
+	
+	// effect parameters
+   delay = float(1.0);
+   decay = float(0.5);
+
+	// CREATE THE CONTROLS PROGRAMMATICALLY.
+	wxStaticText * pStaticText;
+
+   wxBoxSizer * pBoxSizer_Dialog = new wxBoxSizer(wxVERTICAL);
+
+	// heading
+   pStaticText = new wxStaticText(this, -1, 
+												_("Echo"),
+												wxDefaultPosition, wxDefaultSize, 0);
+   pBoxSizer_Dialog->Add(pStaticText, 0, wxALIGN_CENTER | wxALL, 8);
+
+   pStaticText = new wxStaticText(this, -1, 
+												_("by Dominic Mazzoni && Vaughan Johnson"),
+												wxDefaultPosition, wxDefaultSize, 0);
+   pBoxSizer_Dialog->Add(pStaticText, 0, wxALIGN_CENTER | wxTOP | wxLEFT | wxRIGHT, 8);
+
+   pBoxSizer_Dialog->Add(0, 8, 0); // spacer
+
+
+	// delay
+   wxBoxSizer * pBoxSizer_Delay = new wxBoxSizer(wxHORIZONTAL);
+   
+   pStaticText = new wxStaticText(this, -1, _("Delay time (seconds):"),
+												wxDefaultPosition, wxDefaultSize, 0);
+   pBoxSizer_Delay->Add(pStaticText, 0, 
+								wxALIGN_CENTER_VERTICAL | wxALIGN_RIGHT | wxALL, 4);
+
+	//v Override wxTextValidator to disallow negative values <= -100.0?
+   m_pTextCtrl_Delay = 
+		new wxTextCtrl(this, ID_TEXT_DELAY, "1.0", 
+							wxDefaultPosition, wxSize(64, -1), 0,
+							wxTextValidator(wxFILTER_NUMERIC));
+   pBoxSizer_Delay->Add(m_pTextCtrl_Delay, 0, 
+								wxALIGN_CENTER_VERTICAL | wxALIGN_LEFT | wxALL, 4);
+
+   pBoxSizer_Dialog->Add(pBoxSizer_Delay, 0, wxALIGN_CENTER | wxALL, 4);
+
+
+	// decay
+   wxBoxSizer * pBoxSizer_Decay = new wxBoxSizer(wxHORIZONTAL);
+   
+   pStaticText = new wxStaticText(this, -1, _("Decay factor:"),
+												wxDefaultPosition, wxDefaultSize, 0);
+   pBoxSizer_Decay->Add(pStaticText, 0, 
+								wxALIGN_CENTER_VERTICAL | wxALIGN_RIGHT | wxALL, 4);
+
+	//v Override wxTextValidator to disallow negative values <= -100.0?
+   m_pTextCtrl_Decay = 
+		new wxTextCtrl(this, ID_TEXT_DECAY, "0.5", 
+							wxDefaultPosition, wxSize(64, -1), 0,
+							wxTextValidator(wxFILTER_NUMERIC));
+   pBoxSizer_Decay->Add(m_pTextCtrl_Decay, 0, 
+								wxALIGN_CENTER_VERTICAL | wxALIGN_LEFT | wxALL, 4);
+
+   pBoxSizer_Dialog->Add(pBoxSizer_Decay, 0, wxALIGN_CENTER | wxALL, 4);
+
+
+	// Preview, OK, & Cancel buttons
+   pBoxSizer_Dialog->Add(0, 8, 0); // spacer
+
+   wxBoxSizer * pBoxSizer_OK = new wxBoxSizer(wxHORIZONTAL);
+
+   wxButton * pButton_Preview = 
+		new wxButton(this, ID_BUTTON_PREVIEW, m_pEffect->GetPreviewName());
+   pBoxSizer_OK->Add(pButton_Preview, 0, wxALIGN_CENTER | wxALL, 4);
+   pBoxSizer_OK->Add(40, 8); // horizontal spacer
+
+   wxButton * pButton_OK =
+       new wxButton(this, wxID_OK, _("OK"), wxDefaultPosition, wxDefaultSize, 0);
+   pButton_OK->SetDefault();
+   pButton_OK->SetFocus();
+   pBoxSizer_OK->Add(pButton_OK, 0, wxALIGN_CENTER | wxALL, 4);
+
+   wxButton * pButton_Cancel =
+       new wxButton(this, wxID_CANCEL, _("Cancel"), wxDefaultPosition, wxDefaultSize, 0);
+   pBoxSizer_OK->Add(pButton_Cancel, 0, wxALIGN_CENTER | wxALL, 4);
+
+   pBoxSizer_Dialog->Add(pBoxSizer_OK, 0, wxALIGN_CENTER | wxALL, 8);
+
+
+   this->SetAutoLayout(true);
+   this->SetSizer(pBoxSizer_Dialog);
+   pBoxSizer_Dialog->Fit(this);
+   pBoxSizer_Dialog->SetSizeHints(this);
+}
+
+bool EchoDialog::Validate()
+{
+   return true; 
+}
+
+bool EchoDialog::TransferDataToWindow()
+{
+   m_bLoopDetect = true;
+
+	wxString str;
+	if (m_pTextCtrl_Delay) {
+		str.Printf("%f", delay);
+		m_pTextCtrl_Delay->SetValue(str);
+	}
+	if (m_pTextCtrl_Decay) {
+		str.Printf("%f", decay);
+		m_pTextCtrl_Decay->SetValue(str);
+	}
+
+	m_bLoopDetect = false;
+	return true;
+}
+
+bool EchoDialog::TransferDataFromWindow()
+{
+   double newValue;
+	wxString str;
+   if (m_pTextCtrl_Delay) {
+      str = m_pTextCtrl_Delay->GetValue();
+      str.ToDouble(&newValue);
+		delay = (float)(newValue);
+	}
+   if (m_pTextCtrl_Decay) {
+      str = m_pTextCtrl_Decay->GetValue();
+      str.ToDouble(&newValue);
+		decay = (float)(newValue);
+	}
+   return true;
+}
+
+// handler implementations for EchoDialog
+
+void EchoDialog::OnPreview(wxCommandEvent &event)
+{
+   TransferDataFromWindow();
+
+	// Save & restore parameters around Preview, because we didn't do OK.
+	float oldDelay = m_pEffect->delay;
+	float oldDecay = m_pEffect->decay;
+
+   m_pEffect->delay = delay;
+   m_pEffect->decay = decay;
+
+   m_pEffect->Preview();
+   
+   m_pEffect->delay = oldDelay;
+   m_pEffect->decay = oldDecay;
+}
+
+void EchoDialog::OnOk(wxCommandEvent & event)
+{
+   TransferDataFromWindow();
+   
+   if (Validate()) 
+      EndModal(true);
+   else 
+      event.Skip();
+}
+
+void EchoDialog::OnCancel(wxCommandEvent & event)
+{
+   EndModal(false);
+}
+
+
--- sf-cvs/trunk/audacity-src/src/effects/Echo.h
+++ sf-cvs/trunk/audacity-src/src/effects/Echo.h
@@ -5,6 +5,7 @@
   Echo.h
 
   Dominic Mazzoni
+  Vaughan Johnson (dialog)
 
 **********************************************************************/
 
@@ -13,7 +14,14 @@
 
 class wxString;
 
+#include <wx/button.h>
+#include <wx/dialog.h>
+#include <wx/stattext.h>
+#include <wx/textctrl.h>
+#include <wx/sizer.h>
+
 #include <wx/intl.h>
+
 #include "Effect.h"
 
 class WaveTrack;
@@ -45,6 +53,51 @@ class EffectEcho:public Effect {
  
    float delay;
    float decay;
+
+friend class EchoDialog;
 };
 
-#endif
+//----------------------------------------------------------------------------
+// EchoDialog
+//----------------------------------------------------------------------------
+
+class EchoDialog:public wxDialog {
+ public:
+   EchoDialog(EffectEcho * effect,
+					wxWindow * parent, wxWindowID id, 
+					const wxString & title, 
+					const wxPoint & pos = wxDefaultPosition, 
+					const wxSize & size = wxDefaultSize, 
+					long style = wxDEFAULT_DIALOG_STYLE);
+
+   virtual bool Validate();
+   virtual bool TransferDataToWindow();
+   virtual bool TransferDataFromWindow();
+
+ private:
+	// handlers
+	//vvv void OnText_Delay(wxCommandEvent & event);
+	//vvv void OnText_Decay(wxCommandEvent & event);
+
+   void OnPreview( wxCommandEvent &event );
+   void OnOk(wxCommandEvent & event);
+   void OnCancel(wxCommandEvent & event);
+
+ private:
+	bool				m_bLoopDetect;
+	EffectEcho *	m_pEffect;
+
+   // controls
+   wxTextCtrl *	m_pTextCtrl_Delay;
+   wxTextCtrl *	m_pTextCtrl_Decay;
+
+ public:
+	// effect parameters
+   float delay;
+   float decay;
+
+ private:
+   DECLARE_EVENT_TABLE()
+};
+
+#endif // __AUDACITY_EFFECT_ECHO__
