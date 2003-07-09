6cf269b3f2229de83238af3a369de6e025c7b343
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-07-09 17:32:05 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/no/about.php
@@ -0,0 +1,45 @@
+<?php BoxTop($aboutStr); ?>
+
+<p>
+Audacity er et fritt og gratis lydredigeringsprogram.
+Det kan spille inn lyder, spille av lyder, importere og eksportere formatene WAV, AIFF og MP3 filer og mer.
+Benytt det til å redigere dine lyder med funksjoner som Klipp, Kopier og Lim Inn (med ubegrensede angremuligheter), mikse spor sammen eller legge effekter på dine opptak.
+Det har også innebygd redigering av amplitydeprofil,
+en justerbar spektrogramfunksjon og et
+vindu for frekvensanalyse.
+Innebygde effekter inkluderer Bassforsterkning, Wahwah og Støyfjerner, 
+og Audacity støtter også VST innstikksmoduler.
+</p>
+
+<p>
+Audacity utgave 1.0.0 er den mest stabile versjonen som er utgitt,
+og mange er av den mening at programmets unike funksjoner og intuitive
+brukergrensesnitt gjør det lettere å bruke enn andre lydredigeringsprogrammer,
+inkludert de som selges kommersielt.
+</p>
+
+<p>
+Audacitys utviklere har også gitt ut utgave 1.1.0, en en beta-kvalitets smakebit på nye
+funksjoner vi jobber aktivt med.
+Denne utgaven inneholder mange, mange nye funksjoner, men er ikke fullt så velpolert
+og stabil.
+</p>
+
+<center>
+<a href="http://wxwindows.org/"><img src="images/wxwindows.gif" border="0"></a>
+</center>
+
+<p>
+Audacity utvikles av et lag med frivillige under en åpen-kildekode modell.
+Det er skrevet i C og C++ og benytter seg av flerplattforms-verktøysettet <a href="http://www.wxwindows.org/">wxWindows</a>.
+Hele kildekoden til programmet er tilgjengelig under lisensen <a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License</a>, som i hovedsak tillater alle og enhver å endre kildekoden såfremt endringene blir publisert.
+Kildekoden er lagret på <a href="http://www.sourceforge.net">SourceForge</a>.
+ </p>
+
+<p>
+Spørsmål/kommentarer?
+Send en e-post til <a href="mailto:audacity-help@lists.sourceforge.net"
+>audacity-help@lists.sourceforge.net</a>.  Denne listen er engelskspråklig.
+</p>
+
+<?php BoxBottom(); ?>
