abc7b8da4209936d0c3a35b690a88b3a1da9e44d
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-07-23 22:32:02 +0000

    automatic

--- sf-cvs/trunk/htdocs/de/tutorials.php
+++ sf-cvs/trunk/htdocs/de/tutorials.php
@@ -4,5 +4,7 @@
 <a href="http://www.daniel.uklinux.net/tutorial/">Eine Audacity Anleitung
 von Daniel James</a> - mit Beispieldateien für eigene Mixes.
 </p>
-
+<p>
+<a href="http://www.audacity.de">Audacity.de</a> - Deutschsprachiges Portal zu Audacity mit Tutorials, Downloads, Webkatalog.
+</p>
 <?php BoxBottom(); ?>
