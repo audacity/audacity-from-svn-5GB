6cd5152032933a03d49b30566fa32f66231bde6c
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-02 19:32:02 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/de/news-1.2.0-pre1.php
@@ -0,0 +1,23 @@
+<?php BoxTop('02. August 2003: Audacity 1.2.0-pre1 freigegeben'); ?>
+
+<p>
+Audacity 1.2.0-pre1 ist eine Testversion des freien Audioeditors, die den Anwender in die Lage versetzt, die neuen Features, welche die in voraussichtlich einem Monat erscheinende Stable Version 1.2.0 bietet, kennen zu lernen und zu testen.
+</p>
+
+<p>
+Audacity 1.2.0-pre1 ermöglicht professionelle Audiobearbeitung und bietet neue Möglichkeiten wie das Absenken oder Anheben der Geschwindigkeit sowie die Veränderung der Tonlage des zu bearbeitenden Tracks.
+</p>
+
+<p>
+Erstmals ist es jetzt möglich, ältere im Audacity 1.0-Format gespeicherte Projektdateien ebenso zu öffnen wie Projektdateien im neuen XML-Format von Audacity 1.1 und höher. Audacity 1.2.0-pre1 behebt eine Vielzahl der Fehler aus der letzten Betaversion.
+</p>
+
+<p>
+Unter <a href="beta.php?<?php print "$langLinkStr"; ?>">Beta Versionen</a> finden Sie neben den vollständigen Angaben zu Audacity 1.2.0-pre1 die entsprechenden Links für den Download der Software.
+</p>
+
+<p>
+Anmerkung: Das Laden von VST-Plugins wurde auf Grund von Lizenzfragen in Audacity 1.2.0-pre1 vorerst deaktiviert. Wir hoffen auf eine baldige Klärung, um die Unterstützung von VST-Plugins rechtzeitig zum Erscheinen der endgültigen Version von Audacity 1.2.0 wieder ermöglichen zu können.
+</p>
+
+<?php BoxBottom(); ?>
