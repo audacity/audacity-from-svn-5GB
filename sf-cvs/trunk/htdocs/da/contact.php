26a0fba7b3ff6fc70126dfdd18cd9585a13e1d66
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-02 21:32:03 +0000

    automatic

--- sf-cvs/trunk/htdocs/da/contact.php
+++ sf-cvs/trunk/htdocs/da/contact.php
@@ -9,24 +9,22 @@ Hvis du har nogen spørgsmål om Audacity, kan du bruge disse resourcer:
 
 <li>
 Læs <?php print "<a href=faq.php?$langLinkStr>$faqStr"; ?></a><br>
-Hvis du vil vide mere om Audacity generelt eller har problemer med at
+Hvis du vil vide noget om Audacity generelt eller har problemer med at
 installere og køre programmet, bør du læse denne side først for at se
 om dine spørgsmål allerede er besvaret.
 <p>
 
 <li>
-Meld dig til: <a href="http://lists.sourceforge.net/lists/listinfo/audacity-users">Audacity-bruger postlisten</a> (primært på engelsk).<br>
-Kig i: <a href="http://www.geocrawler.com/lists/3/SourceForge/14680/0/">Audacity-bruger postarkivet</a> (på engelsk)<br>
-Dette er en ny postliste, hvor brugere af Audacity kan tale sammen og dele
-ideer. Hvis du tror, andre Audacity-brugere kan svare på dit spørgsmål, anbefaler
-vi at du melder dig på postlisten og spørger her. Du kan også søge arkiverne
-for at se, om dit spørgsmål allerede har været besvaret.
+Meld dig til: <a href="http://lists.sourceforge.net/lists/listinfo/audacity-users">Audacity-bruger postlisten</a> (primært på engelsk, danskere til stede).<br>
+Browse: <a href="http://sourceforge.net/mailarchive/forum.php?forum_id=827">Audacity-Users Archives</a><br>
+Dette er en postliste, hvor brugere af Audacity kan tale sammen og dele
+ideer. Hvis du tror, andre Audacity-brugere kan svare på dit spørgsmål, anbefaler vi at du melder dig på postlisten og spørger her. Du kan også søge arkiverne for at se, om dit spørgsmål allerede har været besvaret.
 <p>
 
 <li>
 E-mail: <a href="mailto:audacity-help@lists.sourceforge.net"
 >audacity-help@lists.sourceforge.net</a><br>
-eller gennemse: <a href="http://www.geocrawler.com/lists/3/SourceForge/14679/0/">Audacity-hjælp arkiverne</a><br>
+or kig i <a href="http://sourceforge.net/mailarchive/forum.php?forum_id=828">Audacity-Help Archives</a><br>
 Hvis du har et teknisk spørgsmål om Audacity der er for specifikt til at 
 stille til andre brugere, kan du sende post på denne adresse. Din post vil
 blive læst af Audacitys udviklere. Brug også denne adresse, hvis du har en
