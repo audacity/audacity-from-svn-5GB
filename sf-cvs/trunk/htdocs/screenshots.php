78a933998c58d28a0350a657d4c8d25fa3255faf
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-02 19:03:11 +0000

    New screenshots for 1.2.0-pre1.

--- sf-cvs/trunk/htdocs/screenshots.php
+++ sf-cvs/trunk/htdocs/screenshots.php
@@ -22,10 +22,10 @@ Audacity 1.0.0 / Linux.
 </p>
 
 <p>
-<a href="images/audacity-1.1.png"
-><img src="images/audacity-1.1-small.png" border=0 width=304 height=224></a>
+<a href="images/audacity-1.2.0-pre1.png"
+><img src="images/audacity-1.2.0-pre1-small.png" border=0 width=304 height=224></a>
 <br>
-Audacity 1.1.0 / Linux
+Audacity 1.2.0-pre1 / Linux
 </p>
 
 <p>
