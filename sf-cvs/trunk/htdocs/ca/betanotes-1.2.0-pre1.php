271422055e9ba7c81188ae4b28ff1d4c3f915ded
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-02 10:32:03 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/ca/betanotes-1.2.0-pre1.php
@@ -0,0 +1,59 @@
+<pre>
+Noves característiques de l'Audacity 1.2.0-pre1:
+
+  * Interfície de l'usuari
+    - Ampliació vertical de les peces.
+    - S'ha millorat l'aparença i localització de les barres d'eines.
+    - Més cursors del ratolí.
+    - Complete implementation of editable keyboard shortcuts.
+    - Find zero-crossings.
+    - La roda del ratolí pot ser utilitzada per apropar i allunyar.
+    - Mode multi eina.
+    - Amplify using envelope.
+    - Les etiquetes poden emmagatzemar seleccions (com ara l'Audacity 1.0.0).
+
+  * Efectes
+    - Repeat Last Effect command.
+    - Improved VST plug-in support.
+    - Most effects now have a Preview button.
+    - Compressor (Dynamic Range Compressor).
+    - Canvia el to (sense canviar el temps de compàs).
+    - Canvia el temps de compàs (sense canviar el to).
+    - Canvia la velocitat (canviant tant el temps com la velocitat).
+    - Repeteix (útil per crear bucles).
+    - Normalitza (ajusta el volum i DC bias).
+
+  * Àudio Entrada/Sortida
+    - ordre de previsualització d'un segon.
+    - Reproducció en bucleLooped play.
+
+  * Fitxer Entrada/Sortida
+    - Audacity 1.2.0 opens project files from all previous versions
+      of Audacity from 0.98 through 1.1.3.
+    - Open multiple files from the same dialog.
+    - Use a text file to specify a list of audio files to open with
+      offsets.
+
+  * S'ha actualitzat el manual d'usuari.
+
+
+Errors fixats en l'Audacity 1.2.0-pre1
+
+  * Els fitxers de projectes amb caràcters especials han deixat de ser invàlids.
+
+  * S'han fixat els sorolls de ratllades causats per mals trucaments .
+
+  * L'Audacity ja no exporta fitxers Ogg invàlids i no talla els últims pocs segons dels fitxers Ogg exportats.
+
+  * Els fitxers MP3 Mono exporten a la velociitat correcta.
+
+  * Many incorrect results from the Envelope tool have been fixed.
+
+  * L'ordre de "Exportar etiquetes" ara sorebescriu correctament els fitxers existents.
+  
+  * Ara es mostra correctament el número correcte de l'octava de les notes.
+
+  * S'ha fixat diverses pèrdues de mèmoria.
+
+  * Molts altres petits erros fixats.
+</pre>
