8a79c0b6b961ec5106dc3e4287e73f8cf96f1455
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-03 21:32:27 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/it/beta-1.2.0-pre1.php
@@ -0,0 +1,16 @@
+<h1>Audacity 1.2.0-pre1</h1>
+
+<p>
+Audacity 1.2.0-pre1 è una versione pubblica di Audacity, l'editor audio libero.  Lo scopo di questa versione è di permettere agli utenti di testare le nuove funzionalità che saranno disponibili a partire dalle versione 1.2.0.
+</p>
+
+<p>
+Per favore spedite tutti i report dei bug o i commenti a:
+<a href="mailto:audacity-help@lists.sourceforge.net">
+audacity-help@lists.sourceforge.net</a>
+</p>
+
+<p>
+I vostri feedback ci aiuteranno a rilasciare la versione finale stabile e esente da bug!
+Prevediamo di rilasciare la versione finale nel mese di settembre 2003.
+</p>
