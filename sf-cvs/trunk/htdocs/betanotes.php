e5303a6701590c111f5bdbb0b40569fd94af9480
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-03 13:19:20 +0000

    Fix syntax.

--- sf-cvs/trunk/htdocs/betanotes.php
+++ sf-cvs/trunk/htdocs/betanotes.php
@@ -4,7 +4,7 @@ require_once 'main.inc.php';
 $title = $releaseNotesStr;
 include 'top.inc.php';
 
-php BoxTop("$releaseNotesStr 1.2.0-pre1");
+BoxTop("$releaseNotesStr 1.2.0-pre1");
 include GetTranslation("betanotes-1.2.0-pre1");
 BoxBottom();
 
