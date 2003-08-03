10ea55281b31683f995e3186ea91f0b570618e7e
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-03 12:50:55 +0000

    Update screenshots link.

--- sf-cvs/trunk/htdocs/screenshots.php
+++ sf-cvs/trunk/htdocs/screenshots.php
@@ -15,17 +15,17 @@ BoxTop($screenshotsStr);
 </p>
 
 <p>
-<a href="images/audacity-1.0.png"
-><img src="images/audacity-1.0-small.png" border=0 width=304 height=224></a>
-<br>
-Audacity 1.0.0 / Linux.
-</p>
-
-<p>
 <a href="images/audacity-1.2.0-pre1.png"
 ><img src="images/audacity-1.2.0-pre1-small.png" border=0 width=273 height=226></a>
 <br>
 Audacity 1.2.0-pre1 / Linux
+</p>
+
+<p>
+<a href="images/audacity-1.0.png"
+><img src="images/audacity-1.0-small.png" border=0 width=304 height=224></a>
+<br>
+Audacity 1.0.0 / Linux.
 </p>
 
 <p>
