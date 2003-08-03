2fadfa86870beead6d9b7c82511b7a92694768e7
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-03 13:19:51 +0000

    Fix spacing.

--- sf-cvs/trunk/htdocs/betanotes.php
+++ sf-cvs/trunk/htdocs/betanotes.php
@@ -7,6 +7,7 @@ include 'top.inc.php';
 BoxTop("$releaseNotesStr 1.2.0-pre1");
 include GetTranslation("betanotes-1.2.0-pre1");
 BoxBottom();
+print "<p>\n";
 
 include GetTranslation("betanotes");
 include 'bottom.inc.php';
