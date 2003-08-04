8e87dfcfd51275e489f35af136f35cd020416a36
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-04 03:32:03 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/pt/betanotes-1.2.0-pre1.php
@@ -0,0 +1,58 @@
+<pre>
+Novos recursos no Audacity 1.2.0-pre1:
+
+  * Interface
+    - Controle de zoom vertical das faixas.
+    - Posicionamento e aparência das barras de ferramentas melhorados.
+    - Novos cursores de mouse.
+    - Implemetação total de atalhos de teclado personalizáveis.
+    - Zero-crossings.
+    - Controle de zoom pela roda do mouse.
+    - Modo de Multi-Ferramenta.
+    - Amplificação pela aplicação do envelope.
+    - Faixas de rótulo agora comportam seleções (como na versão 1.0.0).
+
+  * Efeitos
+    - Novo comando "Repetir último efeito".
+    - Suporte a plug-ins VST melhorado.
+    - Preview do efeito a ser aplicado.
+    - Compressor (Dynamic Range Compressor).
+    - Alterar Altura (sem alterar o tempo).
+    - Alterar Tempo (sem alterar a altura).
+    - Alterar Velocidade (alterando a altura e o tempo).
+    - Repetir (útil na criação de loops).
+    - Normalizar.
+
+  * E/S de Áudio
+    - Comando: Preview de 1-segundo.
+    - Reprodução contínua.
+
+  * E/S de Arquivo
+    - Abertura de todos os formatos salvos pelo Audacity desde a versão 0.98.
+    - Abertura de múltiplos arquivos a partir do comando Abrir.
+
+
+  * Manual do usuário atualizado disponível em inglês.
+
+
+
+Lista de falhas corrigidas no Audacity 1.2.0-pre1: 
+
+  * Arquivos de projeto com nomes compostos de caracteres especiais.
+
+  * Redução de ruídos causados por cortes mal-executados no som.
+
+  * Exportação correta de arquivos em formato OGG, sem cortes nos últimos segundos do som.
+
+  * Arquivos de áudio mono exportados como MP3 à velocidade correta.
+
+  * Corrigidas falhas geradas pela ferramenta Envelope.
+
+  * O comando "Exportar Faixas de rótulo" sobregrava arquivos corretamente.
+  
+  * A janela de Espectro exibe as oitavas de notas corretamente.
+
+  * Corrigidas falhas no uso da memória.
+
+  * Inúmeras falhas menores corrigidas.
+</pre>
