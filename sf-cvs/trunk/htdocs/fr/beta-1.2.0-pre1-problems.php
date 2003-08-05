7f87f07b39ffd261584262b78108ba154f4fd7f1
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-05 21:32:08 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/fr/beta-1.2.0-pre1-problems.php
@@ -0,0 +1,28 @@
+<h2>Problèmes connus</h2>
+
+<p>
+Problèmes connus avec Audacity 1.2.0-pre1:
+</p>
+
+<ul>
+<li>Le chargement des plugin VST a été désactivé dans cette version car nous travaillons sur des problèmes potentiels de licences. Nous prévoyons d'inclure à nouveau le support VST dans la version finale d'Audacity 1.2.0.</li>
+
+<li>Le mode d'emploi est toujours en préparation. Les mises-à-jour pour Audacity 1.2.0 sont complètes à 75%. Il peut y avoir des pages manquantes ou incomplètes.</li>
+
+<li>Audacity peut "planter" si de l'audio est éditée pendant que le programme lit ou enregistre.</li>
+
+<li>(MacOS X seulement?) Si vous sélectionner quitter, mais que vous annulez l'opération, alors les commandes quitter et préférences ne fonctionnent plus.</li>
+
+<li>Le curseur de volume dans la console de mixage peut réinitialiser le volume pendant que le programme démarre sur certaines configurations.</li>
+
+<li>Audacity peut importer et afficher des fichiers midi, mais il ne peuvent être lus ou édités.</li>
+
+<li>Audacity "plante" quand on essaie de convertir un fichier à un taux d'échantillonnage de plus de 12 fois plus rapide ou lent que le taux original.</li>
+
+<li>Enregistrer en full duplex (lire des piste existantes pendant l'enregistrement) peut sur certains systèmes Linux faire que l'enregistrement soit comme ralenti.</li>
+
+<li>MacOS X seulement: La command Trouver Zero-Crossings a "Command-Z" listé comme raccourci, mais le raccourci actuel est "Z". Les raccourcies pour "Aide en Ligne" et "Ouvrir" entrent en conflit.</li>
+
+<li>MacOS X seulement: Pour de l'audio avec amplitude zéro, Audacity peut ne rien afficher
+(au lieu d'une fine ligne droide).</li>
+</ul>
