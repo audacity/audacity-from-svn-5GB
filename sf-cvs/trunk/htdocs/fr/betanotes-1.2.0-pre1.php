542cafad5dae12f420a9cad713990851da945b85
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-05 22:32:02 +0000

    automatic

--- sf-cvs/trunk/htdocs/fr/betanotes-1.2.0-pre1.php
+++ sf-cvs/trunk/htdocs/fr/betanotes-1.2.0-pre1.php
@@ -7,7 +7,8 @@ Nouvelles fonctionnalités d'Audacity 1.2.0-pre1:
     - Nouveau curseurs de souris améliorés.
     - Implémentation complète des raccourcis clavier.
     - Recherche des zero-crossings.
-    - La roulette de la souris peut-être utilisée pour le zoom avant ou arrière.
+    - La roulette de la souris peut-être utilisée pour le zoom avant ou
+      arrière.
     - Mode multi-outils.
     - Amplifier en utilisant l'envelope.
     - Labels peuvent stocker les sélections(comme Audacity 1.0.0).
@@ -28,26 +29,31 @@ Nouvelles fonctionnalités d'Audacity 1.2.0-pre1:
     - Lecture en boucle.
 
   * E/S Fichier
-    - Audacity 1.2.0 ouvre les projets de toutes les versions précédentes d'Audacity, de la 0.98 à la 1.1.3.
+    - Audacity 1.2.0 ouvre les projets de toutes les versions précédentes
+      d'Audacity, de la 0.98 à la 1.1.3.
     - Ouvre plusieurs fichier depuis la même boite de dialogue.
-    - Utilise un fichier texte pour spécifier un liste de fichier audio à ouvrir avec offsets.
+    - Utilise un fichier texte pour spécifier un liste de fichier
+      audio à ouvrir avec offsets.
 
   * Mode d'emploi mis à jour.
 
 
 Bogues corrigés dans Audacity 1.2.0-pre1
 
-  * Les fichier projet contenant des caractères spéciaux ne sont plus invalides.
+  * Les fichier projet contenant des caractères spéciaux ne sont plus
+    invalides.
 
   * Les bruits de "Scratch" causés par une saturation sont corrigés.
 
-  * Audacity n'exporte plus des fichiers Ogg invalides, et ne coupe plus les dernières secondes des fichiers Ogg exportés.
+  * Audacity n'exporte plus des fichiers Ogg invalides, et ne coupe plus
+    les dernières secondes des fichiers Ogg exportés.
 
   * Les fichiers MP3 mono sont maintenant exportés à la bonne vitesse.
 
   * Plusieurs résultats incorrects de l'outil d'enveloppe ont été corrigés.
 
-  * La commande "Exporter Labels" remplace correctement les fichiers existants.
+  * La commande "Exporter Labels" remplace correctement les fichiers
+    existants.
   
   * La fenêtre "Plot Spectrum" affiche le nombre correct d'octaves pour les notes.
 
