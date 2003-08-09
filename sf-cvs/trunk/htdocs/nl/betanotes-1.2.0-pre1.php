03bd8a86048568bb654c3e3eba0af43a6dfc0a69
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-09 19:32:03 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/nl/betanotes-1.2.0-pre1.php
@@ -0,0 +1,60 @@
+<pre>
+Nieuwe mogelijkheden in Audacity 1.2.0-pre1:
+
+  * Gebruikers Interface
+    - Verticale zooming van tracks.
+    - Verbeterd uiterlijk en plaatsing van de werkbaken.
+    - Nieuwe standaard muis cursors.
+    - Complete implementatie van bewerkbare keyboard sneltoetsen.
+    - Vinden van zero-crossings.
+    - Mouse wheel kan gebruikt worden voor in- en uitzoomen.
+    - Multi-Tool mode.
+    - Versterk door gebruik van envelope.
+    - Labels kan selecties opslaan (zoals in Audacity 1.0.0).
+
+  * Effecten
+    - Herhaal laatst gegeven Effecten opdracht.
+    - Verbeterde VST plug-in ondersteuning.
+    - Meeste effecten hebben nu een Preview knop.
+    - Compressor (Dynamic Range Compressor).
+    - Verander Pitch (zonder tempo te veranderen).
+    - Verander Tempo (zonder pitch te veranderen).
+    - Verander snelheid (verandering van zowel pitch als tempo).
+    - Herhaal (bruikbaar bij het maken van loops).
+    - Normalisatie (aanpassen van volume en DC bias).
+
+  * Audio I/O
+    - 1-seconde preview opdracht.
+    - Looped play.
+
+  * Bestand I/O
+    - Audacity 1.2.0 opent project bestanden van alle vorige versies
+      van Audacity van 0.98 tot 1.1.3.
+    - Open veelvoudige bestanden van dezelfde dialog.
+    - Gebruik een text bestand omeen lijst met audiobestanden te specificeren
+      te openen met offsets.
+
+  * Geupdate gebruikershandleiding.
+
+
+Fout herstel in Audacity 1.2.0-pre1
+
+  * Projectbestanden met speciale characters zijn niet langer ongeldig.
+
+  * "Kras" geluiden veroorzaakt door slecht knippen zijn verholpen.
+
+  * Audacity exporteert niet langer ongeldige Ogg bestanden, en verwijderd niet
+    langer de laatste paar seconden van een geëxporteerd Ogg bestand.
+
+  * Mono MP3 bestanden worden nu met de correcte snelheid geëxporteerd.
+
+  * Vele onjuuiste resultaten van de Envelope tool zijn hersteld.
+
+  * De "Export Labels" opdracht overschrijft bestaande bestanden nu correct.
+  
+  * De "Plot Spectrum" scherm toont nu correcte octave nummersa voor notities.
+
+  * Verschillende geheugenlekken zijn hersteld.
+
+  * Vele andere kleine fouten zijn hersteld.
+</pre>
