d74b52ff8beadbe35cd3dcd368c283f8ae6df630
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-16 16:32:07 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/no/tutorials.php
@@ -0,0 +1,7 @@
+<?php BoxTop($tutorialsStr); ?>
+
+<p>
+<a href="http://www.daniel.uklinux.net/tutorial/">Et opplæringskurs av Daniel James</a> - med medfølgende lydfiler som du kan blande sammen.
+</p>
+
+<?php BoxBottom(); ?>
