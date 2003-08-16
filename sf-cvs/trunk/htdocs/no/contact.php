f67e769bd45fac79e2fb502975cecd32145435f4
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-16 14:32:04 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/no/contact.php
@@ -0,0 +1,42 @@
+<?php BoxTop($contactUsStr); ?>
+
+<p>
+Det finnes mange ressurser for deg som har spørsmål om Audacity:
+</p>
+
+<p>
+<ul>
+
+<li>
+Les listen over <?php print "<a href=faq.php?$langLinkStr>$faqStr"; ?></a><br>
+hvis du lurer på hva Audacity er, eller du har problemer med installasjonen
+og å få programmet til å kjøre, bør denne siden være det første du sjekker.
+<p>
+
+<li>
+Abbonére: <a href="http://lists.sourceforge.net/lists/listinfo/audacity-users">Audacity-Users Mailing List</a><br>
+Bla gjennom: <a href="http://sourceforge.net/mailarchive/forum.php?forum_id=827">Audacity-Users Archives</a><br>
+Dette er en ny e-post liste som benyttes av brukere av Audacity for å kommunisere og dele ideer.  Hvis du tror ditt spørsmål kan besvares av en annen Audacity-bruker, abbonére på
+denne e-post listen.  Du kan også bla gjennom arkivene for å sjekke om ditt spørsmål
+allerede har blitt stilt.
+<p>
+
+<li>
+E-post: <a href="mailto:audacity-help@lists.sourceforge.net"
+>audacity-help@lists.sourceforge.net</a><br>
+eller les gjennom: <a href="http://sourceforge.net/mailarchive/forum.php?forum_id=828">Audacity-Help Archives</a><br>
+Hvis du har et teknisk problem med Audacity og det er for spesifikt for at andre Audacity
+brukere skal kunne svare på det, send en post til denne adressen.
+Posten leses av en av utviklerene og vedlikeholderene av Audacity.
+Du bør også bruke denne adressen dersom du virkelig trenger å komme i direkte kontakt
+med utviklerne av Audacity.
+<p>
+Audacity blir utviklet av frivillige fra hele verden.  Det er derfor
+ikke noe telefonnummer eller postadresse du kan kontakte oss på.
+All utvikling av Audacity skjer på Internettet, og den eneste måten
+du kan nå utviklerene på er på en av disse e-post listene eller e-post adressene.
+<p>
+
+</ul>
+
+<?php BoxBottom(); ?>
