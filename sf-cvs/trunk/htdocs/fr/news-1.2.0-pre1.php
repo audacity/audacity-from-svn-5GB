5f90e3434b76ab3cd6ef274ed5ba166d7a2621bd
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-17 16:32:03 +0000

    automatic

--- sf-cvs/trunk/htdocs/fr/news-1.2.0-pre1.php
+++ sf-cvs/trunk/htdocs/fr/news-1.2.0-pre1.php
@@ -6,16 +6,16 @@ Le but de cette version est de permettre aux utilisateurs de tester les nouvelle
 </p>
 
 <p>
-Cette version ajoute un traitement audio de qualité professionnelle, et de nouvelles fonctionnalités majeurs comme la possibilité d'accélérer, de ralentir ou d'altérer la hauteur de note d'une piste. C'est la première version qui peut ouvrir des projets enregistrés dans l'ancien format Audacity 1.0 aussi bien que le nouveau format XML utilisé par Audacity 1.1 et supérieur. Audacity 1.2.0-pre1 corrige plusieurs bugs de la dernière version bêta.
+Cette version ajoute un traitement audio de qualité professionnelle, et de nouvelles fonctionnalités majeures comme la possibilité d'accélérer, de ralentir ou d'altérer la hauteur de note d'une piste. C'est la première version qui peut ouvrir des projets enregistrés dans l'ancien format Audacity 1.0 aussi bien que le nouveau format XML utilisé par Audacity 1.1 et supérieur. Audacity 1.2.0-pre1 corrige plusieurs bogues de la dernière version bêta.
 
 </p>
 
 <p>
-La page <a href="beta.php?<?php print "$langLinkStr"; ?>">Beta Versions</a> comporte des notes complètes et les liens de téléchargement.
+La page <a href="beta.php?<?php print "$langLinkStr"; ?>">Versions Beta</a> comporte des notes complètes et les liens de téléchargement.
 </p>
 
 <p>
-Note: Le chargement des plugin VST a été désactivé dans Audacity 1.2.0-pre1 car nous travaillons sur des problèmes de licence potentiels. Nous espèrons résoudre tous les conflits à temps pour inclure le support VST dans la version finale 1.2.0 d'Audacity.
+Note: Le chargement des plugiciels VST a été désactivé dans Audacity 1.2.0-pre1 car nous travaillons sur des problèmes de licence potentiels. Nous espèrons résoudre tous les conflits à temps pour inclure le support VST dans la version finale 1.2.0 d'Audacity.
 </p>
 
 <?php BoxBottom(); ?>
