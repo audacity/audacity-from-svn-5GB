0977a428ef3c522cf9fdba1151247b7cdc34286e
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-08-28 07:58:19 +0000

    Script to discard messages from a Sourceforge mailman mailing list.

---
+++ sf-cvs/trunk/audacity-src/scripts/mailman-discard.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+
+print "This script uses 'curl' to connect to Sourceforge's mailman server and";
+print "discard all of the pending messages in a mailbox.  This is useful";
+print "when an account gets hit with a virus or lots of spam.";
+print "";
+print "Note that ALL pending messages are discarded, with no notice sent";
+print "to anyone!";
+print "";
+
+print "Enter Sourceforge list name (such as 'audacity-devel'): ";
+$list = <STDIN>;
+
+print "Enter list password: ";
+$pass = <STDIN>;
+chop $pass;
+
+print "Logging in and getting info...\n";
+
+$out = `curl --data 'adminpw=$pass' --dump-header cookiefile https://lists.sourceforge.net/lists/admindb/$list`;
+
+$data = "";
+
+@lines = split("\n", $out);
+
+$count = 0;
+foreach $line (@lines) {
+    if ($line =~ 'INPUT name="([0-9]+)" type="RADIO" value="3"') {
+	$count++;
+	if ($data eq "") {
+	    $data = "$1=3";
+	}
+	else {
+	    $data = "$data&$1=3";
+	}
+    }
+}
+
+if ($count == 0) {
+    print "Couldn't find any messages to discard!\n";
+    exit;
+}
+
+print "Discarding $count messages from $list\n";
+
+$out = `curl --cookie cookiefile --data '$data' https://lists.sourceforge.net/lists/admindb/$list`;
+
+@lines = split("\n", $out);
+
+foreach $line (@lines) {
+    if ($line =~ 'no pending requests') {
+	print "Success!\n";
+	exit;
+    }
+}
+
+print "Done, but couldn't verify success.";
+
