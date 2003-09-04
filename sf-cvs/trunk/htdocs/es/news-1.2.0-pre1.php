910bb069b2b5c0ead37d9a9d42d8f1ec1f633999
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-09-04 20:32:03 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/es/news-1.2.0-pre1.php
@@ -0,0 +1,31 @@
+<?php BoxTop(2 de Agosto del 2003: Audacity 1.2.0-pre1 lanzada'); ?>
+
+<p>
+Audacity 1.2.0-pre1 es un lanzamiento de prueba público del editor de
+sonido gratuito Audacity. El propósito de este lanzamiento es permitir
+a los usuarios previsualizar y probar las nuevas características que
+aparecerán en nuestra próxima versión estable 1.2.0, la cual esperamos
+lanzar el próximo mes.
+</p>
+
+<p>
+Este lanzamiento agrega procesamiento de audio de calidad profesional,
+y características nuevas importantes como la habilidad de acelerar,
+desacelerar, y alterar el tono de una pista. Es la primer versión que
+puede abrir archivos de proyecto guardados en el formato anterior Audacity 1.0
+tanto como en el nuevo formato XML usado por Audacity 1.1 y superiores.
+Audacity 1.2.0-pre1 soluciona muchos errores (bugs) de la última versión beta.
+</p>
+
+<p>
+La página <a href="beta.php?<?php print "$langLinkStr"; ?>">Versiones Beta</a>
+tiene notas completas de esta versión y links para download.
+</p>
+
+<p>
+Nota: la carga de plugins VST fue deshabilitada en Audacity 1.2.0-pre1 mientras
+solucionamos potenciales problemas de licencia. Esperamos resolver cualquier
+conflicto a tiempo para incluir soporte VST en Audacity 1.2.0 versión final.
+</p>
+
+<?php BoxBottom(); ?>
