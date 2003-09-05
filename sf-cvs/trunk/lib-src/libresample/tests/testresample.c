3298b59d4086abe40bb9af49aab0d2319b9ebf9f
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-09-05 04:06:28 +0000

    Renamed include file

--- sf-cvs/trunk/lib-src/libresample/tests/testresample.c
+++ sf-cvs/trunk/lib-src/libresample/tests/testresample.c
@@ -11,7 +11,7 @@
 
 **********************************************************************/
 
-#include "../include/resample.h"
+#include "../include/libresample.h"
 
 #include <stdio.h>
 #include <stdlib.h>
