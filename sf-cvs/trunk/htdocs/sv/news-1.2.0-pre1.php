80b3b730830573a73a97cf5eae078b8e4c29ae3d
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-09-09 13:32:15 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/sv/news-1.2.0-pre1.php
@@ -0,0 +1,17 @@
+<?php BoxTop('August  2, 2003: Audacity 1.2.0-pre1 released'); ?>
+
+<p>
+Audacity 1.2.0-pre1 är en "pröva på"-release för Audacity ljudeditor. Meningen med denna release är att låta användare få en förhandstitt och få testa nya funktioner som finns i vår nästa stabila version 1.2.0, som vi hoppas kunna släppa i nästa månad. 
+</p>
+
+<p>
+I denna release ingår funktioner för professionell ljudkvalité, och kraftfulla nya funktioner som möjligheter att öka och minska tempot, och ändra tonhöjden på ett spår. Den är den första releasen där man kan öppna projektfiler sparade i äldre Audacity 1.0-format liksom i de nya XML-formatet som används av Audacity 1.1 och högre. I Audacity 1.2.0-pre1 är många buggar borta som fanns i den senaste betaversionen som släpptes. 
+</p>
+<p>
+Sidan med <a href="beta.php?<?php print "$langLinkStr"; ?>">Betaversioner</a> har komplett dokumentation och länkar för att ladda hem filer. 
+</p>
+<p>
+Obs: Funktionen att kunna importera och använda VST plugin är just nu avstängd medan vi arbetar på eventuella småsaker kring licens. Vi hoppas att vi kan lösa eventuella problem i tid för att kunna inkludera VST support i den slutgiltiga releasen av Audacity 1.2.0. 
+</p>
+
+<?php BoxBottom(); ?>
