c4d1e99eeb6c9a4dd6862b9757ddbee4e2fa6e3b
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-09-09 14:32:12 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/sv/news5.php
@@ -0,0 +1,18 @@
+<?php BoxTop('19 mars, 2003: Version 1.1.3 släppt'); ?>
+
+<p>
+Gå till <?php print "<a href=beta.php?$langLinkStr>"; ?>Betaversioner</a> för att ladda ner. 
+Denna version lägger till en volym- och panoreringkontroll för varje spår, stödjer resampling av hög kvalité, och har många nya funktioner och buggfixar.
+<?php print "<a href=betanotes.php?$langLinkStr>"; ?>Lista på nya funktioner i 1.1.3.</a>
+</p>
+
+<p>
+Lägg märke till att Audacity 1.1.3 inte kan läsa projektfiler skapade i en annan version av Audacity, så du skall kanske inte använda denna version om du har projektfiler som du fortfarande arbetar med. 
+Exportera ditt ljud som WAV eller AIFF först om du vill kunna arbeta i den nya versionen.
+</p>
+
+<p>
+Nästa steg i utvecklingen är att vår nästa release av Audacity skall vara en stabil version för alla användare.
+</p>
+
+<?php BoxBottom(); ?>
