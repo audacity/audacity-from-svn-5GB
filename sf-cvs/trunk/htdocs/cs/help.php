819745c895d7b07eea54db6680483c17131cf7f3
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-09-09 11:32:06 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/cs/help.php
@@ -0,0 +1,46 @@
+<?php BoxTop($helpStr); ?>
+
+<p>
+<a href="docs/contents.html">Pøeètìte si dokumentaci on-line</a>
+</p>
+
+<p>
+<?php print $docsStr; ?>:
+<ul>
+<li><a href="audacity-manual-1.0.0-A.zip">Anglicky</a>
+<li><a href="audacity-manual-1.0.0-bg.zip">Bulharsky</a>
+<li><a href="audacity-manual-1.0.0-es.zip">Španìlsky</a>
+</ul>
+</p>
+
+<p>
+<?php print "<a href=tutorials.php?$langLinkStr>";
+?>Návody (Tutorials)</a>
+</p>
+
+<p>
+<?php print "<a href=faq.php?$langLinkStr>";
+?>Èasto kladené otázky (F.A.Q.)</a>
+</p>
+
+<p>
+<a href="http://sourceforge.net/mailarchive/forum.php?forum_id=828"
+>Archiv e-mailové konference</a>
+</p>
+
+<p>
+Napište na: 
+<a href="mailto:audacity-help@lists.sourceforge.net"
+>audacity-help@lists.sourceforge.net</a>
+</p>
+
+<p>
+<hr>
+</p>
+
+<p>
+<a href="docs1.1">(Nedokonèená) dokumentace k Audacity 1.1</a>
+</p>
+
+<?php BoxBottom(); ?>
+
