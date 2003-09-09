a9fedc5e466e7fbe50e9dee422d97d504017fcbd
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-09-09 08:32:11 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/cs/beta-1.2.0-pre1.php
@@ -0,0 +1,16 @@
+<h1>Audacity 1.2.0-pre1</h1>
+
+<p>
+Audacity 1.2.0-pre1 je veøejná testovací verze svobodného (free) zvukového editoru Audacity. Úèelem této verze je umožnit uživatelùm seznámit se a otestovat nové vlastnosti, které se objeví v naší pøíští stabilní verzi 1.2.0.
+</p>
+
+<p>
+Hlášení jakýchkoliv chyb nebo komentáøe prosím posílejte na:
+<a href="mailto:audacity-help@lists.sourceforge.net">
+audacity-help@lists.sourceforge.net</a>
+</p>
+
+<p>
+Vaše hlášení nám pomohou udìlat finální verzi stabilní a bezchybnou! Doufáme, že novou stabilní verzi uvolníme v záøí 2003.
+</p>
+
---
+++ sf-cvs/trunk/htdocs/cs/news5.php
@@ -0,0 +1,18 @@
+<?php BoxTop('19. bøezna 2003: Verze 1.1.3 uvonìna'); ?>
+
+<p>
+Stahujte ze stránky <?php print "<a href=beta.php?$langLinkStr>"; ?>Beta verzí</a>.
+Tato verze pøidává nastavení "gain" a panorama (pan) ke každé stopì, podporuje vysoce kvalitní pøevzorkování a obsahuje mnoho dalších nových vlastností a oprav chyb.
+<?php print "<a href=betanotes.php?$langLinkStr>"; ?>Seznam nových vlastností ve verzi 1.1.3.</a>
+</p>
+
+<p>
+Upozoròujeme, že Audacity 1.1.3 nedokáže naèíst projektové soubory uložené jakoukoliv jinou verzí, takže tuto verzi nezkoušejte, pokud máte projekty, na kterých stále pracujete. Pøed otevøením v nové verzi vyexportujte svoje nahrávky jako WAV nebo AIFF. 
+</p>
+
+<p>
+Další uvolnìná verze by podle plánu mìla být stabilní a urèená pro všechny uživatele.
+</p>
+
+<?php BoxBottom(); ?>
+
