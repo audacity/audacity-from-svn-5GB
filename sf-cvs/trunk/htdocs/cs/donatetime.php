b769de83c89b68db7124a2a1cda88f3ac8abe957
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-09-09 09:32:23 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/cs/donatetime.php
@@ -0,0 +1,68 @@
+<?php BoxTop($donateTimeStr); ?>
+
+<p>
+Audacity píšou dobrovolníci ve svém volném èase. Jestli se vám líbí a chtìli byste pomoci, tady je nìkolik nápadù:
+</p>
+
+<ul>
+
+<li><b>Pošlete nám pozdrav!</b>
+<p>
+Jestli (si) uíváte Audacity, øeknìte nám to! Pošlete e-mail na 
+<a href="mailto:audacity-help@lists.sourceforge.net"
+>audacity-help@lists.sourceforge.net</a>
+ a dejte nám vìdìt, e se vám líbí. Vìdomí e je lidem ku prospìchu nás dìlá šasnımi. Také se nám nebojte poslat svoje komentáøe a doporuèení - stále Audacity vylepšujeme a ceníme si vašich pøíspìvkù. 
+(Pozn. pøekladatele: Smìle do toho, ale udìlejte to v angliètinì! :))
+<p>
+
+<li><b>Pøipojte se k uivatelské komunitì</b>
+<p>
+Tato e-mailová konference je místem, kde uivatelé Audacity mohou diskutovat s ostatními a sdílet svoje nápady.
+<a href="http://lists.sourceforge.net/lists/listinfo/audacity-users">Pøipojte se ke konferenci uivatelù Audacity</a><br>
+
+
+<li><b>Be a translator (Buïte pøekladatelem)</b>
+<p>
+If you are fluent in both English and another language, you can help
+out other people who speak your language.  We need people to translate
+the program menus and dialogs, the documentation, and the website.
+We also need people to join mailing lists for people to ask questions about
+Audacity in their native language - you can help out either by answering
+questions, or by translating the questions into English and posting them
+to one of the English mailing lists.
+<p>
+<a href="http://audacity.sourceforge.net/translation/"
+>Click here to learn more about translation.</a>
+<p>
+
+<li><b>Hlaste chyby</b>
+<p>
+Jestlie v Audacity najdete chybu, 
+<a href="mailto:audacity-help@lists.sourceforge.net"
+> dejte nám vìdìt</a>.
+Nejuiteènìjší bude, kdy si udìláte èas, pokusíte se chybu zopakovat a pošlete nám podrobnı popis toho, co je tøeba udìlat aby se chyba objevila. Ujistìte se, e nám napíšete jakı operaèní systém pouíváte (tj. Windows 2000, Mac OS 9, atd.).
+<p>
+
+
+<li><b>Buïte beta-testerem</b>
+<p>
+Kromì poslední stabilní verze Audacity máme vdy také beta verzi. Tato verze obvykle obsahuje mnoho novıch funkcí, které ještì nebyly otestovány. Jestli chcete pomoci, stáhnìte si tuto verzi Audacity a pohrajte si s ní. Dejte nám vìdìt co se vám líbí a co ne a øeknìte nám jak si myslíte, e by mohla bıt vylepšena. Hlídejte si regrese - vìci, které døíve fungovaly, ale v budoucnu u nebudou.
+<p>
+
+<li><b>Buïte vıvojáøem</b>
+<p>
+Jestlie u jste zkušenı programátor v C++ a dobøe zacházíte s nástroji jako je CVS, budeme skuteènì velmi rádi za pomoc pøi vıvoji Audacity. Stáhnìte si prosím poslední verzi zdrojového kódu z CVS a zkompilujte. Potom se pøedstavte tady:
+<a href=http://lists.sourceforge.net/lists/listinfo/audacity-devel>
+<?php print $develListStr; ?></a>.
+<p>
+
+<li><b>Vìnujte peníze</b>
+<p>
+Jestlie vám nevyhovuje ani jedna z uvedenıch moností, zvate 
+<?php print "<a href=donatemoney.php?$langLinkStr>"; ?>vìnovat peníze</a>.
+</p>
+
+</ul>
+
+<?php BoxBottom(); ?>
+
