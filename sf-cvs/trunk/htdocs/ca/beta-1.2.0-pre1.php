137ee7d0e7430326ad4a94cc99f9c1107fae9082
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-09-12 14:32:16 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/ca/beta-1.2.0-pre1.php
@@ -0,0 +1,16 @@
+<h1>Audacity 1.2.0-pre1</h1>
+
+<p>
+L'Audacity 1.2.0-pre1 és un llençament públic de prova de l'editor gratuït d'àudio Audacity. El propòsit d'aquest llençament és permetre al usuaris de previsulitzar i provar les noves característiques que apareixeran a la nostra següent versió estable 1.2.0.
+</p>
+
+<p>
+Informeu-nos si us plau de qualsevol error o comentari a:
+<a href="mailto:audacity-help@lists.sourceforge.net">
+audacity-help@lists.sourceforge.net</a>
+</p>
+
+<p>
+La vostra atenció ens ajudarà a fer una versió final estable i lliure d'errors!
+Esperem poder llençar la nova versió estable el septembre del 2003.
+</p>
