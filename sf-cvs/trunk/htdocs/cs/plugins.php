e296f15e1a6b000729a058d87be975257ee336d1
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-09-23 06:32:10 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/cs/plugins.php
@@ -0,0 +1,16 @@
+<p>
+Audacity podporuje plug-inové efekty VST na platformách Macintosh a Windows.
+Najít mùžete plug-iny, které poskytují všechny druhy filtrù, ekvalizaci, echo, chorus a další vìci. Seznam míst, kde mùžete stahovat nebo koupit plug-iny je níže.
+</p>
+
+<p>
+Podpora plug-inù v Audacity se zlepšuje. Dejte nám vìdìt, pokud jste našli VST plug-in, který v Audacity nechce fungovat a my zkusíme zjednat nápravu.
+</p>
+
+<p>
+Abyste mohli použít VST plug-iny, vytvoøte v adresáøi, kde máte Audacity, podadresáø s názvem <b>VST</b> (pokud ho tam už nemáte) a VST plug-iny uložte tam. Pøi dalším spuštìní Audacity budou plug-iny v menu Efekty.
+</p>
+
+<p>
+Tady je nìkolik míst, kde mùžete stahovat nebo koupit VST plug-iny:
+</p>
