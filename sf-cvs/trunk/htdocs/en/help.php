379a33da495269643b8ee9852fcd3cc81523fb2d
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-09-29 06:56:35 +0000

    Provided new links to 1.2 manual and online help

--- sf-cvs/trunk/htdocs/en/help.php
+++ sf-cvs/trunk/htdocs/en/help.php
@@ -1,16 +1,18 @@
-<?php BoxTop($helpStr); ?>
+<?php BoxTop("$helpStr - Audacity 1.2.0"); ?>
 
 <p>
-<a href="docs/contents.html">Read documentation Online</a>
+Audacity 1.2 Complete Manual: 
+[<a href="manual-1.2">Browse Online</a>]
+[<a href="audacity-manual-1.2.zip">Download (450k ZIP)</a>]
 </p>
 
 <p>
-<?php print $docsStr; ?>:
-<ul>
-<li><a href="audacity-manual-1.0.0-A.zip">English</a>
-<li><a href="audacity-manual-1.0.0-bg.zip">Bulgarian</a>
-<li><a href="audacity-manual-1.0.0-es.zip">Spanish</a>
-</ul>
+Audacity 1.2 Online Help:
+[<a href="onlinehelp-1.2">Browse Online</a>]
+</p>
+
+<p>
+<hr>
 </p>
 
 <p>
@@ -33,12 +35,45 @@
 >audacity-help@lists.sourceforge.net</a>
 </p>
 
+<?php BoxBottom(); ?>
+
+<?php BoxTop("$helpStr - Audacity 1.0.0"); ?>
+
+<p>
+<a href="docs/contents.html">Read documentation Online</a>
+</p>
+
+<p>
+<?php print $docsStr; ?>:
+<ul>
+<li><a href="audacity-manual-1.0.0-A.zip">English</a>
+<li><a href="audacity-manual-1.0.0-bg.zip">Bulgarian</a>
+<li><a href="audacity-manual-1.0.0-es.zip">Spanish</a>
+</ul>
+</p>
+
 <p>
 <hr>
 </p>
 
 <p>
-<a href="docs1.1">(Unfinished) documentation for Audacity 1.1</a>
+<?php print "<a href=tutorials.php?$langLinkStr>";
+?>Tutorials</a>
+</p>
+
+<p>
+<?php print "<a href=faq.php?$langLinkStr>";
+?>Frequently Asked Questions (F.A.Q.)</a>
+</p>
+
+<p>
+<a href="http://sourceforge.net/mailarchive/forum.php?forum_id=828"
+>Audacity-Help Mailing List Archives</a>
+</p>
+
+<p>
+<a href="mailto:audacity-help@lists.sourceforge.net"
+>audacity-help@lists.sourceforge.net</a>
 </p>
 
 <?php BoxBottom(); ?>
