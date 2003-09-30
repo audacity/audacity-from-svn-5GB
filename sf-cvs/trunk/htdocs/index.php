a57ccc92e78087fcb0d4f784e06f9f3c6fa6bcf0
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-09-30 08:04:31 +0000

    Upgrading to version 1.2.0-pre2

--- sf-cvs/trunk/htdocs/index.php
+++ sf-cvs/trunk/htdocs/index.php
@@ -4,6 +4,8 @@ require_once 'main.inc.php';
 $title = "Audacity";
 include 'top.inc.php';
 
+include GetTranslation("news-1.2.0-pre2");
+print "<p>\n";
 include GetTranslation("news-1.2.0-pre1");
 print "<p>\n";
 include GetTranslation("news5");
