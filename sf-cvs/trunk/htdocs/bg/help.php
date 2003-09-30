30c89e5a0c1946334f11bd897d088751444af8ef
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-09-30 15:32:06 +0000

    automatic

--- sf-cvs/trunk/htdocs/bg/help.php
+++ sf-cvs/trunk/htdocs/bg/help.php
@@ -1,7 +1,52 @@
-<?php BoxTop($helpStr); ?>
+<?php BoxTop("$helpStr - Audacity 1.2.0"); ?>
+
+<p>
+Пълно ръководство за Audacity 1.2: 
+[<a href="manual-1.2">Разглеждане он-лайн</a>]
+[<a href="audacity-manual-1.2.zip">Сваляне (450 кБ, ZIP)</a>]
+</p>
+
+<p>
+Справочен файл (help) на Audacity 1.2:
+[<a href="onlinehelp-1.2/contents.htm">Разглеждане он-лайн</a>]
+</p>
 
 <p>
 <a href="docs/contents.html">Ръководство за потребителя/документация</a>
+</p>
+
+<p>
+<hr>
+</p>
+
+<p>
+<?php print "<a href=tutorials.php?$langLinkStr>";
+?>Уроци</a>
+</p>
+
+<p>
+<?php print "<a href=faq.php?$langLinkStr>";
+?>Често задавани въпроси (ЧЗВ, FAQ)</a>
+</p>
+
+<p>
+<a target=_top href="http://sourceforge.net/mailarchive/forum.php?forum_id=828"
+>Архиви на пощенския списък Audacity-Help</a>
+</p>
+
+<p>
+<a href="mailto:audacity-help@lists.sourceforge.net"
+>audacity-help@lists.sourceforge.net</a>
+</p>
+
+<?php BoxBottom(); ?>
+
+<p>
+
+<?php BoxTop("$helpStr - Audacity 1.0.0"); ?>
+
+<p>
+<a href="docs/contents.html">Четене на документацията он-лайн</a>
 </p>
 
 <p>
@@ -33,12 +78,5 @@
 >audacity-help@lists.sourceforge.net</a>
 </p>
 
-<p>
-<hr>
-</p>
-
-<p>
-<a href="docs1.1">(Недовършена) документация за Audacity 1.1</a>
-</p>
 
 <?php BoxBottom(); ?>
