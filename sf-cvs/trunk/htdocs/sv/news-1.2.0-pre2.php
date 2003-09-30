ab18f8cc124515e71aaf161ebe408daf6e6a5571
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-09-30 10:32:08 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/sv/news-1.2.0-pre2.php
@@ -0,0 +1,23 @@
+<?php BoxTop('30 september, 2003: Audacity 1.2.0-pre2 släppt'); ?>
+
+<p>
+Audacity 1.2.0-pre2 är en testversion av ljudredigeringsprogrammet Audacity (gratis). I denna version är buggarna i Audacity 1.2.0-pre1 lösta,
+och vi förväntar oss att det blir ytterst få ändringar mellan denna version och den slutliga skarpa versionen 1.2.0, som släpps inom några veckor. Alla som testar 1.2.0-pre1 eller tidigare versioner av Audacity (1.1.x) uppmanas att uppgradera genast.
+</p>
+
+<p>
+Gå till sidan med <a href="beta.php?<?php print "$langLinkStr"; ?>">Betaversioner</a> för att ladda ner programmet och läsa om nyheterna i denna version.
+</p>
+
+<p>
+Här hittar du uppdaterad 
+<a href="manual-1.2">Complete Manual</a> eller 
+<a href="onlinehelp-1.2/contents.htm">Online Help</a> för Audacity 1.2.
+För mer hjälp, gå till <?php print "<a href=help.php?$langLinkStr>"; ?>
+Hjälp</a>.
+</p>
+
+<p>
+I denna version behöver du, för att kunna använda VST plug-ins, ladda ner "VST Enabler" separat. Den kommer att bli tillgänglig snart.</p>
+
+<?php BoxBottom(); ?>
