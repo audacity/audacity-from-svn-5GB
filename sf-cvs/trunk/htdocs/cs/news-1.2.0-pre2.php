1439ea6fcc937cd840b0b11123ac6439e25ba98f
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-10-01 08:32:15 +0000

    automatic

--- sf-cvs/trunk/htdocs/cs/news-1.2.0-pre2.php
+++ sf-cvs/trunk/htdocs/cs/news-1.2.0-pre2.php
@@ -1,7 +1,7 @@
 <?php BoxTop('30. záøí 2003: Audacity 1.2.0-pre2 uvolnìna'); ?>
 
 <p>
-Audacity 1.2.0-pre2 je veøejná testovací verze svobodného zvukového editoru. Tato verze opravuje všechny známé významné chyby, které byly ve verzi 1.2.0-pre1 a oèekáváme, že mezi touto a finální verzí 1.2.0 uvolnìnou v pøíštích nìkolika týdnech už bude jen velmi málo zmìn. Všem, kteøí testují 1.2.0-pre1, nebo nìkterou z pøedchozích beta verzí Audacity (1.1.x), doporuèujeme co nejdøíve upgradovat.
+Audacity 1.2.0-pre2 je veøejná testovací verze svobodného zvukového editoru. Tato verze opravuje všechny známé významné chyby, které byly ve verzi 1.2.0-pre1 a oèekáváme, že mezi touto a finální verzí 1.2.0, uvolnìnou v pøíštích nìkolika týdnech, už bude jen velmi málo zmìn. Všem, kteøí testují 1.2.0-pre1, nebo nìkterou z pøedchozích beta verzí Audacity (1.1.x), doporuèujeme co nejdøíve upgradovat.
 </p>
 
 <p>
@@ -18,7 +18,7 @@ Nápovìda</a>.
 </p>
 
 <p>
-Tato verze bude podporovat VST plug-iny pokud stáhnete samostatný VST Enabler. Ten bude k dispozici v nejbližší dobì.
+Tato verze bude podporovat VST plug-iny, pokud stáhnete samostatný VST Enabler. Ten bude k dispozici v nejbližší dobì.
 </p>
 
 <?php BoxBottom(); ?>
