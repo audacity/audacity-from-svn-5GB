8adeaf4ba46ae799fdb4d3f438156e431b78298f
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-10-02 23:32:26 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/cs/beta-1.2.0-pre2-problems.php
@@ -0,0 +1,26 @@
+<h2>Známé problémy</h2>
+
+<p>
+Toto jsou známé problémy v Audacity 1.2.0-pre2:
+</p>
+
+<ul>
+  <li>Audacity  umí importovat a zobrazit MIDI soubory, ale nelze je pøehrávat nebo editovat.
+    <p>
+  <li>Obousmìrné nahrávání (full duplex - pøehrávání existující stopy bìhem nahrávání) mùže na nìkterých Linuxových systémech zpùsobit, že nahraný zvuk je zpomalený.
+    <p>
+  <li>
+Pouze MacOS X: Audacity neumí pracovat se soubory nebo adresáøi, které jsou obsaženy v adresáøích s mezinárodními znaky (èárky, háèky, atd.) v názvu. Pøitom soubory i projekty Audacity s tìmito znaky fungují. Jde jen o ty soubory, které jsou uvnitø adresáøù s tìmi znaky, což zpùsobuje problémy.
+    <p>
+
+  <li>Pouze Windows: Audacity není kompatibilní s nìkterými profesionálními
+    zvukovými kartami a mùže zhavarovat, pokud je nìkterá z tìchto karet výchozí
+    pøi spuštìní Audacity. Jako nouzové øešení nastavte jako výchozí pro
+    Audacity jinou kartu, ale dejte nám prosím vìdìt, jestli to zabralo, abychom
+    mohli tento problém najít a opravit.
+    <p>
+    Vezmìte také na vìdomí, že windowsový instalátor nahradí instalace verze
+    1.0.0 a nikoliv instalace beta verzí 1.1.x.
+    <p>
+</ul>
+
