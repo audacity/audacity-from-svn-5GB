c7674a63cc02df80acce66f8bc5abcef173a90ae
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-10-27 09:30:39 +0000

    Added new screenshots

---
+++ sf-cvs/trunk/htdocs/screenshots-win98.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once 'main.inc.php';
+$title = $screenshotsStr;
+include 'top.inc.php';
+
+BoxTop("$screenshotsStr - Windows 98");
+
+?>
+
+<center>
+<img src="images/screen/win98/main.png" border=0>
+<p>
+<img src="images/screen/win98/samples.png" border=0>
+<p>
+<img src="images/screen/win98/prefs.png" border=0>
+<p>
+<img src="images/screen/win98/noise.png" border=0>
+<p>
+<img src="images/screen/win98/pitch.png" border=0>
+<p>
+<img src="images/screen/win98/freq.png" border=0>
+<p>
+<img src="images/screen/win98/raw.png" border=0>
+<p>
+<img src="images/screen/win98/envelope.png" border=0>
+</center>
+
+<?php
+
+BoxBottom();
+
+include 'bottom.inc.php';
+
+?>
+
+
+
+
+
+
