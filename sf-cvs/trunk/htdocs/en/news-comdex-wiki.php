bd848dbe119655c13cc08c255002b9b9a51bf857
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-10-28 20:56:27 +0000

    Added news item

---
+++ sf-cvs/trunk/htdocs/en/news-comdex-wiki.php
@@ -0,0 +1,47 @@
+<?php BoxTop('October 28, 2003: COMDEX, Screenshots, Wiki, T-Shirts'); ?>
+
+We fixed a bunch of bugs in the last public test release, and we will be
+releasing Audacity 1.2.0-pre3 in the next few days, along with the
+VST Enabler, finally.  In the meantime, here's some other news:
+
+<p>
+Audacity was nominated as a possible open-source
+program that <a href="http://www.oreilly.com/">O'Reilly</a> could
+send to <a href="http://www.comdex.com/">COMDEX</a> in Las Vegas,
+November 17-20.  <a href="http://www.oreillynet.com/contest/comdex/"
+>Click here</a> for more information and to vote for
+Audacity (free registration at O'Reilly required to vote).
+Voting ends on October 31st.
+</p>
+
+<p>
+The <a href="http://audacityteam.org/wiki/">Audacity Wiki</a> is a
+publicly editable website for anything relating to Audacity.  It
+currently includes tips, documentation, and links, plus a place for
+feature requests.  The best part is that it's easy for anyone to
+contribute - just click on a link at the bottom of any page to edit
+or add your own content.  We hope you'll find this to be a fun and
+valuable resource.
+</p>
+
+<p>
+We now have <a href="http://audacityteam.org/tshirt.php">Audacity T-Shirts</a> available as a thank-you for people who contribute.
+</p>
+
+<p>
+Both the <a href="http://audacityteam.org/wiki/">Wiki</a> and the
+<a href="http://audacityteam.org/tshirt.php">T-Shirt Offer</a>
+are hosted on a new website,
+<a href="http://audacityteam.org/">AudacityTeam.org</a>,
+which we've set up for extra Audacity content and topics related
+to the Audacity development team, as opposed to Audacity in general.
+</p>
+
+<p>
+Check back soon for Audacity 1.2.0-pre3.  If you haven't tried
+a beta or prerelease version of Audacity lately, check out our
+updated <a href="http://audacity.sourceforge.net/screenshots.php"
+>Screenshots</a>. 
+</p>
+
+<?php BoxBottom(); ?>
