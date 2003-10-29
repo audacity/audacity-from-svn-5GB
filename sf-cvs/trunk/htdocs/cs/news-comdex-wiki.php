c9f3b410937abf362252775e1b12240940fee6fb
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-10-29 10:32:12 +0000

    automatic

--- sf-cvs/trunk/htdocs/cs/news-comdex-wiki.php
+++ sf-cvs/trunk/htdocs/cs/news-comdex-wiki.php
@@ -3,27 +3,15 @@
 Opravili jsme spoustu chyb v poslední veøejné testovací verzi. Audacity 1.2.0-pre3 uvolníme bìhem nìkolika dnù a to koneènì s VST Enablerem. Mezitím jsou tady nìjaké další novinky:
 
 <p>
-Audacity was nominated as a possible open-source
-program that <a href="http://www.oreilly.com/">O'Reilly</a> could
-send to <a href="http://www.comdex.com/">COMDEX</a> in Las Vegas,
-November 17-20.  <a href="http://www.oreillynet.com/contest/comdex/"
->Click here</a> for more information and to vote for
-Audacity (free registration at O'Reilly required to vote).
-Voting ends on October 31st.
+Audacity byla nominována jako program s otevøeným kódem, který by nakladatelství <a href="http://www.oreilly.com/">O'Reilly</a> mohlo vyslat na <a href="http://www.comdex.com/">COMDEX</a> v Las Vegas 17. - 20. listopadu. Chcete-li další informace a hlasovat pro Audacity, <a href="http://www.oreillynet.com/contest/comdex/">kliknìte sem</a>. (Pro hlasování je tøeba bezplatná registrace.) Hlasování konèí 31. øíjna.
 </p>
 
 <p>
-The <a href="http://audacityteam.org/wiki/">Audacity Wiki</a> is a
-publicly editable website for anything relating to Audacity.  It
-currently includes tips, documentation, and links, plus a place for
-feature requests.  The best part is that it's easy for anyone to
-contribute - just click on a link at the bottom of any page to edit
-or add your own content.  We hope you'll find this to be a fun and
-valuable resource.
+<a href="http://audacityteam.org/wiki/">Audacity Wiki</a> je veøejnì editovatelná webová stránka urèená pro cokoliv, co souvisí s Audacity. V souèasné dobì obsahuje tipy, dokumentaci a odkazy a navíc místo pro požadavky na nové vlastnosti. Nejlepší na tom je, že kdokoliv mùže jednoduše pøispìt - prostì jen kliknìte na odkaz na konci jakékoliv stránky a mùžete editovat nebo pøidat svùj vlastní obsah. Doufáme, že pro vás bude cenným a zábavným zdrojem.
 </p>
 
 <p>
-We now have <a href="http://audacityteam.org/tshirt.php">Audacity T-Shirts</a> available as a thank-you for people who contribute.
+Jako podìkování tìm, kteøí pøispìli máme nyní <a href="http://audacityteam.org/tshirt.php">trièka Audacity</a>.
 </p>
 
 <p>
