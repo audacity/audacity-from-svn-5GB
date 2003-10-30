c9baef49c1554bb0b57d3bf1d8eb1ec5f6fc4092
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-10-30 21:32:10 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/ca/news-comdex-wiki.php
@@ -0,0 +1,35 @@
+<?php BoxTop('28 d'octubre del 2003: COMDEX, captures de pantalla, Wiki, Samarretes'); ?>
+
+Hem fixat un munt d'errors des de l'últim llençament públic de prova i llançarem l'Audacity 1.2.0-pre3 en els propers pocs die, along with the
+VST Enabler, finally.  Mentrestant ací teniu algunes notícies:
+
+<p>
+L'Audacity ha estat nominat com un dels possibles programaris que <a href="http://www.oreilly.com/">O'Reilly</a> podria enviar a <a href="http://www.comdex.com/">COMDEX</a> a Les Vegues,
+Novembre 17-20.  <a href="http://www.oreillynet.com/contest/comdex/"
+>Cliqueu ací</a> per a més informació i per votar per l'Audacity (per votar us cla registrar-vos-hi gratuïtament).
+La votació acaba el 31 d'octubre.
+</p>
+
+<p>
+El <a href="http://audacityteam.org/wiki/">Wiki de l'Audacity</a> és un lloc web que ttohom pot editar llirement per qualsevol cosa relacionada amb l'Audacity.  Actualment inclou consells, documentació, i enllaços més un lloc per a la sol·licitud de característiques.  La millor part és que és fàcil per tothom contribuir-hi - just click on a link at the bottom of any page to edit
+or add your own content.  We hope you'll find this to be a fun and
+valuable resource.
+</p>
+
+<p>
+Ara tenim <a href="http://audacityteam.org/tshirt.php">Samarretes de l'Audacity</a> disponibles per agrair a la gent que ha contribuit.
+</p>
+
+<p>
+Tant el <a href="http://audacityteam.org/wiki/">Wiki</a> com
+<a href="http://audacityteam.org/tshirt.php">l'oferta de samarretes</a>
+està hostatjada en el lloc web de l'<a href="http://audacityteam.org/">AudacityTeam.org</a>,
+que s'ha creat per continguts relacionats amb l'Audacity i l'equip de desenvolupament com oposat a l'Audacity en general.
+</p>
+
+<p>
+Tornarem aviat amb l'1.2.0-pre3.  Si últimament no heu triat cap prellançament beta de l'Audacity reviseu les nostres últimes <a href="http://audacity.sourceforge.net/screenshots.php"
+>Captures de pantalla</a>. 
+</p>
+
+<?php BoxBottom(); ?>
