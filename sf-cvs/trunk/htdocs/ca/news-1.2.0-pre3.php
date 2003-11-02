2f78cbebf95828a3be62f2167cc948fd9f7cfba2
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-11-02 20:32:37 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/ca/news-1.2.0-pre3.php
@@ -0,0 +1,23 @@
+<?php BoxTop('2 de novembre del 2003: Llançament de Audacity 1.2.0-pre3'); ?>
+
+<p>
+L'Audacity 1.2.0-pre3 és un llançament públic de prova de l'editor de so l'Audacity.  Aquesta versió fixa tots els majors errors desconeguts de la versió 1.2.0-pre2 i afegeix suport per a <a href="http://audacityteam.org/vst/"
+>VST Enabler</a>.
+Tothom que estigui provant la versió 1.2.0-pre2 o anterior de l'Audacity (1.1.x) se li recomana de s encouraged to upgrade immediately.
+</p>
+
+<p>
+La pàgina de <a href="beta.php><?php print "$langLinkStr"; ?>">versions beta</a> conté notes de llançament i enllaços de descàrrega.
+</p>
+
+<p>
+Podeu navegar per al 
+<a href="manual-1.2">complet manualManual</a> actualitzat o
+<a href="onlinehelp-1.2/contents.htm">referències ràpides</a> per a l'Audacity 1.2.
+PEr a més opcions, vegeu la
+<?php print "<a href=help.php?$langLinkStr>"; ?>
+Help</a> pàgina del <a href="http://www.audacityteam.org/wiki"
+>Wiki de l'Audacity</a>.
+</p>
+
+<?php BoxBottom(); ?>
