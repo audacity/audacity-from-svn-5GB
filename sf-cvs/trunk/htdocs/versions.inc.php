1d5bfb2bf228d8ec7be6f24e0011a4ff544e09b2
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-11-02 10:46:35 +0000

    Updating to 1.2.0-pre3

--- sf-cvs/trunk/htdocs/versions.inc.php
+++ sf-cvs/trunk/htdocs/versions.inc.php
@@ -6,12 +6,12 @@
   // beta/versions.inc.php...
 
   $stableVersion =   "1.0.0";
-  $betaVersion =     "1.2.0-pre2";
+  $betaVersion =     "1.2.0-pre3";
 
   $winStableVersion = "1.0.0";
   $winStableSize =    "1.6 MB";
   $winStableZipSize = "1.3 MB";
-  $winBetaVersion =   "1.2.0-pre2";
+  $winBetaVersion =   "1.2.0-pre3";
   $winBetaSize =      "2.7 MB";
   $winBetaZipSize =   "2.5 MB";
 
@@ -22,13 +22,16 @@
 
   $macOSXStableVersion = "1.0.0";
   $macOSXStableSize =    "1.9 MB";
-  $macOSXBetaVersion =   "1.2.0-pre2";
+  $macOSXBetaVersion =   "1.2.0-pre3";
   $macOSXBetaSize =      "2.6 MB";
 
   $srcStableVersion = "1.0.0-2";
   $srcStableSize =    "1.6 MB";
-  $srcBetaVersion =   "1.2.0-pre2";
+  $srcBetaVersion =   "1.2.0-pre3";
   $srcBetaSize =      "2.7 MB";
+
+  $linuxBetaVersion =   "1.2.0-pre3";
+  $linuxBetaSize =      "2.5 MB";
 
   $rpmStableVersion = "1.0.0-2";
   $rpmStableSize =    "750K";
