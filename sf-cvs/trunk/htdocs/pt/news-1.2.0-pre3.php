dcbd165b1162dae6f14fd0a942efb3da38a9f295
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-11-02 22:32:14 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/pt/news-1.2.0-pre3.php
@@ -0,0 +1,21 @@
+<?php BoxTop('Liberada para testes a versão 1.2.0-pre3 do Audacity!   <font size=1>02/11/03</font>'); ?>
+
+<p>
+O Audacity 1.2.0-pre3 é o terceiro teste público da versão 1.2.0. Nesta versão, algumas falhas encontradas nas versões anteriores foram corrigidas e o suporte a plug-ins <a href="http://audacityteam.org/vst/">VST</a> habilitado.
+
+Aconselhamos a todos os usuários das versões beta a copiar e testar esta versão imediatamente. 
+</p>
+<p>
+A página <a href="beta.php?<?php print "$langLinkStr"; ?>">Versões Beta</a>  apresenta a lista completa de novos recursos e o link para copiar o programa.
+</p>
+
+<p>
+Já é possível acessar <a href="manual-1.2">Manual Completo do Audacity 1.2</a> (em inglês) ou o <a href="onlinehelp-1.2/contents.htm">Guia de Referência Rápida</a>, em processo de tradução.
+Outras fontes disponíveis de ajuda:<p></p>
+<?php print "<a href=help.php?$langLinkStr>"; ?>
+Página da Ajuda Online</a>;
+<p></p>
+<a href="http://www.audacityteam.org/wiki">Audacity Wiki</a>.
+</p>
+
+<?php BoxBottom(); ?>
