3d4aa56f77edc683e9b337dc07d118ad8b77af48
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-11-03 07:32:12 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/cs/news-1.2.0-pre3.php
@@ -0,0 +1,22 @@
+<?php BoxTop('2. listopadu 2003: Audacity 1.2.0-pre3 uvolnìna'); ?>
+
+<p>
+Audacity 1.2.0-pre3 je veøejná testovací verze svobodného zvukového editoru. Tato verze opravuje všechny známé významné chyby v 1.2.0-pre2 a pøidává podporu <a href="http://audacityteam.org/vst/"
+>VST Enableru</a>. Všichni, kdo testují 1.2.0-pre2 nebo jakoukoliv pøedchozí verzi (1.1.x) by mìli co nejdøíve upgradovat.
+</p>
+
+<p>
+Na <a href="beta.php?<?php print "$langLinkStr"; ?>">stránce Beta verzí</a> 
+najdete kompletní poznámky k vydání a odkazy na stažení.
+</p>
+
+<p>
+
+Prohlédnìte si aktualizovaný <a href="manual-1.2">Kompletní manuál</a> nebo <a href="onlinehelp-1.2/contents.htm">referenèní pøíruèku</a>.
+Další nápovìdu najdete na <?php print "<a href=help.php?$langLinkStr>"; ?>
+stránce s nápovìdou</a> nebo na <a href="http://www.audacityteam.org/wiki"
+>Audacity Wiki</a>.
+</p>
+
+<?php BoxBottom(); ?>
+
