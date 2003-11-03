fef853b8500d9f96d9ed30f9bb7e3fbf5ab1411e
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-11-03 18:32:19 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/it/beta-1.2.0-pre3-problems.php
@@ -0,0 +1,16 @@
+
+<p>
+I seguenti sono i bug conosciuti di Audacity 1.2.0-pre3:
+</p>
+
+<ul>
+  <li>Audacity può importare e mostrare i file MIDI, ma non può modificarli o riprodurli.
+    <p>
+  <li>La registrazione in full duplex (riprodurre le tracce esistenti durante una registrazione) su alcuni sistemi Linux può causare problemi nella registrazione dei suoni.
+    <p>
+  <li>Solo MacOS X: Audacity non può lavorare con file o cartelle che sono contenute all'interno di cartelle con caratteri internazionali ( accenti, ...) nel loro nome. Fate attenzione: i file o i progetti di Audacity con caratteri accentati funzionano senza problemi.  Danno problemi solo i file contenuti all'interno di cartelle che hanno  caratteri accentati nel nome.
+    <p>
+
+   <li>Solo Windows: fate attenzione! l'installer di Windows sostituirà le installazioni di Audacity 1.0.0, ma non le versioni beta (1.1.x). 
+    <p>
+</ul>
