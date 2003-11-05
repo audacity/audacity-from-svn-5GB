b527608b9bbe8741690292874fe3783db1b7e98a
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-11-05 20:32:19 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/sv/tutorials.php
@@ -0,0 +1,8 @@
+<?php BoxTop($tutorialsStr); ?>
+
+<p>
+<a href="http://www.daniel.uklinux.net/tutorial/">En lathund för Audacity 
+av Daniel James</a> - tillsammans med ljudfiler som du kan pröva att mixa ihop.
+</p>
+
+<?php BoxBottom(); ?>
