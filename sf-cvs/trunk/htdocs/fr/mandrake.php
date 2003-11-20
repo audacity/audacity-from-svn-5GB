6a3b7706368bf538227d9b4a5fc5249d3c2dc39c
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-11-20 03:32:12 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/fr/mandrake.php
@@ -0,0 +1,30 @@
+<?php BoxTop("Mandrake"); ?>
+
+<p>
+Mandrake users can install Audacity just by typing (as root):
+</p>
+
+<ul>
+<tt>urpmi audacity</tt>
+</ul>
+
+<p>
+Pour télécharger et installer les packages Audacity pour Mandrake:
+</p>
+<p>
+Le nom du package est"audacity" pour la version 1.0 et "hackaudacity" pour la version 1.1, et elles peuvent être trouvées sur ce site FTP:
+<ul>
+<li><a href="ftp://sunsite.uio.no/pub/unix/Linux/Mandrake/Mandrake-devel/contrib/">ftp://sunsite.uio.no/pub/unix/Linux/Mandrake/Mandrake-devel/contrib/</a>
+</ul>
+Ou vous pouvez trouvez un autre site de download Mandrake Cooker:
+<ul>
+<li><a href="http://www.mandrakelinux.com/en/cookerdevel.php3">The Mandrake Linux Cooker</a>
+</ul>
+
+</p>
+
+<p>
+
+</p>
+
+<?php BoxBottom(); ?>
