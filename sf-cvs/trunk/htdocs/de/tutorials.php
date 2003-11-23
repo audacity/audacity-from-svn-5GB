e72edf05e0046e8dd557537cbc0202528cf3a8d7
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-11-23 10:32:28 +0000

    automatic

--- sf-cvs/trunk/htdocs/de/tutorials.php
+++ sf-cvs/trunk/htdocs/de/tutorials.php
@@ -5,6 +5,6 @@
 von Daniel James</a> - mit Beispieldateien für eigene Mixes.
 </p>
 <p>
-<a href="http://www.audacity.de">Audacity.de</a> - Deutschsprachiges Portal zu Audacity mit Tutorials, Downloads, Webkatalog.
+<a href="http://alphasound.homelinux.com">AlphaSound</a> - Deutschsprachiges Portal zu Audacity mit Tutorials, Downloads, Webkatalog. Hinweis: Ursprünglich zeigte der Link auf audacity.de, doch wegen ständiger Serverprobleme ziehe ich diese Supportseiten erstmal auf meine eigene Maschine um...
 </p>
 <?php BoxBottom(); ?>
