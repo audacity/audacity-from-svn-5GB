7c6704ca23c4bb8013ffcfc9186248dc1a583032
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-12-01 20:32:32 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/nl/beta-1.2.0-pre3-problems.php
@@ -0,0 +1,26 @@
+h2>Bekende Problemen</h2>
+
+<p>
+Het volgende zijn bekende problemen in Audacity 1.2.0-pre3:
+</p>
+
+<ul>
+  <li>Audacity kan MIDI bstanden importeren en tonen, maar ze kunnen niet
+    afgespeeld of bewerkt worden.
+    <p>
+  <li>Opname in full duplex (afspelen van bestaande tracks tijdens opnemen)
+    kan op sommige Linux systemen de opnames verlangzaamd laten klinken.
+    <p>
+  <li>Alleen voor MacOS X: Audacity kan niet met bestanden of mappen werken
+    die geplaatst zijn in mappen met internationale characters
+    (geaccentueerde markeringen e.d.) in de naam.  Bestanden met geaccentueerde
+    characters werken wel en Audacity projecten met geaccentueerde
+    characters werken ook. Dit geldt alleen voor mappen met geaccentueerde
+    characters
+    <p>
+
+   <li>Alleen voor Windows:
+    De Windows installer neemt de plaats in van 1.0.0 installaties, 
+    maar niet die van de 1.1.x beta installatie.
+    <p>
+</ul>
