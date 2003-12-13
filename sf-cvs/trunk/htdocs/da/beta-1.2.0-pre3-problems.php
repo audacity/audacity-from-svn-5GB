8cff7c7df3495a642d254e299a1660fe2eca8ab4
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-12-13 19:32:13 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/da/beta-1.2.0-pre3-problems.php
@@ -0,0 +1,18 @@
+<h2>Known Problems</h2>
+
+<p>
+Kendte problemer i Audacity 1.2.0-pre3:
+</p>
+
+<ul>
+  <li>Audacity kan importere og vise MIDI filer, men de kan ikke spilles eller redigeres.
+    <p>
+  <li>Nogle Linux-systemer: Optagelse i fuld duplex (samtidig optagelse og afspilning) kan få den indspillede lyd til at løbe for langsomt.
+    <p>
+  <li>Kun MacOS X: Audacity kan ikke arbejde med filer eller mapper, der ligger indeni mapper med internationale tegn (æ, ø, å m.fl.) i navnene. Bemærk, at filnavne og projektnavne med internationale tegn i sig selv virker fint, det er kun filer placeret i mapper med disse tegn i navnet, der giver problemer.
+    <p>
+
+   <li>Kun Windows:
+    Bemærk, at Windows installationsprogrammet vil erstatte Audacity version 1.0.0, men ikke 1.1.x betaudgaverne.
+    <p>
+</ul>
