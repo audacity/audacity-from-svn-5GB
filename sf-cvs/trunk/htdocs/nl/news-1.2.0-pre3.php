016020b3755263bd4ce0d8c35eeaa6633bc55f55
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2003-12-27 09:32:12 +0000

    automatic

--- sf-cvs/trunk/htdocs/nl/news-1.2.0-pre3.php
+++ sf-cvs/trunk/htdocs/nl/news-1.2.0-pre3.php
@@ -1,22 +1,22 @@
-<?php BoxTop(2 november 2003: Audacity 1.2.0-pre3 vrijgegeven'); ?>
+<?php BoxTop('2 november 2003: Audacity 1.2.0-pre3 vrijgegeven'); ?>
 
 <p>
-Audacity 1.2.0-pre3 is een publieke testvrijgave van de gratis geluidseditor Audacity. In deze versie zijn alle fouten uit versie 1.2.0-pr2 hersteld en is ondersteuning toegevoegd voor de <a href="http://audacityteam.org/vst/"
+Audacity 1.2.0-pre3 is een publike testvrijgave van het gratis Audacity (geluidsbewerker). Deze versie herstelt alle bekende grote fouten in 1.2.0-pre2
+plus toegevoegde ondersteuning van de <a href="http://audacityteam.org/vst/"
 >VST Enabler</a>.
-Iedereen die versie 1.2.0-pre2 test of een vorige beta versie van
-Audacity (1.1.x)wordt hierbij aangemoedigd om direct een upgrade uit te voeren.
+Iedereen die 1.2.0-pre2 of een andere beta versie van Audacity (1.1.x) test, wordt hierbij aangemoedigd direct een upgrade van het programma uit te voeren.
 </p>
 
 <p>
 De <a href="beta.php?<?php print "$langLinkStr"; ?>">Beta Versies</a> pagina
-bevat de complete vrijgave notities en download links.
+bevat de complete handleiding en download links over deze vrijgave.
 </p>
 
 <p>
-Je kunt de geupdate
-<a href="manual-1.2">Complete Handleiding</a> doornemen of
+Je kunt browsen door de geupdate
+<a href="manual-1.2">Complete Manual</a> of
 <a href="onlinehelp-1.2/contents.htm">Quick Reference</a> voor Audacity 1.2.
-Voor meer helpopties zie de
+Voor meer hulp opties, bekijk de
 <?php print "<a href=help.php?$langLinkStr>"; ?>
 Help</a> pagina of de <a href="http://www.audacityteam.org/wiki"
 >Audacity Wiki</a>.
