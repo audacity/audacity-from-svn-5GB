bd7b7c071a607dd9d30d7c564b05f508dfc31890
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-01-03 22:38:40 +0000

    Describe 1.2.0

--- sf-cvs/trunk/htdocs/en/plugins.php
+++ sf-cvs/trunk/htdocs/en/plugins.php
@@ -1,21 +1,45 @@
 <p>
-Audacity supports VST Plug-in effects on the Macintosh and Windows platforms.
+<table border=0 cellpadding=4 cellspacing=1>
+<tr>
+<td width=50% bgcolor=#000099><font color=#ffffff size=+2>Audacity 1.0</font></td>
+<td width=50% bgcolor=#000099><font color=#ffffff size=+2>Audacity 1.2.0-pre3</font></td>
+</tr>
+<td valign=top bgcolor=#eeeeee>
+<p>
+Audacity 1.0 supports VST Plug-in effects on the Macintosh and Windows platforms.
 You can find Plug-ins that do all sorts of filters, equalization, reverb,
 chorus, and more!  A list of places to download or purchase Plug-ins
 is below.
 </p>
+<p>
+To use a VST Plug-in effect in Audacity 1.0, just create a folder called
+<b>VST</b> in your Audacity folder, if you don't already have one, and
+drop your Plug-ins there!  The next time you open Audacity, the Plug-ins
+will be in your Effects menu.
+</p>
+</td>
+<td valign=top bgcolor=#d9d9d9>
+<p>
+Audacity 1.2.0 supports <a href="http://www.ladspa.org/">LADSPA</a> and
+<a href="nyquist.php">Nyquist</a> plug-ins natively, and VST plug-ins
+with the optional <a href="http://audacityteam.org/vst/">Audacity VST Enabler</a>.
+</p>
+<p>
+Audacity 1.2.0 already comes with some sample LADSPA and Nyquist plug-ins in the
+<b>Plug-ins</b> folder.  Put the VST Enabler and more plug-ins in this folder in
+order to use them with Audacity.  The next time you open Audacity, the Plug-ins
+will be in your Effect menu (or possible Generate or Analyze).  Plug-in effects
+always appear at the bottom of these menus, with built-in effects at the top.
+</p>
+
+</td>
+</tr>
+</table>
 
 <p>
 Audacity's Plug-in support is improving.  Please let us know if you
 find a VST Plug-in that doesn't seem to work in Audacity, and we'll
 try to fix it. 
-</p>
-
-<p>
-To use a VST Plug-in effect in Audacity, just create a folder called
-<b>VST</b> in your Audacity folder, if you don't already have one, and
-drop your Plug-ins there!  The next time you open Audacity, the Plug-ins
-will be in your Effects menu.
 </p>
 
 <p>
