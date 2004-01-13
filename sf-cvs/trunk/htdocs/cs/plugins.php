4136d875b9253682f4b9de111eb9bc4b488f0d8b
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-01-13 08:32:10 +0000

    automatic

--- sf-cvs/trunk/htdocs/cs/plugins.php
+++ sf-cvs/trunk/htdocs/cs/plugins.php
@@ -1,14 +1,34 @@
 <p>
-Audacity podporuje plug-inové efekty VST na platformách Macintosh a Windows.
-Najít mùžete plug-iny, které poskytují všechny druhy filtrù, ekvalizaci, echo, chorus a další vìci. Seznam míst, kde mùžete stahovat nebo koupit plug-iny je níže.
+<table border=0 cellpadding=4 cellspacing=1>
+<tr>
+<td width=50% bgcolor=#000099><font color=#ffffff size=+2>Audacity 1.0</font></td>
+<td width=50% bgcolor=#000099><font color=#ffffff size=+2>Audacity 1.2.0-pre3</font></td>
+</tr>
+<td valign=top bgcolor=#eeeeee>
+<p>
+Audacity podporuje plug-inové efekty VST na platformách Macintosh a Windows. Najít mùžete plug-iny, které poskytují všechny druhy filtrù, ekvalizaci, echo, chorus a další vìci. Seznam míst, kde mùžete stahovat nebo koupit plug-iny je níže.
 </p>
+<p>
+<p>
+Abyste mohli použít VST plug-iny, vytvoøte v adresáøi, kde máte Audacity, podadresáø s názvem <b>VST</b> (pokud ho tam už nemáte) a VST plug-iny uložte tam. Pøi dalším spuštìní Audacity budou plug-iny v menu Efekty.
+</p>
+</p>
+</td>
+<td valign=top bgcolor=#d9d9d9>
+<p>
+Audacity 1.2.0 nativnì podporuje <a href="http://www.ladspa.org/">LADSPA</a> a
+<a href="nyquist.php">Nyquist</a> plug-iny, a volitelnì s <a href="http://audacityteam.org/vst/">Audacity VST Enablerem</a> také plug-iny VST.
+</p>
+<p>
+Audacity 1.2.0 má už po instalaci v adresáøi <b>Plug-ins</b> nìkolik ukázkových LADSPA a Nyquist plug-inù. Abyste mohli použít další plug-iny, nakopírujte je spoleènì s VST Enablerem právì do tohoto adresáøe. Pøi dalším spuštìní Audacity budou v menu Efekty (nebo pøípadnì v menu Generovat nebo Analyzovat).  Plug-inové efekty se vždy objevují ve spodní èásti tìchto menu, zatímco vestavìné efekty jsou nahoøe.
+</p>
+
+</td>
+</tr>
+</table>
 
 <p>
 Podpora plug-inù v Audacity se zlepšuje. Dejte nám vìdìt, pokud jste našli VST plug-in, který v Audacity nechce fungovat a my zkusíme zjednat nápravu.
-</p>
-
-<p>
-Abyste mohli použít VST plug-iny, vytvoøte v adresáøi, kde máte Audacity, podadresáø s názvem <b>VST</b> (pokud ho tam už nemáte) a VST plug-iny uložte tam. Pøi dalším spuštìní Audacity budou plug-iny v menu Efekty.
 </p>
 
 <p>
