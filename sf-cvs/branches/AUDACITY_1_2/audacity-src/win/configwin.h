976335d62e9cb7f052c55499d18ac58e28d8f1eb
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-01-20 09:47:30 +0000

    Added comments so that the source tarball is more clear

--- sf-cvs/branches/AUDACITY_1_2/audacity-src/win/configwin.h
+++ sf-cvs/branches/AUDACITY_1_2/audacity-src/win/configwin.h
@@ -1,5 +1,11 @@
 // Microsoft Windows specific include file
 
+// These settings are used for official stable releases of Audacity
+// with all of its support libraries included.  However, if you
+// downloaded the source tarball of Audacity, some of these libraries
+// might be missing, so there might be lines later in this file that
+// disables support for these libraries.
+
 #define MP3SUPPORT 1
 #define USE_LADSPA 1
 #define USE_LIBID3TAG 1
