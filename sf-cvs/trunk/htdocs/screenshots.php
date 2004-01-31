3090a0b1cdc7110acb27a188df20f75eacfb9c43
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-01-31 22:08:44 +0000

    Formatting.

--- sf-cvs/trunk/htdocs/screenshots.php
+++ sf-cvs/trunk/htdocs/screenshots.php
@@ -9,15 +9,17 @@ BoxTop($screenshotsStr);
 ?>
 
 <center>
+<p>
 <?php print "<a href=\"screenshots-win98.php$langQueryStr\">"; ?><img src="images/screen/win98/thumb.png" border=0 width=299 height=255><br>Windows 98</a>
-</center>
+</p>
 
-<center>
+<p>
 <?php print "<a href=\"screenshots-macosx.php$langQueryStr\">"; ?><img src="images/screen/macosx/thumb.png" border=0 width=300 height=229><br>Mac OS X</a>
-</center>
+</p>
 
-<center>
+<p>
 <?php print "<a href=\"screenshots-linux.php$langQueryStr\">"; ?><img src="images/screen/linux/thumb.png" border=0 width=300 height=265><br>Linux</a>
+</p>
 </center>
 
 
