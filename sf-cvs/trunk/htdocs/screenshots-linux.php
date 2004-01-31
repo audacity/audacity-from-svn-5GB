ad151e792e123eecc8c2df493fd57abd42880701
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-01-31 22:06:49 +0000

    Add Linux screenshots.

---
+++ sf-cvs/trunk/htdocs/screenshots-linux.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'main.inc.php';
+$title = $screenshotsStr;
+include 'top.inc.php';
+
+BoxTop("$screenshotsStr - Linux");
+
+?>
+
+<center>
+<img src="images/screen/linux/main.png" border=0>
+<p>
+<img src="images/screen/linux/compressor.png" border=0>
+</center>
+
+<?php
+
+BoxBottom();
+
+include 'bottom.inc.php';
+
+?>
--- sf-cvs/trunk/htdocs/screenshots.php
+++ sf-cvs/trunk/htdocs/screenshots.php
@@ -9,12 +9,17 @@ BoxTop($screenshotsStr);
 ?>
 
 <center>
-<?php print "<a href=screenshots-win98.php$langQueryStr>"; ?><img src="images/screen/win98/thumb.png" border=0 width=299 height=255><br>Windows 98</a>
+<?php print "<a href=\"screenshots-win98.php$langQueryStr\">"; ?><img src="images/screen/win98/thumb.png" border=0 width=299 height=255><br>Windows 98</a>
 </center>
 
 <center>
-<?php print "<a href=screenshots-macosx.php$langQueryStr>"; ?><img src="images/screen/macosx/thumb.png" border=0 width=300 height=229><br>Mac OS X</a>
+<?php print "<a href=\"screenshots-macosx.php$langQueryStr\">"; ?><img src="images/screen/macosx/thumb.png" border=0 width=300 height=229><br>Mac OS X</a>
 </center>
+
+<center>
+<?php print "<a href=\"screenshots-linux.php$langQueryStr\">"; ?><img src="images/screen/linux/thumb.png" border=0 width=300 height=265><br>Linux</a>
+</center>
+
 
 <?php
 
