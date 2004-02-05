dc8b2c5087cb8d3a1f20ba3c7b7fa50019b16a12
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-02-05 05:30:06 +0000

    automatic

--- sf-cvs/trunk/htdocs/pt/plugins.php
+++ sf-cvs/trunk/htdocs/pt/plugins.php
@@ -8,8 +8,14 @@ O suporte a plug-ins do Audacity está sendo melhorado. Se você conhece algum plu
 </p>
 
 <p>
-Para usar um plug-in VST no Audacity crie uma pasta com o nome <b>VST</b> na pasta do Audacity e arraste os arquivos de plug-in para dentro dela. Na próxima vez que o programa for aberto os novos efeitos já estarão disponíveis no menu Efeitos.
+Para usar um plug-in VST no Audacity 1.0 crie uma pasta com o nome <b>VST</b> na pasta do Audacity e arraste os arquivos de plug-in para dentro dela. Na próxima vez que o programa for aberto os novos efeitos já estarão disponíveis no menu Efeitos.
 </p>
+
+<p>
+O Audacity 1.2.0 tem suporte nativo a plug-ins <a href="http://www.ladspa.org/">LADSPA</a> e <a href="nyquist.php">Nyquist</a> e a plug-ins VST com o pacote opcional <a href="http://audacityteam.org/vst/">"Audacity VST Enabler"</a> (Habilitador para VST do Audacity).
+</p>
+<p>
+O Audacity 1.2.0 vêm com alguns plug-ins de demonstração LADSPA e Nyquist na pasta <b>Plug-ins</b>. Copie o "VST Enabler" e todos os plug-ins que adquirir ou copiar da Internet nesta pasta para poder utiliza-los. Na próxima vez que o Audacity for inicializado os novos plug-ins aparecerão no menu Efeitos (alguns efeitos se instalam no menu Gerar ou Analisar).  Os plug-ins novos aparecerão na parte inferior dos menus.</p>
 
 <p>
 Aqui há uma lista de sites para download e compra de plug-ins VST (em inglês):</p>
