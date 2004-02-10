5f070eb5b6989d28d3fe1341e8c37bcd63b6e482
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-02-10 19:23:26 +0000

    Update introduction to beta page.

---
+++ sf-cvs/trunk/htdocs/cs/beta-about.php
@@ -0,0 +1,15 @@
+<h1>Audacity <?php print $betaVersion; ?></h1>
+
+<p>
+Audacity <?php print $betaVersion; ?> je veøejná testovací verze svobodného zvukového editoru. Úèelem této verze je, umožnit uživatelùm seznámit se a otestovat nové vlastnosti, které se objeví v naší pøíští stabilní verzi 1.2.0.
+</p>
+
+<p>
+Oznámení jakýchkoliv chyb nebo komentáøe posílejte do:
+<a href="mailto:audacity-help@lists.sourceforge.net">
+audacity-help@lists.sourceforge.net</a>
+</p>
+
+<p>
+Vaše pøipomínky nám pomohou udìlat finální verzi stabilní a bezchybnou!
+</p>
