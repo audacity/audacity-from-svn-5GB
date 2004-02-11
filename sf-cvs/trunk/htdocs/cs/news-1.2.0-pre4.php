c627a200dd7afb3239fdb530e287ad71945eef18
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-02-11 10:29:29 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/cs/news-1.2.0-pre4.php
@@ -0,0 +1,14 @@
+<?php BoxTop('10. února 2004: Audacity 1.2.0-pre4 pro Windows a Linux'); ?>
+
+<p>
+Audacity 1.2.0-pre4 je veøejná zkušební verze svobodného zvukového editoru. Tato verze opravuje mnoho menších chyb nalezených v Audacity 1.2.0-pre3. Toto je "release candidate - kandidát na vydání". Pokud nebudou nalezeny nové chyby, uvolníme ke konci tohoto mìsíce novou stabilní verzi 1.2.0.</p>
+
+<p>
+Audacity 1.2.0-pre4 je k dispozici pro Windows a Linux. Nová verze pro Mac OS X bude uvolnìna, až na této platformì odstraníme nìkolik významných chyb.
+</p>
+
+<p>
+Kompletní poznámky k vydání a odkazy na stažení najdete na stránce <a href="beta.php<?php print "$langQueryStr"; ?>">Beta verzí</a>.
+</p>
+
+<?php BoxBottom(); ?>
