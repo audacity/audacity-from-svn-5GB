6c116a60797dfa4a7b645a2b908d11b964721f76
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-02-11 13:29:34 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/pt/news-1.2.0-pre4.php
@@ -0,0 +1,16 @@
+<?php BoxTop('Liberada para testes a versão 1.2.0-pre4 do Audacity!   <font size=1>02/08/03</font>'); ?>
+
+<p>
+O Audacity 1.2.0-pre4 é o novo teste público da versão 1.2.0 e corrige todas as falhas encontradas nas versões de teste anteriores. 
+</p>
+Esta é a versão Release Candidate do programa, e caso não sejam relatadas novas falhas, a versão estável 1.2.0 será liberada em algumas semanas. 
+
+</p>
+<p>
+Audacity 1.2.0-pre4 está diponível para Windows e Linux. A versão para MacOS X estará diponível em breve.
+</p>
+<p>
+A página <a href="beta.php<?php print "$langQueryStr"; ?>">Versões Beta</a>
+já dispõe de links para cópia e notas de versão.
+</p>
+<?php BoxBottom(); ?>
