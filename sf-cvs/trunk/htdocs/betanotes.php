b32f36e06e843ac8708eb5984e51309ed17b44a4
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-02-16 16:52:04 +0000

    Change pre3 to pre4 in heading.

--- sf-cvs/trunk/htdocs/betanotes.php
+++ sf-cvs/trunk/htdocs/betanotes.php
@@ -4,7 +4,7 @@ require_once 'main.inc.php';
 $title = $releaseNotesStr;
 include 'top.inc.php';
 
-BoxTop("$releaseNotesStr 1.2.0-pre3");
+BoxTop("$releaseNotesStr 1.2.0-pre4");
 include GetTranslation("betanotes-1.2.0-pre4");
 include GetTranslation("betanotes-1.2.0-pre1");
 BoxBottom();
