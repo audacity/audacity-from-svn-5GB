4ac0b93011ee83e1051f3a4bc991a3f77da30d10
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-02-17 07:30:45 +0000

    Update G5 note to indicate progress

--- sf-cvs/trunk/htdocs/en/g5note.php
+++ sf-cvs/trunk/htdocs/en/g5note.php
@@ -1,5 +1,5 @@
 Note: Audacity 1.2.0-pre3 is apparently not compatible with the G5.
 If you have a G5 or an audio device that is not supported in
-version 1.2.0-pre3, <a href="http://audacityteam.org">see this page</a>
+version 1.2.0-pre3, <a href="http://audacityteam.org/mac/">see this page</a>
 for more information.  We now have a test version of Audacity that should
 work on all of these systems, but we are still working on a final build.
