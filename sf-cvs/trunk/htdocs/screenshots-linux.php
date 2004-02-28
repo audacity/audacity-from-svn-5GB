9d7bd3d6972d2a8b4478da9daa3c852adfdbae53
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-02-28 03:43:53 +0000

    More Linux screenshots.

--- sf-cvs/trunk/htdocs/screenshots-linux.php
+++ sf-cvs/trunk/htdocs/screenshots-linux.php
@@ -12,6 +12,10 @@ BoxTop("$screenshotsStr - Linux");
 <img src="images/screen/linux/main.png" border=0>
 <p>
 <img src="images/screen/linux/compressor.png" border=0>
+<p>
+<img src="images/screen/linux/change-pitch.png" border=0>
+<p>
+<img src="images/screen/linux/noise-removal.png" border=0>
 </center>
 
 <?php
