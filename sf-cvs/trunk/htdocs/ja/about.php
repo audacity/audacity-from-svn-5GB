929972118a745e694d8c28afa4c8e82837ec68e6
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-02-29 06:27:08 +0000

    automatic

--- sf-cvs/trunk/htdocs/ja/about.php
+++ sf-cvs/trunk/htdocs/ja/about.php
@@ -1,7 +1,7 @@
 <?php BoxTop($aboutStr); ?>
 
 <p>
-　Audacityはフリーのオーディオ編集ソフトです。録音、再生はもちろんのこと、WAVE, AIFF, Ogg, MP3などさまざまなファイル形式での読み込み・書き込みに対応しています。切り取り・コピー・貼り付けは何度でも元に戻すことができますし、複数のトラックをミックスしたり、録音したものにエフェクトを付加したりもできます。また、内蔵エンベロープエディタ、スペクトログラムモード、及び周波数解析ウィンドウを備えています。内蔵エフェクトには、エコー・テンポチェンジ・ノイズ除去なども備えています。さらに、一般に出回っているVSTやLADSPAのプラグインエフェクトを使うこともできます。
+　Audacityはフリーのオーディオ編集ソフトです。録音、再生はもちろんのこと、WAV, AIFF, Ogg Vorbis, MP3などさまざまなファイル形式での読み込み・書き込みに対応しています。切り取り・コピー・貼り付けは何度でも元に戻すことができますし、複数のトラックをミックスしたり、録音したものにエフェクトを付加したりもできます。また、内蔵エンベロープエディタ、スペクトログラムモード、及び周波数解析ウィンドウを備えています。内蔵エフェクトには、エコー・テンポチェンジ・ノイズ除去なども備えています。さらにVSTやLADSPAのプラグインエフェクトを使うこともできます。
 </p>
 
 <center>
@@ -9,19 +9,12 @@
 </center>
 
 <p>
-　Audacityは、ボランティアのチームによってオープンソースで開発されています。Audacityは、クロスプラットフォームのライブラリ<a href="http://wxwidgets.org/">wxWidgets</a>を使い、C言語とC++言語で書かれています。全ソースコードは、<a href="http://www.gnu.org/copyleft/gpl.html"
->GNU General Public License</a>に基づき、誰でも手に入れることができます。このライセンスにおいては、ソースコードに変更を加えることは、その変更を公開するのであれば許されています。
-
-ソースコードは<a href="http://www.sourceforge.net">SourceForge</a>上にあります。
+　Audacityは、ボランティアのチームによってオープンソースで開発されています。Audacityは、クロスプラットフォームのツールキットである<a href="http://wxwidgets.org/">wxWidgets</a>を使って、C言語やC++言語で書かれています。全ソースコードは、<a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License</a>に基づき、誰でも手に入れることができます。このライセンスにおいては、ソースコードに変更を加えることは、その変更を公開するのであれば許されています。ソースコードは<a href="http://www.sourceforge.net">SourceForge</a>上にあります。
 </p>
 
 <p>
-　疑問点やコメントがありましたら、<a href="mailto:audacity-help@lists.sourceforge.net"
->audacity-help@lists.sourceforge.net</a>までメールをください。
+　疑問点やコメントがありましたら、<a href="mailto:audacity-help@lists.sourceforge.net">audacity-help@lists.sourceforge.net</a>までメールをください(ただし、このメールアドレスは今のところ英語専用です。いつかは日本語用のメールアドレスを用意する予定です)。
 </p>
-
-<!-- このメールアドレスは英語専用です。
--->
 
 <?php BoxBottom(); ?>
 
