0ad0f8c22db8f328ef2d2d2074f43e684b3812bf
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-02-29 03:27:07 +0000

    automatic

--- sf-cvs/trunk/htdocs/jp/main.inc.php
+++ sf-cvs/trunk/htdocs/jp/main.inc.php
@@ -1,3 +1,37 @@
 <?php
 $charset = "UTF-8";
+$translatorName = "kuzu";
+$languageStr = "Japanese";
+$metaDescription = "Audacity フリーでクロスプラットフォームのデジタルオーディオエディッタ";
+$helpSectionStr = "ヘルプ";
+$aboutStr = "Audacityとは？";
+$screenshotsStr = "スクリーンショット";
+$helpStr = "オンラインヘルプ";
+$faqStr = "よくある質問と答え";
+$tutorialsStr = "チュートリアル";
+$linksStr = "リンク集";
+$contactUsStr = "連絡先";
+$downloadSectionStr = "ダウンロード";
+$stableStr = "完成版";
+$betaStr = "ベータ版";
+$windowsStr = "Windows";
+$macStr = "Mac OS 9/X";
+$unixStr = "Linux / Unix";
+$betaVersionsStr = "ベータ版";
+$pluginsStr = "プラグイン";
+$mailingListStr = "新バージョンをEメールで通知します:";
+$emailAddressStr = "Eメールアドレス";
+$addEmailStr = "追加";
+$removeEmailStr = "消去";
+$privacyPolicyStr = "プライバシーポリシー";
+$communitySectionStr = "コミュニティー";
+$usersListStr = "Audacity ユーザーズリスト";
+$translationStr = "翻訳プロジェクト";
+$donateTimeStr = "時間をください";
+$donateMoneyStr = "寄付をお願いします";
+$develSectionStr = "開発者";
+$develListStr = "Audacity 開発者リスト";
+$develNewsStr = "最新の開発情報";
+$sourceForgeStr = "SourceForge プロジェクト";
+$browseSourceStr = "ソースコードを見る";
 ?>
