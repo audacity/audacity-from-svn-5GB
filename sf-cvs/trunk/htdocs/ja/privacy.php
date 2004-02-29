67c819903fbdcb01ec9b0e6ae095368bdeb9711a
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-02-29 10:27:28 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/ja/privacy.php
@@ -0,0 +1,23 @@
+<?php BoxTop($privacyPolicyStr); ?>
+
+<p>　もしあなたが、新しいAudacityが公開されたときに連絡をほしいのであれば、左のテキストボックスの中にメールアドレスを入れて"追加"ボタンをクリックしてください。そうすれば個人的なメーリングリストに追加され、私たちは簡単な通知を送るでしょう(※この通知は英語です)。
+</p>
+
+<!-- Translators - please add a sentence which explains that these
+ announcements will usually be in English only.  Possibly people can
+ join users lists in their native languages later on, to hear
+ announcements there.
+-->
+
+<p>
+　私たちはあなたのプライバシーを大切にします。
+</p>
+
+<p>　どんな理由であろうとも、Audacityの開発者やメーリングリストの管理者は、リストを他の団体と共有することはありません。私たちはリスト内にメールアドレス以外の情報を蓄えることはしませんし（あなたの名前も蓄えません）、メーリングリストを誰でもアクセスできる場所に流すことも決してありません。
+</p>
+
+<p>
+　もしあなたが私たちのメーリングリストから除去してほしいのであれば、あなたのメールアドレスを左のテキストボックスに入れて、"消去"ボタンを押してください。これでただちにあなたの情報が私たちのリストから除去できます。何の認証も必要もありませんし、遅れもありません。もし何らかの理由でうまくいかないのであれば、どうかdominic <b>at</B> minorninth.com までメールをください。そうすれば彼が助けてくれるでしょう。
+</p>
+
+<?php BoxBottom(); ?>
