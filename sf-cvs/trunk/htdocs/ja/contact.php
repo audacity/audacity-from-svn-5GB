cbb021d30aa6ef242a9cc0a72e089730f1ca8b16
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-02-29 09:27:20 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/ja/contact.php
@@ -0,0 +1,31 @@
+<?php BoxTop($contactUsStr); ?>
+
+<p>
+何かわからないことがありましたら、多くの資料がありますので参照してください。
+</p>
+
+<p>
+<ul>
+
+<li>
+<?php print "<a href=faq.php?$langLinkStr>$faqStr"; ?></a>をご覧ください。<br>
+　もしあなたがAudacityとは何だろうと思っているのであれば、また、インストールや実行方法について困っているのあれば、まず最初にこのページをご覧になり、あなたの疑問点が載っているかどうかお調べください。
+<p>
+
+<li>
+<a href="http://lists.sourceforge.net/lists/listinfo/audacity-users">Audacityユーザーズメーリングリスト</a>に参加してください。<br>
+<a href="http://sourceforge.net/mailarchive/forum.php?forum_id=827">Audacityユーザーズアーカイブ</a>をご覧ください。<br>
+　これは、Audacityのユーザーがお互いに話し合うことのできる、そしてアイデアを共有することのできるメーリングリストです。もしあなたが他のAudacityユーザーが答えられそうな質問をお持ちであれば、どうぞメーリングリストに参加して、疑問点を尋ねてください。また、既に出た質問であれば、記録文書で見ることもできます。
+<p>
+
+<li>
+<a href="mailto:audacity-help@lists.sourceforge.net">audacity-help@lists.sourceforge.net</a>までメールをください(※英語のみ)。<br>
+又は<a href="http://sourceforge.net/mailarchive/forum.php?forum_id=828">Audacityヘルプアーカイブ</a>をご覧ください。<br>
+　もしあなたが技術的な問題を抱えているのであれば、また他のユーザーもわからなくて戸惑ってしまうような場合は、このアドレスにメールをください。そうすればAudacityの開発者や管理者が読むことでしょう。また、何らかの理由で開発者と直接連絡をとりたい場合も、このアドレスにメールをください。
+<p>
+　Audacityは世界中のボランティアによって開発されているのです。それゆえ、私たちに通じている電話番号や住所はございません。Audacityの開発はすべてインターネット上で行われておりますので、開発者と連絡をとる方法は、やはりメーリングリストか、上のEメールアドレスのいずれかになります。
+<p>
+
+</ul>
+
+<?php BoxBottom(); ?>
