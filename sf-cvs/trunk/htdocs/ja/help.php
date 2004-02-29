0373e4aca032243f344b6aaa0ff48e6fed69bd97
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-02-29 14:27:08 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/ja/help.php
@@ -0,0 +1,107 @@
+<?php BoxTop("$helpStr - Audacity 1.2.0"); ?>
+
+<p>
+Audacity 1.2 全マニュアル(英語): 
+[<a href="manual-1.2">オンラインで閲覧する</a>]
+[<a href="audacity-manual-1.2.zip">ダウンロードする(450k ZIP)</a>]
+</p>
+
+<p>
+Audacity 1.2 クイックリファレンス(英語):
+[<a href="onlinehelp-1.2/contents.htm">オンラインで閲覧する</a>]
+</p>
+
+<p>
+<a href="http://www.audacityteam.org/wiki">Audacity Wiki</a>
+(編集可能なドキュメント、技術情報など)(英語)
+</p>
+
+<p>
+
+Audacity関連のウェブサイトや文書を検索:
+
+<form method="post" action="http://audacityteam.org/isearch/index.php" target="_self">
+
+<table border="0" cellpadding="3" cellspacing="1">
+  <tr>
+    <td>
+
+      <input maxLength="255" name="s" size="20" value=''>
+      <input type="submit" value="Search">
+      <a TARGET="_blank" HREF="http://audacityteam.org/isearch/help.php"><font size="-1"> Help with searching</font></a>
+    </td>
+  </tr>
+</table>
+
+<input type="hidden" name="action" value="search">
+</form>
+</p>
+
+<p>
+<hr>
+</p>
+
+<p>
+<?php print "<a href=tutorials.php?$langLinkStr>";
+?>チュートリアル</a>
+</p>
+
+<p>
+<?php print "<a href=faq.php?$langLinkStr>";
+?>良くある質問と答え(F.A.Q.)</a>
+</p>
+
+<p>
+<a href="http://sourceforge.net/mailarchive/forum.php?forum_id=828"
+>Audacity-Help メーリングリスト記録文書</a>
+</p>
+
+<p>
+<a href="mailto:audacity-help@lists.sourceforge.net"
+>audacity-help@lists.sourceforge.net</a>
+</p>
+
+<?php BoxBottom(); ?>
+
+<p>
+
+<?php BoxTop("$helpStr - Audacity 1.0.0"); ?>
+
+<p>
+<a href="docs/contents.html">Read documentation Online</a>
+</p>
+
+<p>
+<?php print $docsStr; ?>:
+<ul>
+<li><a href="audacity-manual-1.0.0-A.zip">English</a>
+<li><a href="audacity-manual-1.0.0-bg.zip">Bulgarian</a>
+<li><a href="audacity-manual-1.0.0-es.zip">Spanish</a>
+</ul>
+</p>
+
+<p>
+<hr>
+</p>
+
+<p>
+<?php print "<a href=tutorials.php?$langLinkStr>";
+?>Tutorials</a>
+</p>
+
+<p>
+<?php print "<a href=faq.php?$langLinkStr>";
+?>Frequently Asked Questions (F.A.Q.)</a>
+</p>
+
+<p>
+<a href="http://sourceforge.net/mailarchive/forum.php?forum_id=828"
+>Audacity-Help Mailing List Archives</a>
+</p>
+
+<p>
+<a href="mailto:audacity-help@lists.sourceforge.net"
+>audacity-help@lists.sourceforge.net</a>
+</p>
+
+<?php BoxBottom(); ?>
