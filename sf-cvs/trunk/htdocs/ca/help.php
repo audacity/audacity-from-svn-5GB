76fb5ef034f556fc6f614ddc2b40a630d34abe69
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-03-01 15:27:29 +0000

    automatic

--- sf-cvs/trunk/htdocs/ca/help.php
+++ sf-cvs/trunk/htdocs/ca/help.php
@@ -1,15 +1,44 @@
-<?php BoxTop($helpStr); ?>
+<?php BoxTop("$helpStr - Audacity 1.2.0"); ?>
 
 <p>
-<a href="docs/contents.html">Llegiu la documentació en línia</a>
+Manual complet de l'Audacity 1.2: 
+[<a href="manual-1.2">Consultar-lo en línia</a>]
+[<a href="audacity-manual-1.2.zip">Descarregar-lo (450k ZIP)</a>]
 </p>
 
 <p>
-<?php print $docsStr; ?>:
-<ul>
-<li><a href="audacity-manual-1.0.0-A.zip">Anglès</a>
-<li><a href="audacity-manual-1.0.0-bg.zip">Búlgar</a>
-</ul>
+Guia de referència ràpida de l'Audacity 1.2:
+[<a href="onlinehelp-1.2/contents.htm">Consultar-la en línia</a>]
+</p>
+
+<p>
+<a href="http://www.audacityteam.org/wiki">Wiki de l'Audacity</a>
+(Documentació públicament editable, consells, trucs i més coses)
+</p>
+
+<p>
+
+Cercar als llocs web i a la documentació de l'Audacity:
+
+<form method="post" action="http://audacityteam.org/isearch/index.php" target="_self">
+
+<table border="0" cellpadding="3" cellspacing="1">
+  <tr>
+    <td>
+
+      <input maxLength="255" name="s" size="20" value=''>
+      <input type="submit" value="Search">
+      <a TARGET="_blank" HREF="http://audacityteam.org/isearch/help.php"><font size="-1"> Ajuda sobre com cercar</font></a>
+    </td>
+  </tr>
+</table>
+
+<input type="hidden" name="action" value="search">
+</form>
+</p>
+
+<p>
+<hr>
 </p>
 
 <p>
@@ -19,12 +48,55 @@
 
 <p>
 <?php print "<a href=faq.php?$langLinkStr>";
-?>Preguntes més freqüents (P.M.F.)</a>
+?>Preguntes Més Freqüents (P.M.F.)</a>
 </p>
 
 <p>
-<a href="http://www.geocrawler.com/lists/3/SourceForge/14679/0/"
->Arxius del llistat de correu de l'ajuda de l'Audacity</a>
+<a href="http://sourceforge.net/mailarchive/forum.php?forum_id=828"
+>Arxius de la llista de correu Audacity-Help</a>
+</p>
+
+<p>
+<a href="mailto:audacity-help@lists.sourceforge.net"
+>audacity-help@lists.sourceforge.net</a>
+</p>
+
+<?php BoxBottom(); ?>
+
+<p>
+
+<?php BoxTop("$helpStr - Audacity 1.0.0"); ?>
+
+<p>
+<a href="docs/contents.html">Llegir la documentació en línia</a>
+</p>
+
+<p>
+<?php print $docsStr; ?>:
+<ul>
+<li><a href="audacity-manual-1.0.0-A.zip">Anglès</a>
+<li><a href="audacity-manual-1.0.0-bg.zip">Búlgar</a>
+<li><a href="audacity-manual-1.0.0-es.zip">Espanyol</a>
+</ul>
+</p>
+
+<p>
+<hr>
+</p>
+
+<p>
+<?php print "<a href=tutorials.php?$langLinkStr>";
+?>Tutorials</a>
+</p>
+
+<p>
+<?php print "<a href=faq.php?$langLinkStr>";
+?>Preguntes Més Freqüents (P.M.F.)</a>
+</p>
+
+<p>
+<a href="http://sourceforge.net/mailarchive/forum.php?forum_id=828"
+>Arxius de la llista de correu Audacity-Help</a>
 </p>
 
 <p>
