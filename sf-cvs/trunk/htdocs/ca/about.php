2425b960964016efb477e508618fb7cc131a8f7b
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-03-01 17:27:16 +0000

    automatic

--- sf-cvs/trunk/htdocs/ca/about.php
+++ sf-cvs/trunk/htdocs/ca/about.php
@@ -1,50 +1,22 @@
 <?php BoxTop($aboutStr); ?>
 
 <p>
-L'Audacity és un editor gratuït d'àudio. Podeu enregistrar sons, reproduir sons, importar i exportar arxius WAV, AIFF, i MP3, i molt més. Utilitzeu-lo per editar els vostres sons emprant les eines de Tallar, Copiar i Enganxar (amb il·limitada capacitat per desfer), barrejar pistes, o aplicar efectes a les vostres gravacions. També posseeix un editor envoltant d'amplitud propi, un mode espectrograma ajustable a mida i una finestra d'analisi de freqüència per aplicacions d'anàlisi d'àudio. Efectes propis inclosos el Bass Bost (Realçador de greus), Wahwah, i l'extractor de soroll, i també ofereix suport per efectes de pedaços VST. 
-
-</p>
-
-<p>
-La versió 1.0.0 de l'Audacity és la versió més estable que hem
-llençat fins ara, i molta gent ha trobat que les seves capacitats
-úniques i la seva interfície intuïtiva fan que el seu ús sigui més agradable que el d'altres editors d'àudio, incloent-hi editors comercials.
-</p>
-
-<p> Els desenvolupadors de l'Audacity hem llançat també la versió 1.1.0,
-que és un anticipi amb qualitat Beta de les noves coses en les que
-estem treballant-hi activament. Conté realment moltes noves funcions,
-però no és una versió tan polida i estable.
+L'Audacity és un programa lliure d'edició d'àudio. Amb ell podeu enregistrar sons, interpretar-los, importar i exportar fitxers WAV, AIFF, Ogg Vorbis i MP3, i moltes coses més. Podeu fer-lo servir per editar els vostres sons emprant les eines de retallar, copiar i enganxar (amb il·limitada capacitat per desfer), barrejar pistes o aplicar efectes als vostres enregistraments. 
+També té un editor integrat de l'envolupant d'amplitud, un espectrograma totalment configurable i una finestra d'anàlisi de freqüències per aplicacions d'anàlisi d'àudio. Els efectes incorporats inclouen Eco, Canviar de temps, Reducció de sorolls, 
+i suporta també els filtres i complements dissenyats per als sistemes VST i LADSPA.
 </p>
 
 <center>
-<a href="http://wxwindows.org/"><img src="images/wxwindows.gif" border="0"></a>
+<a href="http://wxwidgets.org/"><img src="images/wxwindows.gif" border="0"></a>
 </center>
 
 <p>
-L'Audacity està sent desenvolupat per un grup de voluntaris sota el
-model de codi font obert (open-source).
-Està escrit en C i C++, emprant el joc d'eines
-multi-plataforma <a href="http://www.wxwindows.org/">wxWindows</a>.
-Tot el codi font del programa
-es troba disponible sota la llicència
-<a href="http://www.gnu.org/copyleft/gpl.html"
->GNU General Public License</a>, que essencialment permet a modificar
-qualsevol el codi font mentre publiqueu els canvis.
-El codi es troba hostetjat a <a href="http://www.sourceforge.net">SourceForge</a>.
+L'Audacity s'està desenvolupant gràcies al treball d'un equip de voluntaris i sota el model de codi font obert (open-source). Està escrit en C i C++, emprant el joc d'eines multi-plataforma <a href="http://wxwidgets.org/">wxWidgets</a>. Tot el codi font del programa està disponible sota els termes de la llicència <a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License</a>, que bàsicament permet modificar el codi font a qui ho desitgi, sempre que faci públics els canvis efectuats. El codi font s'allotja a <a href="http://www.sourceforge.net">SourceForge</a>.
 </p>
 
 <p>
-Preguntes/comentaris?
-(La següent adreça és per a qui domini l'anglès, aviat es
-faran disponibles correus en cadena per a cada idioma.)
-Envieu un missatge a <a href="mailto:audacity-help@lists.sourceforge.net"
->audacity-help@lists.sourceforge.net</a>
-</p>
-
-<br>
-(<b>Nota:</b> tot i que aquest lloc Web es trobi en català, la versió 1.0 de
-l'Audacity només es troba disponible en anglès. En canvi l'Audacity 1.1 es troba en català i en diversos altres idiomes. Si desitgeu per tant una versió de l'Audacity en es vostre idioma descarregueu la versió Beta. Sinó haureu d'utilitzar una versió únicament en anglès.)
+Preguntes/comentaris? 
+Envieu un missatge en anglès a <a href="mailto:audacity-help@lists.sourceforge.net">audacity-help@lists.sourceforge.net</a> Esperem que en el futur hi hagi també adreces de suport en altres idiomes. 
 </p>
 
 <?php BoxBottom(); ?>
