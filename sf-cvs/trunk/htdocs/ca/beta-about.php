171ed9e086176ac901cf944ffcb6fdf93f10e7cd
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-03-02 23:27:14 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/ca/beta-about.php
@@ -0,0 +1,15 @@
+<h1>Audacity <?php print $betaVersion; ?></h1>
+
+<p>
+L'Audacity <?php print $betaVersion; ?> és una edició pública de proves de l'editor d'àudio digital Audacity.  El propòsit d'aquesta versió és permetre als usuaris poder veure i provar les noves funcionalitats que apareixeran en la propera versió estable.
+</p>
+
+<p>
+Si us plau, informeu-nos de qualsevol problema que trobeu o envieu-nos comentaris a :
+<a href="mailto:audacity-help@lists.sourceforge.net">
+audacity-help@lists.sourceforge.net</a>
+</p>
+
+<p>
+La vostra participació ens ajudarà a obtenir una versió final estable i sense errors!
+</p>
