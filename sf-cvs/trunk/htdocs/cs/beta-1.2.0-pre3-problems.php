bdaeb73ff004c4a29d5138634865eaa64011a13d
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-03-03 10:27:03 +0000

    automatic

--- sf-cvs/trunk/htdocs/cs/beta-1.2.0-pre3-problems.php
+++ sf-cvs/trunk/htdocs/cs/beta-1.2.0-pre3-problems.php
@@ -1,19 +1,16 @@
 <h2>Známé problémy</h2>
 
 <p>
-Toto jsou známé problémy v Audacity 1.2.0-pre3:
+Toto jsou známé problémy v Audacity 1.2.0:
 </p>
 
 <ul>
-  <li>Audacity  umí importovat a zobrazit MIDI soubory, ale nelze je pøehrávat nebo editovat.
-    <p>
-  <li>Obousmìrné nahrávání (full duplex - pøehrávání existující stopy bìhem nahrávání) mùže na nìkterých Linuxových systémech zpùsobit, že nahraný zvuk je zpomalený.
-    <p>
-  <li>Pouze MacOS X: Audacity neumí pracovat se soubory nebo adresáøi, které jsou obsaženy v adresáøích s mezinárodními znaky (èárky, háèky, atd.) v názvu. Pøitom soubory i projekty Audacity s tìmito znaky fungují. Jde jen o ty soubory, které jsou uvnitø adresáøù s tìmi znaky, což zpùsobuje problémy.
-    <p>
+<li><p>Audacity  umí importovat a zobrazit MIDI soubory, ale nelze je pøehrávat nebo editovat.</p></li>
 
-   <li>Pouze Windows:
-    Vezmìte také na vìdomí, že windowsový instalátor nahradí instalace verze
-    1.0.0 a nikoliv instalace beta verzí 1.1.x.
-    <p>
+<li><p>Obousmìrné nahrávání (full duplex - pøehrávání existující stopy bìhem nahrávání) mùže na nìkterých Linuxových systémech zpùsobit, že nahraný zvuk je u mono nahrávek zpomalený.</p></li>
+
+<li><p>Pouze MacOS X: Audacity neumí pracovat se soubory nebo adresáøi, které jsou obsaženy v adresáøích s mezinárodními znaky (èárky, háèky, atd.) v názvu. Pøitom soubory i projekty Audacity s tìmito znaky fungují. Jde jen o ty soubory, které jsou uvnitø adresáøù s tìmi znaky, což zpùsobuje problémy.</p></li>
+
+<li><p>Efekt zmìny výšky ladìní (Change Pitch) mùže zhavarovat, pokud je použit na stopu se vzorkovací frekvencí 96 KHz.</p></li>
+
 </ul>
