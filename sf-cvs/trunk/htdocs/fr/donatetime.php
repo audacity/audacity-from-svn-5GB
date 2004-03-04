5b98afdc27466c9995addea6f7af203a9b966d68
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-03-04 11:27:21 +0000

    automatic

--- sf-cvs/trunk/htdocs/fr/donatetime.php
+++ sf-cvs/trunk/htdocs/fr/donatetime.php
@@ -1,7 +1,7 @@
 <?php BoxTop($donateTimeStr); ?>
 
 <p>
-Audacity a été créé par des bénévoles dans leurs temps libres.  Si vous appréciez Audacity et  que vous aimeriez faire votre part, voici quelques suggestions:
+Audacity a été créé par des bénévoles durant leur temps libre.  Si vous appréciez Audacity et que vous aimeriez participer, voici quelques suggestions:
 </p>
 
 <ul>
@@ -18,7 +18,7 @@ Cette liste de diffusion permet aux utilisateurs d'Audacity d'échanger et de par
 
 <li><b>Traduisez</b>
 <p>
-Si vous avez une bonne connaissance à la fois de l'anglais et d'une autre langue, vous pouvez être utile aux gens qui parlent la même langue que vous.  Nous avons besoin de gens pour traduire les menus et les dialogues du logiciel, la documentation et le site web.  Nous avons aussi besoin de gens sur nos listes de diffusion afin que les utilisateurs puissent poser des questions dans leur langue.  Il s'agirait alors soit de répondre à leurs questions, soit de traduire ces questions en anglais et de les diffuser sur la liste de langue anglaise.
+Si vous avez une bonne connaissance à la fois de l'anglais et d'une autre langue, vous pouvez être utile aux gens qui parlent la même langue que vous.  Nous avons besoin de personnes pour traduire les menus et les dialogues du logiciel, la documentation et le site web.  Nous avons aussi besoin de gens sur nos listes de diffusion afin que les utilisateurs puissent poser des questions dans leur langue.  Il s'agirait alors soit de répondre à leurs questions, soit de traduire ces questions en anglais et de les diffuser sur la liste de langue anglaise.
 <p>
 <a href="http://audacity.sourceforge.net/translation/">Cliquez ici pour en savoir plus au sujet de la traduction.</a>
 <p>
@@ -32,7 +32,7 @@ Votre aide nous est particulièrement précieuse lorsque vous prenez le temps de r
 <p>
 En plus de la dernière version stable, il y a toujours une version bêta d'Audacity.  Cette version comporte habituellement de nombreuses caractéristiques et fonctions nouvelles qui n'ont pas encore été testées.  Si vous souhaitez nous aider, téléchargez cette version d'Audacity et essayez-la.  Dites-nous ce que vous aimez, ce que vous n'aimez pas et ce qui, selon vous, pourrait être amélioré.  Soyez attentif(ve) aux<i> régressions</i>, c'est-à-dire à ce qui fonctionnait auparavant et qui ne fonctionne plus.
 <p>
-Si vous êtes un programmeur d'expérience en C++ et que vous êtes familier avec des outils tel CVS, il nous ferait plaisir d'avoir de l'aide pour le développement d'Audacity.  Dans ce cas, téléchargez le code source le plus récent de CVS, compilez-le et présentez-vous sur la <a href=http://lists.sourceforge.net/lists/listinfo/audacity-devel>liste de diffusion des développeurs d'Audacity.
+Si vous êtes un programmeur d'expérience en C++ et que vous êtes familier avec des outils tel CVS, nous serions heureux d'avoir de l'aide pour le développement d'Audacity.  Dans ce cas, téléchargez le code source le plus récent de CVS, compilez-le et présentez-vous sur la <a href=http://lists.sourceforge.net/lists/listinfo/audacity-devel>liste de diffusion des développeurs d'Audacity.
 <?php print $develListStr; ?></a>.
 <p>
 
