c69c3249e20c2316a44c5836db0466a379d992ff
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-03-04 14:27:20 +0000

    automatic

--- sf-cvs/trunk/htdocs/fr/plugins.php
+++ sf-cvs/trunk/htdocs/fr/plugins.php
@@ -1,1 +1,15 @@
-<p>Audacity accepte les plugiciels VST sur Macintosh et Windows.Il existe des plugiciels qui donnent toutes sortes d'effets: filtres, égalisation, réverbération, effets de choeur etc!  Vous trouverez plus bas une liste de sites où vous pourrez télécharger ou acheter de tels plugiciels. </p><p>La compatibilité d'Audacity avec les plugiciels s'améliore.  Veuillez nous aviser si vous avez un plugiciel VST qui ne fonctionne pas avec Audacity, nous tenterons d'apporter les corrections nécessaires.</p><p>Pour utiliser un effet VST dans Audacity, il suffit de créer une chemise appelée <b>VST</b> à l'intérieur de la chemise Audacity, si cette chemise n'existe pas déjà, et d'y déposer vos plugiciels!  Lorsque vous ouvrirez Audacity, les plugiciels seront listés dans le menu Effets.</p><p>Voici des sites où télécharger ou acheter des plugiciels VST:</p>
+<p>
+Audacity 1.2 intègre les modules <a href="http://www.ladspa.org/">LADSPA</a> at
+<a href="nyquist.php">Nyquist</a> en mode natif, et les modules VST avec Audacity VST Enabler<a href="http://audacityteam.org/vst/"> optionnel </a>.
+</p>
+<p>
+Audacity 1.2.0 intègre déjà quelques échantillons de modules LADSPA and Nyquist dans le dossier <b>Plug-ins</b>.  Placer le VST Enabler et autres modules dans ce répertoire pour pouvoir les utiliser avec Audacity.  A la prochaine ouverture d'Audacity, ces modules seront accessibles depuis le mebnu Effet (ou Générer ou Analyser).  Ces modules d'effets apparaissent toujours en bas des menus, alors que les effests internes se trouvent en haut.
+</p>
+
+<p>
+La compatibilité d'Audacity avec les plugiciels s'améliore.  Veuillez nous aviser si vous avez un plugiciel VST qui ne fonctionne pas avec Audacity, nous tenterons d'apporter les corrections nécessaires.
+</p>
+
+<p>
+Voici des sites où télécharger ou acheter des plugiciels VST:
+</p>
