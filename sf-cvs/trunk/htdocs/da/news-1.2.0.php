a9c49fef14c5be0fa7dff3235681da7946e3fa69
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-03-04 21:27:10 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/da/news-1.2.0.php
@@ -0,0 +1,20 @@
+<?php BoxTop('1. marts 2004: Audacity 1.2.0 frigivet'); ?>
+
+<p>
+Audacity 1.2.0 er den nye stabile udgave af den fri og gratis lydeditor Audacity.
+Denne udgave har mange store forbedringer i forhold til version 1.0, 
+blandt andet nye effekter, forbedret lydkvalitet og en opdateret brugerflade.
+</p>
+
+<p>Hent Audacity 1.2.0 til:</p>
+<ul>
+<li><a href="windows.php<?php print $langQueryStr; ?>">Windows</a>
+<li><a href="mac.php<?php print $langQueryStr; ?>">Mac OS X</a>
+<li><a href="unix.php<?php print $langQueryStr; ?>">Linux/Unix</a>
+</ul>
+
+<p>
+Læs <a href="stablenotes.php<?php print $langQueryStr; ?>">Udgivelsesnoter</a> for at se hvad der er nyt, samt for at se tilbageværende problemer.
+</p>
+
+<?php BoxBottom(); ?>
