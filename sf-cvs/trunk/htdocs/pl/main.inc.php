9c125552f3d01bb5a75491c9c092c6d10d7a12c0
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-03-10 20:26:56 +0000

    automatic

--- sf-cvs/trunk/htdocs/pl/main.inc.php
+++ sf-cvs/trunk/htdocs/pl/main.inc.php
@@ -1,3 +1,33 @@
 <?php
 $charset = "UTF-8";
+$translatorName = "Tomasz Bandura";
+$metaDescription = "Audacity, darmowy wielopltformowy edytor audio";
+$helpSectionStr = "Pomoc";
+$aboutStr = "Czym jest Audacity?";
+$screenshotsStr = "Zrzuty ekranu";
+$helpStr = "Pomoc Oline";
+$faqStr = "F.A.Q.";
+$tutorialsStr = "Przewodniki";
+$linksStr = "Linki";
+$contactUsStr = "Kontakt z nami";
+$downloadSectionStr = "Pobierz";
+$stableStr = "Stabilna";
+$betaStr = "Beta";
+$windowsStr = "Windows";
+$macStr = "Mac OS 9/X";
+$unixStr = "Linux/Unix";
+$betaVersionsStr = "Wersje beta";
+$pluginsStr = "Wtyczki efektowe";
+$emailAddressStr = "Adres email:";
+$addEmailStr = "Dodaj";
+$removeEmailStr = "Usun";
+$communitySectionStr = "Spolecznosc";
+$usersListStr = "Lista uzytkownikow Audacity";
+$develSectionStr = "Deweloperzy";
+$linuxStr = "Linux - Binaria";
+$zipStr = "plik ZIP";
+$rpmStr = "Binaria RPM";
+$latestStableStr = "Ostatnia stabilna wersja:";
+$latestBetaStr = "Ostatnia wersja Beta:";
+$vstLinksHereStr = "Pobierz wtyczki VST tutaj:";
 ?>
