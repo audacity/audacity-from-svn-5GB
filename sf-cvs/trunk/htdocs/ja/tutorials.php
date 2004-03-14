47c91758cbe37c3f5dda335dfe14655b8b78fb00
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-03-14 17:26:45 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/ja/tutorials.php
@@ -0,0 +1,8 @@
+<?php BoxTop($tutorialsStr); ?>
+
+<p>
+<a href="http://www.daniel.uklinux.net/tutorial/">An Audacity Tutorial     
+Daniel James 著</a>(英語) - サンプルサウンドファイル付
+</p>
+
+<?php BoxBottom(); ?>
