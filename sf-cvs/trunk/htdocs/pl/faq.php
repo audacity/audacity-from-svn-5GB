10628a1d2a0d1c30592c63b7004c289b083f319d
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-03-18 12:27:03 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/pl/faq.php
@@ -0,0 +1,440 @@
+<?php BoxTop("Frequently Asked Questions"); ?>
+
+<!--
+<table width=100% cellpadding=6 width=100%><tr><td bgcolor="#ffd0d0" align="center">
+<font size="+3">Najczęściej zadawane pytania (F.A.Q.)</font>
+</td></tr></table>
+-->
+
+<p>
+Ogólne:<br>
+<a href="#g1">Czy rzeczywiście Audacity jest darmowy? Dlaczego?</a><br>
+<a href="#g2">Kto rozwija Audacity?</a><br>
+<a href="#g3">Jak mogę pomóc w rozwoju Audacity?</a><br>
+<a href="#g4">Znalazłem(-am) błąd! No i co teraz?</a><br>
+
+<p>
+
+
+Instalacja:<br>
+
+<a href="#i1">Windows: Co zrobić z plikiem .exe?</a><br>
+<a href="#i2">Windows: Jak odinstalować Audacity?</a><br>
+<a href="#i3">Mac: Co zrobić z plikiem .sit?</a><br>
+<a href="#i4">Unix: Dlaczego mam niespełnione zależności jeżeli instaluję z pliku
+              RPM?</a><br>
+<a href="#i5">Unix: Dlaczego pojawia się błąd w czasie wczytywania współdzielonej biblioteki
+              libwx_gtk.so?</a><br>
+
+<p>
+
+<p>
+
+Używanie Audacity:<br>
+
+<a href="#a1">Jak mogę zmiksować (zmieszać) razem dwie ścieżki?</a><br>
+<a href="#a2">Czy można usunąć wokal z nagrania?</a><br>
+
+<a href="#a3">When I try to record harmonies with myself,
+              why are the two tracks out of sync?</a><br>
+<a href="#a4">Dlaczego Audacity nie jest rozpowrzechniany razem enkoderem MP3?</a><br>
+<a href="#a5">Czy można nagrywać RealAudio lub inne strumienie audio?</a><br>
+<a href="#a6">Pomocy! Naciskam Nagrywaj, a otrzymuję tylko ciszę!</a><br>
+<a href="#a7">Jak mogę podzielić plik na niezależne ścieżki</a><br>
+<a href="#a8">Jak można "wyciągnąć" utwór z płyty audio CD?</a><br>
+
+<p>
+
+<table cellpadding=6 width=100%><tr><td bgcolor="#ffd0d0" align="center">
+<font size="+1">Ogólne</font>
+</td></tr></table>
+
+<p>
+
+<a name="g1"></a>
+<font size=+1><b>
+1. Czy rzeczywiście Audacity jest darmowy? Dlaczego?<br>
+
+</b></font>
+
+<p>
+
+Tak, prawie pod każdym względem. Audacity nie tylko jest rozpowrzechniany
+bez żadnych opłat, ale także możesz z nim za darmo zrobić praktycznie wszystko co chcesz.
+<p>
+Audacity jest rozpowrzechniany na licencji:
+<a href="http://www.gnu.org/copyleft/gpl.html"
+>General Public License (GPL)</a>.
+Jest darmowy zarówno wprzypadku korzystania z niego prywatnie, jak również w celach komercyjnych.
+You are also free to give it away or sell it.
+Kody źródłowe programu są udostępnione za darmo w internecie,
+możesz bez przeszkód modyfikować je na własny użytek, jednak
+każdą zmianę musisz także rozpowrzechniać na zasadach GPL.
+<p>
+Audacity wykorzystuje bibliotekę wxWidgets, która jest rozpowrzechniana 
+na mniej restrykcyjnych zasadach LGPL.
+Aby uzyskać więcej informacji odwiedź stronę
+<a href="http://wxwidgets.org/">wxwidgets.org</a>.
+<p>
+Autorzy programu Audacity postanowili go rozpowrzechniać na licencji GPL
+z wielu powodów. Niektórzy z nas robią go ze zwykłej wspaniałomyślności.
+Niektórzy z nas z pobudek moralnych, ponieważ uważamy że oporgramowanie powinno
+być darmowe, podczas gdy inni z nas wierzą że jest potrzebne dla obu:
+darmowych i komercyjnych programów na świecie.
+<p>
+Jedną z przyczyn tego że Audacity jest darmowe jest to że dzięki temu
+ma szansę być bardziej popularny. Dużo z nas wolałoby raczej widzieć 
+milion zadowolonych ludzi korzystających z Audacity niż mieć tysiąc
+ludzi którzy nam płacą. Dużo osób jest milszych jeżeli mogą coś wziąc za darmo.
+<p>
+JEszcze jeden powód jest popieranie współpracy. Jeżeli Audacity byłoby 
+shareware, staje się niemożliwe żeby ktokolwiek na świecie chciałby
+współtworzyć program, usuwać błedy, tworzyć dokumentację i grafikę.
+<p>
+<a name="g2"></a>
+<font size=+1><b>
+2. Kto napisał Audacity?<br>
+</b></font>
+
+<p>
+
+Audacity powstało jesienią 1999 roku z rąk Dominic'a Mazzoni w czasie stawania się 
+absolwentem Carnegie Mellon University w Pitsburgu (USA).
+Pracował wtedy nad projektem razem z profesorem Roger'em Dannenberg'iem
+i obaj potrzebowali narzędzia które umożliwiło by im wizualizację
+wyników algorytmó do analizy sygnałów audio. 
+Z czasem program zaczął stawać się edytorem audio i inni ludze zaczynali
+mu pomagać.
+
+<p>
+Obecnie Audacity jest rozwijane w Sourceforge, stronę która umożliwia
+współprawcować ludziom z całego świata nad darmowymi projektami informatycznymi.
+ Zobacz <a href="http://www.sourceforge.net"
+>sourceforge.net</a> aby uzyskać więcej informacji.
+Tuziny ludzi współpracuje ze sobą tworząc Audacity, a ich liczba ciągle rośnie.
+
+<p>
+<a name="g3"></a>
+<font size=+1><b>
+3. Jak mogę pomóc w rozwoju Audacity??<br>
+</b></font>
+
+Znajdź błędy i powiedz nam o nich.
+Pisz kod. Tłumacz program na swój język. Twórz grafikę.
+Dołącz do listy mailingowej użytkowników Audacity (Audacity-Users).
+Prześlij nam pieniądze.
+
+<p>
+Proszę, odwiedź naszą nową stronę
+<a href=donatetime.php?lang=en>Dotacja</a>
+aby uzyskać więcej informacji na temat jak nam możesz pomóc.
+<p>
+
+<a name="g4"></a>
+<font size=+1><b>
+4. Znalazłem(-am) błąd! No i co teraz?<br>
+</b></font>
+<p>
+
+Najważniejszą rzeczą w czasie zgłąszania błedu jest to żeby
+błąd był opisany dak dokładnie jak tylko jest to możliwe.
+Przekaż nam informację wystarczającą do odtworzenia tego błędu
+u nas, w przeciwnym wypadku może nie być możliwe usunięcie go.
+Błędy zgłąszaj do
+<a href="mailto:audacity-help@lists.sourceforge.net">
+<audacity-help@lists.sourceforge.net></a>.
+<p>
+Przekaż nam informację na temat tego jakiego systemu opreacyjnego używasz
+(jak na przykład Windows 98, MacOS 9.1, RedHat Linux 7.1, itd.)
+oraz informację na temat Twojej konfiguracji sprzętowej
+jeżeli uważasz że może to być istotne.
+<p>
+Następnie, czy potrafisz powtórzyć ten błąd? Jeżeli zdarza się tylko w konkretnym przypadku,
+opisz nam dokładnie jakie kroki wykonałeś żeby ten błąd wystąpił.
+Jeżeli pojawił się komuniakt o błędzie, wyślij nam pełny
+tekst tego błędu.
+<p>
+
+Chcemy wykosić wszystkie błędy! Dzięki za poświęcenie nam odrobiny czasu
+aby pomóc nam w wyśledzeniu go i usunięciu.
+
+<p>
+<br>
+<p>
+
+<table cellpadding=6 width=100%><tr><td bgcolor="#ffd0d0" align="center">
+<font size="+1">Instalacja</font>
+</td></tr></table>
+
+<p>
+<a name="i1"></a>
+<font size=+1><b>
+1. Windows: Co zrobić z plikiem .exe?<br>
+</b></font>
+
+<p>
+Z przeglądarki, wybierz opcję "Uruchom z bierzącej lokalizacji". Spowoduje
+to uruchomienie programu instalacyjnego, który zainstaluje program Audacity
+i utworzy pozycję w menu start.
+<p>
+Jeżeli zapisałeś ten plik, kliknij na nim dwa razy myszką uruchamiając 
+w ten sposób program instalacyjny.
+<p>
+Jeżeli nie preferujesz takiego sposobu instalacji programu,
+możesz także ściągnąć Audacity jako archiwum ZIP i rozpakować
+go przy użyciu np WinZip'a.
+<p>
+
+<a name="i2"></a>
+<font size=+1><b>
+2. Windows: Jak odinstalować Audacity?<br>
+</b></font>
+
+<p><font size=+0>
+Audacity może zostać odinstalowane poprzez otwarcie
+"Dodaj/Usuń programy" w panelu sterownia. Wybierz "Audacity"
+z listy i naciśnij przycisk "Dodaj/Usuń".
+Spowoduje to uruchomienie programu deinstalacyjnego.
+
+<p>
+<a name="i3"></a> <font size=+1><b> 3. Mac: What do I do with
+the .sit file?<br> </b></font>
+
+<p>
+You need a recent version of StuffIt Expander.  StuffIt Expander
+comes with all Macintoshes and is usually configured by default
+with all Mac web browsers.  If Audacity does not decompress
+automatically, drag "audacity.sit" to StuffIt Expander to decompress
+it.
+<p>
+There is no need to install Audacity.  Just drag the Audacity
+folder to your Applications folder, or wherever else you would
+like to put it.  To uninstall, just drag the entire folder to
+the trash.
+
+<p>
+<a name="i4"></a>
+<font size=+1><b>
+4. Unix: Dlaczego mam niespełnione zależności jeżeli instaluję z pliku RPM?
+<br></b></font>
+
+<p><font size=+0>Po pierwsze sprawdź czy pakiet wxGTK jest zainstalowany. Link do niej możesz znaleźć na naszej stronie Linuksowej - http://audacity.sourceforge.net/linux.html. 
+Jeżeli w dalszym ciągu pojawiają się błędy podczas instalacji, to mógł wystąpić nierozpoznany błąd. Spróbuj zainstalować RPM'a z opcją -nodeps.</font>
+
+<p>
+<a name="i5"></a>
+<font size=+1><b>
+
+5. Unix: Why do I get an error while loading
+	the shared library libwx_gtk.so?<br></b></font>
+
+<p><font size=+0>This could be one of two problems:
+<br>- If you installed the wxWindows RPM, be sure to run "ldconfig" (as
+root) so your system knows about the new library.
+
+<br><br>- If you installed wxWidgets
+from source, it may have been installed in /usr/local, but /usr/local/lib may
+not be in your library path. Add /usr/local/lib to the file "/etc/ld.so.conf"
+and then run "ldconfig". You may also want to add "/usr/local/bin" to your path,
+so that programs can find the "wx-config" utility.<br>
+
+<p>
+<table cellpadding=6 width=100%><tr><td bgcolor="#ffd0d0" align="center">
+<font size="+1">Using Audacity</font>
+</td></tr></table>
+
+<p>
+<a name="a1"></a>
+<font size=+1><b>
+
+1. How do I mix two tracks together?
+</b></font>
+<p>
+
+Audacity mixes automatically.  All you have to do is
+import two tracks into the same project, which you can
+do using the "Import Audio" command in the Project
+menu, or simply by dragging the two audio files to
+Audacity.  When you press the record button, Audacity
+generates a new track automatically, also.
+
+<p>
+
+In order to save your mix, you can either Export the
+project, which will automatically mix all of your
+tracks together, or you can select your tracks and
+use the "Quick Mix" command in the Project menu.
+
+<p>
+<a name="a2"></a>
+<font size=+1><b>
+2. Can I remove the vocals from a recording?
+</b></font>
+<p>
+
+With some stereo recordings, it is possible to remove the vocals
+because of the way in which the recording was mixed at the studio.
+Often, the vocals are placed in the exact center of the recording,
+while all other instruments are slightly off-center.  If you
+subtract the right channel from the left channel, the vocals get
+completely canceled out, leaving only the other instruments.
+<p>
+
+<b>This only works on some recordings!</b>
+<p>
+To attempt this in Audacity, import a stereo recording, then
+click on the track pop-up menu (the little down-arrow next to
+the name of the track) and select "Split Stereo Track".
+Now select the lower track (the right channel) and use the
+"Invert" effect (from the Effect menu).  Finally, use the
+track pop-up menus to make both channels Mono channels,
+and then mix them together using Quick Mix.  If you're lucky,
+the vocals will be gone.
+
+<p>
+<a name="a3"></a>
+<font size=+1><b>
+3. When I try to record harmonies with myself,
+              why are the two tracks out of sync?
+</b></font>
+<p>
+This is normal and it happens because there is a small delay
+between when Audacity starts playing sound and when it actually
+reaches your speaker.  Audacity does not automatically try to
+correct for this delay, and you must do it manually.
+<p>
+To correct this delay, use the
+Time Shift tool to slide
+one of the tracks over until they line up.  Note that you can
+use the tool while you are listening to the recording.
+
+<p>
+
+<a name="a4"></a>
+<font size=+1><b>
+4. Why doesn't Audacity ship with an MP3 encoder?
+</b></font>
+<p>
+
+Unfortunately, the algorithm to encode or create MP3 files
+is patented, and <b>Audacity</b> would not be able to
+include an MP3 exporting algorithm without either
+charging a fee or violating the law in many countries.
+<p>
+As a compromise, Audacity comes with the ability to use
+other MP3 encoders, which you must download separately.
+It is up to you to make sure you are in compliance with
+any licensing restrictions imposed by MP3 encoders.
+<p>
+For more information, see the page on Exporting MP3 files in
+our Online Help.
+
+<p>
+
+
+
+<a name="a5"></a>
+<font size=+1><b>
+5. Can I record RealAudio or other streaming audio?
+</b></font>
+</p><p>
+
+Not automatically.  Most streaming audio, including most music delivered to
+the RealOne Player, Windows Media Player, and Quicktime, is specifically
+designed to prevent you from recording it.
+</p><p>
+Still, it's almost always still possible to record anything your computer
+can play by looping your sound-out jack to your sound-in/microphone jack
+on your computer's sound card.
+</p><p>
+Use a 1/8 inch stereo mini to 1/8 inch stereo mini cable.  Connect one end
+to the sound-out jack on your computer or sound card (where you would
+plug speakers or headphones - it's often colored green).  Connect the other
+end to the microphone or line-in jack (it's often colored red).
+</p><p>
+Now Audacity can record anything your computer plays.  Press Record
+in Audacity, and then press Play in your streaming audio program.
+</p><p>
+
+<a name="a6"></a>
+<font size=+1><b>
+6. Help! I press Record, but all I get is silence!
+</b></font>
+</p><p>
+Audacity records from the default input source that is selected by
+your operating system.  If you have multiple input sources (such as
+a built-in microphone, an external microphone, line-in jack, or an
+audio CD) you have to select one of these to record from.  If you are
+trying to record from a microphone, for example, but your input source
+is set to "line in", then Audacity will just record silence.
+</p><p>
+If you use Windows, you can set the input source by right-clicking
+on the volume icon in your tool tray (in the lower-right corner of
+your screen).
+</p><p>
+If you use Mac OS 9, use the Sound control panel.  On Mac OS X, use
+the Sound panel in the System Preferences (there's an Input tab if you
+have 10.2 or higher).
+</p><p>
+On Linux, use a mixer like "xmixer" or "kmix".
+</p><p>
+
+<a name="a7"></a>
+<font size=+1><b>
+7. How do I split a single file into multiple tracks?
+</b></font>
+</p><p>
+Sometimes you might end up with a recording of multiple songs that are
+in a single file.  If you want to burn these songs onto a CD as separate
+tracks, you need to create a separate file for each song.
+</p><p>
+There are lots of ways to split a recording into multiple tracks using
+Audacity.  Here are a few methods you can try:
+</p><p>
+Method 1: Export Selection
+</p><ul>
+<li>Select the region of audio corresponding to the first track.
+</li><li>Choose "Export Selection as WAV" from the File menu, and save it to
+    disk.
+</li><li>Continue for all remaining tracks.
+</li></ul>
+<p>
+Method 2: Delete and Undo
+</p><ul>
+<li>Delete everything <i>except</i> for the first track.
+</li><li>Choose "Export as WAV" from the File menu, and save the first track
+    to disk.
+</li><li>Select "Undo" from the Edit menu.  Keep undo-ing until you have the rest
+    of your audio back.  (Audacity has unlimited undo.)
+</li><li>Continue for all remaining tracks.
+</li></ul>
+<p>
+Method 3: Split and Export
+</p><ul>
+<li>Select what you would like to be the first track.
+</li><li>Choose "Split" from the Edit menu, which moves the selection into
+    a separate track within Audacity.
+</li><li>Keep doing this until all of the songs are in separate tracks.
+    Don't forget that you can Undo at any point if you make a mistake.
+</li><li>Now go back through and use "Export Selection as WAV" to export each
+    track to a separate file.  To quickly select each track, click on its
+    label (anywhere in the area to the left of the waveform, underneath
+    the title).
+</li></ul>
+<p>
+
+<a name="a8"></a>
+<font size=+1><b>
+8. How do I get songs from an audio CD?
+</b></font>
+</p><p>
+Audacity does not extract songs from audio CDs.  You need to use a
+"ripper" program for this.
+</p><p>
+For Windows, we recommend <a href="http://cdexos.sourceforge.net/">CDex</a>.
+</p><p>
+For Mac OS (9 and X), we recommend Apple's <a href="http://www.apple.com/itunes/">iTunes</a>.
+</p><p>
+For Linux, try <a href="http://www.xiph.org/paranoia/">Paranoia</a>.
+
+<?php BoxBottom(); ?>
