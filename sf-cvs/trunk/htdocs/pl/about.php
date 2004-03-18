328167e334f18d002a271e9412c34cc837b10623
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-03-18 11:26:35 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/pl/about.php
@@ -0,0 +1,38 @@
+<?php BoxTop($aboutStr); ?>
+
+<p>
+Audacity jest darmowym edytorem audio.
+Możesz nagrywać i odtwarzać sygnały audio, importować i eksportować pliki w formatach WAV,AIFF, Ogg Vorbis,pliki MP3 i inne.
+Używaj go do edycji dźwięków z wykorzystaniem wycinania,kopiowania i wklejania 
+( z możliwością dowolnego cofania wykonanych operacji), do miksowania ścieżek oraz dodawania efektów do Twoich nagrań.
+Program posiada także wbudowany edytor obwiedni/amplitudy, regulowany moduł do wyświetlania spektrogramów i analizy częstotliwościowej dla programów do analizy audio.
+Posiada wbudowane efekty echa, zmiany tempa, usuwania szumu, a także umożliwia korzystanie z wtyczek efektowych VST i LADSPA.
+</p>
+
+<center>
+<a href="http://wxwidgets.org/"><img src="images/wxwindows.gif" border="0"></a>
+</center>
+
+<p>
+Audacity jest tworzony przez zespół wolontariuszy jako projekt open source.
+Jest pisany w językach C i C++, korzysta z biblioteki
+<a href="http://wxwidgets.org/">wxWidgets</a> umożliwiającej tworzenie programów na wiele platform.
+Cały kod źródłowy programu jest udostępniany w ramach licencji 
+<a href="http://www.gnu.org/copyleft/gpl.html"
+>GNU General Public License</a>, której intencją jest umożliwienie
+każdemu modyfikowania kodu źródłowego pod warunkiem że opublikuje wszystkie wprowadzone zmiany.
+The code is hosted on <a href="http://www.sourceforge.net">SourceForge</a>.
+</p>
+
+<p>
+Pytania/Komentarze?
+Wyślij maila na adres <a href="mailto:audacity-help@lists.sourceforge.net"
+>audacity-help@lists.sourceforge.net</a>
+</p>
+
+<!-- Translators: please make a note here that the "audacity-help"
+     address is intended for people who speak English.  We will have
+     alternative mailing lists for other languages eventually...
+-->
+
+<?php BoxBottom(); ?>
