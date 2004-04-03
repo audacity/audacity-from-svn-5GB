04ab4e9baded57eab2dfc2894277c3a58341e3ef
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-04-03 04:26:07 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/no/privacy.php
@@ -0,0 +1,25 @@
+<?php BoxTop($privacyPolicyStr); ?>
+
+<p> Hvis du er interessert i å motta meldinger hver gang nye versjoner av Audacity kommer ut kan du gjerne skrive inn e-postadressen din i boksen til venstre, og klikke "Legg til". Du vil bli lagt til vår private e-postliste, hvor vi bare sender korte og heller sjeldne meldinger.
+Disse meldingene vil vanligvis bare være på engelsk. Senere kan det bli mulighet for e-postlister i andre språk. 
+</p>
+
+<!-- Translators - please add a sentence which explains that these
+ announcements will usually be in English only.  Possibly people can
+ join users lists in their native languages later on, to hear
+ announcements there.
+-->
+
+<p>
+Vi respekterer ditt privatliv.
+</p>
+
+<p> Under ingen omstendigheter vil Audacity-utviklerne eller administratorene for e-postlisten noen gang dele listen med utenforstående, uansett årsak. Vi vil ikke lagre noen annen informasjon om brukerne på denne listen (ikke en gang navnet ditt!) og vi vil aldri publisere e-postlisten på et offentlig tilgjengelig sted.
+</p>
+
+<p>
+Hvis du ønsker å fjerne adressen din fra våre e-postlister, vennligst skriv inn e-postadressen i boksen til venstre og klikk "Fjern"-knappen. Dette vil fjerne deg fra listen vår øyeblikkelig; det kreves ingen bekreftelse fra din side og det blir ingen ventetid. Hvis det av en eller annen grunn ikke virker, vennligst send en e-post til dominic <b>hos</b> minornith.com så vil han prøve å hjelpe.
+</p>
+
+<?php BoxBottom(); ?>
+
