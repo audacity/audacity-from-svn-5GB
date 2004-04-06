9157bb6c4b0cc67d8065dae90bbb94227a20684b
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-04-06 11:25:50 +0000

    automatic

--- sf-cvs/trunk/htdocs/bg/gentoo.php
+++ sf-cvs/trunk/htdocs/bg/gentoo.php
@@ -1,16 +1,14 @@
 <?php BoxTop("Gentoo Linux"); ?>
 
 <p>
-<a target=_top href="http://www.gentoo.org/">Gentoo Linux</a> в момента съдържа audacity версия 1.1.1-3 като "стабилна".
-За инсталиране:
-<p>
-<xmp>$ emerge audacity</xmp>
+Audacity е включена в системата Portage system на <a href="http://www.gentoo.org/">Gentoo Linux</a>. За инсталиране на Audacity, стартирайте:
+<kbd>emerge audacity</kbd>
 </p>
 
 <p>
 Поддържа се от: 
 Nick Hadaway <br>
-Е-поща: <tt>raker <b>@</b> gentoo.org</tt>
+е-поща: <tt>raker <b>@</b> gentoo.org</tt>
 </p>
 
 <?php BoxBottom(); ?>
