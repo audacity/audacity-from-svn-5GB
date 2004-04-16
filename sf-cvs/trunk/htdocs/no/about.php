d5412714db7d192990e9ed39e8638a9f027622ff
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-04-16 22:32:59 +0000

    automatic

--- sf-cvs/trunk/htdocs/no/about.php
+++ sf-cvs/trunk/htdocs/no/about.php
@@ -2,35 +2,18 @@
 
 <p>
 Audacity er et fritt og gratis lydredigeringsprogram.
-Det kan spille inn lyder, spille av lyder, importere og eksportere formatene WAV, AIFF og MP3 filer og mer.
+Det kan spille inn lyder, spille av lyder, importere og eksportere formatene WAV, AIFF, Ogg Vorbis og MP3 filer og mer.
 Benytt det til å redigere dine lyder med funksjoner som Klipp, Kopier og Lim Inn (med ubegrensede angremuligheter), mikse spor sammen eller legge effekter på dine opptak.
-Det har også innebygd redigering av amplitydeprofil,
-en justerbar spektrogramfunksjon og et
-vindu for frekvensanalyse.
-Innebygde effekter inkluderer Bassforsterkning, Wahwah og Støyfjerner, 
-og Audacity støtter også VST innstikksmoduler.
-</p>
-
-<p>
-Audacity utgave 1.0.0 er den mest stabile versjonen som er utgitt,
-og mange er av den mening at programmets unike funksjoner og intuitive
-brukergrensesnitt gjør det lettere å bruke enn andre lydredigeringsprogrammer,
-inkludert de som selges kommersielt.
-</p>
-
-<p>
-Audacitys utviklere har også gitt ut utgave 1.1.0, en en beta-kvalitets smakebit på nye
-funksjoner vi jobber aktivt med.
-Denne utgaven inneholder mange, mange nye funksjoner, men er ikke fullt så velpolert
-og stabil.
+Det har også innebygd redigering av amplitydeprofil, en justerbar spektrogramfunksjon og et vindu for frekvensanalyse for bruk ved lydanalyse.
+Innebygde effekter inkluderer Ekko, Forandre Tempo og Støyfjerner og den støtter også VST og LADSPA innstikksmoduler.
 </p>
 
 <center>
-<a href="http://wxwindows.org/"><img src="images/wxwindows.gif" border="0"></a>
+<a href="http://wxwidgets.org/"><img src="images/wxwindows.gif" border="0"></a>
 </center>
 
 <p>
-Audacity utvikles av et lag med frivillige under en åpen-kildekode modell.
+Audacity utvikles av et lag med frivillige under en modell for åpen-kildekode.
 Det er skrevet i C og C++ og benytter seg av flerplattforms-verktøysettet <a href="http://www.wxwindows.org/">wxWindows</a>.
 Hele kildekoden til programmet er tilgjengelig under lisensen <a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License</a>, som i hovedsak tillater alle og enhver å endre kildekoden såfremt endringene blir publisert.
 Kildekoden er lagret på <a href="http://www.sourceforge.net">SourceForge</a>.
@@ -39,7 +22,7 @@ Kildekoden er lagret på <a href="http://www.sourceforge.net">SourceForge</a>.
 <p>
 Spørsmål/kommentarer?
 Send en e-post til <a href="mailto:audacity-help@lists.sourceforge.net"
->audacity-help@lists.sourceforge.net</a>.  Denne listen er engelskspråklig.
+>audacity-help@lists.sourceforge.net</a>. Denne listen er engelskspråklig.
 </p>
 
 <?php BoxBottom(); ?>
