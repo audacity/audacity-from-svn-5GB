4c6d21adcbd93a36a9a7fa149359fc6efc492736
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-04-16 21:32:58 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/no/debian.php
@@ -0,0 +1,10 @@
+<?php BoxTop("Debian"); ?>
+
+<p>
+Audacity er i den offisielle Debian-pakkesamlingen. For å laste ned og installere Audacity, kjør:
+</p>
+<p>
+<kbd>apt-get install audacity</kbd>
+</p>
+
+<?php BoxBottom(); ?>
