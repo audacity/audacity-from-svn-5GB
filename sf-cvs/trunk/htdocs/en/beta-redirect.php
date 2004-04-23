d1bc7e41677bc9598318903423300a160b8d2c14
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-04-23 15:01:11 +0000

    Make sure version number is updated as appropriate.

--- sf-cvs/trunk/htdocs/en/beta-redirect.php
+++ sf-cvs/trunk/htdocs/en/beta-redirect.php
@@ -1,4 +1,4 @@
 <h2>Audacity 1.2 is no longer in beta testing!</h2>
 
 <p>Download our new stable version
-<a href="index.php<?php print $langQueryStr; ?>">Audacity 1.2.0</a>.
+<a href="index.php<?php print $langQueryStr; ?>">Audacity <?php print $stableVersion; ?></a>.
