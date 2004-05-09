6ad19ee4e5f39f442fe63d900153f7eeac9404ea
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-05-09 09:32:32 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/ca/news-1.2.1.php
@@ -0,0 +1,20 @@
+<?php BoxTop('May 8, 2004: Audacity 1.2.1 released'); ?>
+
+<p>
+Audacity 1.2.1 és la nova versió estable de l'editor d'àudio digital Audacity.
+En aquest lliurament s'han corregit alguns petits problemes que es van detectar a l'Audacity 1.2.0. 
+També s'hi inclouen noves traduccions a altres idiomes i actualitzacions de les existents.
+</p>
+
+<p>Descarregar l'Audacity per a:</p>
+<ul>
+<li><a href="windows.php<?php print $langQueryStr; ?>">Windows</a>
+<li><a href="mac.php<?php print $langQueryStr; ?>">Mac OS X</a>
+<li><a href="unix.php<?php print $langQueryStr; ?>">Linux/Unix</a>
+</ul>
+
+<p>
+Llegiu les <a href="stablenotes.php<?php print $langQueryStr; ?>">Notes de la versió</a> per veure què és el que ha canviat, així com una llista dels problemes coneguts d'aquesta versió.
+</p>
+
+<?php BoxBottom(); ?>
