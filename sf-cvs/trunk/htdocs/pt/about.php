2b46f68e63b90cc7830ed5f47b9d7daab7371321
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-05-09 17:32:35 +0000

    automatic

--- sf-cvs/trunk/htdocs/pt/about.php
+++ sf-cvs/trunk/htdocs/pt/about.php
@@ -6,27 +6,18 @@ Você pode gravar, reproduzir, importar e exportar sons em formato WAV, AIFF, MP3
 Use-o para editar sons usando recursos simples como Cortar e Colar, misture faixas diferentes e aplique efeitos especiais. 
 Não se preocupe em errar, o Audacity não tem limites para o comando Desfazer. 
 Há também recursos avançados como um amplificador em envelope, um espectrograma personalizável e um analisador de freqüências.
-É possível também utilizar efeitos sonoros com plugins VST e os incluídos no próprio programa, como Reforço de Graves e Remoção de ruídos.
+É possível também utilizar efeitos sonoros com plugins VST e LADSPA e os incluídos no próprio programa, como Reforço de Graves, Eco e Remoção de ruídos.
 </p>
-
-<p>
-A versão final do Audacity é a 1.0.0, e muitas pessoas acharam alguns recursos mais fáceis e intuitivos de usar que em outros editores de som, incluindo alguns editores comerciais.
-</p>
-
-<p> Os desenvolvedores do Audacity também liberaram a <a href="<?php print "beta.php?$langLinkStr"; ?>">
-versão 1.2.0-pre3</a>, com novos recursos, mas ainda em fase de testes.
-</p>
-
 <center>
-<a href="http://wxwindows.org/"><img src="images/wxwindows.gif" border="0"></a>
+<a href="http://wxwidgets.org/"><img src="images/wxwindows.gif" border="0"></a>
 </center>
 
 <p>
-O Audacity é desenvolvido por um time de voluntários como código - aberto. 
+O Audacity é desenvolvido por um time de voluntários como um programa livre, de código-fonte aberto, podendo ser livremente copiado e modificado por qualquer pessoa.<p>
 Foi escrito em C e C++, usando o
-<a href="http://www.wxwindows.org/">wxWindows</a>, um conjunto de ferramentas para criação de software em múltiplas plataformas.
+<a href="http://wxwidgets.org/">wxWidgets</a> um conjunto de ferramentas para criação de software em múltiplas plataformas.
 Todo o código-fonte está disponível sob a licença <a href="http://www.gnu.org/copyleft/gpl.html"
->GNU General Public License</a>, que basicamente permite a qualquer pessoa modificar o código-fonte como quiser, mantendo apenas as novas versões também públicamente disponíveis.
+>GNU General Public License</a>, que basicamente permite a qualquer pessoa modificar o código-fonte como quiser, mantendo apenas as novas versões também publicamente disponíveis.
 
 O código está disponível no serviço<a href="http://www.sourceforge.net">SourceForge</a>.
 </p>
@@ -38,6 +29,7 @@ Envie seu email para <a href="mailto:audacity-help@lists.sourceforge.net"
 </p>
 
 <p>
-Nota: As mensagens devem estar em inglês.
+Nota: As mensagens devem estar em inglês. <p>
+Caso deseje criar ou manter uma lista de ajuda do Audacity em português brasileiro, fique à vontade para nos escrever! 
 </p>
 <?php BoxBottom(); ?>
