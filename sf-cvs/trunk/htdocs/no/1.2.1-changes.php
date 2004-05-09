5546fc86b9491f142b81c1789110803a0907ab7e
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-05-09 15:32:35 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/no/1.2.1-changes.php
@@ -0,0 +1,32 @@
+<h2>Forandringer i Audacity 1.2.1</h2>
+
+<ul>
+  <li><p>De følgende oversettelsene har blitt lagt til eller oppdatert: Finsk, Fransk, Ungarsk, Italiensk, Japansk, Norsk, Polsk, Russisk.
+
+  <li><p>Rettet en feil som kunne få data til å gå tapt ved kopiering av lyd fra et prosjekt til et annet, etter at det første prosjektet hadde blitt lagret og lukket.
+
+  <li><p>Rettet en mulig programstans ved åpning eller forandring av størrelse på Equalization vinduet, spesielt når systemet bruker store skrifttyper. 
+
+  <li><p>Tillater ikke prosenter mindre enn -100% i effektene Juster tonehøyde/hastighet/tempo (retter en mulig programstans).
+
+  <li><p>Rettet en programstans som forekommer når den midlertidige mappen ikke er tilgjengelig ved oppstart av Audacity.
+
+  <li><p>Åpner nå ID3-tags som er lagret i Audacity prosjektfiler på korrekt måte.
+
+  <li><p>På Linux og OS X: Lagrer låsfiler i den midlertidige mappen istedenfor i brukeren sin hjemmekatalog. Dette retter problemer i lab-miljøer hvor brukere har ikke har tilgang til hjemmekataloger eller de er montert på nettverk.
+
+  <li><p>Rettet en feil som hindret Nyquist-effekter i å kjøre når visse regionale innstillinger var aktivert.
+
+  <li><p>Rettet en feil i Rask miks kommandoen som kunne gjøre at gamle midlertidige filer ikke ble slettet.
+
+  <li><p>Linux: Rettet endianness-problemer ved avspilling på PowerPC.
+
+  <li><p>Linux: Rettet kompileringsproblemer i Nyquist under MIPS.
+
+  <li><p>Linux: Inkluderte et nyere snapshot av PortAudio v19 (retter kompileringsproblemer ved bygging med --with-portaudio=v19 valget).
+
+  <li><p>To nye Nyquist-innstikksmoduler: "Cross Fade In" og "Cross Fade Out."
+  
+  <li><p>Andre mindre feilrettelser.
+</ul>
+
