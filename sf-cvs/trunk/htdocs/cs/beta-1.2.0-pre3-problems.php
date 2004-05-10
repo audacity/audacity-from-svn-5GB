a23ba29b7123e51e2183b66b16b59f82af45ef63
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-05-10 08:32:41 +0000

    automatic

--- sf-cvs/trunk/htdocs/cs/beta-1.2.0-pre3-problems.php
+++ sf-cvs/trunk/htdocs/cs/beta-1.2.0-pre3-problems.php
@@ -1,16 +1,21 @@
 <h2>Známé problémy</h2>
 
 <p>
-Toto jsou známé problémy v Audacity 1.2.0:
+Toto jsou známé problémy v Audacity 1.2.1:
 </p>
 
 <ul>
 <li><p>Audacity  umí importovat a zobrazit MIDI soubory, ale nelze je pøehrávat nebo editovat.</p></li>
 
-<li><p>Obousmìrné nahrávání (full duplex - pøehrávání existující stopy bìhem nahrávání) mùže na nìkterých Linuxových systémech zpùsobit, že nahraný zvuk je u mono nahrávek zpomalený.</p></li>
+<li><p>Efekt Zmìna ladìní mùže zhavarovat, pokud je aplikován na zvukovou stopu se vzorkovací rychlostí 96 KHz.</p></li>
+
+<li><p>Jen Windows: Audacity zhavaruje, pokud je na èasové stopì použit pøíkaz "Nastavit rychlost".</p></li>
+
+<li><p>Linux: Obousmìrné nahrávání (full duplex - pøehrávání existující stopy bìhem nahrávání) mùže na nìkterých Linuxových systémech zpùsobit, že nahraný zvuk je u mono nahrávek zpomalený.</p></li>
 
 <li><p>Pouze MacOS X: Audacity neumí pracovat se soubory nebo adresáøi, které jsou obsaženy v adresáøích s mezinárodními znaky (èárky, háèky, atd.) v názvu. Pøitom soubory i projekty Audacity s tìmito znaky fungují. Problémy zpùsobují jen soubory, které jsou uvnitø adresáøù se zmínìnými znaky.</p></li>
 
-<li><p>Efekt zmìny výšky ladìní (Change Pitch) mùže zhavarovat, pokud je použit na stopu se vzorkovací frekvencí 96 KHz.</p></li>
+<li><p>Okno "O programu" špatnì zobrazuje znaky mimo západní kódování (pozn. pøekladatele: vè. èeštiny).</p></li>
+<?/* ******************************* */ ?>
 
 </ul>
