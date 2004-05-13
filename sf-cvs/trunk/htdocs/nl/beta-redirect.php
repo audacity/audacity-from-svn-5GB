0dad45697646cecf7936723de539041f1e98c7c1
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-05-13 15:32:59 +0000

    automatic

--- sf-cvs/trunk/htdocs/nl/beta-redirect.php
+++ sf-cvs/trunk/htdocs/nl/beta-redirect.php
@@ -1,4 +1,4 @@
 <h2>Audacity 1.2 is niet langer in de beta testfase!</h2>
 
 <p>Download onze nieuwe stabiele versie
-<a href="index.php<?php print $langQueryStr; ?>">Audacity 1.2.0</a>.
+<a href="index.php<?php print $langQueryStr; ?>">Audacity <?php print $stableVersion; ?></a>.
