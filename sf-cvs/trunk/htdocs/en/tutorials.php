3ee706fe3ca72ac8f5e279b1d2762774fcbea94a
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-05-19 16:38:52 +0000

    Updated link to Daniel James's tutorial.

--- sf-cvs/trunk/htdocs/en/tutorials.php
+++ sf-cvs/trunk/htdocs/en/tutorials.php
@@ -1,8 +1,8 @@
 <?php BoxTop($tutorialsStr); ?>
 
 <p>
-<a href="http://www.daniel.uklinux.net/tutorial/">An Audacity Tutorial
-by Daniel James</a> - comes with sample sound files for you to mix
+<a href="http://www.djcj.org/LAU/quicktoots/toots/audacity/">An Audacity
+Tutorial by Daniel James</a> - comes with sample sound files for you to mix
 together.
 </p>
 
