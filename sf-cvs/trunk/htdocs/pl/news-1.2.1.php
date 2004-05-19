95ed998a222ed4cf178e1f98d14d3f14cde2e03e
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-05-19 10:32:24 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/pl/news-1.2.1.php
@@ -0,0 +1,23 @@
+<?php BoxTop('8 maja 2004: Ukończono  Audacity w wersji 1.2.1'); ?>
+
+<p>
+Audacity 1.2.1 jest nową stabilną wersją darmowego edytora dźwięku.
+W tej wersji zostało poprawionych kilka drobnych błędów, które zostały
+znalezione w wersji Audacity 1.2.0.
+Dodatowo zawiera nowe właściwości oraz uzupełnione tłumaczenia. 
+</p>
+
+<p>Pobierz Audacity dla systemu:</p>
+<ul>
+<li><a href="windows.php<?php print $langQueryStr; ?>">Windows</a>
+<li><a href="mac.php<?php print $langQueryStr; ?>">Mac OS X</a>
+<li><a href="unix.php<?php print $langQueryStr; ?>">Linux/Unix</a>
+</ul>
+
+<p>
+Przeczytaj <a href="stablenotes.php<?php print $langQueryStr; ?>">Release
+Notes</a> żeby dowiedzieć się co zostało zmienione oraz żeby poznać
+listę problemów w tej wersji.
+</p>
+
+<?php BoxBottom(); ?>
