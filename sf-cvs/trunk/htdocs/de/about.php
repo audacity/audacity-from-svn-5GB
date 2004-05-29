13c4592a7b52e79d0ab0834803464514e2d0f933
msmeyer <msmeyer@47890b92-0858-11df-a26f-8b716316a5bc>
2004-05-29 07:26:31 +0000

    Updated german translation

--- sf-cvs/trunk/htdocs/de/about.php
+++ sf-cvs/trunk/htdocs/de/about.php
@@ -1,71 +1,49 @@
 <?php BoxTop($aboutStr); ?>
 
 <p>
-Audacity ist ein freier Audio - Editor.
-Du kannst mit ihm Musik aufnehmen, abspielen, Audiodaten wie WAV, AIFF, MP3 im- und exportieren und vieles mehr.
-Gängige Operationen wie Ausschneiden, Kopieren und Einfügen (unbegrenztes Rückgängigmachen) sind gleichermaßen möglich,
-wie das Mixen und Hinzufügen von speziellen Klangeffekten, wie Echo, oder Geschwindigkeitsveränderungen.
-Das Programm besitzt unter anderem ein Werkzeug um die Amplituden einfach zu verändern (fade-in und -out) 
-und ein einstellbares Frequenzanalysetool.
-Die eingebauten Effekte sind zur Zeit "Bass Boost", "Wahwah" und ein Lärm- bzw. Rauschfilter; 
-es können aber auch beliebige VST-Plugins gechrieben und verwendet werden.
+Audacity ist ein freier Mehrspur-Audio-Editor für Linux, Mac und Windows. Mit 
+Audacity können Töne aufgenommen und wiedergegeben werden sowie Audiodaten 
+im- und exportiert werden in den Formaten wav, mp3 und ogg. Die Töne können 
+auf einer unbegrenzten Zahl Spuren geschnitten, kopiert oder mit hunderten 
+von Effekten versehen werden. Audacity verfügt über zahlreiche Werkzeuge um 
+Lautstärken zu verändern oder mit wenigen Handgriffen Idealpegel 
+einzustellen.
 </p>
 
 <p>
-Audacity 1.0.0 ist im Moment die einzige stabile Version und viele Benutzer finden die einzigartigen Möglichkeiten
- und die intuitive Oberfläche erfrischender und brauchbarer als so manches kommerzielles Programm.
+Auffallend an Audacity in der aktuellen Version 1.2.1 ist die außergewöhnlich 
+intuitiv zu bediendende Oberfläche. Wer Audacity gestartet hat kann sofort 
+mit einem einzigen Mausklick die erste Aufnahme starten, die gebräuchlichsten 
+Arbeitsschritte lehnen sich an gängige Computer-Konventionen an. Für Audacity 
+gibt es zahlreiche Sprachmodule, so ist auch eine komplett deutsch-sprachige 
+Menüführung möglich. 
 </p>
 
 <p>
-Audacity 1.0 ist <b>nur in Englisch erhältlich</b>. 
-Diese Version besitzt noch keinen Sprachsupport und wird auch keinen mehr bekommen. 
-Wer das Programm auf Deutsch nutzen möchte, soll die Beta 1.1.0 versuchen (noch nicht stabil!).
+Audacity eignet sich hervorragend für Musikproduktionen, gerade wenn auf 
+mehreren Spuren ein Mix produziert werden soll. Ebenso können 
+Radioproduktionen außergewöhnlich zügig realisiert werden, da Audacity mit 
+seinem schlanken Aufbau die erforderlichen Funktionen unmittelbar zur 
+Verfügung stellt und häufig auftretende Abläufe auf beliebige Tasten gelegt 
+werden können. In zukünftigen Versionen sollen die Audios noch flexibler auf 
+den jeweiligen Spuren bewegt werden können. 
 </p>
 
 <p>
-Um Audacity auf Deutsch nutzen zu können müssen folgende Schritte durchgeführt werden:
-
-<ol start="1" type="1">
-<li>Starte das Programm</li>
-<li>Wähle "Preferences" im Menü "File"</li>
-<li>Wähle "Interface" an</li>
-<li>Ändere "en" nach "de"</li>
-<li>Beende Audacity und starte es erneut - die Oberfläche ist nun Deutsch</li>
-</ol>
-
-Die Windowsversion findet die Sprachdatei nicht, wenn das Programm über 
-einen Link (auf dem Desktop oder im Startmenü) gestartet wird. Um die Sprache 
-einzustellen muß es also "direkt" gestartet werden. Dazu mußt du mit deinem 
-Dateimanager (Windows Explorer) in das Programmverzeichnis wechseln 
-(meist C:\Prorgamme\Audacity 1.1) und das Programm dort durch einen 
-Doppelklick starten. Dieser Fehler wird in Version 1.1.1 behoben sein.
-</p>
-
-<p>
-Die Version 1.1.0 ist zur Zeit noch im Beta - Stadium und soll lediglich eine Vorschau auf 
-die zahlreichen zukünftigen Funktionen des Programmes sein.
-Vor dem Einsatz mit wichtigen Dateien ist dringend abzuraten!
-</p>
-
-<center>
-<a href="http://wxwindows.org/"><img src="images/wxwindows.gif" border="0"></a>
-</center>
-
-<p>
-Audacity wird von einem Team aus Freiwilligen unter dem Open-Source Modell entwickelt.
-Das Programm ist in C und C++ geschrieben und verwendet das
-<a href="http://www.wxwindows.org/">wxWindows</a> cross-platform toolkit.
-Der gesamte Queltext des Programmes ist unter der
-<a href="http://www.gnu.org/copyleft/gpl.html"
->GNU General Public License</a> verfügbar, welche es jedem erlaubt, 
-den Code zu verändern, solange die Änderungen wieder veröffentlicht und somit der Community zur Verfügung gestellt werden.
-Der Queltext wird von <a href="http://www.sourceforge.net">SourceForge</a> gehostet.
+Audacity wird von einem Team auf Freiwilligen unter dem Open-Source-Modell 
+entwickelt. Das Programm ist in C++ geschrieben und verwendet das 
+Cross-Platform-Toolkit <a href="http://www.wxwidgets.org/">wxWidgets</a>.
+Der gesamte Quelltext des Programms ist unter der
+<a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License</a>
+verfügbar: Dadurch ist es erlaubt, den Code zu verändern, solange die Änderungen
+wieder veröffentlicht und somit der Community zur  Verfügung gestellt werden.
+Der Quelltext wird von <a href="http://www.sourceforge.net">SourceForge</a> gehostet.
 </p>
 
 <p>
 Fragen/Kommentare?
-Send email to <a href="mailto:audacity-help@lists.sourceforge.net"
->audacity-help@lists.sourceforge.net</a> (Nur Englisch)
+Dann einfach eine eMail an <a href="mailto:audacity-help@lists.sourceforge.net"
+>audacity-help@lists.sourceforge.net</a> (in deutsch oder englisch)
 </p>
 
 <?php BoxBottom(); ?>
