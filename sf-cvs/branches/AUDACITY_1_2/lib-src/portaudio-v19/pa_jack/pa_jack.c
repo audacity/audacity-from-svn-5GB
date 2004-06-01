f1529a18ea1041f8ee81fdbf80aaa5911ac2bccb
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-06-01 09:27:17 +0000

    Fixed StreamTime

--- sf-cvs/branches/AUDACITY_1_2/lib-src/portaudio-v19/pa_jack/pa_jack.c
+++ sf-cvs/branches/AUDACITY_1_2/lib-src/portaudio-v19/pa_jack/pa_jack.c
@@ -1,5 +1,5 @@
 /*
- * $Id: pa_jack.c,v 1.1.2.2 2004-06-01 09:21:11 dmazzoni Exp $
+ * $Id: pa_jack.c,v 1.1.2.3 2004-06-01 09:26:50 dmazzoni Exp $
  * PortAudio Portable Real-Time Audio Library
  * Latest Version at: http://www.portaudio.com
  * JACK Implementation by Joshua Haberman
@@ -976,7 +976,7 @@ static PaTime GetStreamTime( PaStream *s )
 
     /* TODO: what if we're recording-only? */
     int delta_t = jack_frame_time( stream->jack_client ) - stream->t0;
-    return delta_t / jack_get_sample_rate( stream->jack_client );
+    return delta_t / (PaTime)jack_get_sample_rate( stream->jack_client );
 }
 
 
