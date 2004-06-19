6ed947346974b58da0854d4f8b1519338052aa34
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-06-19 15:32:41 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/es/news-1.2.1.php
@@ -0,0 +1,23 @@
+<?php BoxTop('8 de Mayo del 2004: Lanzamiento de Audacity 1.2.1'); ?>
+
+<p>
+Audacity 1.2.1 es una versión nueva estable del editor de sonido libre
+y gratuito Audacity. Este lanzamiento soluciona varios errores menores
+que fueron encontrados en Audacity 1.2.0. También contiene algunas
+traducciones nuevas y actualizadas.
+</p>
+
+<p>Descarga Audacity para:</p>
+<ul>
+<li><a href="windows.php<?php print $langQueryStr; ?>">Windows</a>
+<li><a href="mac.php<?php print $langQueryStr; ?>">Mac OS X</a>
+<li><a href="unix.php<?php print $langQueryStr; ?>">Linux/Unix</a>
+</ul>
+
+<p>
+Lée las <a href="stablenotes.php<?php print $langQueryStr; ?>">Notas
+de Lanzamiento</a> Para ver qué cambió, y la lista de problemas conocidos
+en esta versión.
+</p>
+
+<?php BoxBottom(); ?>
