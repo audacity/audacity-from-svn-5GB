97a938cb8b3f3e7d247ba51dbf4f53006b28b757
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-07-06 18:32:35 +0000

    automatic

--- sf-cvs/trunk/htdocs/no/beta-redirect.php
+++ sf-cvs/trunk/htdocs/no/beta-redirect.php
@@ -1,5 +1,6 @@
 <h2>Audacity 1.2 er ute av beta-stadiet!</h2>
 
 <p>Last ned den nyeste stabile versjonen
-<a href="index.php<?php print $langQueryStr; ?>">Audacity 1.2.0</a>.
+<a href="index.php<?php print $langQueryStr; ?>">Audacity <?php print $stableVersion; ?></a>.
+
 
