a28e78091bd656fd858134ec1a3e6eb959a4f1b1
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-07-06 17:32:34 +0000

    automatic

--- sf-cvs/trunk/htdocs/no/tutorials.php
+++ sf-cvs/trunk/htdocs/no/tutorials.php
@@ -1,7 +1,7 @@
 <?php BoxTop($tutorialsStr); ?>
 
 <p>
-<a href="http://www.daniel.uklinux.net/tutorial/">Et opplæringskurs av Daniel James</a> - med medfølgende lydfiler som du kan blande sammen.
+<a href="http://www.djcj.org/LAU/quicktoots/toots/audacity/">Et opplæringskurs for Audacity av Daniel James</a> - med medfølgende lydfiler som du kan blande sammen.
 </p>
 
 <?php BoxBottom(); ?>
