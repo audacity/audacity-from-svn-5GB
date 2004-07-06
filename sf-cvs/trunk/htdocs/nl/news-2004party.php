0faa93e9e6639468cf28229fa372e4a8f0fe3084
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-07-06 18:32:35 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/nl/news-2004party.php
@@ -0,0 +1,18 @@
+<?php BoxTop('Audacity Feest in Seattle op 25 juli 2004'); ?>
+
+<center>
+<a href="http://audacityteam.org/party.php"><font size=+1><b>Audacity Feest 2004</b></font></a><br>
+Seattle, WA, USA<br>
+Zondag, 25 juli 2004<br>
+6:00 pm - 10:00 pm
+</center>
+
+<p> Als je een Audacity fan bent en je woont in de buurt van Seattle WA of je hebt belangstelling om deze zomer er naartoe te reizen, zouden we het leuk vinden om je te ontmoeten! Dit is je kans om de ontwikkelaars van Audacity te ontmoeten, rond te hangen met andere Audacity gebruikers, demo's van mooie nieuwe mogelijkheden die ontwikkeld worden te zien en om een leuke tijd te hebben.  <a
+href="http://audacityteam.org/party.php">Klik hier</a> voor meer
+informatie en om RSVP.  </p>
+
+<p>
+Het andere nieuws is, dat Audacity versie 1.2.2 bijna klaar is.  VU meters zijn toegevoegd en ondersteuning voor exporteren van meerdere bestanden ineens plus verschillende andere mogelijkheden en herstelde fouten. Houdt dit in de gaten!
+</p>
+
+<?php BoxBottom(); ?>
