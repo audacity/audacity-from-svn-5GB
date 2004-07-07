d236f118bbed9b7e49952071ab6c3e3f00b50d39
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-07-07 01:17:37 +0000

    Added note about POTM

--- sf-cvs/trunk/htdocs/index.php
+++ sf-cvs/trunk/htdocs/index.php
@@ -4,6 +4,8 @@ require_once 'main.inc.php';
 $title = "Audacity";
 include 'top.inc.php';
 
+include GetTranslation("news-potm");
+print "<br>\n";
 include GetTranslation("news-2004party");
 print "<p>\n";
 include GetTranslation("news-1.2.1");
