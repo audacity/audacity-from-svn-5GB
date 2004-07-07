4fc8ab4e692de3237542fa2aecd1bc7608048713
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-07-07 01:18:57 +0000

    Added space after POTM

--- sf-cvs/trunk/htdocs/index.php
+++ sf-cvs/trunk/htdocs/index.php
@@ -5,7 +5,7 @@ $title = "Audacity";
 include 'top.inc.php';
 
 include GetTranslation("news-potm");
-print "<br>\n";
+print "<p>\n";
 include GetTranslation("news-2004party");
 print "<p>\n";
 include GetTranslation("news-1.2.1");
