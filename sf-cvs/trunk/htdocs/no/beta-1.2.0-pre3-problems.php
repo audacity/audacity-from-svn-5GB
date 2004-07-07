0616c89a5d7119fb7364f53fbb3c94959df45c73
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-07-07 19:33:05 +0000

    automatic

---
+++ sf-cvs/trunk/htdocs/no/beta-1.2.0-pre3-problems.php
@@ -0,0 +1,18 @@
+<h2>Kjente problemer</h2>
+
+<p>
+Her følger noen kjente feil i Audacity 1.2.1:
+</p>
+
+<ul>
+  <li><p>Audacity kan importere og vise MIDI-filer, men de kan ikke spilles av eller redigeres.
+    </p></li>
+  <li><p>Juster tonehøyde-effekten kan krasje når den er brukt på et spor med en datapunktrate på 96 KHz.</p></li>
+  <li><p>Bare Windows: Audacity krasjer når en bruker tidssporet sin "Sett rate"-funksjon.</p></li>
+  <li><p>Linux: Innspilling i full duplex (avspilling på eksisterende spor samtidig med innspilling) gjør på noen systemer at mono-innspillinger høres saktere ut.</p></li>
+  <li><p>MacOS X: Oppstart av Audacity kan få innstillingene til noen USB-lydenheter til å bli feil. Dette kan rettes ved å forandre innstillingene tilbake til det normale med Apples "Audio MIDI Setup"-program mens Audacity ennå kjører.</p></li>
+  <li><p>MacOS X: Audacity kan ikke jobbe med filer eller mapper som ligger inni mapper med internasjonale tegn (aksent-tegn osv.) i navnene deres. Merk at filer med aksenter virker, og Audacity-prosjekter med aksenter virker. Bare filer inni mapper med aksenter i navnene vil skape problemer.
+    </p></li>
+  <li><p>Om Audacity-vinduet vises ikke riktig i språk som bruker ikke-vestlige alfabeter.</p></li>
+</ul>
+
---
+++ sf-cvs/trunk/htdocs/no/news-comdex-wiki.php
@@ -0,0 +1,29 @@
+<?php BoxTop('28.oktober 2003: COMDEX, Skjermbilder, Wiki, T-Skjorter'); ?>
+
+Vi rettet en haug med feil i den sist utgitte testutgaven og vi vil publisere Audacity 1.2.0-pre3 en gang de neste dagene, sammen med VST Enabler, endelig. I mellomtida, her er noen andre nyheter:
+
+<p>
+Audacity ble nominert som et mulig åpen-kildekode-program som <a href="http://www.oreilly.com/">O'Reilly</a> kan sende til <a href="http://www.comdex.com/">COMDEX</a> i Las Vegas, 17.-20. november.  <a href="http://www.oreillynet.com/contest/comdex/"
+>Klikk her</a> for mer informasjon og for å stemme på Audacity (du må registrere deg - gratis - hos O'Reilly for å stemme).
+Du kan stemme fram til  31. oktober.
+</p>
+
+<p>
+<a href="http://audacityteam.org/wiki/">Audacity Wiki</a> er en nettside som er mulig å redigere av alle, for alt som har med Audacity å gjøre. Den har foreløpig tips, dokumentasjon og linker, i tillegg til et sted for å be om nye programfunksjoner. Det beste er at det er lett for enhver å bidra - bare klikk på en lenke på bunnen av siden for å redigere eller legge til ditt eget materiale. Vi håper du vil synes at dette er en morsom og verdifull ressurs.
+</p>
+
+<p>
+Vi har nå <a href="http://audacityteam.org/tshirt.php">Audacity T-skjorter</a> tilgjengelige som en takk til folk som kontribuerer.
+</p>
+
+<p>
+Både vår <a href="http://audacityteam.org/wiki/">Wiki</a> og <a href="http://audacityteam.org/tshirt.php">T-skjorte-tilbudet</a> ligger på et nytt nettsted, <a href="http://audacityteam.org/">AudacityTeam.org</a>, som vi har satt opp for ekstra Audacity-innhold og emner knyttet til selve utviklerne av Audacity, i motsetning til Audacity generelt.
+</p>
+
+<p>
+Kom tilbake snart for Audacity 1.2.0-pre3. Hvis du ikke har prøvd en beta- eller forhåndsutgave av Audacity i det siste, sjekk våre oppdaterte <a href="http://audacity.sourceforge.net/screenshots.php"
+>Skjermbilder</a>. 
+</p>
+
+<?php BoxBottom(); ?>
+
