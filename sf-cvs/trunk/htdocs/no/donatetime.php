fc394eaf88e396f44b6fec8ae1918e0895f32073
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-07-07 15:32:33 +0000

    automatic

--- sf-cvs/trunk/htdocs/no/donatetime.php
+++ sf-cvs/trunk/htdocs/no/donatetime.php
@@ -8,13 +8,14 @@ Audacity er skrevet av frivillige programmerere på fritida deres. Hvis du liker 
 
 <li><b>Si Hei!</b>
 <p>
-Hvis du liker Audacity vil vi gjerne vite det! Send e-mail til <a href="mailto:audacity-help@lists.sourceforge.net"
+Hvis du liker Audacity, si ifra! Send e-post til <a href="mailto:audacity-help@lists.sourceforge.net"
 >audacity-help@lists.sourceforge.net</a> og fortell oss at du liker programmet. Det gleder oss å vite at programmet blir verdsatt. Og du er velkommen til å sende kommentarer og forslag - vi forbedrer stadig Audacity, og vi setter pris på dine bidrag. 
+<br><?php print "$listPrivacyStr"; ?>
 <p>
 
-<li><b>Vær med blandt Audacity-brukerne</b>
+<li><b>Vær med blant Audacity-brukerne</b>
 <p>
-E-postlisten er et sted hvor Audacitybrukere kan snakke sammen og utveksle ideer.
+Denne e-postlisten er et sted hvor Audacity-brukere kan snakke sammen og utveksle ideer.
 <a href="http://lists.sourceforge.net/lists/listinfo/audacity-users">Påmelding til Audacity-brukernes e-postliste</a><br>
 
 
@@ -43,19 +44,17 @@ fortelle hvilket operativsystem du bruker (f.eks. Windows 2000, Mac OS 9 osv.).
 
 <li><b>Bli en betatester</b>
 <p>
-I tillegg til den seneste stabile utgaven av Audacity har vi alltid en betautgave.
-Denne utgaven har mange nye funksjoner som ikke er ferdigtestede 
+I tillegg til den seneste stabile utgaven av Audacity har vi alltid en betautgave. Denne utgaven har mange nye funksjoner som ikke er ferdigtestede 
 ennå. Hvis du har lyst til å hjelpe kan du laste ned denne utgaven
 av Audacity og prøve den ut. Fortell oss hva du liker og hva du ikke liker, og si hva du synes bør forbedres. Let spesielt etter
-<i>regresjoner</i> - dvs. ting som virket i tidligere utgaver men ikke
+<i>regresjoner</i> - dvs. ting som virket i tidligere utgaver, men ikke
 i denne.
 <p>
 
 <li><b>Bli en utvikler</b>
 <p>
 Hvis du allerede er en erfaren C++ programmør og kan bruke verktøy som CVS
-vil vi gjerne ha din hjelp til å utvikle Audacity. Last ned den nyeste kildekoden
-fra CVS og kompiler den, og introduser deg på 
+vil vi gjerne ha din hjelp til å utvikle Audacity. Last ned den nyeste kildekoden fra CVS og kompiler den, og introduser deg selv på 
 <a href=http://lists.sourceforge.net/lists/listinfo/audacity-devel>
 <?php print $develListStr; ?></a>.
 <p>
@@ -69,4 +68,3 @@ Hvis ingen av de ovenstående mulighetene er mulige for deg, kan du overveie å
 </ul>
 
 <?php BoxBottom(); ?>
-
