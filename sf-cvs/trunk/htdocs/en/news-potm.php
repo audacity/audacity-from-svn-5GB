dc7b173bf7c19c9f1586594aee4240c82db4f3d7
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-07-09 07:16:59 +0000

    Added space

--- sf-cvs/trunk/htdocs/en/news-potm.php
+++ sf-cvs/trunk/htdocs/en/news-potm.php
@@ -1,1 +1,1 @@
-<font size=+1>Audacity is SourceForge's<a href="http://sourceforge.net/potm/potm-2004-07.php">Project of the month</a> for July 2004!</font>
+<font size=+1>Audacity is SourceForge's <a href="http://sourceforge.net/potm/potm-2004-07.php">Project of the month</a> for July 2004!</font>
