95bd16ffd6b906897db7b9410b509ad673cd36e8
nestify <nestify@47890b92-0858-11df-a26f-8b716316a5bc>
2004-07-24 18:27:34 +0000

    Adding the "VoiceKey", which finds the beginning and end of speech.  Used by the transcription toolbar.

---
+++ sf-cvs/trunk/audacity-src/src/VoiceKey.h
@@ -0,0 +1,77 @@
+/***************************************************************************
+
+   Audacity: A Digtial Audio Editor
+
+   VoiceKey.h: a class implementing a voice key
+
+   (c) 2002-2004  Shane T. Mueller
+   Distributed under the terms of the GPL Version 2 or later.
+
+***************************************************************************/
+#ifndef __AUDACITY_VOICEKEY__
+#define __AUDACITY_VOICEKEY__
+
+#include "WaveTrack.h"
+
+
+#ifndef M_PI
+#define	M_PI		3.14159265358979323846  /* pi */
+#endif
+
+
+class VoiceKey {
+
+ public:
+	VoiceKey();
+	~VoiceKey();
+	sampleCount OnForward   (const  WaveTrack & t, sampleCount start, sampleCount len);
+	sampleCount OnBackward  (const  WaveTrack & t, sampleCount start, sampleCount len);
+	sampleCount OffForward  (const  WaveTrack & t, sampleCount start, sampleCount len);
+	sampleCount OffBackward (const  WaveTrack & t, sampleCount start, sampleCount len);
+
+	void CalibrateNoise(const  WaveTrack & t, sampleCount start, sampleCount len);
+	void AdjustThreshold(double t);
+
+ private:
+	
+	double mWindowSize;                 //Size of analysis window, in milliseconds
+	
+	double mThresholdAdjustment;               //User-accessible sensitivity calibration variable
+
+	double mEnergyMean;
+	double mEnergySD;
+	double mSignChangesMean;
+	double mSignChangesSD;
+	double mDirectionChangesMean;
+	double mDirectionChangesSD;
+
+	double mThresholdEnergy;                   // Threshold[*] is equal to [*]Mean + [*]SD * ThresholdAdjustment
+	double mThresholdSignChangesLower;        
+	double mThresholdSignChangesUpper;        
+	double mThresholdDirectionChangesLower;   
+	double mThresholdDirectionChangesUpper;   
+
+	double mSilentWindowSize;           //Time in milliseconds of below-threshold windows required for silence
+	double mSignalWindowSize;           //Time in milliseconds of above-threshold windows required for speech
+	
+	double testEnergy (const WaveTrack & t, sampleCount start,sampleCount len);
+	double testSignChanges (const WaveTrack & t, sampleCount start, sampleCount len);
+	double testDirectionChanges(const WaveTrack & t, sampleCount start, sampleCount len);	
+	
+	void testEnergyUpdate (double & prevErg, int length, const sampleFormat & drop, const sampleFormat & add);
+	void testSignChangesUpdate(double & currentsignchanges,int length, const sampleFormat & a1,
+				   const sampleFormat & a2,   const sampleFormat & z1, const sampleFormat & z2);
+	void testDirectionChangesUpdate(double & currentdirectionchanges,int length, 
+					int & atrend, const sampleFormat & a1, const sampleFormat & a2,
+					int & ztrend, const sampleFormat & z1, const sampleFormat & z2);
+	
+};
+
+
+inline int sgn(int  number){ return (number<0) ? -1: 1;};
+
+//This returns a logistic density based on a z-score
+// a logistic distn has variance (pi*s)^2/3
+
+//inline float inline float logistic(float z){   return fexp(-1 * z/(pi / sqrt(3)) / (1 + pow(fexp(-1 * z(pi / sqrt(3))),2)));};
+#endif
