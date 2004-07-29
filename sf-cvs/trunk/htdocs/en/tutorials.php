f8d81de25a15c1f27374df492e85bd1306595e1a
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-07-29 08:05:03 +0000

    Added more tutorials

--- sf-cvs/trunk/htdocs/en/tutorials.php
+++ sf-cvs/trunk/htdocs/en/tutorials.php
@@ -1,7 +1,15 @@
 <?php BoxTop($tutorialsStr); ?>
 
 <p>
-<a href="http://www.djcj.org/LAU/quicktoots/toots/audacity/">An Audacity
+<a href="http://www.edhsonline.org/other/audacity/">An Audacity 1.2 Tutorial by Dan Eliot.</a>  Covers opening an existing sound file, making simple edits, and exporting it again.  Comes with Flash animations that walk you through everything in great detail!
+</p>
+
+<p>
+<a href="http://www.agnula.org/documentation/dp_tutorials/audacity/">Multitrack recording in Audacity using the AGNULA Linux distribution</a>
+</p>
+
+<p>
+<a href="http://www.djcj.org/LAU/quicktoots/toots/audacity/">An Audacity 1.0
 Tutorial by Daniel James</a> - comes with sample sound files for you to mix
 together.
 </p>
