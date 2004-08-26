97bb165d542a1f36a62db5059f924fb2acd55b53
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-08-26 04:12:24 +0000

    Updating website for Audacity 1.2.2 release.

---
+++ sf-cvs/trunk/htdocs/ca/news-1.2.2.php
@@ -0,0 +1,19 @@
+<?php BoxTop('August 25, 2004: Audacity 1.2.2 released'); ?>
+
+<p>
+Audacity 1.2.2 és la nova versió estable del programari lliure d'edició de so digital Audacity.
+Aquesta versió inclou noves funcionalitats, com ara inicadors visuals de nivells, exportació de fitxers múltiple i disminució del temps de latència en l'enregistrament de múltiples pistes. Inclou també diverses correccions d'errors detectats en versions anteriors.
+</p>
+
+<p>Descarregueu-vos l'Audacity per a:</p>
+<ul>
+<li><a href="windows.php<?php print $langQueryStr; ?>">Windows</a>
+<li><a href="mac.php<?php print $langQueryStr; ?>">Mac OS X</a>
+<li><a href="unix.php<?php print $langQueryStr; ?>">Linux/Unix</a>
+</ul>
+
+<p>
+Llegiu les <a href="stablenotes.php<?php print $langQueryStr; ?>">Notes de la versió</a> per veure què és el que ha canviat, així com la llista dels possibles problemes d'aquesta versió.
+</p>
+
+<?php BoxBottom(); ?>
