832d4a67569deebdea68447b55902d854e59ebb8
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-08-28 18:47:23 +0000

    Manual update.

---
+++ sf-cvs/trunk/htdocs/ja/news-1.2.2.php
@@ -0,0 +1,21 @@
+<?php BoxTop('2004年8月25日: Audacity 1.2.2 公開'); ?>
+
+<p>
+Audacity 1.2.2 はフリーのサウンドエディタであるAudacityの新しい完成版です。
+この公開版には、レベルメーター、マルチファイルエクスポート、より低レイテンシーな
+マルチトラックレコーディングなど、新しい特徴が追加されています。
+また、多くのバグが修正されています。
+</p>
+
+<p>Audacityをダウンロードする:</p>
+<ul>
+<li><a href="windows.php<?php print $langQueryStr; ?>">Windows</a>
+<li><a href="mac.php<?php print $langQueryStr; ?>">Mac OS X</a>
+<li><a href="unix.php<?php print $langQueryStr; ?>">Linux/Unix</a>
+</ul>
+
+<p>
+新たな変更点や既知の問題点につきましては、<a href="stablenotes.php<?php print $langQueryStr; ?>">リリースノート</a>をご覧ください。
+</p>
+
+<?php BoxBottom(); ?>
