ddd63fdf6c5033ea47d5d0715cfc4d654a85454f
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-09-01 19:19:12 +0000

    Manual update.

---
+++ sf-cvs/trunk/htdocs/tr/macos9note.php
@@ -0,0 +1,3 @@
+<p>
+Not: Herhangi bir firma/kiþi finansal yada teknik olarak bize yardým etmediði sürece Mac OS 9 için herhangi bir Audacity versiyonu daðýtýmý yapmayý planlamamaktayýz. Tüm bunlara raðmen Mac OS 9 için bize yardým etmek istiyorsanýz lütfen bize e-posta gönderiniz.
+</p>
