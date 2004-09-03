e61f3f4e124db1deb13b0f6c5864213fcf01389d
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-09-03 15:37:50 +0000

    New Chinese (Simplified) translation from XiaoXi Liu.

--- sf-cvs/branches/AUDACITY_1_2/audacity-src/src/Languages.cpp
+++ sf-cvs/branches/AUDACITY_1_2/audacity-src/src/Languages.cpp
@@ -107,6 +107,7 @@ void GetLanguages(wxArrayString &langCodes, wxArrayString &langNames)
    localLanguageName["ru"] = "Russky";
    localLanguageName["sl"] = "Slovenscina";
    localLanguageName["sv"] = "Svenska";
+   localLanguageName["zh"] = "Chinese(Simplified)";
 
    wxArrayString audacityPathList = wxGetApp().audacityPathList;
    wxGetApp().AddUniquePathToPathList(wxString::Format("%s/share/locale",
