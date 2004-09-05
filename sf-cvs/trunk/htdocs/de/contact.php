c768fb0a59f5c0ca4fa1f648b04e1b485d01ba03
msmeyer <msmeyer@47890b92-0858-11df-a26f-8b716316a5bc>
2004-09-05 21:59:12 +0000

    Added pointers to new German forum

--- sf-cvs/trunk/htdocs/de/contact.php
+++ sf-cvs/trunk/htdocs/de/contact.php
@@ -1,38 +1,45 @@
 <?php BoxTop($contactUsStr); ?>
 
 <p>
-Wenn du Fragen zu Audacity hast, gibt es mehrere Möglichkeiten:
+Wenn Sie Fragen zu Audacity haben, gibt es mehrere Möglichkeiten:
 </p>
 
 <p>
 <ul>
 
 <li>
-Lies die <?php print "<a href=faq.php?$langLinkStr>$faqStr"; ?></a><br>
-Wenn du wissen willst, was Audacity denn überhaupt ist oder Probleme bei der Installation oder Konfiguration hast,
-schau dir diese Seite bitte an, um zu sehen ob deine Frage schon beantwortet ist.
+Lesen Sie die <?php print "<a href=faq.php?$langLinkStr>$faqStr"; ?></a><br>
+Wenn Sie wissen wollen, was Audacity &uuml;berhaupt ist oder Probleme bei der Installation oder Konfiguration auftreten,
+lesen Sie bitte zuerst diese Seite, um zu sehen, ob das Problem eventuell bereits bekannt ist.
 </li>
+<br>
+<li>
+Im neuen <a href="http://audacity.fuchsi.de/">deutschsprachigen Audacity-Forum</a> k&ouml;nnen Sie
+Fragen zu Audacity stellen und Anmerkungen sowie Kritik loswerden. Eine Reihe von kompetenten und
+erfahrenen Benutzern und Entwicklern haben stets ein offenes Ohr f&uuml;r Ihr Anliegen.
+</li>
+<br>
 
 <li>
-Trag dich in die: <a href="http://lists.sourceforge.net/lists/listinfo/audacity-users">Audacity-Benutzer Mailing Liste</a> ein<br>
-Durchsuche die: <a href="http://www.geocrawler.com/lists/3/SourceForge/14680/0/">Audacity-Benutzer Archive</a><br>
-Dies ist eine neue Mailing-Liste, wo sich Audacity - Benutzer miteinander unterhalten und Ideen austauschen können.
-Wenn du glaubst, von anderen Benutzern eine Antwort erhalten zu können, schreib dich in die Liste ein und stelle deine Fragen.
-Im Archiv kannst du nachschauen, ob deine Frage vielleicht schon einmal gestellt und/oder beantwortet wurde.
+Es gibt auch eine Mailing-Liste für Audacity, auf der Sie Hilfe erhalten k&ouml;nnen, sowie eine
+Benutzer-Mailing-Liste, mit der Benutzer von Audacity untereinander Informationen austauschen
+können. Bitte benutzen Sie
+aber nach M&ouml;glichkeit das Forum für deutschsprachige Anfragen zu Audacity. Wollen Sie dennoch
+lieber eine Anfrage an eine der Mailinglisten schicken, formulieren Sie diese bitte in englischer Sprache.
 </li>
+<br>
 
 <li>
-Schick eine Mail an: <a href="mailto:audacity-help@lists.sourceforge.net">audacity-help@lists.sourceforge.net</a><br>
-oder durchsuche das <a href="http://www.geocrawler.com/lists/3/SourceForge/14679/0/">Audacity-Hilfe Archiv</a><br>
-Wenn du ein technisches Problem mit Audacity hast, das zu spezifisch ist, als dass es von anderen Benutzern beantwortet werden könnte,
-schick eine E-Mail an obige Adresse. Deine Frage wird dann von einem Mitglied des Entwicklerteams bearbeitet.
-Diese Adresse ist auch dein Fall, wenn du jemanden aus dem Team anderweitig kontaktieren mußt.
-<p>
-Audacity wird von Freiwilligen aus der ganzen Welt entwickelt. 
-Aus diesem Grund gibt es keine Telefonnummer oder E-Mail Adresse, 
-einer Einzelperson oder Firma. Die gesamte Entwicklung läuft über das 
-Interenet ab und die einzige Kontaktmöglichkeit besteht durch die Mailing-Liste oder die projekteigene E-Mail Adresse.
+Das <a href="http://sourceforge.net/mailarchive/forum.php?forum_id=828">Audacity-Help Mailinglisten-Archiv</a> lesen<br>
+Eine Anfrage an <a href="mailto:audacity-help@lists.sourceforge.net">audacity-help@lists.sourceforge.net</a> schicken (in englischer Sprache)
 </li>
+<br>
+
+<li>
+Das <a href="http://www.geocrawler.com/lists/3/SourceForge/14680/0/">Audacity-User Mailinglisten-Archiv</a> lesen<br>
+<a href="http://lists.sourceforge.net/lists/listinfo/audacity-users">In Audacity-Benutzer-Mailingliste eintragen</a>
+</li>
+<br>
 
 </ul>
 
--- sf-cvs/trunk/htdocs/de/help.php
+++ sf-cvs/trunk/htdocs/de/help.php
@@ -1,7 +1,12 @@
 <?php BoxTop($helpStr); ?>
 
 <p>
-<a href="docs/contents.html">Benutzeranleitung / Dokumentation</a>
+<a href="docs/contents.html">Benutzeranleitungen / Dokumentation</a>
+</p>
+
+<p>
+Bitte beachten Sie, dass das Benutzerhandbuch zu Audacity leider momentan nur in englischer
+Sprache verf&uuml;gbar ist.
 </p>
 
 <p>
@@ -15,13 +20,25 @@
 </p>
 
 <p>
+Im neuen <a href="http://audacity.fuchsi.de/">deutschsprachigen Audacity-Forum</a> k&ouml;nnen Sie
+Fragen zu Audacity stellen und Anmerkungen sowie Kritik loswerden. Eine Reihe von kompetenten und
+erfahrenen Benutzern und Entwicklern haben stets ein offenes Ohr f&uuml;r Ihr Anliegen.
+</p>
+
+<p>
+Es gibt auch eine Mailing-Liste für Audacity, auf der Sie Hilfe erhalten k&ouml;nnen. Bitte benutzen Sie
+aber nach M&ouml;glichkeit das Forum für deutschsprachige Anfragen zu Audacity. Wollen Sie dennoch
+lieber eine Anfrage an die Mailingliste schicken, formulieren Sie diese bitte in englischer Sprache.
+</p>
+
+<p>
 <a href="http://sourceforge.net/mailarchive/forum.php?forum_id=828"
->Audacity-Hilfe Mailing Listen Archive</a>
+>Audacity-Help Mailing List Archive</a>
 </p>
 
 <p>
 <a href="mailto:audacity-help@lists.sourceforge.net"
->audacity-help@lists.sourceforge.net</a>
+>audacity-help@lists.sourceforge.net</a> (in englischer Sprache)
 </p>
 
 <?php BoxBottom(); ?>
