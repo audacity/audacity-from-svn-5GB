5b7d836d19f86423b1c95e5746e90ff222a08465
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-09-08 19:59:57 +0000

    Manual update.

---
+++ sf-cvs/trunk/htdocs/pl/news-1.2.2.php
@@ -0,0 +1,23 @@
+<?php BoxTop('25 września 2004: Udostępniono Audacity w wersji 1.2.2'); ?>
+
+<p>
+Audacity 1.2.2 jest nową stabilną wersją darmowego edytora audio Audacity.
+Ta wersja  zawiera nowe cechy takie jak miernik poziomu, eksport wielu plików
+i niższe opóźnienie przy nagrywaniu wielościeżkowym. Poprawionych zostało także
+wiele błędów.
+</p>
+
+<p>Pobierz Audacity dla systemów:</p>
+<ul>
+<li><a href="windows.php<?php print $langQueryStr; ?>">Windows</a>
+<li><a href="mac.php<?php print $langQueryStr; ?>">Mac OS X</a>
+<li><a href="unix.php<?php print $langQueryStr; ?>">Linux/Unix</a>
+</ul>
+
+<p>
+Przeczytaj<a href="stablenotes.php<?php print $langQueryStr; ?>">
+Release Notes</a> żeby zobaczyć co zostało zmienione, oraz jakie są
+znane problemy bieżącej wersji.
+</p>
+
+<?php BoxBottom(); ?>
