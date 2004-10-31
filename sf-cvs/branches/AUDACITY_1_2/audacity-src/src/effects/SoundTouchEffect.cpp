6581ca9eb43bbb8c1feb53d59b8c11d6d9183a4a
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2004-10-31 21:27:31 +0000

    Upgrade to SoundTouch 1.2.2pre2

--- sf-cvs/branches/AUDACITY_1_2/audacity-src/src/effects/SoundTouchEffect.cpp
+++ sf-cvs/branches/AUDACITY_1_2/audacity-src/src/effects/SoundTouchEffect.cpp
@@ -18,10 +18,9 @@
 
 #include <math.h>
 
-#include <SoundTouch.h>
-
 #include "SoundTouchEffect.h"
 #include "../WaveTrack.h"
+
 
 bool EffectSoundTouch::Process()
 {
@@ -161,3 +160,15 @@ bool EffectSoundTouch::ProcessOne(WaveTrack *track,
 }
 
 #endif // USE_SOUNDTOUCH
+
+// Indentation settings for Vim and Emacs and unique identifier for Arch, a
+// version control system. Please do not modify past this point.
+//
+// Local Variables:
+// c-basic-offset: 3
+// indent-tabs-mode: nil
+// End:
+//
+// vim: et sts=3 sw=3
+// arch-tag: 43212604-7b03-4b49-85bd-adde0f909460
+
