24374f262bec2589756c72306937325879d03c4c
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-11-10 07:36:09 +0000

    automatic

--- sf-cvs/trunk/htdocs/de/help.php
+++ sf-cvs/trunk/htdocs/de/help.php
@@ -1,26 +1,49 @@
 <?php BoxTop($helpStr); ?>
 
+<p></p>
+Das Benutzerhandbuch findet man hier:<p>
+[<a href="manual-1.2">Online anschauen</a> <img src="http://audacity.sourceforge.net/flags/en.gif">]<br>
+[<a href="audacity-manual-1.2.zip">Download des Benutzerhandbuches(450k ZIP)</a> )<img src="http://audacity.sourceforge.net/flags/en.gif">]
+
+</p>
+
 <p>
-<a href="docs/contents.html">Benutzeranleitungen / Dokumentation</a>
+<hr>
+</hr>
+
+
+<p>
+<h3><b>Durchsuchen Sie die Audacity Webseite und Dokumentationen:</b></h3>
+<font size="-1"> ( Beachten Sie bitte, das nach englischen Begriffen gesucht wird und ebenfalls nur die englischen Seiten angezeigt werden)</font>
+<form method="post" action="http://audacityteam.org/isearch/index.php" target="_self">
+
+<table border="0" cellpadding="3" cellspacing="1">
+  <tr>
+    <td>
+
+      <input maxLength="255" name="s" size="20" value=''>
+      <input type="submit" value="Search">
+      <a TARGET="_blank" HREF="http://audacityteam.org/isearch/help.php"><font size="-1">Hilfe zur Suchfunktion</font></a>
+    </td>
+  </tr>
+</table>
+
+<input type="hidden" name="action" value="search">
+</form>
 </p>
 
 <p>
-Bitte beachten Sie, dass das Benutzerhandbuch zu Audacity leider momentan nur in englischer Sprache verfügbar ist.<p>
-
-Dieses Benutzerhandbuch findet man hier:<p>
-[<a href="manual-1.2">Online anschauen</a>]
-[<a href="audacity-manual-1.2.zip">Download des Benutzerhandbuches(450k ZIP)</a>]
-
-</p>
+<hr>
+</hr>
 
 <p>
 <?php print "<a href=tutorials.php?$langLinkStr>";
-?>Anleitungen</a>
+?>Anleitungen</a> <img src="http://audacity.sourceforge.net/flags/de.gif">
 </p>
 
 <p>
 <?php print "<a href=faq.php?$langLinkStr>";
-?>Häufig gestellte Fragen (F.A.Q.)</a>
+?>Häufig gestellte Fragen (F.A.Q.)</a> <img src="http://audacity.sourceforge.net/flags/de.gif">
 </p>
 
 <p>
@@ -28,6 +51,12 @@ Im neuen <a href="http://audacity.fuchsi.de/">deutschsprachigen Audacity-Forum</
 Fragen zu Audacity stellen und Anmerkungen sowie Kritik loswerden. Eine Reihe von kompetenten und
 erfahrenen Benutzern und Entwicklern haben stets ein offenes Ohr für Ihr Anliegen.
 </p>
+<p>
+<a href="http://www.audacityteam.org/wiki">Audacity Wiki</a>
+(Dokumentation, durch Nutzer, für Nutzer, komplett änderbar mit Tipps und Tricks und vielem mehr...)<img src="http://audacity.sourceforge.net/flags/en.gif">
+</p>
+
+<p>
 
 <p>
 Es gibt auch eine Mailing-Liste für Audacity, auf der Sie Hilfe erhalten können. Bitte benutzen Sie
@@ -37,12 +66,12 @@ lieber eine Anfrage an die Mailingliste schicken, formulieren Sie diese bitte in
 
 <p>
 <a href="http://sourceforge.net/mailarchive/forum.php?forum_id=828"
->Audacity-Help Mailing List Archive</a>
+>Audacity-Help Mailing List Archive</a> <img src="http://audacity.sourceforge.net/flags/en.gif">
 </p>
 
 <p>
 <a href="mailto:audacity-help@lists.sourceforge.net"
->audacity-help@lists.sourceforge.net</a> (in englischer Sprache)
+>audacity-help@lists.sourceforge.net</a> <img src="http://audacity.sourceforge.net/flags/en.gif">
 </p>
 
 <?php BoxBottom(); ?>
