9dfe0b11d8b5ca0737bc85b49e723ff3aee88e85
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2004-11-10 07:36:09 +0000

    automatic

--- sf-cvs/trunk/htdocs/de/tutorials.php
+++ sf-cvs/trunk/htdocs/de/tutorials.php
@@ -1,14 +1,22 @@
 <?php BoxTop($tutorialsStr); ?>
 
 <p>
+<h3>Anleitungen für Effekte</h3>
+<a href="http://audacity.sourceforge.net/de/docs/effekte/reverb/index.htm"> 
+Anleitung für den Reverb Effekt</a> - Eine bebilderte Anleitung zum Thema Arbeiten mit Effekten, beinhaltet Grundlagen, sowie spezielle Informationen zum Reverb-Effekt. <b> Komplett in deutsch </b> <img src="http://audacity.sourceforge.net/flags/de.gif"></p> 
+
+
+<p>
+<h3>Allgemeine Anleitungen</h3>
+<p>
 <a href="http://www.daniel.uklinux.net/tutorial/">Eine Audacity Anleitung
-von Daniel James (für Audacity Version 1.0.0) </a> - mit Beispieldateien für eigene Mixes (Das ganze bisher in Englisch).
+von Daniel James (für Audacity Version 1.0.0) </a> - mit Beispieldateien für eigene Mixes (Das ganze bisher in Englisch). <img src="http://audacity.sourceforge.net/flags/en.gif"><br>
 </p>
 <p>
-<a href="http://www.agnula.org/documentation/dp_tutorials/audacity/">Multitrack Aufnahmen in Audacity </a> - beutzt wird dabei die AGNULA Linux Distribution (bisher in Englisch)
+<a href="http://www.agnula.org/documentation/dp_tutorials/audacity/">Multitrack Aufnahmen in Audacity </a> - beutzt wird dabei die AGNULA Linux Distribution (bisher in Englisch) <img src="http://audacity.sourceforge.net/flags/en.gif">
 </p>
 <p>
-<a href="http://www.edhsonline.org/other/audacity/">Ein Audacity 1.2 Tutorial von Dan Eliot.</a>  Zeigt dir wie man existierende Musikstuecke öffnet, einfache Dinge ändert und das ganze wieder speichert, zum Teil mit Flash Animation (Englisch) !!</p>
+<a href="http://www.edhsonline.org/other/audacity/">Ein Audacity 1.2 Tutorial von Dan Eliot.</a>  Zeigt dir wie man existierende Musikstuecke öffnet, einfache Dinge ändert und das ganze wieder speichert, zum Teil mit Flash Animation (Englisch) <img src="http://audacity.sourceforge.net/flags/en.gif">!!</p>
 
 
 <?php BoxBottom(); ?>
