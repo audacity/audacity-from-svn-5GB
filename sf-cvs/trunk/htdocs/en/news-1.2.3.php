7849272311f827430e79a76c775c35f9353287e0
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-11-19 17:48:33 +0000

    Add info about Mac bug fix to release announcement.

--- sf-cvs/trunk/htdocs/en/news-1.2.3.php
+++ sf-cvs/trunk/htdocs/en/news-1.2.3.php
@@ -3,7 +3,9 @@
 <p>
 Audacity 1.2.3 is a new stable version of the free Audacity sound editor.
 This release fixes a bug that interfered with long recordings on some Windows
-systems.  It also includes several updated translations, and some other minor bug fixes and improvements.
+systems, and another bug that causes random crashes on Mac OS X.  It also
+includes several updated translations, and some other minor bug fixes and
+improvements.
 </p>
 
 <p>Download Audacity for:</p>
