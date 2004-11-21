12e9c43fc9cb405d432af28a043d449e301f2467
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-11-21 16:37:57 +0000

    Manual update.

--- sf-cvs/trunk/htdocs/nl/beta-1.2.0-pre3-problems.php
+++ sf-cvs/trunk/htdocs/nl/beta-1.2.0-pre3-problems.php
@@ -1,31 +1,23 @@
 <h2>Bekende Problemen</h2>
 
 <p>
-Het volgende zijn bekende problemen in Audacity 1.2.1:
+Het volgende zijn bekende problemen in Audacity 1.2.3:
 </p>
 
 <ul>
   <li><p>Audacity kan MIDI bstanden importeren en tonen, maar ze kunnen niet
     afgespeeld of bewerkt worden.
     <p></li>
-  <li><p>Het wijzig pitch effect kan crahsen als dit gebruikt wordt op een track 
-    met een smple rate van 96 KHz. </p></li>
-  <li><p>Alleen Windows: Audacity zal crashen bij gebruik van het Time Track
-    "Set Rate" commando.</p></li>
   <li><p>Linux:  Opname in full duplex (afspelen van bestaande tracks tijdens 
-    opnemen)kan op sommige Linux systemen de opnames verlangzaamd laten 
-    klinken. <p></li>
-  <li><p>MacOS X: Opstarten van Audacity zou de instellingen van sommige USB 
-    audioapparatuur foutief kunnen wijzigen.  Dit kan worden hersteld door met 
-    gebruik van Apple's "Audio MIDI Setup" programma de instellingen terug te 
-    zetten naar de normale waarden, terwijl Audacity nog actief is.</p></li>
-  <li><p>MacOS X: Audacity kan niet met bestanden of mappen werken
+    opnemen)kan op sommige Linux systemen mono opnames met storing veroorzaken.   
+    Om dit probleem te voorkomen , dien je Audacity in te stellen om in 
+    stereo op te nemen.
+    <p></li>
+   <li><p>MacOS X: Audacity kan niet met bestanden of mappen werken
     die geplaatst zijn in mappen met internationale characters
     (geaccentueerde markeringen e.d.) in de naam.  Bestanden met geaccentueerde
     characters werken wel en Audacity projecten met geaccentueerde
     characters werken ook. Het probleem ontstaat alleen bij bestanden in mappen 
     met geaccentueerde characters
     <p></li>
-  <li><p>Het Over Audacity venster wordt niet correct weergegeven in talen met 
-    niet-westerse karaktersets.</p></li>
-</ul>
+  </ul>
