d60d21938b1d5f7b11691bae96866c72a5ceace4
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-11-23 04:18:05 +0000

    Merging changes from live site.

--- sf-cvs/trunk/htdocs/latest/audacity-i386-rpm.php
+++ sf-cvs/trunk/htdocs/latest/audacity-i386-rpm.php
@@ -2,6 +2,12 @@
 
 # perform a redirect to the latest version, so links to it can remain static
 
+// logging
+$d = date("Y-m-d G:i:s");
+$fp = fopen("download_stats.txt", "a");
+fwrite($fp, "$d\ti386-rpm\n");
+fclose($fp);
+
 include 'mirror.php.inc';
 include 'versions.php.inc';
 header('Location: '.mirror.'audacity-'.i386rpm.'.i386.rpm');
