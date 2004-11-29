8436d449b1e95cc467cb91c4c2b2ccabff507d38
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-11-29 22:48:56 +0000

    Manual update.

--- sf-cvs/trunk/htdocs/no/tutorials.php
+++ sf-cvs/trunk/htdocs/no/tutorials.php
@@ -1,7 +1,16 @@
 <?php BoxTop($tutorialsStr); ?>
 
 <p>
-<a href="http://www.djcj.org/LAU/quicktoots/toots/audacity/">Et opplæringskurs for Audacity av Daniel James</a> - med medfølgende lydfiler som du kan blande sammen.
+<a href="http://www.edhsonline.org/other/audacity/">En veiledning for Audacity 1.2 av Dan Eliot.</a> Dekker det å åpne en eksisterende lydfil, gjøre enkel redigering og eksportere igjen. Kommer med Flash-animasjoner som detaljert følger deg gjennom alt!
+</p>
+
+<p>
+<a href="http://www.agnula.org/documentation/dp_tutorials/audacity/">Flersporsopptak i Audacity vha. Linux-distribusjonen AGNULA.</a>
+</p>
+
+<p>
+<a href="http://www.djcj.org/LAU/quicktoots/toots/audacity/">En veiledning for Audacity 1.0
+av Daniel James</a> - med medfølgende lydfiler som du kan mikse sammen.
 </p>
 
 <?php BoxBottom(); ?>
