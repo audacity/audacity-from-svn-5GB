2b338601e95125852d22573409c6f8a6726a02e2
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2004-12-05 23:13:29 +0000

    Manual update.

---
+++ sf-cvs/trunk/htdocs/hu/beta-1.2.0-pre3-problems.php
@@ -0,0 +1,21 @@
+<h2>Ismert Problémák</h2>
+
+<p>
+Az alábbiak az Audacity 1.2.3 verzió ismert hibái:
+</p>
+
+<ul>
+  <li><p>
+  Az Audacity képes MIDI fájlok importálására és kijelzésére, de nem tudja azokat lejátszani vagy szerkeszteni.
+    </p></li>
+  <li><p>
+  Linux: A full duplex felvételek (létezõ sávok lejátszása egy újabb felvétele közben) esetén
+  néhány rendszeren a monó felvételek torzítva kerülnek felvételre. A problémát ki lehet küszöbölni, ha
+  eleve sztereóra van állítva a felvétel formátuma.
+    </p></li>
+  <li><p>
+  MacOS X: Az Audacity nem tudja kezelni azokat	a mappákat vagy fájlokat, amelyek olyan mappában találhatóak, melynek neve 
+  nemzetközi (ékezetes) karaktereket tartalmaz. Azaz: az ékezetes nevû fájlokat (és projekteket) tudja olvasni, 
+  csak azokat a fájlokat nem, melyek olyan mappában találhatóak, melynek a nevében bármilyen nemzetközi (ékezetes) karakter található.
+    </p></li>
+</ul>
