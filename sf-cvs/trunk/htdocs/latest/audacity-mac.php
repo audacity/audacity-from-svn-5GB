d47847548747e40d58e19380483b40952d54dc23
mbrubeck <mbrubeck@47890b92-0858-11df-a26f-8b716316a5bc>
2005-02-09 04:11:35 +0000

    Remove logging code.

--- sf-cvs/trunk/htdocs/latest/audacity-mac.php
+++ sf-cvs/trunk/htdocs/latest/audacity-mac.php
@@ -2,12 +2,6 @@
 
 # perform a redirect to the latest version, so links to it can remain static
 
-// logging
-$d = date("Y-m-d G:i:s");
-$fp = fopen("download_stats.txt", "a");
-fwrite($fp, "$d\tmacos9\n");
-fclose($fp);
-
 include 'mirror.inc.php';
 include 'versions.inc.php';
 header('Location: '.download_url('audacity-mac-'.mac_classic_version.'.sit'));
