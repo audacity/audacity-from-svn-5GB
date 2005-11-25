4e9c055f76aedef6755eca5fd985702e13095f5d
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2005-11-25 04:10:35 +0000

    Added descriptions on last page

--- sf-cvs/trunk/htdocs/download/features-1.3-g.php
+++ sf-cvs/trunk/htdocs/download/features-1.3-g.php
@@ -13,19 +13,33 @@
 
 <h2><?=$pageTitle?></h2>
 
-<p><?=_("Better performance with large projects")?></p>
+<p><b><?=_("Better performance with large projects")?></b></p>
 
-<p><?=_("Project integrity check on open")?></p>
+<p><?=_("As before, Audacity stores projects using a directory full of small files.  Audacity 1.3 now uses a hierarchical directory structure that results in better performance on large projects (tens of hours worth of audio across many tracks).")?></p>
 
-<p><?=_("Transcription toolbar")?></p>
+<p><b><?=_("Project integrity check on open")?></b></p>
 
-<p><?=_("Upload (?)")?></p>
+<p><b><?=_("When you open an Audacity project in version 1.3, it checks to make sure that all data files are present.")?></b></p>
 
-<p><?=_("Batch")?></p>
+<p><b><?=_("Transcription toolbar")?></b></p>
 
-<p><?=_("Cut lines")?></p>
+<p><?=_("A new toolbar for creating transcriptions of speech in Label Tracks.  Includes features that enable you to change the playback speed, create labels automatically based on onsets, and more.  To enable this feature, go to the Interface tab of the Preferences.")?></p>
 
-<p><?=_("CleanSpeech")?></p>
+<p><b><?=_("Upload via FTP")?></b></p>
+
+<p><?=_("Audacity 1.3 has an experimental new dialog for uploading files to a server via FTP.  Currently it is just a simple FTP client; in the future we will integrate this feature into Audacity more so you can publish a project directly to a site as a Podcast, for example.")?></p>
+
+<p><b><?=_("Based on wxWidgets 2.6")?></b></p>
+
+<p><?=_("Previous versions of Audacity were based on wxWidgets 2.4.  This change brings support for GTK2 widgets on Unix and better Unicode support on all platforms, plus many other minor enhancements under the hood.")?></p>
+
+<p><b><?=_("Batch / CleanSpeech")?></b></p>
+
+<p><?=_("Audacity has a new feature that allows you to process a bunch of files, for example normalizing and converting to MP3.  The CleanSpeech mode provides a simplified interface for some standard adjustments typically made on speech recordings.  You can access these features in the Batch tab of the Preferences dialog.")?></p>
+
+<p><b><?=_("Cut lines")?></b></p>
+
+<p><?=_("When this feature is enabled (in the Interface tab of the Preferences), cutting audio in the middle of a clip does not remove it permanently - at any time in the future, just click on the cut line to restore it.")?></p>
 
 <style type="text/css">
 .navbox {
