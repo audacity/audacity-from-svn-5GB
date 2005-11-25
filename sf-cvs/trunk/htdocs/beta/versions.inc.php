6774b5e23016065e35fa8f98fa0add77910990c4
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2005-11-25 06:14:39 +0000

    Adding beta versions

---
+++ sf-cvs/trunk/htdocs/beta/versions.inc.php
@@ -0,0 +1,20 @@
+<?php
+// Note: Sizes are in MB.
+define('macosx_version', '1.3.0');
+define('macosx_size',    3.4);
+
+define('win_exe_version', '1.3.0');
+define('win_exe_size',    2.5);
+
+define('win_zip_version', '1.3.0');
+define('win_zip_size',    2.5);
+
+define('src_version', '1.3.0');
+define('src_size',    5.0);
+define('src_suffix',  '.tar.gz');
+
+$win_exe_url = "audacity-win/audacity-win-".win_exe_version.".exe";
+$win_zip_url = "audacity-win-zip/audacity-win-".win_zip_version.".zip";
+$src_url = "audacity-src/audacity-src-".src_version.src_suffix;
+$macosx_url = "audacity-macosx/audacity-macosx-".macosx_version.".dmg";
+?>
