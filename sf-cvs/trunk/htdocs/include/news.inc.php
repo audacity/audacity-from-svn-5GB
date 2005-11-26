f5678b7dfcfba7e74c294048db7d166260f00be6
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2005-11-26 08:06:20 +0000

    Mention Audacity Forum

--- sf-cvs/trunk/htdocs/include/news.inc.php
+++ sf-cvs/trunk/htdocs/include/news.inc.php
@@ -40,7 +40,10 @@ add_news_item(
 	"1.2.4-release",
   _("Audacity 1.2.4 and 1.3.0 Released"),
   _('<p>Audacity 1.2.4 is a new stable version of Audacity.  It includes a couple of bug fixes and minor improvements and is recommended for all users.  Audacity 1.3.0 is a beta release that contains hundreds of <a href="/download/features-1.3-a">new features</a>, but this version is unfinished and unstable, and is recommended primarily for advanced users.  You can install both Audacity 1.2 and 1.3 simultaneously.</p>
-<p>See the <a href="/download/release-notes">1.2.4 Release Notes</a> for a complete list of changes and known problems in Audacity 1.2.4, or see <a href="/download/features-1.3-a">New Features in 1.3</a> for information about the new beta version.</p>')
+<p>See the <a href="/download/release-notes">1.2.4 Release Notes</a> for a complete list of changes and known problems in Audacity 1.2.4, or see <a href="/download/features-1.3-a">New Features in 1.3</a> for information about the new beta version.</p>
+<p>Finally, the best way to get help with Audacity is now the new
+<a href="http://audacityteam.org/forum/">Audacity Forum</a>.
+</p>')
 );
 
 add_news_item(
