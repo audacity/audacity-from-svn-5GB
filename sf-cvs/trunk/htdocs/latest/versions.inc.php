1067cdd2c2ad0fee99c5ab1e9f0ffc9abe068e44
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2005-12-02 08:17:04 +0000

    Updated to version 1.2.4b and fixed file sizes

--- sf-cvs/trunk/htdocs/latest/versions.inc.php
+++ sf-cvs/trunk/htdocs/latest/versions.inc.php
@@ -5,16 +5,16 @@ define('stable_version', '1.2.4');
 // Note: Sizes are in MB.
 define('mac_classic_version', '1.0.0');
 define('mac_classic_size',    1.5);
-define('macosx_version', '1.2.4');
-define('macosx_size',    3.2);
+define('macosx_version', '1.2.4b');
+define('macosx_size',    3.4);
 
 define('win_exe_version', '1.2.4');
-define('win_exe_size',    2.4);
-define('win_zip_version', '1.2.4');
-define('win_zip_size',    2.7);
+define('win_exe_size',    2.5);
+define('win_zip_version', '1.2.4b');
+define('win_zip_size',    3.2);
 
-define('src_version', '1.2.4');
-define('src_size',    4.0);
+define('src_version', '1.2.4b');
+define('src_size',    4.3);
 define('src_suffix',  '.tar.gz');
 
 $win_exe_url = "audacity-win/audacity-win-".win_exe_version.".exe";
