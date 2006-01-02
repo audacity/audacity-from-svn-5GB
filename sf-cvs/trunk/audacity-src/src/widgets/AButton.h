c5913699b4cf4f92d3740acdb047309161c4e945
llucius <llucius@47890b92-0858-11df-a26f-8b716316a5bc>
2006-01-02 02:31:11 +0000

    Adds keyboard navigation and accessibility

--- sf-cvs/trunk/audacity-src/src/widgets/AButton.h
+++ sf-cvs/trunk/audacity-src/src/widgets/AButton.h
@@ -18,15 +18,25 @@
 #ifndef __AUDACITY_BUTTON__
 #define __AUDACITY_BUTTON__
 
+#if wxUSE_ACCESSIBILITY
+#if defined(__WXMSW__)
+#include <oleacc.h>
+#endif
+#include <wx/access.h>
+#endif
+
 #include <wx/window.h>
 
 class wxBitmap;
 class wxImage;
 
-class AButton:public wxWindow {
+class AButton: public wxWindow {
+   friend class AButtonAx;
+
  public:
 
-   AButton(wxWindow * parent, wxWindowID id,
+   AButton(wxWindow * parent,
+           wxWindowID id,
            const wxPoint & pos,
            const wxSize & size,
            wxImage *up,
@@ -35,12 +45,15 @@ class AButton:public wxWindow {
            wxImage *dis,
            bool processdownevents);
 
-   AButton(wxWindow * parent, wxWindowID id,
+   AButton(wxWindow * parent,
+           wxWindowID id,
            const wxPoint & pos,
            const wxSize & size,
-           char **upXPM, char **overXPM, char **downXPM, char **disXPM,
-           bool processdownevents           
-           );
+           char **upXPM,
+           char **overXPM,
+           char **downXPM,
+           char **disXPM,
+           bool processdownevents);
 
    virtual ~ AButton();
 
@@ -50,7 +63,9 @@ class AButton:public wxWindow {
                                    wxImage *dis);
 
    virtual void SetAlternate(bool useAlternateImages);
+   virtual void SetFocusRect(wxRect & r);
 
+   virtual bool IsEnabled() const { return mEnabled; }
    virtual void Disable();
    virtual void Enable();
    void SetEnabled(bool state) {
@@ -60,7 +75,9 @@ class AButton:public wxWindow {
    virtual void PushDown();
    virtual void PopUp();
 
+   virtual void OnErase(wxEraseEvent & event);
    virtual void OnPaint(wxPaintEvent & event);
+   virtual void OnSize(wxSizeEvent & event);
    virtual void OnMouseEvent(wxMouseEvent & event);
 
    virtual bool WasShiftDown(); // returns true if shift was held down
@@ -68,8 +85,7 @@ class AButton:public wxWindow {
    bool IsDown(){ return mButtonIsDown;}
    void SetButtonToggles( bool toggler ){ mProcessDownEvents = toggler;}
    void Toggle(){ mButtonIsDown ? PopUp() : PushDown();}
-
- private:
+   void Click();
 
    enum AButtonState {
       AButtonUp,
@@ -78,6 +94,25 @@ class AButton:public wxWindow {
       AButtonDis
    };
 
+   AButtonState GetState(){ return mButtonState; }
+
+   virtual void OnKeyDown(wxKeyEvent & event);
+   virtual void OnKeyUp(wxKeyEvent & event);
+   virtual void OnSetFocus(wxFocusEvent & event);
+   virtual void OnKillFocus(wxFocusEvent & event);
+
+ private:
+
+   void Init(wxWindow * parent,
+             wxWindowID id,
+             const wxPoint & pos,
+             const wxSize & size,
+             wxBitmap *up,
+             wxBitmap *over,
+             wxBitmap *down,
+             wxBitmap *dis,
+             bool processdownevents);
+
    int mWidth;
    int mHeight;
 
@@ -85,6 +120,7 @@ class AButton:public wxWindow {
 
    bool mWasShiftDown;
 
+   bool mButtonIsFocused;
    bool mButtonIsDown;
    bool mIsClicking;
    bool mEnabled;
@@ -101,10 +137,89 @@ class AButton:public wxWindow {
    wxBitmap *mAltBitmap[4];
    wxWindow *mParent;
 
- public:
+   wxRect mFocusRect;
+
+public:
 
     DECLARE_EVENT_TABLE()
 };
+
+#if wxUSE_ACCESSIBILITY
+
+class AButtonAx: public wxWindowAccessible
+{
+public:
+   AButtonAx(wxWindow * window);
+
+   virtual ~ AButtonAx();
+
+   // Performs the default action. childId is 0 (the action for this object)
+   // or > 0 (the action for a child).
+   // Return wxACC_NOT_SUPPORTED if there is no default action for this
+   // window (e.g. an edit control).
+   virtual wxAccStatus DoDefaultAction(int childId);
+
+   // Retrieves the address of an IDispatch interface for the specified child.
+   // All objects must support this property.
+   virtual wxAccStatus GetChild( int childId, wxAccessible** child );
+
+   // Gets the number of children.
+   virtual wxAccStatus GetChildCount(int* childCount);
+
+   // Gets the default action for this object (0) or > 0 (the action for a child).
+   // Return wxACC_OK even if there is no action. actionName is the action, or the empty
+   // string if there is no action.
+   // The retrieved string describes the action that is performed on an object,
+   // not what the object does as a result. For example, a toolbar button that prints
+   // a document has a default action of "Press" rather than "Prints the current document."
+   virtual wxAccStatus GetDefaultAction( int childId, wxString *actionName );
+
+   // Returns the description for this object or a child.
+   virtual wxAccStatus GetDescription( int childId, wxString *description );
+
+   // Gets the window with the keyboard focus.
+   // If childId is 0 and child is NULL, no object in
+   // this subhierarchy has the focus.
+   // If this object has the focus, child should be 'this'.
+   virtual wxAccStatus GetFocus( int *childId, wxAccessible **child );
+
+   // Returns help text for this object or a child, similar to tooltip text.
+   virtual wxAccStatus GetHelpText( int childId, wxString *helpText );
+
+   // Returns the keyboard shortcut for this object or child.
+   // Return e.g. ALT+K
+   virtual wxAccStatus GetKeyboardShortcut( int childId, wxString *shortcut );
+
+   // Returns the rectangle for this object (id = 0) or a child element (id > 0).
+   // rect is in screen coordinates.
+   virtual wxAccStatus GetLocation( wxRect& rect, int elementId );
+
+   // Gets the name of the specified object.
+   virtual wxAccStatus GetName( int childId, wxString *name );
+
+   // Returns a role constant.
+   virtual wxAccStatus GetRole( int childId, wxAccRole *role );
+
+   // Gets a variant representing the selected children
+   // of this object.
+   // Acceptable values:
+   // - a null variant (IsNull() returns TRUE)
+   // - a list variant (GetType() == wxT("list"))
+   // - an integer representing the selected child element,
+   //   or 0 if this object is selected (GetType() == wxT("long"))
+   // - a "void*" pointer to a wxAccessible child object
+   virtual wxAccStatus GetSelections( wxVariant *selections );
+
+   // Returns a state constant.
+   virtual wxAccStatus GetState(int childId, long* state);
+
+   // Returns a localized string representing the value for the object
+   // or child.
+   virtual wxAccStatus GetValue(int childId, wxString* strValue);
+
+};
+
+#endif // wxUSE_ACCESSIBILITY
 
 #endif
 
