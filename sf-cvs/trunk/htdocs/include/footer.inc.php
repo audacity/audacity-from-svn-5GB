bc5c2593a4a28d67bd093b892cac11b2ebda741f
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2006-03-21 06:11:12 +0000

    Added Google Analytics tracking

--- sf-cvs/trunk/htdocs/include/footer.inc.php
+++ sf-cvs/trunk/htdocs/include/footer.inc.php
@@ -54,4 +54,12 @@
   </License>
 </rdf:RDF>
 -->
+
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-230676-1";
+urchinTracker();
+</script>
+
 </body></html>
