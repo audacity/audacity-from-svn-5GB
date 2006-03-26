45dfff7347bf876e1b26dd3d37b54be748fbaf9b
richardash1981 <richardash1981@47890b92-0858-11df-a26f-8b716316a5bc>
2006-03-26 20:07:50 +0000

    Updating libflac to version 1.1.2 (gentoo 1.1.2-r3)

--- sf-cvs/branches/AUDACITY_1_2/lib-src/libflac/build/exe.mk
+++ sf-cvs/branches/AUDACITY_1_2/lib-src/libflac/build/exe.mk
@@ -1,23 +1,25 @@
 #  FLAC - Free Lossless Audio Codec
-#  Copyright (C) 2001,2002  Josh Coalson
+#  Copyright (C) 2001,2002,2003,2004,2005  Josh Coalson
 #
-#  This program is part of FLAC; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
+#  This file is part the FLAC project.  FLAC is comprised of several
+#  components distributed under difference licenses.  The codec libraries
+#  are distributed under Xiph.Org's BSD-like license (see the file
+#  COPYING.Xiph in this distribution).  All other programs, libraries, and
+#  plugins are distributed under the GPL (see COPYING.GPL).  The documentation
+#  is distributed under the Gnu FDL (see COPYING.FDL).  Each file in the
+#  FLAC distribution contains at the top the terms under which it may be
+#  distributed.
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#  Since this particular file is relevant to all components of FLAC,
+#  it may be distributed under the Xiph.Org license, which is the least
+#  restrictive of those mentioned above.  See the file COPYING.Xiph in this
+#  distribution.
 
 #
 # GNU makefile fragment for building an executable
 #
+
+include $(topdir)/build/config.mk
 
 ifeq ($(DARWIN_BUILD),yes)
 CC          = cc
@@ -27,48 +29,67 @@ CC          = gcc
 CCC         = g++
 endif
 NASM        = nasm
-# LINKAGE can be forced to -static or -dynamic from invocation if desired, but it defaults to -static except on OSX
-ifeq ($(DARWIN_BUILD),yes)
-LINKAGE     = -dynamic
-else
-LINKAGE     = -static
-endif
 LINK        = $(CC) $(LINKAGE)
-BINPATH     = ../../obj/bin
-LIBPATH     = ../../obj/lib
-PROGRAM     = $(BINPATH)/$(PROGRAM_NAME)
+OBJPATH     = $(topdir)/obj
+BINPATH     = $(OBJPATH)/$(BUILD)/bin
+LIBPATH     = $(OBJPATH)/$(BUILD)/lib
+DEBUG_BINPATH   = $(OBJPATH)/debug/bin
+DEBUG_LIBPATH   = $(OBJPATH)/debug/lib
+RELEASE_BINPATH = $(OBJPATH)/release/bin
+RELEASE_LIBPATH = $(OBJPATH)/release/lib
+PROGRAM         = $(BINPATH)/$(PROGRAM_NAME)
+DEBUG_PROGRAM   = $(DEBUG_BINPATH)/$(PROGRAM_NAME)
+RELEASE_PROGRAM = $(RELEASE_BINPATH)/$(PROGRAM_NAME)
 
-all : release
-
-include ../../build/config.mk
-
-debug   : CFLAGS = -g -O0 -DDEBUG $(DEBUG_CFLAGS) -Wall -W -DVERSION=$(VERSION) $(DEFINES) $(INCLUDES)
-release : CFLAGS = -O3 -fomit-frame-pointer -funroll-loops -finline-functions -DNDEBUG $(RELEASE_CFLAGS) -Wall -W -Winline -DFLaC__INLINE=__inline__ -DVERSION=$(VERSION) $(DEFINES) $(INCLUDES)
+debug   : CFLAGS = -g -O0 -DDEBUG $(CONFIG_CFLAGS) $(DEBUG_CFLAGS) -Wall -W -DVERSION=$(VERSION) $(DEFINES) $(INCLUDES)
+valgrind: CFLAGS = -g -O0 -DDEBUG $(CONFIG_CFLAGS) $(DEBUG_CFLAGS) -DFLAC__VALGRIND_TESTING -Wall -W -DVERSION=$(VERSION) $(DEFINES) $(INCLUDES)
+release : CFLAGS = -O3 -fomit-frame-pointer -funroll-loops -finline-functions -DNDEBUG $(CONFIG_CFLAGS) $(RELEASE_CFLAGS) -Wall -W -Winline -DFLaC__INLINE=__inline__ -DVERSION=$(VERSION) $(DEFINES) $(INCLUDES)
 
 LFLAGS  = -L$(LIBPATH)
 
-debug   : $(ORDINALS_H) $(PROGRAM)
-release : $(ORDINALS_H) $(PROGRAM)
+DEBUG_OBJS = $(SRCS_C:%.c=%.debug.o) $(SRCS_CC:%.cc=%.debug.o) $(SRCS_CPP:%.cpp=%.debug.o) $(SRCS_NASM:%.nasm=%.debug.o)
+RELEASE_OBJS = $(SRCS_C:%.c=%.release.o) $(SRCS_CC:%.cc=%.release.o) $(SRCS_CPP:%.cpp=%.release.o) $(SRCS_NASM:%.nasm=%.release.o)
 
-$(PROGRAM) : $(OBJS)
-	$(LINK) -o $@ $(OBJS) $(LFLAGS) $(LIBS)
+debug   : $(DEBUG_PROGRAM)
+valgrind: $(DEBUG_PROGRAM)
+release : $(RELEASE_PROGRAM)
 
-%.o : %.c
+# by default on OS X we link with static libs as much as possible
+
+$(DEBUG_PROGRAM) : $(DEBUG_OBJS)
+ifeq ($(DARWIN_BUILD),yes)
+	$(LINK) -o $@ $(DEBUG_OBJS) $(EXPLICIT_LIBS)
+else
+	$(LINK) -o $@ $(DEBUG_OBJS) $(LFLAGS) $(LIBS)
+endif
+
+$(RELEASE_PROGRAM) : $(RELEASE_OBJS)
+ifeq ($(DARWIN_BUILD),yes)
+	$(LINK) -o $@ $(RELEASE_OBJS) $(EXPLICIT_LIBS)
+else
+	$(LINK) -o $@ $(RELEASE_OBJS) $(LFLAGS) $(LIBS)
+endif
+
+%.debug.o %.release.o : %.c
 	$(CC) $(CFLAGS) -c $< -o $@
-%.o : %.cc
+%.debug.o %.release.o : %.cc
 	$(CCC) $(CFLAGS) -c $< -o $@
-%.i : %.c
+%.debug.o %.release.o : %.cpp
+	$(CCC) $(CFLAGS) -c $< -o $@
+%.debug.i %.release.i : %.c
 	$(CC) $(CFLAGS) -E $< -o $@
-%.i : %.cc
+%.debug.i %.release.i : %.cc
+	$(CCC) $(CFLAGS) -E $< -o $@
+%.debug.i %.release.i : %.cpp
 	$(CCC) $(CFLAGS) -E $< -o $@
 
-%.o : %.nasm
+%.debug.o %.release.o : %.nasm
 	$(NASM) -f elf -d OBJ_FORMAT_elf -i ia32/ $< -o $@
 
 .PHONY : clean
 clean :
-	-rm -f $(OBJS) $(PROGRAM)
+	-rm -f $(DEBUG_OBJS) $(RELEASE_OBJS) $(OBJPATH)/*/bin/$(PROGRAM_NAME)
 
 .PHONY : depend
 depend:
-	makedepend -fMakefile.lite -- $(CFLAGS) $(INCLUDES) -- *.c *.cc
+	makedepend -fMakefile.lite -- $(CFLAGS) $(INCLUDES) -- *.c *.cc *.cpp
