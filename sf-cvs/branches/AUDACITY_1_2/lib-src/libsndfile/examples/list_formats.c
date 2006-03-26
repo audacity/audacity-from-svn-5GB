07146745a90b7f268c79a245c651939628c018b2
richardash1981 <richardash1981@47890b92-0858-11df-a26f-8b716316a5bc>
2006-03-26 14:40:07 +0000

    Update libsndfile to 1.0.15 with Gentoo patch for ac-args

--- sf-cvs/branches/AUDACITY_1_2/lib-src/libsndfile/examples/list_formats.c
+++ sf-cvs/branches/AUDACITY_1_2/lib-src/libsndfile/examples/list_formats.c
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 2001-2004 Erik de Castro Lopo <erikd@mega-nerd.com>
+** Copyright (C) 2001-2005 Erik de Castro Lopo <erikd@mega-nerd.com>
 **
 ** This program is free software; you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
@@ -30,6 +30,7 @@ main (void)
 	char buffer [128] ;
 	int format, major_count, subtype_count, m, s ;
 
+	memset (&sfinfo, 0, sizeof (sfinfo)) ;
 	buffer [0] = 0 ;
 	sf_command (NULL, SFC_GET_LIB_VERSION, buffer, sizeof (buffer)) ;
 	if (strlen (buffer) < 1)
