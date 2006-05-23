70b54268cbc472142196e0c7951188a36e43d4cf
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2006-05-23 17:12:04 +0000

    Fixed wxString logging error

--- sf-cvs/trunk/audacity-src/src/TrackPanel.cpp
+++ sf-cvs/trunk/audacity-src/src/TrackPanel.cpp
@@ -4254,7 +4254,7 @@ void TrackPanel::OnPrevTrack( bool shift )
          else
          {
             EnsureVisible( t );
-            wxLogDebug(wxT("On last track, %s"),t->GetName());
+            wxLogDebug(wxT("On last track, %s"),t->GetName().c_str());
             return;
          }
       }
@@ -4350,7 +4350,7 @@ void TrackPanel::OnNextTrack( bool shift )
          else
          {
             EnsureVisible( t );
-            wxLogDebug(wxT("On last track, %s"),t->GetName());
+            wxLogDebug(wxT("On last track, %s"),t->GetName().c_str());
             return;
          }
       }
