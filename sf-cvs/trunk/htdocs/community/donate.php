3c21d105a27a2248f5af68dddae4c6a540d0d14c
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2006-08-01 19:30:31 +0000

    New section on why we're raising money. Link to Audacity Store.

--- sf-cvs/trunk/htdocs/community/donate.php
+++ sf-cvs/trunk/htdocs/community/donate.php
@@ -2,22 +2,64 @@
 /*
  * Copyright 2003 Dominic Mazzoni
  * Copyright 2004 Matt Brubeck
+ * Copyright 2006 Vaughan Johnson
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/2.0/
  */
   require_once "main.inc.php";
   $pageId = "donate";
-  $pageTitle = _("Make a Donation");
+  $pageTitle = _("Why does the Audacity Team want to raise money?");
   include "../include/header.inc.php";
 ?>
 
 <h2><?=$pageTitle?></h2>
 
-<p><?=_('Audacity is developed by volunteers, working in our spare time. It is not shareware, and we do not expect to make any money from it.  However, by donating money you can help us pay for bandwidth and web hosting, development tools, and audio hardware&mdash;things that help us provide Audacity for free and continue to make it better.')?></p>
+<p>
+  <?=_('The Audacity Team has started exploring new means of bringing money to the project. 
+  These include ads at various locations on our websites, in addition to expanded sales 
+  of associated products and the donations program.')?>
+</p>
 
-<p><?=_('For a special gift if you donate US$20 or more, see our <a href="http://audacityteam.org/tshirt.php">Audacity T-shirt offer</a>.')?></p>
+<p>
+  <?=_('This income is not distributed among previous contributors to Audacity. 
+  There are too many people and it would be difficult to do so fairly. 
+  Also, nobody contributed code to Audacity expecting money in return. 
+  Audacity is developed by volunteers, working in our spare time. 
+  Audacity is not shareware, and we do not expect to make any money from it.')?>
+</p>
 
-<p><?=_('We currently accept money via the Amazon Honor System. Use your credit card and make a payment as small as US$1.00, or up to US$50.00:')?></p>
+<p>
+  <?=_('Rather, the team of Audacity lead developers will spend the money on items and 
+  services that benefit the project as a whole.  
+  Money that comes in to the project helps us pay for bandwidth and web hosting, 
+  development tools, and audio hardware.
+  In the past we used funds from donations to purchase the Audacity trademark and to 
+  subsidize travel costs for Audacity developers to meet for a hackathon, and we plan to 
+  do more of these.
+  Our long-term goal is to bring in enough money to hire a full-time developer who could 
+  devote all of his/her time to the project and take over some of the administrative, 
+  maintenance, and release tasks, freeing volunteers to spend more time on code.')?>
+</p>
+
+<p>
+  <?=_('These are all things that help us provide Audacity for free and continue to make it better.
+  The Audacity Team is completely committed to keeping Audacity free and open source. 
+  We made that commitment in 
+  <a href="http://audacity.sourceforge.net/about/license">
+      licensing Audacity under the GNU General Public License</a>.')?>
+</p>
+
+<p>
+  <?=_('There are many ways to support Audacity, and many degrees of doing so. 
+  We have always welcomed users whose only contribution is simply using Audacity, 
+  giving us feedback on how to improve it, and telling others about it. 
+  Thank you for supporting Audacity.')?>
+</p>
+
+<h2><?= ('Make a Donation')?></h2>
+<p>
+  <?=_('We currently accept donations via the Amazon Honor System. Use your credit card and make a donation as small as US$1.00, or up to US$50.00:')?>
+</p>
 
 <!-- Begin Amazon Honor System Paybox -->
 <p class="logo"><img src="http://s1.amazon.com/exec/varzea/tipbox/A2NTHQTXQE4A60/TVWQG2UJNWCBH" usemap="#amazon" alt="Amazon Honor System" width="150" height="150"></p>
@@ -27,7 +69,7 @@
 </map>
 <!-- End Amazon Honor System Paybox -->
 
-<p><?=_('We also accept PayPal payments through the SourceForge donation system:')?></p>
+<p><?=_('We also accept PayPal donations through the SourceForge donation system:')?></p>
 
 <p class="logo">
   <a title="<?=_('Donate via PayPal')?>" href="http://sourceforge.net/donate/index.php?group_id=6235"><img alt="<?=_('Donate via PayPal')?>" src="../images/paypal.gif"></a>
@@ -35,6 +77,19 @@
 
 <p><?=_('If you prefer to send a personal check or another form of payment, please visit the <a href="../about/credits">credits</a> page and contact one of the lead developers.')?></p>
 
+
+<h2>
+  <?= ('Purchase from the <a href="http://audacitystore.com/">Audacity Store</a>')?>
+</h2>
+<p>
+  <?=_('You're invited to try out the new <a href="http://audacitystore.com/">Audacity Store</a>, which features  
+  Audacity-logo items (T-shirts, embroidered polo shirts, embroidered messenger bags), and consumer electronics.')?>
+</p>
+
+
+<h2>
+  <?= ('Sponsor Audacity Development')?>
+</h2>
 <p><?=_('We would also like to hear from any companies or groups interested in <a href="http://audacityteam.org/sponsor.php">sponsoring Audacity development</a>.')?></p>
 
 <?php
