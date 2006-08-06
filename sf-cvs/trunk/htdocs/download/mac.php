c393f54d97427c6583adf887fddfd86f80b56632
richardash1981 <richardash1981@47890b92-0858-11df-a26f-8b716316a5bc>
2006-08-06 20:54:14 +0000

    Added note about Intel macs and not runnning the PPC binary

--- sf-cvs/trunk/htdocs/download/mac.php
+++ sf-cvs/trunk/htdocs/download/mac.php
@@ -1,6 +1,7 @@
 <?php
 /*
  * Copyright 2004 Matt Brubeck
+ * Richard Ash 2006
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/2.0/
  */
@@ -18,6 +19,10 @@
 <ul>
   <li><p><?php printf(_('For Mac OS X: <a href="%s">Audacity %s</a> (.dmg file, %.1lf MB) - The latest version of the free Audacity audio editor.'), "../latest/".$macosx_url, macosx_version, macosx_size)?></p></li>
   <!-- TODO: Installation instructions? -->
+  <li><p><?php echo _('The above version of Audacity is not suitable for 
+  Intel-based Macs due to issues with Rosetta. Pre-release Native Intel
+  binaries can be found at ');?><a href="http://audacityteam.org/mac/">
+  http://audacityteam.org/mac/</a>.</p></li>
 
   <li><p><?php printf(_('For Mac OS 9: <a href="%s">Audacity %s</a> (.sit file, %.1lf MB)'), "../latest/".$mac_classic_url, mac_classic_version, mac_classic_size)?></p></li>
   <p><?=_("(Unfortunately, we no longer have the resources to provide new versions of Audacity for Mac OS 9.)")?></p></li>
