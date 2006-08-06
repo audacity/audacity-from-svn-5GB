6fb1f52576acd13e968e87142ca5a0ee63736463
richardash1981 <richardash1981@47890b92-0858-11df-a26f-8b716316a5bc>
2006-08-06 21:01:44 +0000

    Create boilerplate for recommnded downloads, and add it to the mac page

---
+++ sf-cvs/trunk/htdocs/download/recommended.inc.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Copyright 2006 Richard Ash
+ * This file is licensed under a Creative Commons license:
+ * http://creativecommons.org/licenses/by/2.0/
+ */
+
+/* this is the boilderplate text for the recommended download */
+?><p><?php echo _('This is a Sourceforge.net download page and you will be
+asked before downloading to select a mirror. A "mirror" is one of a
+group of internet addresses hosting an identical download. If
+possible, simply select the one closest to your actual location. When
+downloading please disable any download managers you may have
+and left click on the link to start your Audacity download');?></p>
