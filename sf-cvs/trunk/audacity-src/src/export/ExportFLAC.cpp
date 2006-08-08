1139bc3121ee4e5bfd80e4881164e127fed3163d
llucius <llucius@47890b92-0858-11df-a26f-8b716316a5bc>
2006-08-08 06:02:48 +0000

    Migrated to common progress dialog handling and removed tabs from source

--- sf-cvs/trunk/audacity-src/src/export/ExportFLAC.cpp
+++ sf-cvs/trunk/audacity-src/src/export/ExportFLAC.cpp
@@ -1,20 +1,20 @@
 /**********************************************************************
 
-  Audacity: A Digital Audio Editor
+Audacity: A Digital Audio Editor
 
-  ExportFLAC.cpp
+ExportFLAC.cpp
 
-  Frederik M.J.V
+Frederik M.J.V
 
-  This program is distributed under the GNU General Public License, version 2.
-  A copy of this license is included with this source.
+This program is distributed under the GNU General Public License, version 2.
+A copy of this license is included with this source.
 
-  Based on ExportOGG.cpp by:
-  Joshua Haberman
+Based on ExportOGG.cpp by:
+Joshua Haberman
 
-  Portions from vorbis-tools, copyright 2000-2002 Michael Smith
-  <msmith@labyrinth.net.au>; Vorbize, Kenneth Arnold <kcarnold@yahoo.com>;
-  and libvorbis examples, Monty <monty@xiph.org>
+Portions from vorbis-tools, copyright 2000-2002 Michael Smith
+<msmith@labyrinth.net.au>; Vorbize, Kenneth Arnold <kcarnold@yahoo.com>;
+and libvorbis examples, Monty <monty@xiph.org>
 
 **********************************************************************/
 
@@ -41,20 +41,20 @@
 #define SAMPLES_PER_RUN 8192
 
 bool ExportFLAC(AudacityProject *project,
-               int numChannels, wxString fName,
-               bool selectionOnly, double t0, double t1, MixerSpec *mixerSpec)
+                int numChannels, wxString fName,
+                bool selectionOnly, double t0, double t1, MixerSpec *mixerSpec)
 {
    double    rate    = project->GetRate();
    wxWindow  *parent = project;
    TrackList *tracks = project->GetTracks();
-
+   
    wxLogNull logNo;            // temporarily disable wxWindows error messages 
    int       eos = 0;
    bool      cancelling = false;
    
    wxString bitDepthPref =
       gPrefs->Read(wxT("/FileFormats/FLACBitDepth"), wxT("16"));
-
+   
    FLAC::Encoder::File *encoder= new FLAC::Encoder::File();
    encoder->set_filename(fName.mb_str());
    encoder->set_channels(numChannels);
@@ -68,12 +68,7 @@ bool ExportFLAC(AudacityProject *project,
    	encoder->set_bits_per_sample(16);
    }
    encoder->init();
-
-   wxProgressDialog *progress = NULL;
-
-   wxYield();
-   wxStartTimer();
-
+   
    int numWaveTracks;
    WaveTrack **waveTracks;
    tracks->GetWaveTracks(selectionOnly, &numWaveTracks, &waveTracks);
@@ -82,59 +77,50 @@ bool ExportFLAC(AudacityProject *project,
                             t0, t1,
                             numChannels, SAMPLES_PER_RUN, false,
                             rate, format, true, mixerSpec);
-
+   
    int i,j;
    i=j=0;
    FLAC__int32 **tmpsmplbuf = new FLAC__int32*[numChannels];
    for(i=0;i<numChannels;i++)
    	tmpsmplbuf[i]=(FLAC__int32*)calloc(SAMPLES_PER_RUN,sizeof(FLAC__int32));
+
+   GetActiveProject()->ProgressShow(_("Export"),
+                                    selectionOnly ?
+                                    _("Exporting the selected audio as FLAC") :
+                                    _("Exporting the entire project as FLAC"));
+
    while(!cancelling && !eos) {
       sampleCount samplesThisRun = mixer->Process(SAMPLES_PER_RUN);
       if (samplesThisRun == 0) {//stop encoding
          break;
       }
       else {
-	for(i=0;i<numChannels;i++){
-		samplePtr mixed = mixer->GetBuffer(i);
-		for(j=0;j<samplesThisRun;j++){
-			if(format==int24Sample)
-				tmpsmplbuf[i][j]= ((int*)mixed)[j];
-			else
-				tmpsmplbuf[i][j]= ((short*)mixed)[j];
-		}
-	}
+         for(i=0;i<numChannels;i++){
+            samplePtr mixed = mixer->GetBuffer(i);
+            for(j=0;j<samplesThisRun;j++){
+               if(format==int24Sample)
+                  tmpsmplbuf[i][j]= ((int*)mixed)[j];
+               else
+                  tmpsmplbuf[i][j]= ((short*)mixed)[j];
+            }
+         }
          encoder->process(tmpsmplbuf,samplesThisRun);
       }
-      if(progress) {
-         int progressvalue = int (1000 * ((mixer->MixGetCurrentTime()-t0) /
-                                          (t1-t0)));
-         cancelling = !progress->Update(progressvalue);
-      }
-      else if(wxGetElapsedTime(false) > 500) {
-            
-         wxString message = selectionOnly ?
-            _("Exporting the selected audio as FLAC") :
-            _("Exporting the entire project as FLAC");
-
-         progress = new wxProgressDialog(
-               _("Export"),
-               message,
-               1000,
-               parent,
-               wxPD_CAN_ABORT | wxPD_REMAINING_TIME | wxPD_AUTO_HIDE);
-      }
+      int progressvalue = int (1000 * ((mixer->MixGetCurrentTime()-t0) /
+                                       (t1-t0)));
+      cancelling = !GetActiveProject()->ProgressUpdate(progressvalue);
    }
    encoder->finish();
+
+   GetActiveProject()->ProgressHide();
+
    for(i=0;i<numChannels;i++)
    	free(tmpsmplbuf[i]);
    delete mixer;
    delete encoder;
-
-   if(progress)
-      delete progress;
-
+   
    delete[] tmpsmplbuf;
-
+   
    return !cancelling;
 }
 
