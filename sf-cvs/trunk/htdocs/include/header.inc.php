0ab2557341d04097ec68bffa62047d13e4d27a90
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2006-08-16 03:36:40 +0000

    Slight changes in the search script: add a channel for reporting.

--- sf-cvs/trunk/htdocs/include/header.inc.php
+++ sf-cvs/trunk/htdocs/include/header.inc.php
@@ -46,10 +46,11 @@
 </table>
 <input type="hidden" name="client" value="pub-2386514291404644"></input>
 <input type="hidden" name="forid" value="1"></input>
+<input type="hidden" name="channel" value="1707102428"></input>
 <input type="hidden" name="ie" value="ISO-8859-1"></input>
 <input type="hidden" name="oe" value="ISO-8859-1"></input>
 <input type="hidden" name="safe" value="active"></input>
-<input type="hidden" name="cof" value="GALT:#008000;GL:1;DIV:#336699;VLC:663399;AH:center;BGC:FFFFFF;LBGC:336699;ALC:0000FF;LC:0000FF;T:000000;GFNT:0000FF;GIMP:0000FF;LH:50;LW:127;L:http://audacity.sourceforge.net/images/Audacity-logo-r_50pct.jpg;S:http://audacity.sourceforge.net/;FORID:1"></input>
+<input type="hidden" name="cof" value="GALT:#008000;GL:1;DIV:#336699;VLC:663399;AH:center;BGC:FFFFFF;LBGC:336699;ALC:0000FF;LC:0000FF;T:000000;GFNT:0000FF;GIMP:0000FF;LH:50;LW:127;L:http://audacity.sourceforge.net/images/Audacity-logo-r_50pct.jpg;S:http://audacity.sourceforge.net;FORID:1"></input>
 <input type="hidden" name="hl" value="en"></input>
 </td></tr></table>
 </form>
