d95fa7dedfcb2c9cdf3211d315c6c2f08c0a25cb
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2006-10-30 13:34:34 +0000

    Fixed typo

--- sf-cvs/trunk/htdocs/include/news.inc.php
+++ sf-cvs/trunk/htdocs/include/news.inc.php
@@ -164,6 +164,6 @@ add_news_item(
 	"1.2.3-release",
   _("Audacity 1.2.3 Released"),
   _('<p>Audacity 1.2.3 is a new stable version of Audacity. This version fixes a bug that interfered with long recordings on some Windows systems, and another bug that causes random crashes on Mac OS X. It also includes several updated translations, and other bug fixes and minor improvements.</p>
-<p>See the <a href="/download/release-notes">Release Notes</a> for a complete list of changes and known problems in this version.</p>')
+<p>See the <a href="/download/release-notes">Release Notes</a> for a complete list of changes and known problems in this version.</p>'));
 
 ?>
