2d0282014932d06ec51647d6aef2e25e0445028b
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2006-11-16 01:06:32 +0000

    Lyrics Window

---
+++ sf-cvs/branches/Audacity_UmixIt/audacity-src/src/LyricsWindow.cpp
@@ -0,0 +1,58 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  LyricsWindow.cpp
+
+  Vaughan Johnson
+  Dominic Mazzoni
+
+**********************************************************************/
+
+#include "LyricsWindow.h"
+#include "Lyrics.h"
+#include "Project.h"
+
+//enum {
+//   LyricsWindowCtlID = 1000,
+//   FontSizeBtnID
+//};
+
+BEGIN_EVENT_TABLE(LyricsWindow, wxFrame)
+   EVT_CLOSE(LyricsWindow::OnCloseWindow)
+   //EVT_BUTTON(FontSizeBtnID, LyricsWindow::OnFontSizeBtn)
+END_EVENT_TABLE()
+
+const wxSize gSize = wxSize(LYRICS_DEFAULT_WIDTH, LYRICS_DEFAULT_HEIGHT);
+
+LyricsWindow::LyricsWindow(AudacityProject *parent):
+  wxFrame(parent, -1, _("Audacity Lyrics"), wxDefaultPosition,  gSize, 
+            wxDEFAULT_FRAME_STYLE | ((parent == NULL) ? 0x0 : wxFRAME_FLOAT_ON_PARENT))
+{
+   mProject = parent;
+   mLyricsPanel = new Lyrics(this, -1, wxPoint(0, 0), gSize);
+
+   // loads either the XPM or the windows resource, depending on the platform
+#if !defined(__WXMAC__) && !defined(__WXX11__)
+   #ifdef __WXMSW__
+      wxIcon ic(wxICON(AudacityLogo));
+   #else
+      wxIcon ic(wxICON(AudacityLogo48x48));
+   #endif
+   SetIcon(ic);
+#endif
+}
+
+LyricsWindow::~LyricsWindow()
+{}
+
+void LyricsWindow::OnCloseWindow(wxCloseEvent & WXUNUSED(event))
+{
+  this->Hide();
+}
+
+//void LyricsWindow::OnFontSizeBtn(wxCommandEvent & event)
+//{
+//   // this->UpdateDisplay();
+//}
+
