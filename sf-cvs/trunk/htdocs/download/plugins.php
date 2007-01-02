2fc4fb3705a04ced345706bac104e8c37d2870df
richardash1981 <richardash1981@47890b92-0858-11df-a26f-8b716316a5bc>
2007-01-02 20:49:14 +0000

    Fix a minor typo (missing full stop)

--- sf-cvs/trunk/htdocs/download/plugins.php
+++ sf-cvs/trunk/htdocs/download/plugins.php
@@ -17,7 +17,7 @@
 <p><?=_('Audacity has built-in support for LADSPA plug-ins.  These plug-ins are mostly built for Linux, but some are available for other operating systems too.  Audacity comes with sample LADSPA effects for Mac. For Windows, a <a href="http://audacity.sourceforge.net/beta/ladspa/ladspa-0.4.15.exe">set of over 90 LADSPA effects</a> is available.  For more information and downloads, see the <a href="http://www.ladspa.org/">LADSPA web site</a>.')?></p>
 
 <h3><?=_("Nyquist Plug-Ins")?></h3>
-<p><?=_('Audacity has built-in support for cross-platform Nyquist effects.  You can download additional <a href="nyquistplugins">Nyquist plug-ins</a>, or create your own using the <a href="../help/nyquist">Nyquist programming language</a>')?>
+<p><?=_('Audacity has built-in support for cross-platform Nyquist effects.  You can download additional <a href="nyquistplugins">Nyquist plug-ins</a>, or create your own using the <a href="../help/nyquist">Nyquist programming language</a>.')?>
 
 <h3><?=_("VST Plug-Ins")?></h3>
 <p><?=_('Audacity can load VST effects (for Mac OS or Windows) using the optional <a href="../help/faq?s=install&amp;i=vst-enabler">VST Enabler</a>.  You can find VST effects at <a href="http://www.kvraudio.com/">KVR Audio</a> and <a href="http://dmoz.org/Computers/Multimedia/Music_and_Audio/Software/Plug-ins/">other plug-in sites</a>.</p>')?>
