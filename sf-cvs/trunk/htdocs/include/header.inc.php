1d6b1e93394d74df45550cb5b381bfd22b9d542a
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2007-05-01 17:37:35 +0000

    SourceForge/AdSense ads -> all pages except home page.

--- sf-cvs/trunk/htdocs/include/header.inc.php
+++ sf-cvs/trunk/htdocs/include/header.inc.php
@@ -97,6 +97,9 @@
 <?php
   if ($sectionNavItems) {
   ?>
+
+  <div id="leftcolumn">
+
   <div id="subnav">
     <?php
       if ($pageId == "") {
@@ -158,6 +161,47 @@
       ?>
     </ul>
   </div>
+
+  <div id="adsense">
+	<?php
+	/* 4/30/2007: Ross Turk of SourceForge says please just direct all revenue to Audacity for one month, 
+		due to error I made because of their documentation. - Vaughan
+
+		<script type="text/javascript">
+		var dfp_ord = Math.random()*10000000000000000;
+		var partner_clientid = 'pub-2386514291404644';
+		var unixgroupname = 'audacity';
+		</script>
+		<script type="text/javascript" src="http://genweb.ostg.com/google/sfproject.js"></script>
+	*/
+	?>
+	<script type="text/javascript"><!--
+	google_ad_client = "pub-2386514291404644";
+	google_ad_width = 160;
+	google_ad_height = 600;
+	google_ad_format = "160x600_as";
+	google_ad_type = "text";
+	//2007-04-30: SourceForge Project Web Ads
+	google_ad_channel = "8140479188";
+	google_color_border = "336699";
+	google_color_bg = "FFFFFF";
+	google_color_link = "0000FF";
+	google_color_text = "000000";
+	google_color_url = "008000";
+	//-->
+	</script>
+	<script type="text/javascript"
+	  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+	</script>
+	
+	<center>
+		<br>
+		<a href="http://audacity.sourceforge.net/community/donate"><font size="-2">Why is Audacity showing ads?</font></a>  
+	</center>
+  </div> <!-- adsense -->
+
+  </div> <!-- leftcolumn -->
+
   <?php
   }
 ?>
