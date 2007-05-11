6b9d0f83ac169b8911261b71cb3783bfdc248f8a
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2007-05-11 00:40:40 +0000

    more info, more links

--- sf-cvs/trunk/htdocs/about/license.php
+++ sf-cvs/trunk/htdocs/about/license.php
@@ -34,7 +34,17 @@ echo _('<h3>License</h3>
 
 echo _('<h3>Advice for Vendors</h3>
 
-<p>Several vendors bundle Audacity with their products, resell, or distribute Audacity (see <a href="../download/bundlers">Vendors that Distribute Audacity</a>). Here are guidelines for bundling or reselling Audacity.</p>
+<p>Several vendors bundle Audacity with their products, resell, or distribute Audacity (see <a href="../download/bundlers">Vendors that Distribute Audacity</a>). Here are guidelines for bundling or reselling Audacity. If you adhere to these guidelines, please contact us for inclusion at <a href="../download/bundlers">Vendors that Distribute Audacity</a> (per instructions there).</p>
+
+<p>You must either include the <a href="http://audacity.sourceforge.net/download/source">Audacity source code</a> or make a written offer to supply it in line with Section 3 of the GPL. If you include our installers and do not modify them, this will comply with the source code requirement as the included README.txt contains a written offer to visit our website to obtain the source code.</p>
+
+<p>We would appreciate your linking to our website: <a href="http://audacity.sourceforge.net">http://audacity.sourceforge.net</a> so your users can download updated versions of Audacity when released. Also, please subscribe to the notifications list at the bottom of <a href="http://audacity.sourceforge.net">http://audacity.sourceforge.net</a> to ensure you are notified of and can distribute the latest version of Audacity.</p>
+
+<p>If the distribution is on CD we would also appreciate it if you put the Audacity documentation from <a href="http://audacity.sourceforge.net/help/documentation">http://audacity.sourceforge.net/help/documentation</a> on the CD to reduce the number of queries we receive, and make it clear in your documentation that we (the Audacity Team) cannot offer support on your own your product, only on Audacity. </p>
+
+<p>IMPORTANT: If you write your own documentation on Audacity, please let us review it for possible errors, <em>before</em> you publish it.</p>
+   
+<p>If you modify Audacity without the express permission of the Audacity Team, you do not have the right to use the name Audacity, which is registered trademark of Dominic Mazzoni.  You must rename the product and provide all of your own tech support.</p>
 
 <p>
   <h4>For vendors selling a hardware product, with Audacity included:</h4>
@@ -43,7 +53,6 @@ echo _('<h3>Advice for Vendors</h3>
     <li>Please distribute Audacity unmodified. If you need to modify it to work with your device, please contact us first.</li>
     <li>Please test your device with Audacity thoroughly.  If there are any specific configuration instructions that users need to follow in order for your device to work with Audacity, make sure they are included with your device.</li>
     <li>Please do not refer customers to Audacity tech support when they are having difficulties using your device.  If you are selling a commercial product, you should provide your own tech support.  When users are having Audacity-specific issues that are not related to your specific device, we will of course be happy to assist them.</li>
-    <li>If you modify Audacity without the express permission of the Audacity team, you do not have the right to use the name Audacity, which is registered trademark of Dominic Mazzoni.  You must rename the product and provide all of your own tech support.</li>
   </ol>
 </p>
 
@@ -52,9 +61,10 @@ echo _('<h3>Advice for Vendors</h3>
   <ol>
     <li>If you are distributing Audacity unmodified, your advertising should make it clear that Audacity is available for free on the Internet, and make it clear what the customer is paying for when they purchase Audacity from you.  Possiblilties include the convenience of Audacity on CD-ROM, other bundled software or music files, a printed manual, or more.</li>
     <li>You may not advertise your business as an authorized distributor of Audacity.  The name Audacity is a registered trademark of Dominic Mazzoni.</li>
-    <li>If you modify Audacity without the express permission of the Audacity team, you do not have the right to use the name Audacity, which is registered trademark of Dominic Mazzoni.  You must rename the product and provide all of your own tech support.</li>
   </ol>
-</p>');
+</p>
+
+<p>See <a href="http://audacityteam.org/wiki/index.php?title=AudacityVendors#Advice_for_Vendors">Advice for Vendors</a> on the <a href="http://audacityteam.org/wiki">Audacity Wiki</a> for further advice.</p>');
 ?>
 
 <!-- TODO: Copyright and licenses for libraries. -->
