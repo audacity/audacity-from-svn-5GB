bdd42813a986f344aaa4d62eda564141af3b5cfc
dmazzoni <dmazzoni@47890b92-0858-11df-a26f-8b716316a5bc>
2007-05-18 10:07:25 +0000

    Upgrade to 1.3.3

--- sf-cvs/trunk/htdocs/beta/versions.inc.php
+++ sf-cvs/trunk/htdocs/beta/versions.inc.php
@@ -1,45 +1,48 @@
 <?php
 // The main version number, when we don't distinguish between platforms
-define('beta_version', '1.3.2');
+define('beta_version', '1.3.3');
 
 // Note: Sizes are in MB.
-define('beta_macosx_version', '1.3.2');
+define('beta_macosx_version', '1.3.3');
 
-define('macosx_ppc_version', '1.3.2');
-define('macosx_ppc_size',    4.2);
+define('macosx_ub_version', '1.3.3');
+define('macosx_ub_size',    7.2);
 
-define('macosx_intel_version', '1.3.2');
-define('macosx_intel_size',    4.1);
+//define('macosx_ppc_version', '1.3.3');
+//define('macosx_ppc_size',    7.2);
 
-define('macosx_ppc_unicode_version', '1.3.2');
-define('macosx_ppc_unicode_size',    4.2);
+//define('macosx_intel_version', '1.3.3');
+//define('macosx_intel_size',    7.2);
+
+//define('macosx_ppc_unicode_version', '1.3.2');
+//define('macosx_ppc_unicode_size',    4.2);
 
 //define('macosx_intel_unicode_version', '1.3.2');
 //define('macosx_intel_unicode_size',    4.0);
 
 
 
-define('beta_win_exe_version', '1.3.2');
+define('beta_win_exe_version', '1.3.3');
 
-define('win_exe_version', '1.3.2');
-define('win_exe_size',    2.7);
+define('win_exe_version', '1.3.3');
+define('win_exe_size',    2.6);
 
-define('win_zip_version', '1.3.2');
-define('win_zip_size',    3.6);
+define('win_zip_version', '1.3.3');
+define('win_zip_size',    3.4);
 
-define('win_exe_unicode_version', '1.3.2');
-define('win_exe_unicode_size',    2.7);
+define('win_exe_unicode_version', '1.3.3');
+define('win_exe_unicode_size',    2.6);
 
-define('win_zip_unicode_version', '1.3.2');
-define('win_zip_unicode_size',    3.6);
+define('win_zip_unicode_version', '1.3.3');
+define('win_zip_unicode_size',    3.4);
 
 
 define('ladspa_version', '0.4.15');
 define('ladspa_size',    1.5);
 
 
-define('beta_src_version', '1.3.2');
-define('src_version', '1.3.2');
+define('beta_src_version', '1.3.3');
+define('src_version', '1.3.3');
 define('src_size',    6.4);
 define('src_suffix',  '.tar.gz');
 
@@ -51,6 +54,8 @@ $win_zip_unicode_url = "audacity-win-unicode-zip/audacity-win-unicode-".win_zip_
 $src_url = "audacity-src/audacity-src-".src_version.src_suffix;
 
 $ladspa_url = "ladspa/ladspa-".ladspa_version.".exe";
+
+$macosx_ub_url = "audacity-macosx-ub/audacity-macosx-ub-".macosx_ub_version.".dmg";
 
 $macosx_intel_url = "audacity-macosx-intel/audacity-macosx-intel-".macosx_intel_version.".dmg";
 $macosx_ppc_url = "audacity-macosx-ppc/audacity-macosx-ppc-".macosx_ppc_version.".dmg";
