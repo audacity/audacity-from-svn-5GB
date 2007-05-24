29bef56f6e9f2714e9afc3b2b3fb888c1d562426
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2007-05-24 23:22:39 +0000

    Add missing Vista versions, extra comments.

--- sf-cvs/trunk/htdocs/download/windows_sys_reqs.inc.php
+++ sf-cvs/trunk/htdocs/download/windows_sys_reqs.inc.php
@@ -11,7 +11,7 @@
   <?=_("Windows 95 and NT are not supported.")?>
 </p>
 <p>
-  <?=_('The values in the "Recommended RAM/processor speed" column below are for tasks like recording for an hour, or editing three 20 minute tracks simultaneously.')?>
+  <?=_('The values in the "Recommended RAM/processor speed" column below are for tasks like recording for an hour, or editing three 20 minute tracks simultaneously. The values in the "Minimum RAM/processor speed" column will be fine for smaller/shorter tasks especially if un-necessary programs are closed.')?>
 </p>
 
 <table border="2" cellpadding="8">
@@ -34,6 +34,11 @@
     <td>Windows Vista Home Basic</td>
     <td>2 GB / 1 GHz</td>
     <td>512 MB / 1 GHz</td>
+  </tr>
+  <tr>
+    <td>Windows Vista Home Premium/Business/Ultimate</td>
+    <td>4 GB / 2 GHz</td>
+    <td>1 GB / 1 GHz</td>
   </tr>
 </table>
 <p>&nbsp;</p>
