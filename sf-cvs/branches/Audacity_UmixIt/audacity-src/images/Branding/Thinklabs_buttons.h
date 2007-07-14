94e9bc5cfa6251e670ef67bfa5fb4f9dfe55b900
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2007-07-14 02:48:01 +0000

    Add ThinkLabs custom buttons.

---
+++ sf-cvs/branches/Audacity_UmixIt/audacity-src/images/Branding/Thinklabs_buttons.h
@@ -0,0 +1,30 @@
+/**********************************************************************
+
+   Audacity: A Digital Audio Editor
+
+   Thinklabs_buttons.h
+
+   Vaughan Johnson, April 2007
+  
+**********************************************************************/
+
+#include "Thinklabs_buttons/Thinklabs_button_Amplify.xpm"
+#include "Thinklabs_buttons/Thinklabs_button_Amplify_dis.xpm"
+#include "Thinklabs_buttons/Thinklabs_button_Amplify_over.xpm"  
+
+#include "Thinklabs_buttons/Thinklabs_button_Display.xpm"  
+#include "Thinklabs_buttons/Thinklabs_button_Display_dis.xpm"  
+#include "Thinklabs_buttons/Thinklabs_button_Display_over.xpm"  
+
+#include "Thinklabs_buttons/Thinklabs_button_Filter.xpm"           
+#include "Thinklabs_buttons/Thinklabs_button_Filter_dis.xpm"     
+#include "Thinklabs_buttons/Thinklabs_button_Filter_over.xpm"        
+
+#include "Thinklabs_buttons/Thinklabs_button_Import.xpm"
+#include "Thinklabs_buttons/Thinklabs_button_Import_dis.xpm"        
+#include "Thinklabs_buttons/Thinklabs_button_Import_over.xpm"        
+
+#include "Thinklabs_buttons/Thinklabs_button_Rate.xpm"  
+#include "Thinklabs_buttons/Thinklabs_button_Rate_dis.xpm"  
+#include "Thinklabs_buttons/Thinklabs_button_Rate_over.xpm"  
+
