4206298f61cf501182a2e961608f01ebc2f3bc91
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2008-02-22 02:03:51 +0000

    Replace -help list with -feedback and tidy

--- sf-cvs/trunk/htdocs/contact/lists.php
+++ sf-cvs/trunk/htdocs/contact/lists.php
@@ -17,47 +17,52 @@
 
 	// i18n-hint: Please add a note that these lists are in English.  If there are
 	// Audacity mailing lists or forums in your language, please link to them here.
-	echo "<p>"._('These mailing lists are for discussion of the Audacity audio editor.  </p>
-<p>Note: These are public mailing lists.  For details, see our <a href="privacy">privacy policy</a>.')."</p>";
+	echo "<p>"._('These mailing lists are for feedback or discussion about the Audacity audio editor.  </p>
+<p><b>Note:</b> In line with our <a href="../contact/privacy">Privacy Policy</a>, these are public mailing lists - messages are seen by all list subscribers, and posted on several web sites where the messages are 
+archived.</p>');
 ?>
 
 <dl>
+<dt id="feedback">audacity-feedback</dt>
+  <dd>
+    <p><?=_('To give us feedback that does not require a personal response, send an email to the <a
+href="&#x6d;&#x61;&#x69;&#x6c;&#x74;&#111;&#x3a;&#x61;&#117;&#x64;&#97;&#x63;&#105;&#x74;&#x79;&#x2d;&#x66;&#x65;&#101;&#100;&#x62;&#x61;&#99;&#x6b;&#x40;&#108;&#x69;&#x73;&#116;&#115;&#x2e;&#x73;&#x6f;&#x75;&#114;&#99;&#x65;&#x66;&#111;&#x72;&#x67;&#x65;&#x2e;&#110;&#x65;&#x74;">audacity-feedback</a>
+list. Tell us your experiences of the Audacity program, documentation or web site, report possible program bugs or suggest new Audacity features. You do not need to subscribe to this list to send a message.')?></p>
+    <p><a href="http://lists.sourceforge.net/lists/listinfo/audacity-feedback"><?php printf($archiveStr,
+"audacity-feedback")?></a>
+  </dd>
+
   <dt id="users">audacity-users</dt>
   <dd>
-    <p><?=_('Discuss Audacity with other users and developers.')?>  <?=$subscriberOnlyStr?></p>
+    <p><?=_('Discuss Audacity with other <a href="../community/users">users</a> and developers.')?>  
+<?=$subscriberOnlyStr?></p>
     <p><a href="http://lists.sourceforge.net/lists/listinfo/audacity-users"><?php printf($archiveStr, "audacity-users")?></a>
+  </dd>
+
+<dt id="devel">audacity-devel</dt>
+  <dd>
+    <p><?=_('For  <a href="../community/developers">developers</a> working with the Audacity source code and 
+documentation, and others interested in following our development process or learning about compiling or developing 
+Audacity source code.')?>  <?=$subscriberOnlyStr?></p>
+    <p><a href="http://lists.sourceforge.net/lists/listinfo/audacity-devel"><?php printf($archiveStr, "audacity-devel")?></a>
+    <p><?=_('Developers may also subscribe to receive automated notification of <a href="http://lists.sourceforge.net/lists/listinfo/audacity-cvs">latest source code changes</a> and of <a 
+href="http://lists.sourceforge.net/lists/listinfo/audacity-bugs">bugs</a> reported to the <a href="http://limpet.net/audacity/bugzilla/">Bugzilla</a> database.')?></p> 
   </dd>
 
   <dt id="translation">audacity-translation</dt>
   <dd>
-    <p><?=_('For translators localizing the Audacity software and web site.')?>  <?=$subscriberOnlyStr?></p>
+    <p><?=_('For <a href="../community/translation">translators</a> localizing the Audacity software, web site and 
+documentation.')?>  <?=$subscriberOnlyStr?></p>
     <p><a href="http://lists.sourceforge.net/lists/listinfo/audacity-translation"><?php printf($archiveStr, "audacity-translation")?></a>
   </dd>
 
-  <dt id="devel">audacity-devel</dt>
-  <dd>
-    <p><?=_('For developers working with the Audacity source code and documentation.')?>  <?=$subscriberOnlyStr?></p>
-    <p><a href="http://lists.sourceforge.net/lists/listinfo/audacity-devel"><?php printf($archiveStr, "audacity-devel")?></a>
-  </dd>
-
-  <dt id="nyquist">audacity-nyquist</dt>
+    <dt id="nyquist">audacity-nyquist</dt>
   <dd>
     <p><?=_('Share questions and tips about programming <a href="../help/nyquist">Nyquist plug-ins</a>.')?>  <?=$subscriberOnlyStr?></p>
     <p><a href="http://lists.sourceforge.net/lists/listinfo/audacity-nyquist"><?php printf($archiveStr, "audacity-nyquist")?></a>
   </dd>
 
-  <dt id="bugs">audacity-bugs</dt>
-  <dd>
-    <p><?=_('Developers can subscribe to this mailing list to receive automatic updates from <a href="../community/developers#bugzilla">Bugzilla</a>.')?></p>
-    <p><a href="http://lists.sourceforge.net/lists/listinfo/audacity-bugs"><?php printf($archiveStr, "audacity-bugs")?></a>
-  </dd>
 
-  <dt id="help">audacity-help</dt>
-  <dd>
-    <p><?=_('Send a message to this list if you have questions or comments for the Audacity developers.  You do not need to subscribe to this list to send a message.')?></p>
-    <p><a href="http://lists.sourceforge.net/lists/listinfo/audacity-help"><?php printf($archiveStr, "audacity-help")?></a>
-  </dd>
-
-<?php
+  <?php
   include "../include/footer.inc.php";
 ?>
