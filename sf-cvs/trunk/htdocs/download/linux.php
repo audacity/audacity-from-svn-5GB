c9f9550fc62d6514ebb18234a2ac4d80992be2e9
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2008-02-23 20:07:53 +0000

    Replace the broken links with working ones

--- sf-cvs/trunk/htdocs/download/linux.php
+++ sf-cvs/trunk/htdocs/download/linux.php
@@ -1,6 +1,7 @@
 <?php
 /*
  * Copyright 2004 Matt Brubeck
+ * 2008 Gale Andrews
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/2.0/
  */
@@ -12,25 +13,20 @@
 
 <h2><?=$pageTitle?></h2>
 
-<p><?=_('The easiest way to install the free Audacity audio editor on GNU/Linux (and other Unix-like systems) is to download a package built for your operating system:')?></p>
+<p><?=_('Installation packages for Audacity on GNU/Linux and other Unix-like systems are often provided by individual distributions:')?></p>
 <ul>
-  <li><a href="http://www.altlinux.com/index.php?module=sisyphus&package=audacity">Alt Linux</a></li>
-  <li><a href="http://packages.debian.org/cgi-bin/search_packages.pl?searchon=names&version=all&exact=1&keywords=audacity">Debian</a></li>
-  <li><a href="http://packages.gentoo.org/packages/?category=media-sound;name=audacity">Gentoo</a></li>
-  <li><a href="http://rpmseek.com/rpm-dl/audacity-1.2.4b-4.2006.SoS.i586.html?hl=com&cx=0:-:0:2750311:0:0:0">Mandriva i586</a> and 
-  <a href="http://seerofsouls.com/x86_64.html">Mandriva x86_64</a></li>
-  <li><a href="ftp://ftp.ac.pld-linux.org/dists/ac/PLD/">PLD Linux</a></li>
-  <li>Red Hat / Fedora Core
-    <ul>
-      <li><a href="http://fedoraproject.org/wiki/Extras/FedoraHOWTO">Fedora Extras</a></li>
-      <li><a href="http://ccrma.stanford.edu/planetccrma/software/">Planet CCRMA</a></li>
-    </ul>
-  </li>
-  <li><a href="http://packman.links2linux.org/?action=286">SuSE Linux</a></li>
-  <li><a href="http://packages.ubuntu.com/audacity">Ubuntu Linux</a></li>
+  <li><a href="http://rpmseek.com/rpm-pl/audacity.html?hl=com&cx=0%3A-%3A0%3A0%3A0%3A0%3A0&qArStr=0&qRtStr=0&qDnStr=48">Alt Linux</a></li>
+  <li><a href="http://packages.debian.org/search?keywords=audacity&searchon=names&suite=stable&section=all">Debian</a></li>
+  <li><a href="http://rpmseek.com/rpm-pl/audacity.html?hl=com&cx=0%3A-%3A0%3A0%3A0%3A0%3A0&qArStr=0&qRtStr=0&qDnStr=82">Fedora Core</a></li>
+  <li><a href="http://rpmseek.com/rpm-pl/audacity.html?hl=com&cx=0%3A-%3A0%3A0%3A0%3A0%3A0&qArStr=0&qRtStr=0&qDnStr=98">Fedora Project</a></li>
+  <li><a href="http://rpmseek.com/rpm-dl/audacity-1.2.4b-4.2006.SoS.i586.html?hl=com&cx=0:-:0:2750311:0:0:0">Mandriva i586</a></li>
+  <li><a href="http://rpmseek.com/rpm-pl/audacity.html?hl=com&cx=0%3A-%3A0%3A0%3A0%3A0%3A0&qArStr=0&qRtStr=0&qDnStr=108">OpenSUSE</a></li>
+  <li><a href="http://rpmseek.com/rpm-pl/audacity.html?hl=com&cx=0%3A-%3A0%3A0%3A0%3A0%3A0&qArStr=0&qRtStr=0&qDnStr=70">Red Hat</a></li>   
+  <li><a href="http://rpmseek.com/rpm-pl/audacity.html?hl=com&cx=0%3A-%3A0%3A0%3A0%3A0%3A0&qArStr=0&qRtStr=0&qDnStr=11">SuSE</a> and <a href="http://rpmseek.com/rpm-pl/audacity.html?hl=com&cx=0%3A-%3A0%3A0%3A0%3A0%3A0&qArStr=0&qRtStr=0&qDnStr=44">packman (suse)</a></li>
+  <li>Ubuntu: <a href="http://packages.ubuntu.com/audacity">packages.ubuntu</a> and <a href="http://rpmseek.com/rpm-pl/audacity.html?hl=com&cx=0%3A-%3A0%3A0%3A0%3A0%3A0&qArStr=0&qRtStr=0&qDnStr=109">rpm.seek</a> </li>
 </ul>
 
-<p><?=_('Linux and Unix users may also compile Audacity from <a href="source">source code</a>.')?></p>
+<p><?=_('The above list is not comprehensive. If you don\'t see an up-to-date package for your own distribution, we recommend searching your distribution\'s web site for the latest information. Alternatively, you can compile Audacity from <a href="source#recdown">source code</a>.')?></p>
 
 <?php
   include "../include/footer.inc.php";
