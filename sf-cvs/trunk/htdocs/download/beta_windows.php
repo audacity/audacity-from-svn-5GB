01342ae56305b879d2f2d350caec732e80cf5af8
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2008-02-23 06:05:36 +0000

    Soften Beta advisory and remove "warning" word as per Documenation Checklist

--- sf-cvs/trunk/htdocs/download/beta_windows.php
+++ sf-cvs/trunk/htdocs/download/beta_windows.php
@@ -2,6 +2,7 @@
 /*
  * Copyright 2005 Dominic Mazzoni
  * Copyright 2004 Matt Brubeck
+ * Copyright 2007-8 Vaughan Johnson, Gale Andrews
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/2.0/
  */
@@ -20,10 +21,13 @@
 <h3><?=_("Recommended Download")?></h3>
 <?php include "recommended.inc.php" ?>
 <ul>
-  <li><p>Windows 2000/XP/Vista: <?php printf(_('<a href="%s">Audacity %s installer</a> (.exe file, %.1lf MB) - The latest version of the free Audacity audio editor.'), "../beta/".$win_exe_unicode_url, win_exe_unicode_version, win_exe_unicode_size)?></p></li>
 
-  <li><p>Windows 98/ME: <?php printf(_('<a href="%s">Audacity %s installer</a> (.exe file, %.1lf MB) - The latest version of the free Audacity audio editor.'), "../beta/".$win_exe_url, win_exe_version, win_exe_size)?></p></li>
+  <li><p>Windows 2000/XP/Vista: <?php printf(_('<a href="%s">Audacity %s installer</a> (.exe file, %.1lf MB) - The latest version of the free Audacity audio editor. See <a href="http://audacityteam.org/wiki/index.php?title=Windows_Vista_OS">further information about Vista</a>.'), "../beta/".$win_exe_unicode_url, win_exe_unicode_version, win_exe_unicode_size)?></p></li>
+
+  <li><p>Windows 98/ME: <?php printf(_('<a href="%s">Audacity %s installer</a> (.exe file, %.1lf MB) - The latest version of the free Audacity audio editor.</a>'), "../beta/".$win_exe_url, win_exe_version, win_exe_size)?></p></li>
 </ul>
+
+<p>Jump to <a href="#sysreq">System Requirements</a>.</p>
 
 <h3 id="optional"><?=_("Optional Downloads")?></h3>
 <?php
@@ -37,16 +41,15 @@
   }
 ?>
 <ul>
-  <li><p><?php printf(_('<a href="%s">LADSPA plugins %s installer</a> (.exe file, %.1lf MB) - over 90 plug-ins'), "../beta/".$ladspa_url, ladspa_version, ladspa_size)?></p></li>
+  <li><p><?php printf(_('<a href="%s">LADSPA plugins %s installer</a> (.exe file, %.1lf MB) - over 90 plug-ins.'), "../beta/".$ladspa_url, ladspa_version, ladspa_size)?></p></li>
 
   <?php include "common.inc.php"; ?>
   <li><p>Windows 2000/XP/Vista: <?php printf(_('<a href="%s">Audacity %s zip file</a> (%.1lf MB)
-  - If you can’t run the installer because you are in a computer lab or other restricted environment, download and unzip this file instead.'),
+  - If you cannot run the installer because you are in a computer lab or other restricted environment, download and unzip this or the file below instead.'),
     "../beta/".$win_zip_unicode_url, win_zip_unicode_version, win_zip_unicode_size)?></p></li>
 
   <li><p>Windows 98/ME: <?php printf('<a href="%s">Audacity %s zip file</a> (%.1lf MB)',
     "../beta/".$win_zip_url, win_zip_version, win_zip_size)?></p></li>
-
 </ul>
 
 <?php include "windows_sys_reqs.inc.php" ?>
