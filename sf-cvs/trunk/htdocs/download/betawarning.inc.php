5a43d78bacaa3fd79ebae306dae69a55463f4726
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2008-02-23 06:05:36 +0000

    Soften Beta advisory and remove "warning" word as per Documenation Checklist

--- sf-cvs/trunk/htdocs/download/betawarning.inc.php
+++ sf-cvs/trunk/htdocs/download/betawarning.inc.php
@@ -1,13 +1,25 @@
 <?php
 /*
  * Copyright 2005 Dominic Mazzoni
+ * Copyright 2008 Martyn Shaw, Gale Andrews 
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/2.0/
  */
 ?>
 
-<h3><?=_("Beta warning")?></h3>
+<h3><?=_("Beta version")?></h3>
 <p>
-  <?=_("Audacity 1.3 is a beta version of Audacity.  Users who want a completely stable, finished program should be using Audacity 1.2.  Audacity 1.3 has hundreds of new features, but some features may be incomplete or confusing, it is more likely to crash, and you are more likely to lose data.  You can install both Audacity 1.2 and 1.3 on the same machine, if you install them to different directories.")?></p>
-<p><?=_("Audacity 1.3 is available in many languages, but most translations are incomplete.  Words and phrases which have not been translated yet will appear in English.")?></p>
+<?=_('Audacity 1.3 Beta is our new, not quite finished, development version which will be the basis of our next "stable" version.')?></p>
 
+<p><?=_('
+ <ul>
+  <li>Contains dozens of new, exciting and finished features.</li>
+  <li>Occasionally, new features might need final polishing, or might not be retained in later versions.</li>
+  <li>Occasionally, a feature might not work as it did before, or might be temporarily disabled.</li>
+  <li>Some parts of the program are not yet documented or translated into different languages.</li>
+ </ul>
+')?></p>
+
+<p><?=_('If you are a new user of Audacity, or new to computer audio programs, you may prefer to use the fully finished 1.2 version of Audacity. Otherwise, please help our development effort by trying the new Beta and <a href="&#x6d;&#x61;&#105;&#108;&#116;&#111;&#x3a;&#97;&#x75;&#x64;&#97;&#99;&#x69;&#x74;&#x79;&#45;&#x66;&#101;&#x65;&#x64;&#x62;&#97;&#x63;&#107;&#x40;&#x6c;&#105;&#115;&#x74;&#115;&#46;&#115;&#111;&#x75;&#x72;&#99;&#101;&#x66;&#x6f;&#114;&#103;&#101;&#46;&#110;&#x65;&#116;&#63;&#x73;&#x75;&#98;&#x6a;&#x65;&#x63;&#x74;&#61;&#x42;&#x65;&#116;&#x61;&#32;&#102;&#101;&#101;&#x64;&#98;&#x61;&#99;&#107;">send us your comments</a>. Please mention what operating system you are on, full details of any problems, and any features that need improvement or no longer work properly. You can install 1.3 on the same machine as your 1.2 version. The best way to do this is to install 1.3 to a different directory.')?></p>
+
+<p><?=_('<b>Note:</b> In line with our <a href="../contact/privacy">Privacy Policy</a>, feedback you give about the Beta is sent to a public mailing list - messages are seen by the list subscribers, and archived on several web sites.')?></p>
