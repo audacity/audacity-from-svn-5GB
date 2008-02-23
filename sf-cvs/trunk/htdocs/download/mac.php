56ad6611c7474b48967d6dfcb37faaf95df4d1aa
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2008-02-23 16:35:26 +0000

    Add LADSPA plug-ins installer, working PPC download for non-English users and "Installation instructions"

--- sf-cvs/trunk/htdocs/download/mac.php
+++ sf-cvs/trunk/htdocs/download/mac.php
@@ -2,6 +2,7 @@
 /*
  * Copyright 2004 Matt Brubeck
  * Richard Ash 2006
+ * Gale Andrews 2008
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/2.0/
  */
@@ -18,19 +19,27 @@
 <h3><?=_("Recommended Download")?></h3>
 <?php include "recommended.inc.php" ?>
 <ul>
-  <li><p><?php printf(_('For Mac OS X (PPC): <a href="%s">Audacity %s</a> (.dmg file, %.1lf MB) - The latest version of the free Audacity audio editor.'), "../latest/".$macosx_url, macosx_version, macosx_size)?></p></li>
-
+  <li><p><?php printf(_('For Mac OS X (PPC, English language): <a href="%s">Audacity %s</a> (.dmg file, %.1lf MB) - The latest version of the free Audacity audio editor.'), "../latest/".$macosx_url, macosx_version, macosx_size)?></p></li>
+  <li><p>For Mac OS X (PPC, other languages): <a href="http://audacity.sourceforge.net/files/audacity-macosx-ppc-1.2.6-test.dmg">Audacity 1.2.6</a> (.dmg file, 3.8 MB) - The latest version of the free Audacity audio editor</p></li>
   <li><p><?php printf(_('For Mac OS X (Intel): <a href="%s">Audacity %s</a> (.dmg file, %.1lf MB) - The latest version of the free Audacity audio editor.'), "../latest/".$macosx_intel_url, macosx_intel_version, macosx_intel_size)?></p></li>
-
-  <!-- TODO: Installation instructions? -->
 
   <li><p><?php printf(_('For Mac OS 9: <a href="%s">Audacity %s</a> (.sit file, %.1lf MB)'), "../latest/".$mac_classic_url, mac_classic_version, mac_classic_size)?></p></li>
   <p><?=_("(Unfortunately, we no longer have the resources to provide new versions of Audacity for Mac OS 9.)")?></p></li>
 </ul>
 
+<p> <b>Installation instructions:</b>
+ <ol>
+   <li>Inside your Applications folder, create a folder called "Audacity"</li>
+   <li>Double-click the downloaded .dmg to mount it</li>
+   <li>Option-drag the whole of the .dmg contents (not the .dmg itself) into the "Audacity" folder you created</li>
+   <li>Double-click Audacity.app inside the Applications folder to launch it</li>
+ </ol>
+</p>
+
 <h3 id="optional"><?=_("Optional Downloads")?></h3>
 <ul>
-  <?php include "common.inc.php"; ?>
+  <li><p><a href="http://ardour.org/files/releases/swh-plugins-0.4.15.dmg">LADSPA plugins installer</a> - over 90 plug-ins.</p></li>
+<?php include "common.inc.php"; ?>
 </ul>
 
 <h3><?=_("System Requirements")?></h3>
