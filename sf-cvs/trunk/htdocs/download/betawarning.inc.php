463530d807202f814122781c8f90eb61b0dd9720
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2008-02-23 08:48:50 +0000

    Trim text a little more and add an anchor to the download

--- sf-cvs/trunk/htdocs/download/betawarning.inc.php
+++ sf-cvs/trunk/htdocs/download/betawarning.inc.php
@@ -7,7 +7,8 @@
  */
 ?>
 
-<h3><?=_("Beta version")?></h3>
+<h3><?=_('Beta version')?></h3> 
+<a href="#recdown">Download</a>
 <p>
 <?=_('Audacity 1.3 Beta is our new, not quite finished, development version which will be the basis of our next "stable" version.')?></p>
 
@@ -20,6 +21,6 @@
  </ul>
 ')?></p>
 
-<p><?=_('If you are a new user of Audacity, or new to computer audio programs, you may prefer to use the fully finished 1.2 version of Audacity. Otherwise, please help our development effort by trying the new Beta and <a href="&#x6d;&#x61;&#105;&#108;&#116;&#111;&#x3a;&#97;&#x75;&#x64;&#97;&#99;&#x69;&#x74;&#x79;&#45;&#x66;&#101;&#x65;&#x64;&#x62;&#97;&#x63;&#107;&#x40;&#x6c;&#105;&#115;&#x74;&#115;&#46;&#115;&#111;&#x75;&#x72;&#99;&#101;&#x66;&#x6f;&#114;&#103;&#101;&#46;&#110;&#x65;&#116;&#63;&#x73;&#x75;&#98;&#x6a;&#x65;&#x63;&#x74;&#61;&#x42;&#x65;&#116;&#x61;&#32;&#102;&#101;&#101;&#x64;&#98;&#x61;&#99;&#107;">send us your comments</a>. Please mention what operating system you are on, full details of any problems, and any features that need improvement or no longer work properly. You can install 1.3 on the same machine as your 1.2 version. The best way to do this is to install 1.3 to a different directory.')?></p>
+<p><?=_('If you are new to Audacity or computer audio programs, you may prefer the fully finished Audacity 1.2. Otherwise, please help our development effort by trying the Beta and <a href="&#x6d;&#x61;&#105;&#108;&#116;&#111;&#x3a;&#97;&#x75;&#x64;&#97;&#99;&#x69;&#x74;&#x79;&#45;&#x66;&#101;&#x65;&#x64;&#x62;&#97;&#x63;&#107;&#x40;&#x6c;&#105;&#115;&#x74;&#115;&#46;&#115;&#111;&#x75;&#x72;&#99;&#101;&#x66;&#x6f;&#114;&#103;&#101;&#46;&#110;&#x65;&#116;&#63;&#x73;&#x75;&#98;&#x6a;&#x65;&#x63;&#x74;&#61;&#x42;&#x65;&#116;&#x61;&#32;&#102;&#101;&#101;&#x64;&#98;&#x61;&#99;&#107;">send us your comments</a>. Please mention your operating system, full details of any problems, and any features that need improvement or no longer work properly. You can install 1.3 and 1.2 on the same machine by installing them to different directories.')?></p>
 
 <p><?=_('<b>Note:</b> In line with our <a href="../contact/privacy">Privacy Policy</a>, feedback you give about the Beta is sent to a public mailing list - messages are seen by the list subscribers, and archived on several web sites.')?></p>
