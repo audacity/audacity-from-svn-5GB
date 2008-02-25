1cba795f8a66fc06895f6f222a21c2045dec1772
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2008-02-25 20:42:30 +0000

    Updated information for discontinued TheOpenCD and new TheOpenDisc

--- sf-cvs/trunk/htdocs/download/buy.php
+++ sf-cvs/trunk/htdocs/download/buy.php
@@ -1,19 +1,24 @@
 <?php
 /*
- * Copyright 2004 Matt Brubeck
+ * Copyright 2004-8 Matt Brubeck, Gale Andrews
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/2.0/
  */
   require_once "main.inc.php";
   $pageId = "buy";
-  $pageTitle = _("Purchase Audacity on CD");
+  $pageTitle = _("Obtain Audacity on CD");
   include "../include/header.inc.php";
 
   echo "<h2>$pageTitle</h2>";
-  echo "<p>"._('Audacity is free to download, but vendors are also free to sell copies of the program (see <a href="../download/bundlers">Vendors that Distribute Audacity</a>), thanks to the <a href="../about/license">GNU General Public License</a>.  If you want a copy of Audacity on CD-ROM, we encourage you to buy from a vendor listed below.')."</p>";
+  echo "<p>"._('Audacity is always free to download from us, but vendors can also sell copies of the program, thanks to the <a href="../about/license#license">GNU General Public License</a>. If you want a copy of Audacity on CD-ROM, we encourage you to use the options listed below.')."</p>";
 
-  echo _('<h3>TheOpenCD (Windows)</h3>
-  <p><a href="http://www.theopencd.org/">TheOpenCD</a> is a collection of high-quality free software for Microsoft Windows, including Audacity.  You can <a href="http://www.theopencd.org/purchase">purchase a copy of TheOpenCD</a> from several different vendors.</p>');
+  echo _('<h3>Windows</h3>
+  <p>
+   <ul>
+    <li><a href="http://www.theopencd.org/">TheOpenCD</a> is a now-discontinued collection of high quality free and open source software, including Audacity. Currently, you can still <a href="http://releases.theopencd.org/">download archived CD images</a> or <a href="http://ztechshop.net/disks/opencd/">purchase a CD online</a> for worldwide shipping.</li>
+    <li><a href="http://theopendisc.com/">TheOpenDisc</a> is a similar collection of high quality free and open source software, including Audacity. Currently, a downloadable <a href="http://downloads.sourceforge.net/opendisc/OpenDisc-07.10.iso?use_mirror=osdn">CD image</a> is available. The project hopes to make available a purchasable CD shortly.</li>
+   </ul>
+  </p>');
 
   include "../include/footer.inc.php";
 ?>
