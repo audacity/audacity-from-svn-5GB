ae88104d5da49b174f20a9cba588a82cc338bb61
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2008-03-05 04:18:12 +0000

    Slight reorganisation, linking to Tips and Tutorials on the Wiki and adding links to additional help resources (e.g. PDF manual)

--- sf-cvs/trunk/htdocs/help/documentation.php
+++ sf-cvs/trunk/htdocs/help/documentation.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2004 Matt Brubeck
+ * Copyright 2004 - 2008 Matt Brubeck, Gale Andrews
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/2.0/
  */
@@ -11,34 +11,42 @@
 
 	echo "<h2>$pageTitle</h2>";
 
-	// i18n-hint:  Please add a note that the manuals below are in English.  If
-	// there is documentation available in your language, link to it here.
-	echo "<p>"._('This page contains manuals and user’s guides for the free Audacity sound editor.  For additional help, see the <a href="faq">Frequently Asked Questions</a> and <a href="tutorials">Tutorials</a>.')."</p>";
+     // i18n-hint:  Please add a note that the manuals below are in English.
+     // If there is documentation available in your language not already
+     // linked to at the bottom of this page, please add the links.
+	echo "<p>"._('This page contains manuals and quick reference guides for the free Audacity sound editor.  For additional help, see the <a href="faq">Frequently Asked Questions</a> and our <a href="http://audacityteam.org/wiki/index.php?title=Tutorials">Tutorials</a> and <a href="http://audacityteam.org/wiki/index.php?title=Tips">Tips</a> on the Audacity <a href="http://audacityteam.org/wiki/">Wiki</a>.')."</p>";
 ?>
 
 <h3><?=_('Quick Reference Guide')?></h3>
 <ul>
-	<li><a href="../onlinehelp-1.2/"><?=_('Read the Audacity reference online.')?></a> <?=_('(You can also access this reference though the "Help" menu in the Audacity program.)')?></li>
-	<li><a href="../onlinehelp-1.2/reference.html"><?=_('Printable version of the Audacity reference.')?></a></li>
-</ul>
+	<li><a href="../onlinehelp-1.2/"><?=_('Read the Audacity reference online.')?></a> <?=_('(You can also access this reference through the "Help" menu in the Audacity program.)')?></li>
+	<li><a href="../onlinehelp-1.2/reference.html"><?=_('Printable version of the Audacity reference.')?></a> <?=_('(This can be printed as a single page using the print function of your browser.)')?></li>
+ </ul>
 
-<h3><?=_('Audacity User’s Manual')?></h3>
+<h3><?=_('Audacity User\'s Manual')?></h3>
 <ul>
 	<li><a href="../manual-1.2/"><?=_('Read the Audacity manual online.')?></a></li>
-	<li><a href="../audacity-manual-1.2.zip"><?=_('Download the Audacity manual.')?></a> <?=_('(To use the downloaded manual, unzip it and then open the "index.html" file.)')?></li>
-</ul>
-
-<h3><?=_('Other Resources')?></h3>
-<ul>
-	<li><?=_('<a href="http://audacityteam.org/wiki/">The Audacity Wiki</a> contains tips, tricks, tutorials, and documentation that anyone can edit.')?></li>
+	<li><a href="../audacity-manual-1.2.zip"><?=_('Download the Audacity manual in HTML format.')?></a> <?=_('(To use the manual, unzip it and open the "index.html" file.)')?></li>
+    	<li><a href="../audacity-manual-1.2.pdf"><?=_('Download the Audacity manual in PDF format.')?></a></li>
 </ul>
 
 <div lang="fr">
   <h3>Aide en Français</h3>
   <ul>
+    <li><a href="../localhelp/fr/audacity-1.2-help.htb">Menu d'aide d'Audacity en français</a> (Remplacer le fichier qui porte comme nom, audacity-1.2-help.htb par celui qu'on vous propose. <a href="http://zonelibre.grics.qc.ca/libre/article.php3?id_article=100">Aide</a>)</li>
     <li><a href="../audacity-mode-d'emploi.pdf">Mode d'emploi en français</a> (PDF, 700 ko.  Ce fichier peut prendre un bon moment de télécharger.)</li>
-  </ul>
+ </ul>
 </div>
+
+<div lang="de">
+  <h3>Hilfe auf Deutsch</h3>
+  <ul>
+    <li><a href="../localhelp/de/audacity-1.2-help.htb">Deutsche Hilfedatei (htb)</a> (Ersetzen Sie audacity-1.2-help.htb im Audacity Installation-Heft mit diesem Download.)</li>
+    <li><a href="../localhelp/de/audacity-deutsch-handbuch-23-Jan-2005.pdf">Deutsches Handbuch (PDF)</a>
+ </ul>
+</div>
+
+
 
 <?php
   include "../include/footer.inc.php";
--- sf-cvs/trunk/htdocs/help/index.php
+++ sf-cvs/trunk/htdocs/help/index.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2004 Matt Brubeck
+ * Copyright 2004 - 2008 Matt Brubeck, Gale Andrews
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/2.0/
  */
@@ -8,34 +8,32 @@
   $pageId = "";
   $pageTitle = _("Documentation and Support");
   include "../include/header.inc.php";
-
-  echo "<h2>$pageTitle</h2>";
-
-  // i18n-hint: If there are any special resources for Audacity users in your
-  // language (forums, documentation, mailing lists...), you can add links to
-  // them here.
-
-  echo '<p>';
-  echo '<b>';
-  echo _('New!');
-  echo '  ';
-  echo _('Have questions?  Post them to the <a href="http://audacityteam.org/forum/">Audacity Forum</a>.');
-  echo '</b>';
-  echo '</p>';
-
-  echo _('<p>If you have a question, check the list of <a href="faq">Frequently Asked Questions</a>.</p>
-<p>For instructions and help using Audacity, see:</p>
-<ul>
-  <li><a href="documentation">Documentation</a> - Read the user’s manual and quick reference.</li>
-  <li><a href="tutorials">Tutorials</a> - Step by step articles teach you how to use Audacity.</li>
-</ul>
-<p>If your question isn’t answered here, please post to the <a href="http://audacityteam.org/forum/">Audacity Forum</a> or <a href="../contact/">contact us</a>.</p>');
 ?>
 
-<h3><?=_('Other Resources')?></h3>
-<ul>
-	<li><?=_('<a href="http://audacityteam.org/wiki/">The Audacity Wiki</a> contains tips, tricks, and documentation that anyone can edit.')?></li>
-</ul>
+<h2><?=$pageTitle?></h2>
+
+<?php
+echo _('<p><b>Questions?</b> First, check our <a href="faq">Frequently Asked Questions</a> and the following resources:</p>
+
+<p>
+ <ul>
+   <li><a href="documentation">Documentation</a> - Read the user manual and quick reference.</li>
+   <li><a href="http://audacityteam.org/wiki">Audacity Wiki</a> is an extensive user-editable help resource including:
+    <ul><li><a href="http://audacityteam.org/wiki/index.php?title=Tutorials">Tutorials</a> - step-by-step guides on performing common tasks using Audacity.</li>
+     <li><a href="http://audacityteam.org/wiki/index.php?title=Tips">Tips</a> - these articles will help you work with and understand Audacity and your audio hardware.</li>
+    </ul>
+  </li>
+ </ul>
+</p>')?>
+
+<?php
+// i18n-hint: If there are any special sources of help (local forums, lists) for
+// Audacity users in your language (other than our Russian, French, German
+// and Spanish forums), please add links to them at the end of this paragraph
+// and at \htdocs\contact\index.php. If there is any translated documentation,
+// please add links to it at the bottom of \htdocs\help\documentation.php
+
+echo _('<p><b>Still need help?</b> Please <a href="../contact/">contact us</a>.</p>')?>
 
 <?php
   include "../include/footer.inc.php";
