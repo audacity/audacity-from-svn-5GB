e1e622f0703db4fa3fc04be2866a34b302c69936
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2008-04-02 18:31:35 +0000

    improve presentation of download boxes/updated Windows image

--- sf-cvs/trunk/htdocs/download/index.php
+++ sf-cvs/trunk/htdocs/download/index.php
@@ -1,6 +1,7 @@
 <?php
 /*
  * Copyright 2004 Matt Brubeck
+ * 2008 Gale Andrews, Vaughan Johnson
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/2.0/
  */
@@ -22,47 +23,52 @@
 <div class="download_container">
   <div class="downloads" id="stable_downloads">
     <div class="downloads_header">
-      <h3><?=_("Stable").": ".$stable_version?></h3>
-      <p><?=_("For all users")?></p>
+      <h3><?=_("Stable: 1.2 - for all users")?></h3>
+      <p><?=_("Latest versions:")?></p>
     </div>
     <div>
-      <h3><a href="windows"><img alt="" src="../images/win.png">
-      <?=_("Windows")?></a></h3>
+      <h3><a href="windows"><img alt="Windows" src="../images/new_win.png">
+      <?=_("Windows")?></a><span class="downloads_smaller"><?=_("&nbsp;&nbsp;1.2.6")?></span></h3>   
   
-      <h3><a href="mac"><img alt="" src="../images/mac.png">
-      <?=_("Mac OS X")?></a></h3>
-  
-      <h3><a href="linux"><img alt="" src="../images/unix.png">
-      <?=_("Linux/Unix")?></a></h3>
-    </div>
+      <h3><a href="mac"><img alt="Mac" src="../images/mac.png">
+      <?=_("Mac OS X")?></a><br><img src="../images/download_spacer.png"><span class="downloads_smaller"><?=_("&nbsp;1.2.5 (Intel) or 1.2.6 (PPC)")?></span></h3>
+
+      <h3><a href="linux"><img alt="Linux" src="../images/unix.png">
+      <?=_("Linux/Unix")?></a><br><img src="../images/download_spacer.png"><span class="downloads_smaller"><?=_("&nbsp;1.2.6 (in source code)")?></span></h3>
   </div>
+</div>
   <div class="downloads" id="beta_downloads">
     <div class="downloads_header">
-      <h3><?=_("Beta").": ".$beta_version?></h3>
-      <p><?=_("For advanced users")?></p>
+      <h3><?=_("Beta: 1.3 - for advanced users")?></h3>
+      <p><?=_("Latest versions:")?></p>
     </div>
     <div>
-      <h3><a href="beta_windows"><img alt="" src="../images/win.png">
-      <?=_("Windows")?></a></h3>
+      <h3><a href="beta_windows"><img alt="Windows" src="../images/new_win.png">
+      <?=_("Windows")?></a><span class="downloads_smaller"><?=_("&nbsp;&nbsp;1.3.4")?></span></h3>
   
-      <h3><a href="beta_mac"><img alt="" src="../images/mac.png">
-      <?=_("Mac OS X")?></a></h3>
+      <h3><a href="mac"><img alt="Mac" src="../images/mac.png">
+      <?=_("Mac OS X")?></a><br><img src="../images/download_spacer.png"><span class="downloads_smaller"><?=_("&nbsp;1.3.3 (Universal Binary)")?></span></h3>
   
-      <h3><a href="beta_linux"><img alt="" src="../images/unix.png">
-      <?=_("Linux/Unix")?></a></h3>
+      <h3><a href="beta_linux"><img alt="Linux" src="../images/unix.png">
+      <?=_("Linux/Unix")?></a><br><img src="../images/download_spacer.png"><span class="downloads_smaller"><?=_("&nbsp;1.3.4 (in source code)")?></span></h3>
   </div>
 </div>
 
 <div class="under_downloads">
-<p><?=_('You can also <a href="buy">purchase</a> a copy of Audacity on CD.')?></p>
 
-<p><?=_('If you wish to compile Audacity yourself, download the <a href="source">source code</a>.')?></p>
+<h3><?=_("Legacy Downloads")?></h3>
+ <p><?=_("For users of <b>Mac OS 9</b>, a legacy 1.0.0 version of Audacity is also available on the ")?><a href="mac"><?=_("Mac downloads page.")?></a></p>
 
-<p><?=_('You may modify, distribute, and sell Audacity under the <a href="../about/license">GNU GPL</a>.')?></p>
+<h3><?=_("Further information")?></h3>
+   <ul>
+   <li><p><?=_('You can also obtain Audacity as a <a href="buy">CD image file or purchasable CD</a>.')?></p></li>
+   <li><p><?=_('If you wish to compile Audacity yourself, please download the <a href="source">source code</a>.')?></p></li>
+   <li><p><?=_('You may copy, distribute, modify and/or resell Audacity, under the terms of the  <a href="../about/license">GNU GPL</a>.')?></p></li>
+  </ul>
 
-<p><?=_('We welcome <a href="../community/donate">donations</a> to support Audacity development.')?></p>
-
+<p>&nbsp;</p>
 </div>
+
 
 <?php
   include "../include/footer.inc.php";
