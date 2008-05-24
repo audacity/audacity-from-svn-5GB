f6456d843eb3856b5ef491d92d6a0dab39687262
llucius <llucius@47890b92-0858-11df-a26f-8b716316a5bc>
2008-05-24 02:57:39 +0000

    Re-indented...it was driving me crazy. Removed all WXDLLEXPORTs.

--- sf-cvs/trunk/lib-src/FileDialog/gtk/private.h
+++ sf-cvs/trunk/lib-src/FileDialog/gtk/private.h
@@ -4,7 +4,7 @@
 // Author:      Vadim Zeitlin
 // Modified by: Leland Lucius
 // Created:     12.03.02
-// RCS-ID:      $Id: private.h,v 1.2 2007-04-10 02:22:21 llucius Exp $
+// RCS-ID:      $Id: private.h,v 1.3 2008-05-24 02:57:39 llucius Exp $
 // Copyright:   (c) 2002 Vadim Zeitlin <vadim@wxwidgets.org>
 // Licence:     wxWindows licence
 //
@@ -23,115 +23,115 @@
 // fail all version tests if the GTK+ version is so ancient that it doesn't
 // even have GTK_CHECK_VERSION
 #ifndef GTK_CHECK_VERSION
-    #define GTK_CHECK_VERSION(a, b, c) 0
+#define GTK_CHECK_VERSION(a, b, c) 0
 #endif
 
 #ifdef __WXGTK20__
 #if wxUSE_UNICODE
-    #define wxGTK_CONV(s) wxConvUTF8.cWX2MB(s)
-    #define wxGTK_CONV_BACK(s) wxConvUTF8.cMB2WX(s)
+#define wxGTK_CONV(s) wxConvUTF8.cWX2MB(s)
+#define wxGTK_CONV_BACK(s) wxConvUTF8.cMB2WX(s)
 #else
-    #define wxGTK_CONV(s) wxConvUTF8.cWC2MB( wxConvLocal.cWX2WC(s) )
-    #define wxGTK_CONV_BACK(s)  wxConvLocal.cWC2WX( (wxConvUTF8.cMB2WC( s ) ) )
+#define wxGTK_CONV(s) wxConvUTF8.cWC2MB( wxConvLocal.cWX2WC(s) )
+#define wxGTK_CONV_BACK(s)  wxConvLocal.cWC2WX( (wxConvUTF8.cMB2WC( s ) ) )
 #endif
 #else
-    #define wxGTK_CONV(s) s.c_str()
-    #define wxGTK_CONV_BACK(s) s
+#define wxGTK_CONV(s) s.c_str()
+#define wxGTK_CONV_BACK(s) s
 #endif
 
 
 // GTK+ 2.0 compatibility define is broken when used from C++ as it
 // casts enum to int implicitly
 #ifdef __WXGTK20__
-    #undef gtk_signal_disconnect_by_func
-    #define gtk_signal_disconnect_by_func(object,func,data) \
-        gtk_signal_compat_matched((object), (func), (data), \
-                                  (GSignalMatchType)(G_SIGNAL_MATCH_FUNC | \
-                                                     G_SIGNAL_MATCH_DATA), 0)
+#undef gtk_signal_disconnect_by_func
+#define gtk_signal_disconnect_by_func(object,func,data) \
+gtk_signal_compat_matched((object), (func), (data), \
+(GSignalMatchType)(G_SIGNAL_MATCH_FUNC | \
+G_SIGNAL_MATCH_DATA), 0)
 #endif
 
 // child is not a member of GTK_BUTTON() any more in GTK+ 2.0
 #ifdef __WXGTK20__
-    #define BUTTON_CHILD(w) GTK_BIN((w))->child
+#define BUTTON_CHILD(w) GTK_BIN((w))->child
 #else
-    #define BUTTON_CHILD(w) GTK_BUTTON((w))->child
+#define BUTTON_CHILD(w) GTK_BUTTON((w))->child
 #endif
 
 // event_window has disappeared from GtkToggleButton in GTK+ 2.0
 #ifdef __WXGTK20__
-    #define TOGGLE_BUTTON_EVENT_WIN(w) GTK_BUTTON((w))->event_window
+#define TOGGLE_BUTTON_EVENT_WIN(w) GTK_BUTTON((w))->event_window
 #else
-    #define TOGGLE_BUTTON_EVENT_WIN(w) GTK_TOGGLE_BUTTON((w))->event_window
+#define TOGGLE_BUTTON_EVENT_WIN(w) GTK_TOGGLE_BUTTON((w))->event_window
 #endif
 
 // gtk_editable_{copy|cut|paste}_clipboard() had an extra argument under
 // previous GTK+ versions but no more
 #if defined(__WXGTK20__) || (GTK_MINOR_VERSION > 0)
-    #define DUMMY_CLIPBOARD_ARG
+#define DUMMY_CLIPBOARD_ARG
 #else
-    #define DUMMY_CLIPBOARD_ARG  ,0
+#define DUMMY_CLIPBOARD_ARG  ,0
 #endif
 
 // _GtkEditable is now private
 #ifdef __WXGTK20__
-    #define GET_EDITABLE_POS(w) gtk_editable_get_position(GTK_EDITABLE(w))
-    #define SET_EDITABLE_POS(w, pos) \
-        gtk_editable_set_position(GTK_EDITABLE(w), (pos))
+#define GET_EDITABLE_POS(w) gtk_editable_get_position(GTK_EDITABLE(w))
+#define SET_EDITABLE_POS(w, pos) \
+gtk_editable_set_position(GTK_EDITABLE(w), (pos))
 #else
-    #define GET_EDITABLE_POS(w) GTK_EDITABLE((w))->current_pos
-    #define SET_EDITABLE_POS(w, pos) \
-        GTK_EDITABLE((w))->current_pos = (pos)
+#define GET_EDITABLE_POS(w) GTK_EDITABLE((w))->current_pos
+#define SET_EDITABLE_POS(w, pos) \
+GTK_EDITABLE((w))->current_pos = (pos)
 #endif
 
 // this GtkNotebook struct field has been renamed
 #ifdef __WXGTK20__
-    #define NOTEBOOK_PANEL(nb)  GTK_NOTEBOOK(nb)->event_window
+#define NOTEBOOK_PANEL(nb)  GTK_NOTEBOOK(nb)->event_window
 #else
-    #define NOTEBOOK_PANEL(nb)  GTK_NOTEBOOK(nb)->panel
+#define NOTEBOOK_PANEL(nb)  GTK_NOTEBOOK(nb)->panel
 #endif
 
 #ifdef __WXGTK20__
-    #define SCROLLBAR_CBACK_ARG
-    #define GET_SCROLL_TYPE(w)   GTK_SCROLL_JUMP
+#define SCROLLBAR_CBACK_ARG
+#define GET_SCROLL_TYPE(w)   GTK_SCROLL_JUMP
 #else
-    #define SCROLLBAR_CBACK_ARG
-    #define GET_SCROLL_TYPE(w)   GTK_RANGE((w))->scroll_type
+#define SCROLLBAR_CBACK_ARG
+#define GET_SCROLL_TYPE(w)   GTK_RANGE((w))->scroll_type
 #endif
 
 // translate a GTK+ scroll type to a wxEventType
 inline wxEventType GtkScrollTypeToWx(guint scrollType)
 {
-    wxEventType command;
-    switch ( scrollType )
-    {
-        case GTK_SCROLL_STEP_BACKWARD:
-            command = wxEVT_SCROLL_LINEUP;
-            break;
-
-        case GTK_SCROLL_STEP_FORWARD:
-            command = wxEVT_SCROLL_LINEDOWN;
-            break;
-
-        case GTK_SCROLL_PAGE_BACKWARD:
-            command = wxEVT_SCROLL_PAGEUP;
-            break;
-
-        case GTK_SCROLL_PAGE_FORWARD:
-            command = wxEVT_SCROLL_PAGEDOWN;
-            break;
-
-        default:
-            command = wxEVT_SCROLL_THUMBTRACK;
-    }
-
-    return command;
+   wxEventType command;
+   switch ( scrollType )
+   {
+      case GTK_SCROLL_STEP_BACKWARD:
+         command = wxEVT_SCROLL_LINEUP;
+         break;
+         
+      case GTK_SCROLL_STEP_FORWARD:
+         command = wxEVT_SCROLL_LINEDOWN;
+         break;
+         
+      case GTK_SCROLL_PAGE_BACKWARD:
+         command = wxEVT_SCROLL_PAGEUP;
+         break;
+         
+      case GTK_SCROLL_PAGE_FORWARD:
+         command = wxEVT_SCROLL_PAGEDOWN;
+         break;
+         
+      default:
+         command = wxEVT_SCROLL_THUMBTRACK;
+   }
+   
+   return command;
 }
 
 inline wxEventType GtkScrollWinTypeToWx(guint scrollType)
 {
-    // GtkScrollTypeToWx() returns SCROLL_XXX, not SCROLLWIN_XXX as we need
-    return GtkScrollTypeToWx(scrollType) +
-            wxEVT_SCROLLWIN_TOP - wxEVT_SCROLL_TOP;
+   // GtkScrollTypeToWx() returns SCROLL_XXX, not SCROLLWIN_XXX as we need
+   return GtkScrollTypeToWx(scrollType) +
+   wxEVT_SCROLLWIN_TOP - wxEVT_SCROLL_TOP;
 }
 
 // Needed for implementing e.g. combobox on wxGTK within a modal dialog.
@@ -140,7 +140,7 @@ void wxRemoveGrab(wxWindow* window);
 
 #ifdef __WXGTK20__
 // Escapes string so that it is valid Pango markup XML string:
-WXDLLIMPEXP_CORE wxString wxEscapeStringForPangoMarkup(const wxString& str);
+wxString wxEscapeStringForPangoMarkup(const wxString& str);
 #endif
 
 // The declaration for gtk_icon_size_lookup was accidentally ifdefed out in
--- sf-cvs/trunk/lib-src/FileDialog/mac/FileDialogPrivate.h
+++ sf-cvs/trunk/lib-src/FileDialog/mac/FileDialogPrivate.h
@@ -4,7 +4,7 @@
 // Author:      Stefan Csomor
 // Modified by: Leland Lucius
 // Created:     1998-01-01
-// RCS-ID:      $Id: FileDialogPrivate.h,v 1.2 2008-03-10 22:20:18 richardash1981 Exp $
+// RCS-ID:      $Id: FileDialogPrivate.h,v 1.3 2008-05-24 02:57:39 llucius Exp $
 // Copyright:   (c) Stefan Csomor
 // Licence:     wxWindows licence
 //
@@ -19,38 +19,38 @@
 // FileDialog
 //-------------------------------------------------------------------------
 
-class WXDLLEXPORT FileDialog: public wxFileDialogBase
+class FileDialog: public wxFileDialogBase
 {
-DECLARE_DYNAMIC_CLASS(FileDialog)
-protected:
-    long m_dialogStyle;
-    wxArrayString m_fileNames;
-    wxArrayString m_paths;
-
-    wxString m_buttonlabel;
-    fdCallback m_callback;
-    void *m_cbdata;
+   DECLARE_DYNAMIC_CLASS(FileDialog)
+ protected:
+   long m_dialogStyle;
+   wxArrayString m_fileNames;
+   wxArrayString m_paths;
+   
+   wxString m_buttonlabel;
+   fdCallback m_callback;
+   void *m_cbdata;
    static wxString ConvertSlashInFileName(const wxString& filePath);
    
-public:
-    FileDialog(wxWindow *parent,
-               const wxString& message = wxFileSelectorPromptStr,
-               const wxString& defaultDir = wxEmptyString,
-               const wxString& defaultFile = wxEmptyString,
-               const wxString& wildCard = wxFileSelectorDefaultWildcardStr,
-               long style = 0,
-               const wxPoint& pos = wxDefaultPosition);
-
-    virtual void GetPaths(wxArrayString& paths) const { paths = m_paths; }
-    virtual void GetFilenames(wxArrayString& files) const { files = m_fileNames ; }
-
-    virtual int ShowModal();
-    
-    // not supported for file dialog, RR
-    virtual void DoSetSize(int WXUNUSED(x), int WXUNUSED(y),
-                           int WXUNUSED(width), int WXUNUSED(height),
-                           int WXUNUSED(sizeFlags) = wxSIZE_AUTO) {}
-
+ public:
+   FileDialog(wxWindow *parent,
+              const wxString& message = wxFileSelectorPromptStr,
+              const wxString& defaultDir = wxEmptyString,
+              const wxString& defaultFile = wxEmptyString,
+              const wxString& wildCard = wxFileSelectorDefaultWildcardStr,
+              long style = 0,
+              const wxPoint& pos = wxDefaultPosition);
+   
+   virtual void GetPaths(wxArrayString& paths) const { paths = m_paths; }
+   virtual void GetFilenames(wxArrayString& files) const { files = m_fileNames ; }
+   
+   virtual int ShowModal();
+   
+   // not supported for file dialog, RR
+   virtual void DoSetSize(int WXUNUSED(x), int WXUNUSED(y),
+                          int WXUNUSED(width), int WXUNUSED(height),
+                          int WXUNUSED(sizeFlags) = wxSIZE_AUTO) {}
+   
    virtual void EnableButton(wxString label, fdCallback cb, void *cbdata);
    virtual void ClickButton(int index);
 };
