6ab84fa08af876f0b793242590fe73353cd19b40
llucius <llucius@47890b92-0858-11df-a26f-8b716316a5bc>
2008-05-27 05:24:09 +0000

    Moving project files.

--- sf-cvs/trunk/audacity-src/win/portaudio.h
+++ sf-cvs/trunk/audacity-src/win/portaudio.h
@@ -5,42 +5,12 @@
 // Don't change anything below here
 #if USE_PORTAUDIO_V19
 
-#  include "..\lib-src\portaudio-v19\include\portaudio.h"
-
-#  if defined(__WXMSW__)
-#     if DEBUG
-#        if wxUSE_UNICODE
-#           pragma comment(lib, "pastaticwmmev19ud")
-#        else
-#           pragma comment(lib, "pastaticwmmev19d")
-#        endif
-#     else
-#        if wxUSE_UNICODE
-#           pragma comment(lib, "pastaticwmmev19u")
-#        else
-#           pragma comment(lib, "pastaticwmmev19")
-#        endif
-#     endif
-#  endif
+#  include "../../../lib-src/portaudio-v19/include/portaudio.h"
+#  pragma comment(lib, "portaudio-v19")
 
 #else
 
-#   include "..\lib-src\portaudio\pa_common\portaudio.h"
-
-#  if defined(__WXMSW__)
-#     if DEBUG
-#        if wxUSE_UNICODE
-#           pragma comment(lib, "pastaticwmmeud")
-#        else
-#           pragma comment(lib, "pastaticwmmed")
-#        endif
-#     else
-#        if wxUSE_UNICODE
-#           pragma comment(lib, "pastaticwmmeu")
-#        else
-#           pragma comment(lib, "pastaticwmme")
-#        endif
-#     endif
-#  endif
+#  include "../../../lib-src/portaudio/pa_common/portaudio.h"
+#  pragma comment(lib, "portaudio")
 
 #endif
