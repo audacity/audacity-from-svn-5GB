c7678b118ab68f1f0d81865a31fe70bff38c6407
llucius <llucius@47890b92-0858-11df-a26f-8b716316a5bc>
2008-06-03 14:35:18 +0000

    Added USE_FFMPEG define (undef'd by default) to get builds working. Removed accessibility information that's no longer needed.

--- sf-cvs/trunk/audacity-src/win/configwin.h
+++ sf-cvs/trunk/audacity-src/win/configwin.h
@@ -14,75 +14,11 @@
 #define USE_SOUNDTOUCH 1
 #define USE_LIBTWOLAME 1
 #undef USE_VST
+#undef USE_FFMPEG
 
 #define INSTALL_PREFIX "."
 
 #define rint(x)   (floor((x)+0.5f)) 
-
-// Override the default wxWidgets control names.
-//
-// When wxUSE_ACCESSIBILITY is defined, wxWidgets supplies a wxAccessible
-// class for each window it creates.  Unfortunately, as part of that process
-// it also provides a default name for the window and Windows (or MSAA) passes
-// that back to the accessibility aid.
-//
-// So, for example, all edit boxes are identified to the user as "text" without
-// any indication of what the field really is.  And the same thing will happen
-// for any wxWidget control that doesn't display its own label.
-//
-// Controls like the radio button or checkbox do display their own label, so
-// these aren't a problem and do not need to be overridden.
-//
-// To workaround the issue, we override the name with a null string.  When
-// Windows (or MSAA) gets the null string, it realizes that it needs to
-// perform a search for an appropriate label to present to the user.
-//
-// In most cases, the search identifies an appropriate label.  However, every
-// so often, it will get it wrong or not be able to find one at all.  In these
-// cases, you may need to specifically precede a control with a label.
-//
-// (A good example is the selection bar.  I still haven't figured out what to
-// do about that one.)
-//
-// If you need to override any others controls:
-//
-// 1)  Add a define to override the class name like:
-//     #define wxTextCtrlNameStr overrideTextCtrlNameStr
-//
-// 2)  Add an extern for the new name like:
-//     extern const char *overrideTextCtrlNameStr;
-//
-// 3)  In "AudacityApp.h", add a null wxString for the new name like:
-//     const char *overrideTextCtrlNameStr = wxT("");
-//
-// 4)  Include "Audacity.h" in the source file creating the control
-//
-#include <wx/defs.h>
-
-#if wxUSE_ACCESSIBILITY
-#if 0
-#define _PTR_
-
-#if wxCHECK_VERSION(2, 8, 0)
-extern WXEXPORT const wxChar overrideTextCtrlNameStr[];
-extern WXEXPORT const wxChar overrideChoiceNameStr[];
-extern WXEXPORT const wxChar overrideComboBoxNameStr[];
-extern WXEXPORT const wxChar overrideSliderNameStr[];
-#else
-extern const wxChar *overrideTextCtrlNameStr;
-extern const wxChar *overrideChoiceNameStr;
-extern const wxChar *overrideComboBoxNameStr;
-extern const wxChar *overrideSliderNameStr;
-#endif
-
-#define wxTextCtrlNameStr overrideTextCtrlNameStr
-#define wxChoiceNameStr overrideChoiceNameStr
-#define wxComboBoxNameStr overrideComboBoxNameStr
-#define wxSliderNameStr overrideSliderNameStr
-
-#undef _PTR_
-#endif
-#endif
 
 #ifdef _DEBUG
     #ifdef _MSC_VER
