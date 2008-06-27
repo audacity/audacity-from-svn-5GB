da15c2b17d5fcaeeccc714d855c3c70378f50ad0
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2008-06-27 23:54:58 +0000

    Remove redundant  USE_FFMPEG. Re-sort back into alpha order. Add commented-out USE_LIBLRDF.

--- sf-cvs/trunk/audacity-src/win/configwin.h
+++ sf-cvs/trunk/audacity-src/win/configwin.h
@@ -1,21 +1,21 @@
 // Microsoft Windows specific include file
 
 #define MP3SUPPORT 1
+#define USE_FFMPEG 1	//define this to build with ffmpeg import/export
 #define USE_LADSPA 1
 #define USE_LIBFLAC 1
 #define USE_LIBID3TAG 1
+// #define USE_LIBLRDF 1
 #define USE_LIBMAD 1
-//#define USE_FFMPEG 1	//define this to build with ffmpeg import/export
 #define USE_LIBRESAMPLE 1
 #undef USE_LIBSAMPLERATE
+#define USE_LIBTWOLAME 1
 #define USE_LIBVORBIS 1
 #define USE_NYQUIST 1
-#define USE_VAMP 1
 #define USE_PORTMIXER 1
 #define USE_SOUNDTOUCH 1
-#define USE_LIBTWOLAME 1
+#define USE_VAMP 1
 #undef USE_VST
-#define USE_FFMPEG
 
 #define INSTALL_PREFIX "."
 
