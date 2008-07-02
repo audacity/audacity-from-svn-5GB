8c817dcbdf68790dac2e43792f9bdefc6bbe45da
martynshaw <martynshaw@47890b92-0858-11df-a26f-8b716316a5bc>
2008-07-02 23:39:28 +0000

    Turning on EXPERIMENTAL_RULER_AUTOSIZE as I believe it now works.  Please report any issues on audacity-devel.

--- sf-cvs/trunk/audacity-src/src/Experimental.h
+++ sf-cvs/trunk/audacity-src/src/Experimental.h
@@ -84,7 +84,9 @@
 // With Waveform (dB) view, at some vertical sizes of track, there appears to be a loop
 // where the ruler gets bigger and smaller repeatedly, using up an awful lot of cycles.
 // Really needs to be fixed before this is turned on.
-//#define EXPERIMENTAL_RULER_AUTOSIZE
+// MJS, 30 June 2008
+// Fixed the loop problem, so turned this on as I think it's valid.
+#define EXPERIMENTAL_RULER_AUTOSIZE
 
 #ifdef LOGARITHMIC_SPECTRUM
    // AM, 20.Nov 2007: 
