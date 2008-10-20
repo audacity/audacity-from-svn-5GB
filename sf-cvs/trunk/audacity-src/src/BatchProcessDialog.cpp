48afed668bbc223d86c24e2d6f9ef59fa6d40feb
prokoudine <prokoudine@47890b92-0858-11df-a26f-8b716316a5bc>
2008-10-20 22:58:53 +0000

    marked user visible messages for translation, updated Russian translation

--- sf-cvs/trunk/audacity-src/src/BatchProcessDialog.cpp
+++ sf-cvs/trunk/audacity-src/src/BatchProcessDialog.cpp
@@ -64,7 +64,7 @@ BatchProcessDialog::BatchProcessDialog(wxWindow * parent):
    AudacityProject * p = GetActiveProject();
    if (p->GetCleanSpeechMode())
    {
-      SetTitle(wxT("CleanSpeech Batch Processing"));
+      SetTitle(_("CleanSpeech Batch Processing"));
    }
 
    SetLabel(_("Apply Chain"));         // Provide visual label
@@ -203,10 +203,10 @@ void BatchProcessDialog::OnApplyToFiles(wxCommandEvent &event)
 
    wxString path = gPrefs->Read(wxT("/DefaultOpenPath"), ::wxGetCwd());
    wxString prompt =  project->GetCleanSpeechMode() ? 
-      wxT("Select vocal file(s) for batch CleanSpeech Chain...") :
+      _("Select vocal file(s) for batch CleanSpeech Chain...") :
       _("Select file(s) for batch processing...");
    wxString fileSelector = project->GetCleanSpeechMode() ? 
-      wxT("Vocal files (*.wav;*.mp3)|*.wav;*.mp3|WAV files (*.wav)|*.wav|MP3 files (*.mp3)|*.mp3") :
+      _("Vocal files (*.wav;*.mp3)|*.wav;*.mp3|WAV files (*.wav)|*.wav|MP3 files (*.mp3)|*.mp3") :
       _("All files (*.*)|*.*|WAV files (*.wav)|*.wav|AIFF files (*.aif)|*.aif|AU files (*.au)|*.au|MP3 files (*.mp3)|*.mp3|Ogg Vorbis files (*.ogg)|*.ogg|FLAC files (*.flac)|*.flac"
        );
 
