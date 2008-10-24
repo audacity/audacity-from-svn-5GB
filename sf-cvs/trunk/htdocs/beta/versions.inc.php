a3c4fc3eb87e964bb9190d3f84c4ccf935984e8e
vjohnson <vjohnson@47890b92-0858-11df-a26f-8b716316a5bc>
2008-10-24 18:21:08 +0000

    update for 1.3.6

--- sf-cvs/trunk/htdocs/beta/versions.inc.php
+++ sf-cvs/trunk/htdocs/beta/versions.inc.php
@@ -1,12 +1,12 @@
 <?php
 // The main version number, when we don't distinguish between platforms
-define('beta_version', '1.3.5');
+define('beta_version', '1.3.6');
 
 // Note: Sizes are in MB.
-define('beta_macosx_version', '1.3.5');
+define('beta_macosx_version', '1.3.6');
 
-define('macosx_ub_version', '1.3.5d');
-define('macosx_ub_size',    8.1);
+define('macosx_ub_version', '1.3.6');
+define('macosx_ub_size',    9.9);
 
 //define('macosx_ppc_version', '1.3.3');
 //define('macosx_ppc_size',    7.2);
@@ -22,29 +22,29 @@ define('macosx_ub_size',    8.1);
 
 
 
-define('beta_win_exe_version', '1.3.5');
+define('beta_win_exe_version', '1.3.6');
 
-define('win_exe_version', '1.3.5');
-define('win_exe_size',    3.0);
+define('win_exe_version', '1.3.6');
+define('win_exe_size',    4.0);
 
-define('win_zip_version', '1.3.5');
-define('win_zip_size',    4.5);
+define('win_zip_version', '1.3.6');
+define('win_zip_size',    6.0);
 
-define('win_exe_unicode_version', '1.3.5');
-define('win_exe_unicode_size',    3.0);
+define('win_exe_unicode_version', '1.3.6');
+define('win_exe_unicode_size',    4.1);
 
-define('win_zip_unicode_version', '1.3.5');
-define('win_zip_unicode_size',    4.6);
+define('win_zip_unicode_version', '1.3.6');
+define('win_zip_unicode_size',    6.1);
 
 
 define('ladspa_version', '0.4.15');
 define('ladspa_size',    1.5);
 
 
-define('beta_src_version', '1.3.5');
-define('src_version', '1.3.5');
-define('src_size',    4.1);
-define('src_suffix',  '.tar.bz2');
+define('beta_src_version', '1.3.6');
+define('src_version', '1.3.6');
+define('src_size',    6.2);
+define('src_suffix',  '.tar.gz');
 
 $win_exe_url = "audacity-win/audacity-win-".win_exe_version.".exe";
 $win_zip_url = "audacity-win-zip/audacity-win-".win_zip_version.".zip";
