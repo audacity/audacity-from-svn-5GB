16125c3db983e133f140d0d3bb7e8213d974e6f9
buanzo <buanzo@47890b92-0858-11df-a26f-8b716316a5bc>
2008-10-26 13:25:09 +0000

    Updated Languages.cpp for new Indonesian translation.

--- sf-cvs/trunk/audacity-src/src/Languages.cpp
+++ sf-cvs/trunk/audacity-src/src/Languages.cpp
@@ -113,6 +113,8 @@ void GetLanguages(wxArrayString &langCodes, wxArrayString &langNames)
    localLanguageName[wxT("fi")] = wxT("Suomi");
    localLanguageName[wxT("fr")] = wxT("Francais");
    localLanguageName[wxT("ga")] = wxT("Gaeilge");
+   // need to replace Indonesian with proper translation. Translator was asked already, but better add here than break app.
+   localLanguageName[wxT("id")] = wxT("Indonesian");
    localLanguageName[wxT("it")] = wxT("Italiano");
    localLanguageName[wxT("ja")] = wxT("Nihongo");
    localLanguageName[wxT("lo")] = wxT("Lao"); // aka Laothian
