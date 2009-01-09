e99e485f4d080099414e6e8c7e3385c378587b9d
llucius <llucius@47890b92-0858-11df-a26f-8b716316a5bc>
2009-01-09 22:36:29 +0000

    Fix Portable Settings issue on OSX. Utilize newer wx classes in PlatformCompatibility.cpp.  (probably could get rid of this now)

--- sf-cvs/trunk/audacity-src/src/PlatformCompatibility.cpp
+++ sf-cvs/trunk/audacity-src/src/PlatformCompatibility.cpp
@@ -25,120 +25,30 @@
 #include <wx/stdpaths.h>
 #include <wx/app.h>
 
-#ifdef __WXMAC__
-#include <wx/mac/private.h>
-#endif
-
 #include "AudacityApp.h"
 #include "PlatformCompatibility.h"
 
 wxString PlatformCompatibility::GetLongFileName(const wxString& shortFileName)
 {
-#ifndef _WIN32
+   wxFileName fn(shortFileName);
 
-   // On other platforms than Win32, this function is only a dummy
-   return shortFileName;
-
-#else
-
-   wxString stringLeft = shortFileName;
-   wxString s;
-   wxString longFileName;
-
-   while (!stringLeft.IsEmpty())
-   {
-      // Get next path component
-      int end = stringLeft.Find('\\');
-      
-      if (end == -1) {
-         s = stringLeft;
-         stringLeft.Empty();
-      } else {
-         s = stringLeft.Left(end);
-         stringLeft = stringLeft.Mid(end + 1);
-      }
-
-      // Process it unless it is empty, or the drive name
-      if (!(s.IsEmpty() || (s.Length() == 2 && s.GetChar(1) == ':')))
-      {
-         // Now resolve for _every_ path component the long file name.
-         // The standard way (only resolve components with '~' in it) is not
-         // enough, because the user may have NameNumericTail turned off.
-         wxString pathToFind = longFileName + s;
-         WIN32_FIND_DATA findData;
-         HANDLE findHandle = FindFirstFile(pathToFind.fn_str(), &findData);
-
-         if (findHandle != INVALID_HANDLE_VALUE)
-         {
-            s = findData.cFileName; // cFileName always contains long file name
-            FindClose(findHandle);
-         }
-      }
-
-      // Note that we must be careful here to prevent discarding any
-      // backslashes. It could e.g. be a network name, too!
-      longFileName += s;
-      if (end != -1)
-         longFileName += wxT("\\");
-   }
-
-   return longFileName;
-
-#endif
+   return fn.GetLongPath();
 }
 
-//
-// Taken from http://wxwidgets.org/docs/technote/install.htm
-//
 wxString PlatformCompatibility::GetExecutablePath()
 {
-    static bool found = false;
-    static wxString path;
+   static bool found = false;
+   static wxString path;
 
-    if (found)
-        return path;
-    else
-    {
-#ifdef __WXMSW__
+   if (!found) {
+      wxStandardPaths std;
 
-        wxChar buf[512];
-        *buf = '\0';
-        GetModuleFileName(NULL, buf, 511);
-        path = buf;
+      path = std.GetExecutablePath();
 
-#elif defined(__WXMAC__)
+      found = true;
+   }
 
-        ProcessInfoRec processinfo;
-        ProcessSerialNumber procno ;
-        FSSpec fsSpec;
-
-        procno.highLongOfPSN = 0 ;
-        procno.lowLongOfPSN = kCurrentProcess ;
-        processinfo.processInfoLength = sizeof(ProcessInfoRec);
-        processinfo.processName = NULL;
-        processinfo.processAppSpec = &fsSpec;
-
-        GetProcessInformation( &procno , &processinfo ) ;
-        path = wxMacFSSpec2MacFilename(&fsSpec);
-#else
-        wxString argv0 = wxGetApp().argv[0];
-
-        if (wxIsAbsolutePath(argv0))
-            path = argv0;
-        else
-        {
-            wxPathList pathlist;
-            pathlist.AddEnvList(wxT("PATH"));
-            path = pathlist.FindAbsoluteValidPath(argv0);
-        }
-
-        wxFileName filename(path);
-        filename.Normalize();
-        path = filename.GetFullPath();
-#endif
-        found = true;
-        return path;
-    }
+   return path;
 }
 
 wxString PlatformCompatibility::ConvertSlashInFileName(const wxString& filePath)
