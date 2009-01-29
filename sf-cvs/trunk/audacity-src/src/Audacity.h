149f110485ccc4fb04658754f5f8c20a2ade3b1c
llucius <llucius@47890b92-0858-11df-a26f-8b716316a5bc>
2009-01-29 05:19:41 +0000

    And we start again...

--- sf-cvs/trunk/audacity-src/src/Audacity.h
+++ sf-cvs/trunk/audacity-src/src/Audacity.h
@@ -20,9 +20,9 @@
 // Increment as appropriate every time you release a new version
 #define AUDACITY_VERSION   1
 #define AUDACITY_RELEASE   3
-#define AUDACITY_REVISION  7
+#define AUDACITY_REVISION  8
 #define AUDACITY_MODLEVEL  0
-#define AUDACITY_SUFFIX    wxT("") // wxT("-alpha_") __TDATE__
+#define AUDACITY_SUFFIX    wxT("-alpha_") __TDATE__ // wxT("") 
 
 #define AUDACITY_MAKESTR( x ) #x
 #define AUDACITY_QUOTE( x ) AUDACITY_MAKESTR( x )
