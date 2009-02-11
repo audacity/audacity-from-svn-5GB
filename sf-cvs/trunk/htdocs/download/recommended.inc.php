e7f2f82a9e82d20f653e6e03a37902b89f0d0241
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2009-02-11 19:42:10 +0000

    SF changed their panel colour to black

--- sf-cvs/trunk/htdocs/download/recommended.inc.php
+++ sf-cvs/trunk/htdocs/download/recommended.inc.php
@@ -7,4 +7,4 @@
 
 /* This is the boilerplate text for the recommended download.
    Note that users no longer have to choose a mirror. */
-?><p><?php echo _('Click the appropriate link below, which takes you to the SourceForge download page - don\'t "save link as.." or "save target as..". The SourceForge download should start automatically. If it does not, click the links in the orange panel marked "direct link" or "mirror". <b>Only these links and the automatic download are authorized versions of Audacity</b>. Disable any automatic download managers if the download is incorrect.');?></p>
+?><p><?php echo _('Click the appropriate link below, which takes you to the SourceForge download page - don\'t "save link as.." or "save target as..". The SourceForge download should start automatically. If it does not, click the links in the black panel marked "direct link" or "mirror". <b>Only these links and the automatic download are authorized versions of Audacity</b>. Disable any automatic download managers if the download is incorrect.');?></p>
