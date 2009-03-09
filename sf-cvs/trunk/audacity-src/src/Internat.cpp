e30d3779927c418c20cb56385436960a3fbffabf
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2009-03-09 23:53:41 +0000

    add an include to fix compilation in MSVC

--- sf-cvs/trunk/audacity-src/src/Internat.cpp
+++ sf-cvs/trunk/audacity-src/src/Internat.cpp
@@ -20,7 +20,7 @@ and on Mac OS X for the filesystem.
 
 *//*******************************************************************/
 
-
+#include <wx/msgdlg.h> 
 #include <wx/log.h>
 #include <wx/intl.h>
 #include <wx/filename.h>
