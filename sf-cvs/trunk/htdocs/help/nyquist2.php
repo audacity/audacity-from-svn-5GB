dc9f65ab4f44a4e69dd8e6e8df456a72002e298c
richardash1981 <richardash1981@47890b92-0858-11df-a26f-8b716316a5bc>
2009-03-13 17:30:17 +0000

    Re-work nyquist help into nice short strings so it is easier to translate, and indeed works at all.

--- sf-cvs/trunk/htdocs/help/nyquist2.php
+++ sf-cvs/trunk/htdocs/help/nyquist2.php
@@ -19,169 +19,84 @@
 	<li><a href="nyquist3"><?=_("Creating Nyquist Plug-ins")?></a></li>
 </ol>
 
-<?=_('<p>What makes Nyquist distinct from Lisp is that it is designed to work with sound, and has lots of built-in primitives and functions that synthesize, analyze, and manipulate sounds.  Within Audacity, this makes it relatively easy to build complicated effects out of Nyquist\'s palette of built-in functions.</p>
+<?php
+printf('<p>%s</p>', _('What makes Nyquist distinct from Lisp is that it is designed to work with sound, and has lots of built-in primitives and functions that synthesize, analyze, and manipulate sounds.  Within Audacity, this makes it relatively easy to build complicated effects out of Nyquist\'s palette of built-in functions.'));
+printf('<p>%s</p>', _('In Nyquist, a variable can hold a sound just as easily as it can hold a number or a string.  There are a lot of functions provided that allow you to stretch, distort, and combine sounds very efficiently.  It is even possible to "rip apart" a sound and access its individual samples, but that\'s beyond the scope of this tutorial.'));
+printf(_('%sTo try out a Nyquist expression in Audacity, you can use "Nyquist Prompt" in the Effect menu.  Whatever audio you have selected will be in the variable %s, and the selection will be replaced with the result of the Nyquist expression you enter.  In Part 3, you will learn how to create a plug-in effect using Nyquist.%s'), '<p>','<code>s</code>', '</p>');
 
-<p>In Nyquist, a variable can hold a sound just as easily as it can hold a number or a string.  There are a lot of functions provided that allow you to stretch, distort, and combine sounds very efficiently.  It is even possible to "rip apart" a sound and access its individual samples, but that\'s beyond the scope of this tutorial.</p>
+printf('<h3>%s</h3>', _('Synthesizing'));
 
-<p>To try out a Nyquist expression in Audacity, you can use "Nyquist Prompt" in the Effect menu.  Whatever audio you have selected will be in the variable <code>s</code>, and the selection will be replaced with the result of the Nyquist expression you enter.  In Part 3, you will learn how to create a plug-in effect using Nyquist.</p>
-
-<h3>Synthesizing</h3>
-
-<p>The following functions all create new sounds.  You can use them to create "generate" plug-in effects, or you can combine these synthesized sounds with selected audio to produce interesting effects.</p>
-
-<table class="function-list" class="function-list">
-<tr>
-<td>(noise)</td>
-<td>Generates white noise</td>
-</tr>
-
-<tr>
-<td>(const value [duration])</td>
-<td>Generates a constant (silent) signal</td>
-</tr>
-
-<tr>
-<td>(sine pitch [duration])</td>
-<td>Generates a sine wave at an indicated pitch and duration.<br>
-    The pitch is a MIDI note number, with 60 for middle C.</td>
-</tr>
-
-<tr>
-<td>(hzosc hz)</td>
-<td>Generates a sine wave at a particular frequency in Hz.</td>
-</tr>
-
-<tr>
-<td>(osc-saw hz)</td>
-<td>Generates a sawtooth wave at a particular frequency in Hz.</td>
-</tr>
-
-<tr>
-<td>(osc-tri hz)</td>
-<td>Generates a triangle wave at a particular frequency in Hz.</td>
-</tr>
-
-<tr>
-<td>(osc-pulse hz bias)</td>
-<td>Generates a square pulse with variable width at the indicated
-    frequency (in Hertz). The bias parameter controls the pulse width 
-    and should be between -1 and +1, giving a pulse width from 0%
-    (always at -1) to 100% (always at +1). When bias is zero,
-    a square wave is generated.</td>
-</tr>
-
-<tr>
-<td>(pluck pitch)</td>
-<td>Generates a sound at the given pitch created using a
-    modified Karplus-Strong plucked string algorithm.</td>
-</tr>
-
-</table>
-
-<p>
-<b>Envelopes</b>
+printf('<p>%s</p>', _('The following functions all create new sounds.  You can use them to create "generate" plug-in effects, or you can combine these synthesized sounds with selected audio to produce interesting effects.'));
+?>
+<table class="function-list" >
+<?php
+printf('<tr><td>(noise)</td><td>%s</td></tr>', _('Generates white noise'));
+printf('<tr><td>(const value [duration])</td><td>%s</td></tr>', _('Generates a constant (silent) signal'));
+printf('<tr><td>(sine pitch [duration])</td><td>%s<br>%s</td></tr>', _('Generates a sine wave at an indicated pitch and duration.'), _('The pitch is a MIDI note number, with 60 for middle C.'));
+printf('<tr><td>(hzosc hz)</td><td>%s</td></tr>', _('Generates a sine wave at a particular frequency in Hz.'));
+printf('<tr><td>(osc-saw hz)</td><td>%s</td></tr>', _('Generates a sawtooth wave at a particular frequency in Hz.'));
+printf('<tr><td>(osc-tri hz)</td><td>%s</td></tr>', _('Generates a triangle wave at a particular frequency in Hz.'));
+printf('<tr><td>(osc-pulse hz bias)</td><td></td></tr>', _('Generates a square pulse with variable width at the indicated frequency (in Hertz). The bias parameter controls the pulse width and should be between -1 and +1, giving a pulse width from 0% (always at -1) to 100% (always at +1). When bias is zero, a square wave is generated.'));
+printf('<tr><td>(pluck pitch)</td><td></td></tr>', _('Generates a sound at the given pitch created using a modified Karplus-Strong plucked string algorithm.'));
+?>
+</table><?php
+printf('<p><b>%s</b></p>', _('Envelopes'));
+printf(_('%sNyquist has support for envelopes.  By applying an envelope to a sound, you can control the overall shape of its amplitude.  One of the easiest ways to construct an envelope is with the %s function, which takes 7 parameters that are commonly used for shaping synthesized musical notes: attack time, decay time, release time, attack level, decay level, sustain level, and overall duration.'), '<p>', '<code>env</code>');
+printf(_('See the figure below:'));
+?>
 </p>
-
-<p>Nyquist has support for envelopes.  By applying an envelope to a sound, you can control the overall shape of its amplitude.  One of the easiest ways to construct an envelope is with the <code>env</code> function, which takes 7 parameters that are commonly used for shaping synthesized musical notes: attack time, decay time, release time, attack level, decay level, sustain level, and overall duration.  See the figure below:</p>
-
-<p><img alt="Nyquist envelope diagram." src="../images/nyquist-envelope" width="467" height="200"></p>
-
-<p>To apply an envelope to a sound, just use the <code>mult</code> function.  So if <code>s</code> is a sound, then this is the sound with a simple envelope applied to it:</p>
-
+<?php
+printf('<p><img alt="%s" src="../images/nyquist-envelope" width="467" height="200"></p>', _('Nyquist envelope diagram.'));
+printf(_('%sTo apply an envelope to a sound, just use the %s function.  So if %s is a sound, then this is the sound with a simple envelope applied to it:%s'), '<p>', '<code>mult</code>', '<code>s</code>', '</p>');
+?>
 <pre>
   (mult s (env 0.1 0.1 0.2 1.0 0.5 0.3 1.0))
 </pre>
-
-<p>One of the most general type of envelope is a piece-wise linear function, which can be constructed with the <code>pwl</code> function.  The pwl function takes a list of parameters which denote (time, value) pairs. There is an implicit initial (time, value) pair of (0, 0), and an implicit final value of 0. There should always be an odd number of parameters, since the final time is not implicit.  For example:</p>
-
+<?php
+printf(_('%sOne of the most general type of envelope is a piece-wise linear function, which can be constructed with the %s function.  The pwl function takes a list of parameters which denote (time, value) pairs. There is an implicit initial (time, value) pair of (0, 0), and an implicit final value of 0. There should always be an odd number of parameters, since the final time is not implicit.  For example:'), '<p>', '<code>pwl</code>', '</p>');
+?>
 <pre>
   ; symmetric rise to 0.7 (at time 1) and fall back to 0 (at time 2):
   (pwl 1.0 0.7 2.0)
 </pre>
+<?php
+printf('<h3>%s</h3>', _('Combining sounds'));
 
-<h3>Combining sounds</h3>
+printf(_('%sBesides multiplying two sounds with the %s function, you can add two sounds (or envelopes) with the %s function.%s'), '<p>', '<code>mult</code>', '<code>add</code>', '</p>');
 
-<p>Besides multiplying two sounds with the <code>mult</code> function, you can add two sounds (or envelopes) with the <code>add</code> function.</p>
+printf('<h3>%s</h3>', _('Filters'));
 
-<h3>Filters</h3>
-
-<p>Nyquist comes with a number of common filters built-in.  Here are some of the more common ones:</p>
-
+printf('<p>%s</p>', _('Nyquist comes with a number of common filters built-in.  Here are some of the more common ones:'));
+?>
 <table class="function-list">
+<?php
+printf('<tr><td>(lp sound cutoff)</td><td></td></tr>', 
+// i18n-hint: Butterworth is the name of the filter design. Not normally something you'd want to translate.
+_('Low-pass filter (first-order Butterworth). Cutoff may be a float or a signal (for time-varying filtering) and expresses hertz.'));
+printf('<tr><td>(hp sound cutoff)</td><td>%s</td></tr>', _('High-pass filter (first-order Butterworth). Cutoff may be a float or a signal (for time-varying filtering) and expresses hertz.'));
+printf('<tr><td>(comb sound hz decay)</td><td>%s</td></tr>', _('Applies a comb filter to sound, which emphasizes (resonates at) frequencies that are multiples of a Hz.'));
+printf('<tr><td>(alpass sound decay hz)</td><td>%s</td></tr>', _('All-pass filter, creating a delay effect without the resonances of a comb filter.'));
+printf('<tr><td>(notch2 sound hz)</td><td></td></tr>', _('Second-order notch filter centered at a frequency of hz.'));
+?>
+</table>
+<?php
+printf('<h3>%s</h3>', _('Transforming and combining sounds'));
 
-<tr>
-<td>(lp sound cutoff)</td>
-<td>Low-pass filter (first-order Butterworth). Cutoff may be a float or a signal (for time-varying filtering) and expresses hertz.</td>
-</tr>
-
-<tr>
-<td>(hp sound cutoff)</td>
-<td>High-pass filter (first-order Butterworth). Cutoff may be a float or a signal (for time-varying filtering) and expresses hertz.</td>
-</tr>
-
-<tr>
-<td>(comb sound hz decay)</td>
-<td>Applies a comb filter to sound, which emphasizes
-    (resonates at) frequencies that are multiples of a hz.
-</td>
-</tr>
-
-<tr>
-<td>(alpass sound decay hz)</td>
-<td>All-pass filter, creating a delay effect without the resonances
-    of a comb filter.  </td>
-</tr>
-
-<tr>
-<td>(notch2 sound hz)</td>
-<td>Second-order notch filter centered at a frequency of hz.</td>
-</tr>
-
+printf(_('%sIt is beyond the scope of this introductory tutorial to explain all of the ways that a sound can be transformed in Nyquist.  These functions do not modify sounds directly, but instead modify the Nyquist %senvironment%s.  In order for these changes to affect sounds, you must use the %s function.%s'), '<p>', '<em>', '</em>', '<code>cue</code>', '</p>');
+?>
+<table class="function-list">
+<?php
+printf('<tr><td>(stretch factor (cue sound))</td><td>%s</td></tr>', _('Changes the length of the sound being cued by the given factor.'));
+printf('<tr><td>(scale factor (cue sound))</td><td>%s</td></tr>', _('Scales the amplitude of the sound being cued by the given factor.'));
+printf('<tr><td>(loud dB (cue sound))</td><td>%s</td></tr>', _('Increases or decreases the volume of the sound being cued by the given number of decibels.'));
+printf('<tr><td>(at t (cue sound))</td><td>%s</td></tr>', _('Starts the given sound at a particular time in seconds. This can\'t be used to add silence at the beginning or end, but it can be used when combining two or more sounds.'));
+printf('<tr><td>(seq (cue s1) (cue s2))</td><td>%s</td></tr>', _('Creates a sequence of sound s1 followed by sound s2.'));
+printf('<tr><td>(sim (cue s1) (cue s2))</td><td>%s</td></tr>', _('Combines two sounds so that they are played simultaneously.'));
+?>
 </table>
 
-<h3>Transforming and combining sounds</h3>
-
-<p>It is beyond the scope of this introductory tutorial to explain all of the ways that a sound can be transformed in Nyquist.  These functions do not modify sounds directly, but instead modify the Nyquist <em>environment</em>.  In order for these changes to affect sounds, you must use the <code>cue</code> function.</p>
-
-<table class="function-list">
-
-<tr>
-<td>(stretch factor (cue sound))</td>
-<td>Changes the length of the sound being cued by the given factor.</td>
-</tr>
-
-<tr>
-<td>(scale factor (cue sound))</td>
-<td>Scales the amplitude of the sound being cued by the given factor.</td>
-</tr>
-
-<tr>
-<td>(loud dB (cue sound))</td>
-<td>Increases or decreases the volume of the sound being cued by the<br>
-    given number of decibels.</td>
-</tr>
-
-<tr>
-<td>(at t (cue sound))</td>
-<td>Starts the given sound at a particular time in seconds.<br>
-    This can\'t be used to add silence at the beginning or end,<br>
-    but it can be used when combining two or more sounds.</td>
-</tr>
-
-<tr>
-<td>(seq (cue s1) (cue s2))</td>
-<td>Creates a sequence of sound s1 followed by sound s2.</td>
-</tr>
-
-<tr>
-<td>(sim (cue s1) (cue s2))</td>
-<td>Combines two sounds so that they are played simultaneously.
-</tr>
-
-</table>
-
-<h3><a href="nyquist3">Next: Creating Nyquist Plug-Ins</a></h3>')?>
 
 <?php
+printf('<h3><a href="nyquist3">%s</a></h3>', _('Next: Creating Nyquist Plug-Ins'));
   include "../include/footer.inc.php";
 ?>
--- sf-cvs/trunk/htdocs/help/nyquist3.php
+++ sf-cvs/trunk/htdocs/help/nyquist3.php
@@ -19,8 +19,9 @@
 	<li><b><?=_("Creating Nyquist Plug-ins")?></b></li>
 </ol>
 
-<?=_('<p>Creating a plug-in for Audacity using Nyquist is as simple as creating a text file with the extension ".ny" with some Nyquist code, adding a few comments to indicate the type of plug-in, and placing the file in Audacity\'s <code>plug-ins</code> directory.  Here\'s a very simple plug-in as an example:</p>
-
+<?php
+printf(_('%sCreating a plug-in for Audacity using Nyquist is as simple as creating a text file with the extension ".ny" with some Nyquist code, adding a few comments to indicate the type of plug-in, and placing the file in Audacity\'s %s directory.  Here\'s a very simple plug-in as an example:%s'), '<p>', '<code>plug-ins</code>', '</p>');
+?>
 <pre>
   ;nyquist plug-in
   ;version 1
@@ -29,25 +30,24 @@
   ;action "Fading In..."
   (mult (ramp) s)
 </pre>
-
-<p>The first two lines of a Nyquist plug-in must be exactly as in the example above, or Audacity will not load it.  (The version number is to support future expansions of the plug-in format.) The next line is the type of plug-in, which is discussed below.  Then comes the name of the plug-in, which is what is displayed in the menu bar, and then the "action", which is what Audacity displays while it is busy processing the plug-in.  There are other optional lines that may follow.  Any line that does not begin with a semicolon (;) is assumed to contain Nyquist code and will be executed.</p>
-
-<p>Audacity has support for three types of plug-ins that can be written in Nyquist:</p>
-
+<?php
+printf('<p>%s</p>', _('The first two lines of a Nyquist plug-in must be exactly as in the example above, or Audacity will not load it.  (The version number is to support future expansions of the plug-in format.) The next line is the type of plug-in, which is discussed below.  Then comes the name of the plug-in, which is what is displayed in the menu bar, and then the "action", which is what Audacity displays while it is busy processing the plug-in.  There are other optional lines that may follow.  Any line that does not begin with a semicolon (;) is assumed to contain Nyquist code and will be executed.'));
+printf('<p>%s</p>', _('Audacity has support for three types of plug-ins that can be written in Nyquist:'));
+?>
 <pre>
   ;type generate
   ;type process
   ;type analyze
 </pre>
+<?php 
+printf('<p>%s</p>', _('These correspond to the three menus that can contain plug-ins: Generate, Effect, and Analyze.  Generate plug-ins are expected to generate new audio from scratch, Effect plug-ins ("process") modify existing audio in-place, and Analyze plug-ins process audio but do not modify it (though they are allowed to add labels).'));
 
-<p>These correspond to the three menus that can contain plug-ins: Generate, Effect, and Analyze.  Generate plug-ins are expected to generate new audio from scratch, Effect plug-ins ("process") modify existing audio in-place, and Analyze plug-ins process audio but do not modify it (though they are allowed to add labels).</p>
+printf(_('%sFor Effect and Analyze plug-ins, Audacity sets up the Nyquist environment so that the audio the user has selected is in the variable %s.  All of the expressions in the plug-in file are executed in order, and the return value of the last expression is substituted for the selection in Audacity.  If the last expression does not return audio, Audacity returns an error.%s'), '<p>', '<code>s</code>', '</p>');
 
-<p>For Effect and Analyze plug-ins, Audacity sets up the Nyquist environment so that the audio the user has selected is in the variable <code>s</code>.  All of the expressions in the plug-in file are executed in order, and the return value of the last expression is substituted for the selection in Audacity.  If the last expression does not return audio, Audacity returns an error.</p>
+printf('<h3>%s</h3>', _('Parameter dialogs'));
 
-<h3>Parameter dialogs</h3>
-
-<p>Audacity has limited support for plug-ins showing a dialog to get parameters from the user.  Here is an example of a plug-in that opens a dialog:</p>
-
+printf('<p>%s</p>', _('Audacity has limited support for plug-ins showing a dialog to get parameters from the user.  Here is an example of a plug-in that opens a dialog:'));
+?>
 <pre>
   ;nyquist plug-in
   ;version 1
@@ -66,67 +66,67 @@
         (loud decay (at delay (delays s decay delay (- count 1)))))))
   (stretch-abs 1 (delays s (- 0 decay) delay count))
 </pre>
+<?php
+printf('<p>%s</p>', _('If Audacity finds at least one "control" line, it will open a dialog to prompt the user for certain parameters to the plug-in.  Each parameter consists of a text box and a slider, and after the user has entered each one, the final value will be stored in a Nyquist variable with a name that you specify on the "control" line.  Here\'s what the dialog for the Delay effect shown above looks like in Audacity for Linux:'));
 
-<p>If Audacity finds at least one "control" line, it will open a dialog to prompt the user for certain parameters to the plug-in.  Each parameter consists of a text box and a slider, and after the user has entered each one, the final value will be stored in a Nyquist variable with a name that you specify on the "control" line.  Here\'s what the dialog for the Delay effect shown above looks like in Audacity for Linux:</p>
+printf('<p><img alt="%s" src="../images/nyquist-plugin-screenshot" width="414" height="221"></p>', _('Nyquist plugin screenshot'));
 
-<p><img alt="Nyquist plugin screenshot" src="../images/nyquist-plugin-screenshot" width="414" height="221"></p>
+printf('<p>%s</p>', _('Note that the "info" line is displayed at the top of the dialog, and that the "\n" becomes a newline.  The parameters to the "control" line affect the appearance and limitations of the parameter.  Each "control" line must consist of exactly the following 8 elements, in order:'));
 
-<p>Note that the "info" line is displayed at the top of the dialog, and that the "\n" becomes a newline.  The parameters to the "control" line affect the appearance and limitations of the parameter.  Each "control" line must consist of exactly the following 8 elements, in order:</p>
+printf('<ol><li>%s</li>', _('The word "control"'));
+printf('<li>%s</li>', _('The name of the control - this is the name of the Nyquist variable that will get set when the user manipulates the dialog.'));
+printf('<li>%s</li>', _('The label to the left of the control'));
+printf(_('%sThe type of value: either %s (integer) or %s.'), '<li>', '<code>int</code>', '<code>real</code>', '</li>');
+printf('<li>%s</li>', _('The label to the right of the value (usually the units like "Hz" or "dB").'));
+printf('<li>%s</li>', _('The default/initial value of the parameter'));
+printf('<li>%s</li>', _('The minimum value of the parameter'));
+printf('<li>%s</li></ol>', _('The maximum value of the parameter'));
 
-<ol>
-	<li>The word "control"</li>
-	<li>The name of the control - this is the name of the Nyquist variable that will get set when the user manipulates the dialog.</li>
-	<li>The label to the left of the control</li>
-	<li>The type of value: either <code>int</code> (integer) or <code>real</code>.</li>
-	<li>The label to the right of the value (usually the units like "Hz" or "dB").</li>
-	<li>The default/initial value of the parameter</li>
-	<li>The minimum value of the parameter</li>
-	<li>The maximum value of the parameter</li>
-</ol>
+printf('<h3>%s</h3>', _('Returning labels'));
 
-<h3>Returning labels</h3>
-
-<p>Instead of returning audio, a Nyquist plug-in can instead return a list of labels.  A list of labels is simply a list of time/label pairs, for example:</p>
-
+printf('<p>%s</p>', _('Instead of returning audio, a Nyquist plug-in can instead return a list of labels.  A list of labels is simply a list of time/label pairs, for example:'));
+?>
 <pre>
   ((0.0 "start") (30.0 "middle") (60.0 "end"))
 </pre>
+<?php
+printf('<p>%s</p>', _('When a plug-in returns a list of exactly this form, Audacity will create a new label track and add the labels at those positions.  This style of plug-in is usually of type "analyze".'));
 
-<p>When a plug-in returns a list of exactly this form, Audacity will create a new label track and add the labels at those positions.  This style of plug-in is usually of type "analyze".</p>
-
-<p>New!  Beginning with Audacity version 1.3.1, you can now optionally return both a start and an end time, like this:</p>
-
+printf('<p>%s</p>', _('New!  Beginning with Audacity version 1.3.1, you can now optionally return both a start and an end time, like this:'));
+?>
 <pre>
   ((0.0 25.0 "start") (30.0 45.0 "middle") (60.0 75.0 "end"))
 </pre>
-
-<p>Note that labels are allowed to overlap in Audacity 1.3; the end time of one can be after the start time of the next.</p>
-
-<h3>Processing stereo tracks</h3>
-
-<p>Nyquist represents stereo tracks as an array of sounds (not a list).  Many Nyquist functions automatically work with these arrays, but not all, so sometimes you may find it necessary to split up a stereo array, or reassemble one.  Here are some useful functions:</p>
-
-<table class="function-list">
-<tr><td><code>(arrayp s)</code></td><td>returns true if <code>s</code> is an array</td></tr>
-<tr><td><code>(aref s 0)</code></td><td>the first element in array <code>s</code> - the left channel</td></tr>
-<tr><td><code>(aref s 1)</code></td><td>the second element in array <code>s</code> - the right channel</td></tr>
-<tr><td><code>(setf s (make-array 2))</code></td><td>makes <code>s</code> into a new array of length 2</td></tr>
-<tr><td><code>(setf (aref s 0) left)</code></td><td>makes <code>left</code> the first element of array <code>s</code></td></tr>
-<tr><td><code>(setf (aref s 1) right)</code></td><td>makes <code>right</code> the second element of array <code>s</code></td></tr>
-</table>
-
-<p>As a convenience, if the input to your Nyquist plug-in is stereo, but you only output a single (mono) sound, Audacity will automatically copy it to both the left and right channels.</p>
-
-<h3>Where to go from here</h3>
-
-<p>Audacity comes with some sample plug-ins that you can examine or modify as a starting point.  The best way to learn Nyquist is to try it.  If you\'re having trouble debugging, consider downloading the standalone version of Nyquist (see the link in Part 1).</p>
-
-<p>If you\'re having trouble getting Nyquist to work at all, please <a href="../contact/">contact us</a>.</p>
-
-<p>If you are working on Nyquist plug-in effects and would like to share them with others, or if you would like to discuss general details of how to achieve certain effects in Nyquist, please join the <a href="../contact/lists#nyquist">audacity-nyquist mailing list</a>.</p>
-
-<p>Don\'t forget to consult the full <a href="http://www-2.cs.cmu.edu/~rbd/doc/nyquist/root.html">Nyquist Reference Manual</a> for more details of how Nyquist works.</p>')?>
-
 <?php
+printf('<p>%s</p>', _('Note that labels are allowed to overlap in Audacity 1.3; the end time of one can be after the start time of the next.'));
+
+printf('<h3>%s</h3>', _('Processing stereo tracks'));
+
+printf('<p>%s</p>', _('Nyquist represents stereo tracks as an array of sounds (not a list).  Many Nyquist functions automatically work with these arrays, but not all, so sometimes you may find it necessary to split up a stereo array, or reassemble one.  Here are some useful functions:'));
+?>
+<table class="function-list">
+<?php
+// i18n-hint: The first and last are html tags, the middle one is a variable name
+printf(_('%sreturns true if %s is an array%s'), '<tr><td><code>(arrayp s)</code></td><td>', '<code>s</code>', '</td></tr>');
+printf(_('%sthe first element in array %s - the left channel%s'), '<tr><td><code>(aref s 0)</code></td><td>', '<code>s</code>', '</td></tr>');
+printf(_('%sthe second element in array %s - the right channel%s'), '<tr><td><code>(aref s 1)</code></td><td>', '<code>s</code>', '</td></tr>');
+printf(_('%smakes %s into a new array of length 2%s'), '<tr><td><code>(setf s (make-array 2))</code></td><td>', '<code>s</code>', '</td></tr>');
+printf(_('%smakes %s the first element of array %s'), '<tr><td><code>(setf (aref s 0) left)</code></td><td>', '<code>left</code>', '<code>s</code></td></tr>');
+printf(_('%smakes %s the second element of array %s'), '<tr><td><code>(setf (aref s 1) right)</code></td><td>', '<code>right</code>', '<code>s</code></td></tr>'); ?>
+</table>
+<?php
+printf('<p>%s</p>', _('As a convenience, if the input to your Nyquist plug-in is stereo, but you only output a single (mono) sound, Audacity will automatically copy it to both the left and right channels.'));
+
+printf('<h3>%s</h3>', _('Where to go from here'));
+
+printf('<p>%s</p>', _('Audacity comes with some sample plug-ins that you can examine or modify as a starting point.  The best way to learn Nyquist is to try it.  If you\'re having trouble debugging, consider downloading the standalone version of Nyquist (see the link in Part 1).'));
+//i18n-hint: All the %s are HTML tags which makes "contact us" into a link
+printf(_('%sIf you\'re having trouble getting Nyquist to work at all, please %scontact us%s.%s'), '<p>', '<a href="../contact/">', '</a>', '</p>');
+//i18n-hint: All the %s are HTML tags which makes "audacity-nyquist mailing list" into a link
+printf(_('%sIf you are working on Nyquist plug-in effects and would like to share them with others, or if you would like to discuss general details of how to achieve certain effects in Nyquist, please join the %saudacity-nyquist mailing list%s.%s'), '<p>', '<a href="../contact/lists#nyquist">', '</a>', '</p>');
+//i18n-hint: All the %s are HTML tags which makes "Nyquist Reference Manual" into a link
+printf(_('%sDon\'t forget to consult the full %sNyquist Reference Manual%s for more details of how Nyquist works.%s'), '<p>', '<a href="http://www-2.cs.cmu.edu/~rbd/doc/nyquist/root.html">', '</a>', '</p>');
+
+
   include "../include/footer.inc.php";
 ?>
