c6f8e320241614223a1b589e13380c2880bc071c
l_r_nightmare <l_r_nightmare@47890b92-0858-11df-a26f-8b716316a5bc>
2009-03-17 17:12:32 +0000

    Added #include <wx/wxprec.h> to get <windows.h> when on Windows (required by some macros and types)

--- sf-cvs/trunk/audacity-src/src/widgets/LinkingHtmlWindow.cpp
+++ sf-cvs/trunk/audacity-src/src/widgets/LinkingHtmlWindow.cpp
@@ -13,6 +13,9 @@
 
 **********************************************************************/
 
+// For compilers that support precompilation, includes "wx/wx.h".
+#include <wx/wxprec.h>
+
 #include "../Audacity.h"
 
 #include <wx/mimetype.h>
