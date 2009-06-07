03c413e3be15682c181bc0fb24d5247850549ffa
dhorgan <dhorgan@47890b92-0858-11df-a26f-8b716316a5bc>
2009-06-07 15:32:22 +0000

    Update the mod-script-pipe test script to get responses again

--- sf-cvs/trunk/audacity-src/scripts/pipe-test.pl
+++ sf-cvs/trunk/audacity-src/scripts/pipe-test.pl
@@ -32,7 +32,7 @@ sub startUp{
 sub finish{
    close TO_SRV;
    close FROM_SRV;
-   print "\nDone\n";
+   print "Done\n";
 }
 
 sub doCommand{
@@ -45,29 +45,36 @@ sub doCommand{
    }
    print "Sent: $command\n";
 
-# Responses are disabled for now.
+#   while(my $ttt = <FROM_SRV>) {
+#      $ttt = chomp($ttt)
+#      print "Recd:$ttt\n";
+#      last if ($ttt =~ /^$/);
+#   }
 
-#   my $ttt = <FROM_SRV>;
+   my $ttt = <FROM_SRV>;
+   chomp($ttt);
 #   if( $ttt =~ /^Lines:(\d+)$/ )
-#   {
-##      This block of code deals with multi-line responses.
-##      When there is more than one line of repsonse,
-##      Audacity starts out by stating how many lines there
-##      will be.
-#      $nLines = $1;
-##      print "Number of lines is $nLines";
-#      print "Recd:$ttt";
-#      for($i=0;$i<$nLines;$i++)
-#      {
-#         $ttt = <FROM_SRV>;
-#         print "Recd:$i:$ttt";
-#      }
-#   }
-#   else
-#   {
-#      print "Recd:$ttt";
-#   }
-#   return $ttt;
+   if( $ttt =~ /Lines:(\d+)/ )
+   {
+#      This block of code deals with multi-line responses.
+#      When there is more than one line of repsonse,
+#      Audacity starts out by stating how many lines there
+#      will be.
+      $nLines = $1;
+      print "Number of lines is $nLines\n";
+      print "Recd: $ttt\n";
+      for($i=0;$i<$nLines;$i++)
+      {
+         $ttt = <FROM_SRV>;
+         chomp($ttt);
+         print "Recd: $i:$ttt\n";
+      }
+   }
+   else
+   {
+      print "Recd: $ttt\n";
+   }
+   return $ttt;
 }
 
 # Send some test commands - commands currently allowed are precisely those
@@ -76,13 +83,14 @@ sub doCommand{
 startUp();
 sleep(1);
 doCommand( 'Amplify: Ratio=0.1' );
-sleep(2);
+sleep(0.2);
 doCommand( 'Echo: Delay=1.0 Decay=0.5' );
-sleep(2);
+sleep(0.2);
 doCommand( 'Amplify: Ratio=2.0' );
-sleep(2);
+sleep(0.2);
 doCommand( 'Echo: Delay=1.0 Decay=0.5' );
-sleep(2);
+sleep(0.2);
 doCommand( 'ExportMp3' );
-sleep(2);
+sleep(0.2);
+doCommand( '<Not a valid command>');
 finish();
