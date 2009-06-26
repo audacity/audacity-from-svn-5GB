4c23d860b17473967ebb01c8adeeb306e3bc05bb
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2009-06-26 18:19:32 +0000

    We welcome non-financial contributions too. Add "Sponsoring Audacity Development" section (placeholder for listing  group/corporate sponsors).

--- sf-cvs/trunk/htdocs/about/index.php
+++ sf-cvs/trunk/htdocs/about/index.php
@@ -1,8 +1,9 @@
 <?php
 /*
  * Copyright 2004 Matt Brubeck
- * 2007-8 Gale Andrews, Vaughan Johnson. This file is licensed under a Creative Commons license:
- * http://creativecommons.org/licenses/by/2.0/
+ * 2007-9 Gale Andrews, Vaughan Johnson. 
+ * This file is licensed under a Creative Commons license:
+ * http://creativecommons.org/licenses/by/3.0/
  */
   require_once "main.inc.php";
   $pageId = "";
@@ -38,7 +39,7 @@
 
 <p>Programs like Audacity are also called <b>open source software</b>, because their source code is available for anyone to study or use.  There are thousands of other free and open source programs, including the <a href="http://www.mozilla.com/">Firefox</a> web browser, the <a href="http://www.openoffice.org/">OpenOffice.org</a> office suite and entire Linux-based operating systems such as <a href="http://www.ubuntulinux.org/">Ubuntu</a>.</p>
 
-<p>We welcome <a href="../community/donate">donations</a> to support Audacity development.</p>');
+<p>We welcome <a href="../community/donate">donations</a> to support Audacity development. Anyone can <a href="http://audacityteam.org/wiki/index.php?title=Contribute">contribute</a> to Audacity by helping us with Beta <a href="http://www.audacityteam.org/manual/index.php?title=Main_Page">documentation</a>, <a href="../community/translation">translations</a>, user <a href="http://audacityteam.org/forum/index.php">support</a> and by <a href="http://audacityteam.org/wiki/index.php?title=Nightly_Builds">testing<a/> our latest code.</p>');
 
 echo _('<p><a href="http://www.ohloh.net/projects/59">Ohloh</a> has statistics on the value of Audacity development. The Ohloh &quot;badge&quot; at the bottom of each page on this site shows updated summary values. We set up the Ohloh pages about Audacity so the statistics show only the values of Audacity-specific development, excluding the third-party code libraries Audacity uses.</p>');
 
@@ -47,6 +48,10 @@ echo _('<p><a href="http://www.ohloh.net/projects/59">Ohloh</a> has statistics o
 <p>Vendors are free to bundle Audacity with their products, or to sell or distribute copies of Audacity (see <a href="../download/bundlers">Vendors and Distributors of Audacity</a>) under the <a href="../about/license#license">GNU General Public License (GPL)</a>.</p>
 
 <p>If you are interested in bundling, selling or distributing Audacity, please review our entire <a href="../about/license">License, and Advice for Vendors and Distributors</a> page.</p>');
+
+  echo _('<h3>Sponsoring Audacity Development</h3>
+
+<p>We are keen to hear from any companies or groups interested in <a href="http://audacityteam.org/sponsor.php">sponsoring<a/> Audacity development.</p>');  
 
   include "../include/footer.inc.php";
 ?>
