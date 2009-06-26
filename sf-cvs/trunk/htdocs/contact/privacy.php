319f152d6d8193870a2a32629311bfc6e6278943
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2009-06-26 18:09:45 +0000

    Advertisements Policy moved to top so that link at bottom of Adsense can anchor  to it properly

--- sf-cvs/trunk/htdocs/contact/privacy.php
+++ sf-cvs/trunk/htdocs/contact/privacy.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Copyright 2003 Dominic Mazzoni
- * Copyright 2005 Matt Brubeck
+ * Copyright 2005-9 Matt Brubeck, Vaughan Johnson
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/3.0/
  */
@@ -18,6 +18,13 @@
  join users lists in their native languages later on, to hear
  announcements there.
 -->
+
+<h3 id="advertising"><?=_("Advertisements Policy")?></h3>
+
+<?=_('<p>On some pages on our main site, we show advertisements from Google\'s AdSense program. See our &ldquo;<a href="http://audacity.sourceforge.net/community/donate">How Does Audacity Raise Money?</a>&rdquo; page for the reasons we show advertisements.</p>
+
+<p>As of April 8, 2009, this includes "interest-based" advertising, which utilizes cookies to try to determine users\' areas of interest (for example, audio editing), to show advertisements of likelier interest. Please see <a href="https://www.google.com/adsense/support/bin/answer.py?hl=en&answer=100557">Google Advertising Cookie and Privacy Policies</a> for information regarding your privacy and how to <a href="http://www.google.com/privacy_ads.html">opt out</a> of this tracking.</p>')?>
+
 <h3 id="announce"><?=_("Announcement List Policy")?></h3>
 
 <?=_('<p>You can subscribe to our announcement list by entering your e-mail address in the <a href="../#announce"> form</a> on the <a href="../">Audacity home page</a>.  We will use this list only to send you brief announcements of new Audacity releases.  We will never share the addresses on this list with anyone.</p>
@@ -31,11 +38,6 @@
 <p><b>Your e-mail address will always be truncated in published archives for your privacy, and the Audacity developers will not share your address with anyone.</b>  However, we <b>cannot</b> prevent other subscribers from seeing or publishing your messages. Therefore we recommend you avoid putting contact details such as street addresses or telephone numbers in your e-mails.</p>
 <p>If you have any questions about this policy or are not comfortable writing to a public mailing list, please send a private e-mail to our <a href="&#109;&#x61;&#x69;&#108;&#x74;&#111;&#x3a;&#x64;&#111;&#x6d;&#105;&#x6e;&#x69;&#x63;&#64;&#97;&#x75;&#100;&#x61;&#99;&#105;&#116;&#121;&#x74;&#x65;&#97;&#109;&#x2e;&#x6f;&#x72;&#x67;&#x3f;&#115;&#x75;&#98;&#106;&#101;&#99;&#116;&#x3d;&#x4d;&#97;&#x69;&#108;&#105;&#110;&#x67;&#x20;&#76;&#x69;&#115;&#x74;&#115;">lead developer</a>.</p>')?>
 
-<h3 id="advertising"><?=_("Advertisements Policy")?></h3>
-
-<?=_('<p>On some pages on our main site, we show advertisements from Google\'s AdSense program. See our &ldquo;<a href="http://audacity.sourceforge.net/community/donate">How Does Audacity Raise Money?</a>&rdquo; page for the reasons we show advertisements.</p>
-
-<p>As of April 8, 2009, this includes "interest-based" advertising, which utilizes cookies to try to determine users\' areas of interest (for example, audio editing), to show advertisements of likelier interest. Please see <a href="https://www.google.com/adsense/support/bin/answer.py?hl=en&answer=100557">Google Advertising Cookie and Privacy Policies</a> for information regarding your privacy and how to <a href="http://www.google.com/privacy_ads.html">opt out</a> of this tracking.</p>')?>
 
 <?php
   include "../include/footer.inc.php";
