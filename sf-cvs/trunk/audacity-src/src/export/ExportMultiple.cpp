9f7ed5bd662586bdd19e10eb7a8b83dbbaef8a9b
richardash1981 <richardash1981@47890b92-0858-11df-a26f-8b716316a5bc>
2009-06-30 21:13:48 +0000

    Fix typo in string

--- sf-cvs/trunk/audacity-src/src/export/ExportMultiple.cpp
+++ sf-cvs/trunk/audacity-src/src/export/ExportMultiple.cpp
@@ -522,7 +522,7 @@ void ExportMultiple::OnExport(wxCommandEvent& event)
       wxString msg;
       msg.Printf(
          ok == eProgressSuccess ? _("Successfully exported the following %ld file(s).")
-           : (ok == eProgressFailed ? _("Something went wrong after exporting the folliwing %ld file(s).")
+           : (ok == eProgressFailed ? _("Something went wrong after exporting the following %ld file(s).")
              : (ok == eProgressCancelled ? _("Export canceled after exporting the following %ld file(s).") 
                : (ok == eProgressStopped ? _("Export stopped after exporting the following %ld file(s).")
                  : _("Something went really wrong after exporting the following %ld file(s).")
