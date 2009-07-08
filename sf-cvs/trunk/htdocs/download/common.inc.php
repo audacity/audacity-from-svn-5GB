1e96d4e773f530150d0abb77c81c81f61995d422
richardash1981 <richardash1981@47890b92-0858-11df-a26f-8b716316a5bc>
2009-07-08 20:50:28 +0000

    Add alternate download links at google code to the download pages

--- sf-cvs/trunk/htdocs/download/common.inc.php
+++ sf-cvs/trunk/htdocs/download/common.inc.php
@@ -10,4 +10,8 @@
   <li><p><a href="../help/faq?s=install&amp;item=lame-mp3"><?=_("LAME MP3 encoder")?></a> - <?=_("Allows Audacity to export MP3 files.")?></p></li>
   <li><p><a href="../help/faq?s=install&amp;item=vst-enabler"><?=_("VST Enabler")?></a> - <?=_('Allows Audacity to load VST plug-ins.')?></p></li>
   <li><p><a href="plugins"><?=_("Plug-Ins")?></a> - <?=_("Download additional effects and filters.")?></p></li>
-  <li><p><?php printf(_('If you have trouble with your download, or need an older version of Audacity, try our <a href="%s">alternate download links</a> (older versions can be viewed by clicking on the appropriate package).'), "http://sourceforge.net/project/showfiles.php?group_id=6235")?></p></li>
+  <li><p><?php printf(_('If you have trouble with your download, or need an older version of Audacity, try our alternate download links:')) ?></p>
+	 <ul><li><?php printf(_('<a href="%s">Sourceforge</a> (older versions can be viewed by clicking on the appropriate package)'), "http://sourceforge.net/project/showfiles.php?group_id=6235")?></li>
+	<li><?php printf(_('<a href="%s">Google Code</a> (click on the headings to sort the list)'), "http://code.google.com/p/audacity/downloads/list")?></li>
+	</ul>
+</li>
