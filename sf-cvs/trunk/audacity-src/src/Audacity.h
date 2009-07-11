61f630b8a60048c44389cf77eadbd2537b5e1a83
richardash1981 <richardash1981@47890b92-0858-11df-a26f-8b716316a5bc>
2009-07-11 10:46:18 +0000

    Bump the version to rc1, I want to build a release tarball

--- sf-cvs/trunk/audacity-src/src/Audacity.h
+++ sf-cvs/trunk/audacity-src/src/Audacity.h
@@ -25,7 +25,7 @@
 #define AUDACITY_RELEASE   3
 #define AUDACITY_REVISION  8
 #define AUDACITY_MODLEVEL  0
-#define AUDACITY_SUFFIX    wxT("-alpha_") __TDATE__ // wxT("") 
+#define AUDACITY_SUFFIX    wxT("-rc1")// __TDATE__ // wxT("") 
 
 #define AUDACITY_MAKESTR( x ) #x
 #define AUDACITY_QUOTE( x ) AUDACITY_MAKESTR( x )
