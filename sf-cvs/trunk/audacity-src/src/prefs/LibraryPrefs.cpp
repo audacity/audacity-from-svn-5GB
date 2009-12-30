a6ac397ca9360c5513dfe22f4b65b812950854ee
richardash1981 <richardash1981@47890b92-0858-11df-a26f-8b716316a5bc>
2009-12-30 16:20:49 +0000

    Make translatable string conform to Translating Audacity wiki page standards, i.e. no long stream of spaces in the middle of the string.

--- sf-cvs/trunk/audacity-src/src/prefs/LibraryPrefs.cpp
+++ sf-cvs/trunk/audacity-src/src/prefs/LibraryPrefs.cpp
@@ -183,8 +183,7 @@ void LibraryPrefs::OnFFmpegFindButton(wxCommandEvent & e)
 
    // Libs are fine, don't show "locate" dialog unless user really wants it
    if (!locate) {
-      int response = wxMessageBox(_("Audacity has automatically detected valid FFmpeg libraries.\
-                                      \nDo you still want to locate them manually?"),
+      int response = wxMessageBox(_("Audacity has automatically detected valid FFmpeg libraries.\nDo you still want to locate them manually?"),
                                   wxT("Success"),
                                   wxCENTRE | wxYES_NO | wxNO_DEFAULT |wxICON_QUESTION);
       if (response == wxYES) {
