ae6bb7a43c447d093b0d6e27e0d6f2db08253037
martynshaw <martynshaw@47890b92-0858-11df-a26f-8b716316a5bc>
2010-01-02 21:46:22 +0000

    f.Open was causing a hang if the file didn't exist, so check for existance first.

--- sf-cvs/trunk/audacity-src/src/blockfile/PCMAliasBlockFile.cpp
+++ sf-cvs/trunk/audacity-src/src/blockfile/PCMAliasBlockFile.cpp
@@ -80,22 +80,21 @@ int PCMAliasBlockFile::ReadData(samplePtr data, sampleFormat format,
    wxFile f;   // will be closed when it goes out of scope
    SNDFILE *sf = NULL;
 
-   if (f.Open(mAliasedFileName.GetFullPath())) {
-      // Even though there is an sf_open() that takes a filename, use the one that
-      // takes a file descriptor since wxWidgets can open a file with a Unicode name and
-      // libsndfile can't (under Windows).
-      ODManager::LockLibSndFileMutex();
-      sf = sf_open_fd(f.fd(), SFM_READ, &info, FALSE);
-      ODManager::UnlockLibSndFileMutex();
+   if (f.Exists(mAliasedFileName.GetFullPath())) { // Don't use Open if file does not exits
+      if (f.Open(mAliasedFileName.GetFullPath())) {
+         // Even though there is an sf_open() that takes a filename, use the one that
+         // takes a file descriptor since wxWidgets can open a file with a Unicode name and
+         // libsndfile can't (under Windows).
+         ODManager::LockLibSndFileMutex();
+         sf = sf_open_fd(f.fd(), SFM_READ, &info, FALSE);
+         ODManager::UnlockLibSndFileMutex();
+      }
    }
 
    if (!sf){
-      
       memset(data,0,SAMPLE_SIZE(format)*len);
-
       if(silence) delete silence;
       mSilentAliasLog=TRUE;
-
       return len;
    }
 
