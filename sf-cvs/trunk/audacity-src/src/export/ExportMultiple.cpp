155f059e3804335d39baeb5136ebdce7316d262b
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2010-01-12 12:28:30 +0000

    add space to title of success dialogue

--- sf-cvs/trunk/audacity-src/src/export/ExportMultiple.cpp
+++ sf-cvs/trunk/audacity-src/src/export/ExportMultiple.cpp
@@ -541,7 +541,7 @@ void ExportMultiple::OnExport(wxCommandEvent& event)
 
       SuccessDialog dlg(this,
                    wxID_ANY,
-                   wxString(_("ExportMultiple")));
+                   wxString(_("Export Multiple")));
       ShuttleGui S(&dlg, eIsCreating);
       S.StartVerticalLay();
       {
