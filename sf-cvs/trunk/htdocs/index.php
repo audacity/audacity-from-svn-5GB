c545716e41dc15f934af95afece057539e4455e3
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2010-01-19 11:47:02 +0000

    Update for 1.3.11

--- sf-cvs/trunk/htdocs/index.php
+++ sf-cvs/trunk/htdocs/index.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Copyright 2005 Matt Brubeck
- * 2007-9 Vaughan Johnson, Gale Andrews
+ * 2007 onwards Vaughan Johnson, Gale Andrews
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/3.0/
  */
@@ -18,7 +18,7 @@
     $download_version = win_exe_version;
     $download_desc = _("for Windows&reg; 98/ME/2000/XP/Vista");
     $download_page = "windows";
-    $beta_download_desc = _("for Windows&reg; 2000/XP/Vista <p>or <a href=\"http://audacity.sourceforge.net/download/beta_windows#ansi\">Audacity 1.3.7</a> <b>(Beta)</b> for Windows&reg; 98/ME</i></p>");
+    $beta_download_desc = _("for Windows&reg; 2000/XP/Vista <p>or <a href=\"http://audacity.sourceforge.net/download/beta_windows#ansi\">Audacity 1.3.7</a> <b>(Beta)</b> for Windows&reg; 98/ME</i></p><p>Read about provisional support for <a href=\"http://audacityteam.org/wiki/index.php?title=Windows_7_OS\">Windows 7</a>");
     $beta_download_page = "beta_windows";
 
     include "beta/versions.inc.php";
@@ -54,9 +54,9 @@
   <p>
     <?=_('Audacity&reg; is free, open source software for recording and editing sounds.  It is available for Mac OS X, Microsoft Windows, GNU/Linux, and other operating systems. <a href="about/">Learn more about Audacity...</a> Also check our <a href="http://audacityteam.org/wiki/">Wiki</a> and <a href="http://audacityteam.org/forum/">Forum</a> for more information.')?></p>
   <p>
-    <?=_('The latest release of Audacity is <a href="/download">1.3.10 (Beta)</a>. Because it is a work in progress and does not yet come with complete documentation or translations into different languages, it is recommended for more advanced users. See <a href="/download/features-1.3-a">New Features in 1.3</a> for more information about the 1.3.x Beta series.')?></p>
+    <?=_('The latest release of Audacity is <a href="/download">1.3.11 (Beta)</a>. Because it is a work in progress and does not yet come with complete documentation or translations into different languages, it is recommended for more advanced users. See <a href="/download/features-1.3-a">New Features in 1.3</a> for more information about the 1.3.x Beta series.')?></p>
   <p>
-    <?=_('For all users, <a href="/download">Audacity 1.2.6</a> is a stable release, complete and fully documented.  You can have Audacity 1.2.6 and 1.3.10 installed on the same machine.')?></p>
+    <?=_('For all users, <a href="/download">Audacity 1.2.6</a> is a stable release, complete and fully documented.  You can have Audacity 1.2.6 and 1.3.11 installed on the same machine.')?></p>
 </div>
 
 <div id="download">
