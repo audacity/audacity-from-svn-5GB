3eade8a8150c3b895fe33b0814da403359eabc78
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2010-02-17 21:18:05 +0000

    * Add link to the 1.3 Manual, because users are managing to look here for it rather than finding it in the Help menu * Add link to Christian's new Audacity book

--- website/trunk/htdocs/help/documentation.php
+++ website/trunk/htdocs/help/documentation.php
@@ -19,15 +19,16 @@
 
 <h3><?=_('Quick Reference Guide')?></h3>
 <ul>
-	<li><a href="../onlinehelp-1.2/contents.htm"><?=_('Read the Audacity reference online.')?></a> <?=_('(You can also access this reference through the "Help" menu in the Audacity program.)')?></li>
-	<li><a href="../onlinehelp-1.2/reference.html"><?=_('Printable version of the Audacity reference.')?></a> <?=_('(This can be printed in one step using the print function of your browser.)')?></li>
+	<li><a href="../onlinehelp-1.2/contents.htm"><?=_('Read the Audacity 1.2 reference online.')?></a> <?=_('(You can also access this reference through the "Help" menu in the Audacity program.)')?></li>
+	<li><a href="../onlinehelp-1.2/reference.html"><?=_('Printable version of the Audacity 1.2 reference.')?></a> <?=_('(This can be printed in one step using the print function of your browser.)')?></li>
  </ul>
 
-<h3><?=_('Audacity User\'s Manual')?></h3>
+<h3><?=_('Audacity User\'s Manuals')?></h3>
 <ul>
-	<li><a href="../manual-1.2/"><?=_('Read the Audacity manual online.')?></a></li>
-	<li><a href="../audacity-manual-1.2.zip"><?=_('Download the Audacity manual in HTML format.')?></a> <?=_('(To use the manual, unzip it and open the "index.html" file.)')?></li>
-    	<li><a href="audacity-manual-1.2.pdf"><?=_('Download the Audacity manual in PDF format.')?></a></li>
+	<li><a href="../manual-1.2/"><?=_('Read the Audacity 1.2 manual online.')?></a></li>
+	<li><a href="../audacity-manual-1.2.zip"><?=_('Download the Audacity 1.2 manual in HTML format.')?></a> <?=_('(To use the manual, unzip it and open the "index.html" file.)')?></li>
+	<li><a href="audacity-manual-1.2.pdf"><?=_('Download the Audacity 1.2 manual in PDF format.')?></a></li>
+	<li><a href="http://manual.audacityteam.org/index.php?title=Main_Page"><?=_('Read the latest Audacity 1.3 Beta manual online.')?></a> <?=_('(You can also access a built-in copy of the manual through the "Help" menu in the Audacity program.)')?></li>
 </ul>
 
 <?php
@@ -43,7 +44,8 @@
     <li><a href="../localhelp/fr/audacity-1.2-help.htb">Menu d'aide d'Audacity en français</a> (Remplacer le fichier qui porte comme nom, audacity-1.2-help.htb par celui qu'on vous propose. <a href="http://zonelibre.grics.qc.ca/spip.php?article100">Aide</a>)</li>
     <li><a href="../localhelp/fr/audacity-fr-manuel-1.2.zip">Manuel d'Audacity en français</a> (zip, 700 ko.)
     <li><a href="../localhelp/fr/audacity-mode-d'emploi.pdf">Mode d'emploi en français</a> (PDF, 700 ko.  Ce fichier peut prendre un bon moment de télécharger.)</li>
- </ul>
+    <li><a href="http://www.pearson.fr/livre/?GCOI=27440100057910">Christian Brochec - Audacity 2</a> Une présentation complète d'Audacity, accompagnée d'exercices pour faciliter la prise en main et aller plus loin dans l'utilisation. <b>Attention:</b> En raison du retard pris par le développement d'Audacity 2.0, ce livre est basé sur la version 1.3.11 qui préfigure largement la version finale.</li>
+  </ul>
 </div>
 
 <div id="de">
