fbc61d030b148fe2c35769c21f7f69c385f33795
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2010-04-23 09:55:27 +0000

    Selection Bar > Selection Toolbar

--- website/trunk/htdocs/download/features-1.3-d.php
+++ website/trunk/htdocs/download/features-1.3-d.php
@@ -1,12 +1,12 @@
 <?php
 /*
- * Copyright 2005 Dominic Mazzoni
+ * Copyright 2005 - 2010 Dominic Mazzoni
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/2.0/
  */
   require_once "main.inc.php";
   $pageId = "features-1.3-d";
-  $pageTitle = _("Audacity 1.3: Selection Bar");
+  $pageTitle = _("Audacity 1.3: Selection Toolbar");
   include "../include/header.inc.php";
   include "../latest/versions.inc.php";
 ?>
@@ -16,29 +16,29 @@
 <p align="center">
 <img src="images/new-sel-bar-1.png">
 <br>
-<?=_("The selection bar")?>
+<?=_("The selection Toolbar")?>
 </p>
 
 <p>
-<?=_("In Audacity 1.2, the current selection is contained in a status bar at the bottom of the window.  In Audacity 1.3, this is replaced by a fully functional Selection Bar, which displays and controls the current selection (your choice of Start and End, or Start and Length), and the current audio position.  The selection bar is fully editable - just click in any field and type to change the current selection precisely.  In addition, many formatting options allow you to view times in different units, such as samples, CD frames, or NTSC video frames.")?>
+<?=_("In Audacity 1.2, the current selection is contained in a status bar at the bottom of the window.  In Audacity 1.3, this is replaced by a fully functional Selection Toolbar, which displays and controls the current selection (your choice of Start and End, or Start and Length), and the current audio position.  Selection Toolbar is fully editable - just click in any field and type to change the current selection precisely.  In addition, many formatting options allow you to view times in different units, such as samples, CD frames, or NTSC video frames.")?>
 </p>
 
 <p align="center">
 <img src="images/selbar003.png">
 <br>
-<?=_("The project sample rate control on the selection bar")?>
+<?=_("The project sample rate control on Selection Toolbar")?>
 </p>
 
 <p align="center">
 <img src="images/new-sel-bar-2.png">
 <br>
-<?=_("Typing a new time directly into the selection bar.")?>
+<?=_("Typing a new time directly into Selection Toolbar.")?>
 </p>
 
 <p align="center">
 <img src="images/selbar006b.png">
 <br>
-<?=_("Different time formats supported by the selection bar")?>
+<?=_("Different time formats supported by Selection Toolbar")?>
 </p>
 
 <style type="text/css">
