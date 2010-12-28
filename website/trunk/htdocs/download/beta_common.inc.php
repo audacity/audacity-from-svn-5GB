2c45e82a039ef0e2f2dfcc52e93319c5cc53be54
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2010-12-28 05:39:53 +0000

    Update text for SourceForge alternative download link to reflect their latest design

--- website/trunk/htdocs/download/beta_common.inc.php
+++ website/trunk/htdocs/download/beta_common.inc.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2009 Gale Andrews
+ * Copyright 20010 Gale Andrews
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/3.0/
  *
@@ -14,7 +14,7 @@
 <h4><?=_("Alternative Download Links")?></h4>    
 <ul>
   <li><p><?php printf(_('If you have trouble with your download, or need an older version of Audacity, try our alternative download links:')) ?>
-	 <ul><li><?php printf(_('<a href="%s">SourceForge</a> (older versions can be viewed by clicking on the appropriate package)'), "http://sourceforge.net/project/showfiles.php?group_id=6235")?></li>
+	 <ul><li><?php printf(_('<a href="%s">SourceForge</a> (older versions can be viewed by clicking to enter the required folder under "Name")'), "http://sourceforge.net/project/showfiles.php?group_id=6235")?></li>
 	<li><?php printf(_('<a href="%s">Google Code</a> (click on the headings to sort the list)'), "http://code.google.com/p/audacity/downloads/list")?></li>
 	</ul>
    </p>
