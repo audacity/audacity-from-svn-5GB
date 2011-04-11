037f7697277199f80820d664a9c113b2037a91d1
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2011-04-11 22:53:09 +0000

    Downgrade likelihood of FTP integration

--- website/trunk/htdocs/download/features-1.3-g.php
+++ website/trunk/htdocs/download/features-1.3-g.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2005 Dominic Mazzoni
+ * Copyright 2005 - 11 Dominic Mazzoni
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/2.0/
  */
@@ -23,19 +23,19 @@
 
 <p><b><?=_("Upload via FTP")?></b></p>
 
-<p><?=_("Audacity 1.3 has an experimental new dialog for uploading files to a server via FTP.  Currently it is just a simple FTP client; in the future we will integrate this feature into Audacity more so you can publish a project directly to a site as a Podcast, for example.")?></p>
+<p><?=_("Up to and including Audacity 1.3.3, an experimental dialog was available for uploading files to a server via FTP. A future version of Audacity may re-introduce this feature as an optional plug-in.")?></p>
 
-<p><b><?=_("Based on wxWidgets 2.6")?></b></p>
+<p><b><?=_("Based on wxWidgets 2.8")?></b></p>
 
 <p><?=_("Previous versions of Audacity were based on wxWidgets 2.4.  This change brings support for GTK2 widgets on Unix and better Unicode support on all platforms, plus many other minor enhancements under the hood.")?></p>
 
 <p><b><?=_("Batch / CleanSpeech")?></b></p>
 
-<p><?=_("Audacity has a new feature that allows you to process a bunch of files, for example normalizing and converting to MP3.  The CleanSpeech mode provides a simplified interface for some standard adjustments typically made on speech recordings.  You can access these features in the Batch tab of the Preferences dialog.")?></p>
+<p><?=_("Audacity has a new feature that allows you to process a batch of files, for example normalizing and converting to MP3.  CleanSpeech mode provides a simplified interface for some standard adjustments typically made on speech recordings.  You can access these features in the Interface Preferences.")?></p>
 
 <p><b><?=_("Cut lines")?></b></p>
 
-<p><?=_("When this feature is enabled (in the Interface tab of the Preferences), cutting audio in the middle of a clip does not remove it permanently - at any time in the future, just click on the cut line to restore it.")?></p>
+<p><?=_("Turning this feature on in the Tracks Preferences means that cutting audio in the middle of a clip does not remove it permanently - click on the cut line at any time to restore it.")?></p>
 
 <style type="text/css">
 .navbox {
