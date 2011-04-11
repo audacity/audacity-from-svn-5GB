c0c462b7ff5f8e4f6efcc0592867bf3e6b278d39
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2011-04-11 22:49:34 +0000

    Add a link to Audacity Forum.

--- website/trunk/htdocs/help/index.php
+++ website/trunk/htdocs/help/index.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * Copyright 2004 - 2008 Matt Brubeck, Gale Andrews
+ * Copyright 2004 - 2011 Matt Brubeck, Gale Andrews
  * This file is licensed under a Creative Commons license:
- * http://creativecommons.org/licenses/by/2.0/
+ * http://creativecommons.org/licenses/by/3.0/
  */
   require_once "main.inc.php";
   $pageId = "";
@@ -18,8 +18,8 @@ echo _('<p><b>Questions?</b> First, check our <a href="faq">Frequently Asked Que
 <p>
  <ul>
    <li><a href="documentation">Documentation</a> - Read the user manual and quick reference.</li>
-   <li><a href="http://audacityteam.org/wiki">Audacity Wiki</a> is an extensive user-editable help resource including:
-    <ul><li><a href="http://wiki.audacityteam.org/wiki/Category:Tutorial">Tutorials</a> - step-by-step guides on performing common tasks in Audacity, such as making ringtones, removing vocals, mixing, creating podcasts and transferring tapes and records to computer. Some <a href="http://audacityteam.org/wiki/index.php?title=MultiLingual">foreign language tutorials</a> are available.</li>
+   <li><a href="http://wiki.audacityteam.org/">Audacity Wiki</a> is an extensive user-editable help resource including:
+    <ul><li><a href="http://wiki.audacityteam.org/wiki/Category:Tutorial">Tutorials</a> - step-by-step guides on performing common tasks in Audacity, such as making ringtones, removing vocals, mixing, creating podcasts and transferring tapes and records to computer. Some tutorials in <a href="http://wiki.audacityteam.org/wiki/MultiLingual">languages other than English</a> are available.</li>
      <li><a href="http://wiki.audacityteam.org/wiki/Category:Tips">Tips</a> - these articles will help you work with and understand Audacity and your audio hardware.</li>
     </ul>
   </li>
@@ -33,7 +33,7 @@ echo _('<p><b>Questions?</b> First, check our <a href="faq">Frequently Asked Que
 // and at \htdocs\contact\index.php. If there is any translated documentation,
 // please add links to it at the bottom of \htdocs\help\documentation.php
 
-echo _('<p><b>Still need help?</b> Please <a href="../contact/">contact us</a>.</p>')?>
+echo _('<p><b>Still need help?</b> Please read <a href="../contact/">Contact Us</a> for how to ask your question on the Audacity <a href="http://forum.audacityteam.org/">Forum</a>.</p>')?>
 
 <?php
   include "../include/footer.inc.php";
