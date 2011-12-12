4bb4d280d4725db0000297925b6651090a90bc69
v.audacity <v.audacity@47890b92-0858-11df-a26f-8b716316a5bc>
2011-12-12 20:53:21 +0000

    This got interrupted, so I'm going to delete and retry.

--- audacity-src/tags/Audacity_1_3_14/lib-src/portmidi/pm_linux/pmlinuxalsa.h
+++
@@ -1,7 +0,0 @@
-/* pmlinuxalsa.h -- system-specific definitions */
-
-PmError pm_linuxalsa_init(void);
-void pm_linuxalsa_term(void);
-
-PmDeviceID find_default_device(char *path, int input, PmDeviceID id);
-
