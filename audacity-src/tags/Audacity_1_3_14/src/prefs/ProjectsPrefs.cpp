e8498ca2e5b95db743ed4764824a9d9b5772465a
v.audacity <v.audacity@47890b92-0858-11df-a26f-8b716316a5bc>
2011-12-12 20:54:11 +0000

    Tag 1.3.14 beta release.

---
+++ audacity-src/tags/Audacity_1_3_14/src/prefs/ProjectsPrefs.cpp
@@ -0,0 +1,91 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  ProjectsPrefs.cpp
+
+  Joshua Haberman
+  Dominic Mazzoni
+  James Crook
+
+*******************************************************************//**
+
+\class ProjectsPrefs
+\brief A PrefsPanel used to select options related to Audacity Project
+handling.
+
+*//*******************************************************************/
+
+#include "../Audacity.h"
+
+#include <wx/defs.h>
+#include <wx/textctrl.h>
+
+#include "../Prefs.h"
+#include "../ShuttleGui.h"
+
+#include "ProjectsPrefs.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+ProjectsPrefs::ProjectsPrefs(wxWindow * parent)
+:   PrefsPanel(parent, _("Projects"))
+{
+   Populate();
+}
+
+ProjectsPrefs::~ProjectsPrefs()
+{
+}
+
+/// Creates the dialog and its contents.
+void ProjectsPrefs::Populate()
+{
+   //------------------------- Main section --------------------
+   // Now construct the GUI itself.
+   // Use 'eIsCreatingFromPrefs' so that the GUI is 
+   // initialised with values from gPrefs.
+   ShuttleGui S(this, eIsCreatingFromPrefs);
+   PopulateOrExchange(S);
+   // ----------------------- End of main section --------------
+}
+
+void ProjectsPrefs::PopulateOrExchange(ShuttleGui & S)
+{
+   S.SetBorder(2);
+
+   S.StartStatic(_("When saving a project that depends on other audio files"));
+   {
+      S.StartRadioButtonGroup(wxT("/FileFormats/SaveProjectWithDependencies"), wxT("ask"));
+      {
+         S.TieRadioButton(_("&Always copy all audio into project (safest)"),
+                          wxT("copy"));
+         S.TieRadioButton(_("Do &not copy any audio"),
+                          wxT("never"));
+         S.TieRadioButton(_("As&k user"),
+                          wxT("ask"));
+      }
+      S.EndRadioButtonGroup();
+   }
+   S.EndStatic();
+}
+
+bool ProjectsPrefs::Apply()
+{  
+   ShuttleGui S(this, eIsSavingToPrefs);
+   PopulateOrExchange(S);    
+   
+   return true;
+}
+
+
+// Indentation settings for Vim and Emacs and unique identifier for Arch, a
+// version control system. Please do not modify past this point.
+//
+// Local Variables:
+// c-basic-offset: 3
+// indent-tabs-mode: nil
+// End:
+//
+// vim: et sts=3 sw=3
+// arch-tag: 427b9e64-3fc6-40ef-bbf8-e6fff1d442f0
