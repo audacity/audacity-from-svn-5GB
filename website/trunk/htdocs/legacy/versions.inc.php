1870bcd1dc40f85890b4544587ff73576dc898ac
v.audacity <v.audacity@47890b92-0858-11df-a26f-8b716316a5bc>
2012-03-09 23:23:28 +0000

    Fix consts settings.
    This whole Legacy directory is not going to be used, as it turns out, but worth keeping, in case we want to do so sometime in future.

--- website/trunk/htdocs/legacy/versions.inc.php
+++ website/trunk/htdocs/legacy/versions.inc.php
@@ -6,18 +6,18 @@ define('stable_version', '1.2.6');
 define('mac_classic_version', '1.0.0');
 define('mac_classic_size',    1.5);
 
-define('macosx_legacy_version', '1.2.6a');
-define('macosx_legacy_size',    3.9);
+define('macosx_version', '1.2.6a');
+define('macosx_size',    3.9);
 define('macosx_intel_version', '1.2.5');
 define('macosx_intel_size',    3.4);
 
-define('win_legacy_exe_version', '1.2.6');
-define('win_legacy_exe_size',    2.1);
-define('win_legacy_zip_version', '1.2.6');
-define('win_legacy_zip_size',    3.0);
+define('win_exe_version', '1.2.6');
+define('win_exe_size',    2.1);
+define('win_zip_version', '1.2.6');
+define('win_zip_size',    3.0);
 
-define('ladspa_legacy_version', '0.4.15');
-define('ladspa_legacy_size',    1.5);
+define('ladspa_version', '0.4.15');
+define('ladspa_size',    1.5);
 
 define('src_version', '1.2.6');
 define('src_size',    4.6);
