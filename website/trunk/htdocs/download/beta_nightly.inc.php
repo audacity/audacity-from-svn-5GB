2fbdb38c25c31730af98a3c7780d0ef2bcfedeef
v.audacity <v.audacity@47890b92-0858-11df-a26f-8b716316a5bc>
2012-03-09 06:17:55 +0000

    Fix typo. Use relative link instead of full URL as it refers to local page.

--- website/trunk/htdocs/download/beta_nightly.inc.php
+++ website/trunk/htdocs/download/beta_nightly.inc.php
@@ -7,4 +7,4 @@
  * This file contains text shared by "beta_mac.php" and "beta_windows.php".
  */
 ?>
-  <?=_('"Nightly Builds" are Alpha builds from latest development code. Your help in testing and <a href="http://audacity.sourceforge.net/contact#feedback">reporting bugs</a> in these builds is invaluable. <b>Note:</b>&nbsp;These builds will definitely be less reliable than officical releases. They should not be used as your production version of Audacity.')?>     
+<?=_('"Nightly Builds" are Alpha builds from latest development code. Your help in testing and <a href="/contact#feedback">reporting bugs</a> in these builds is invaluable. <b>Note:</b>&nbsp;These builds will definitely be less reliable than official releases. They should not be used as your production version of Audacity.')?>     
