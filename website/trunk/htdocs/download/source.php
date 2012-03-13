aba25d1c7e4cfec4eba8ded907bd8ded1fd03846
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2012-03-13 17:25:39 +0000

    Libsndfile is required, not optional. Link to Wiki for optional libraries.

--- website/trunk/htdocs/download/source.php
+++ website/trunk/htdocs/download/source.php
@@ -1,12 +1,16 @@
 <?php
 /*
- * Copyright 2004 Matt Brubeck
- * 2006 - 2012 Richard Ash, Gale Andrews
+ * Copyright 2004 - 2012
+ * Matt Brubeck
+ * Dominic Mazzoni
+ * Richard Ash
+ * Gale Andrews 
+ * Vaughan Johnson
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/3.0/
  */
   require_once "main.inc.php";
-  require_once "../latest/versions.inc.php";
+  require_once "../latest/versions.inc-v.php";
   require_once "../latest/mirror.inc.php";
   $pageId = "source";
   $pageTitle = _("Source Code");
@@ -15,16 +19,20 @@
 
 <h2><?=$pageTitle?></h2>
 
-<h3><?=_("Recommended Download")?></h3>
+<h3 id="recdown"><?=_("Recommended Download")?></h3>
 
 <ul>
-  <li><p><?php printf(_('<a href="%s">Audacity %s release (source tarball)</a> (.tar.gz file, %.1lf MB)'), download_url($src_url), src_version, src_size)?></p></li>
+<li><p><?php printf(_('<a href="%s">Audacity %s source tarball</a> (%s file, %.1lf MB)'), download_url($min_src_url), src_version, src_suffix, min_src_size)?> 
+<?php printf(_('Standard source tarball. Assumes your system has necessary dependencies/libraries installed (see below).'))?></p></li>
 </ul>
 
 <h3><?=_("Optional Downloads")?></h3>
+
 <ul>
-  <li><p><?=_('Get the latest code (incorporating changes since the release tarball) from <a href="../community/developers#svn">SVN</a>.')?></p></li>
-<li><p><?php printf(_('If you have trouble with your download, or need an older version of Audacity, try our alternate download links:')) ?></p>
+<li><p><?php printf(_('<a href="%s">Audacity %s source tarball</a> (%s file, %.1lf MB)'), download_url($full_src_url), src_version, src_suffix, full_src_size)?> 
+<?php printf(_('Full source tarball, for Windows and Mac machines that lack necessary dependencies to compile Audacity.'))?></p></li>
+<li><p><?php printf(_('<a href="%s">Latest SVN Development Code</a>'), "../community/developers#svn")?></p></li>
+<li><p><?php printf(_('If you have trouble with your download, or need an older version of Audacity, try:')) ?></p>
    <ul><li><?php printf(_('<a href="%s">SourceForge</a> (older versions can be viewed by clicking to enter the required folder under "Name")'), "http://sourceforge.net/project/showfiles.php?group_id=6235")?></li>
        <li><?php printf(_('<a href="%s">Google Code</a> (click on the headings to sort the list)'), "http://code.google.com/p/audacity/downloads/list")?></li>
    </ul>
@@ -33,20 +41,15 @@
 
 <h3 id="sysreq"><?=_("System Requirements")?></h3>
  <ul>
-  <li><?=_('Audacity runs best with at least 64 MB RAM and a 300 MHz processor. Please review our operating system support for')?> <a href="./windows#sysreq">Windows</a>, <a href="./mac#sysreq">Mac</a> <?=_('or')?> <a href="./linux#sysreq">Linux</a>.</li>
+  <li><?=_('See operating system support for')?> <a href="./windows#sysreq">Windows</a>, <a href="./mac#sysreq">Mac</a> <?=_('or')?> <a href="./linux#sysreq">GNU/Linux</a>.</li>
  </ul>
 
 
 <h3 id="instructions"><?=_("How to Compile Audacity")?></h3>
 
 <h4><?=_("Dependencies")?></h4>
-<p><?=_('The <a href="http://wxwidgets.org">wxWidgets</a> library is <b>required</b>.  Audacity 1.2 needs wxGTK 2.4, compiled <em>without</em> the unicode options. The next version of Audacity will support newer wxWidgets and GTK libraries.')?></p>
-<p><?=_('Installation of the following libraries is <b>optional</b> - they are included in Audacity obtained from <a href="../community/developers#svn">SVN</a>.')?></p>
-<ul>
-  <li><a href="http://www.underbit.com/products/mad/">libmad</a></li>
-  <li><a href="http://www.mega-nerd.com/libsndfile/">libsndfile</a></li>
-  <li><a href="http://vorbis.com/">Ogg Vorbis</a></li>
-</ul>
+<p><?php printf(_('The <a href="http://wxwidgets.org">wxWidgets</a> library is required. Audacity %s requires wxGTK 2.8.12. The <a href="http://www.mega-nerd.com/libsndfile/">libsndfile</a> library is also required and is included in Audacity obtained from <a href="/community/developers#svn">SVN</a>. Installation of other libraries is <a href="http://wiki.audacityteam.org/wiki/Developing_On_Linux#Optional_Packages_and_Features">optional</a>.'), src_version)?></p>
+
 <p><?=_('If you install libraries using a package management system like Apt or RPM, you need to install the "dev" (development) packages for each library.')?></p>
 
 <h4><?=_("Compilation")?></h4>
@@ -74,14 +77,14 @@
   '<li>',
   '<a href="http://forum.audacityteam.org/viewforum.php?f=19">',
   '</a>',
-  '<a href="http://forum.audacityteam.org/">',
-  '</a>',
+  '<a href="http://forum.audacityteam.org">',
+  '</a>', 
   '</li></ul>');
+
 ?></p>
 
+
 <p>&nbsp;</p>
-
-
 
 <?php
   include "../include/footer.inc.php";
