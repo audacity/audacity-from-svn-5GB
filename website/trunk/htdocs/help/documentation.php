f80c7a07885e7de222a8568b9042ae3853669280
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2012-03-13 18:13:31 +0000

    Update for 2.0.0 including new "legacy_documentation" page for 1.2 Manuals.

--- website/trunk/htdocs/help/documentation.php
+++ website/trunk/htdocs/help/documentation.php
@@ -1,10 +1,11 @@
 <?php
 /*
- * Copyright 2004 - 2011 Matt Brubeck, Gale Andrews
+ * Copyright 2004 - 2012 Matt Brubeck, Gale Andrews
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/3.0/
  */
   require_once "main.inc.php";
+  require_once "../latest/versions.inc.php";
   $pageId = "documentation";
   $pageTitle = _("Documentation");
   include "../include/header.inc.php";
@@ -12,25 +13,25 @@
 	echo "<h2>$pageTitle</h2>";
 
      // i18n-hint:  Please add a note that the manuals below are in English.
-     // If there is documentation available in your language not already
-     // linked to at the bottom of this page, please add the links.
-	echo "<p>"._('This page contains manuals and quick reference guides for the free Audacity sound editor.  For additional help, see the <a href="faq">Frequently Asked Questions</a> and our <a href="http://audacityteam.org/wiki/index.php?title=Tutorials">Tutorials</a> and <a href="http://audacityteam.org/wiki/index.php?title=Tips">Tips</a> on the Audacity <a href="http://audacityteam.org/wiki/">Wiki</a>.')."</p>";
+     // If there is documentation for Audacity 2.x available in your language
+     // but not already linked to at the bottom of this page, please add the links.
+	echo "<p>"._('This page contains links to Manuals for the free Audacity audio editor. For additional help, see the <a href="/faq">Frequently Asked Questions</a> and additional <a href="http://wiki.audacityteam.org/wiki/Category:Tutorial">Tutorials</a> and <a href="http://wiki.audacityteam.org/wiki/Category:Tips">Tips</a> on the Audacity <a href="http://wiki.audacityteam.org/">Wiki</a>.')."</p>";
 ?>
 
-<h3><?=_('Quick Reference Guide')?></h3>
-<ul>
-	<li><a href="../onlinehelp-1.2/contents.htm"><?=_('Read the Audacity 1.2 reference online.')?></a> <?=_('(You can also access this reference through the "Help" menu in the Audacity program.)')?></li>
-	<li><a href="../onlinehelp-1.2/reference.html"><?=_('Printable version of the Audacity 1.2 reference.')?></a> <?=_('(This can be printed in one step using the print function of your browser.)')?></li>
- </ul>
+<p><?=_('Legacy documentation for old versions of Audacity can be found <a href="/help/legacy_documentation">here</a>.')?></p>
 
-<h3><?=_('Audacity User\'s Manuals')?></h3>
+<h3><?=_('Audacity Manuals (with Tutorials)')?></h3>
+
+<p><?=_('The <a href="/download/windows">Windows .exe</a> and <a href="/download/mac">Mac OS X .dmg</a> installers already include a built-in copy of the Manual. To access it, click Help > Manual (in web browser) in the Audacity program.')?></p>
+
+<p>
 <ul>
-	<li><a href="../manual-1.2/"><?=_('Read the Audacity 1.2 manual online.')?></a></li>
-	<li><a href="../audacity-manual-1.2.zip"><?=_('Download the Audacity 1.2 manual in HTML format.')?></a> <?=_('(To use the manual, unzip it and open the "index.html" file.)')?></li>
-	<li><a href="audacity-manual-1.2.pdf"><?=_('Download the Audacity 1.2 manual in PDF format.')?></a></li>
-	<li><a href="http://manual.audacityteam.org/man/Unzipping_the_Manual"><?=_('Download the latest Audacity 1.3 Beta manual in HTML format')?></a> <?=_('(The Audacity Beta installers for Windows and Mac already include a built-in copy of the Manual accessed through the "Help" menu in the Audacity program.)')?></li>
-	<li><a href="http://manual.audacityteam.org/index.php?title=Main_Page"><?=_('Read the latest Audacity 1.3 alpha manual online.</a>')?> <?=_('(The latest Audacity 1.3 alpha Nightly Builds for testing purposes can be downloaded <a href="http://wiki.audacityteam.org/wiki/Nightly_Builds">here</a>.)')?></li>
+<li><a href="http://manual.audacityteam.org/help/manual/
+"><?php printf(_('View the latest Audacity %s Manual online'), src_version)?></a>.</li>
+<li><a href="http://manual.audacityteam.org/help/manual/man/unzipping_the_manual.html"><?php printf(_('Download and install the latest Audacity %s Manual in HTML format'), src_version)?></a>  <?=_('(Most users on <a href="/download/linux">GNU/Linux</a> will need this if they want a built-in Manual).')?></li>
+<li><a href="http://manual.audacityteam.org/index.php?title=Main_Page"><?=_('View the latest Audacity alpha development Manual online.</a>')?> <?=_('(This Manual is only for the latest Audacity alpha <a href="http://wiki.audacityteam.org/wiki/Nightly_Builds">Nightly Builds</a>).')?></li>
 </ul>
+</p>
 
 <?php
 	echo
@@ -38,13 +39,9 @@
      // specific languages and should be left untranslated.
 	_(' ');
 ?>
-
 <div id="fr">
   <h3>Aide en Français</h3>
   <ul>
-    <li><a href="../localhelp/fr/audacity-1.2-help.htb">Menu d'aide d'Audacity en français</a> (Remplacer le fichier qui porte comme nom, audacity-1.2-help.htb par celui qu'on vous propose. <a href="http://zonelibre.grics.qc.ca/spip.php?article100">Aide</a>)</li>
-    <li><a href="../localhelp/fr/audacity-fr-manuel-1.2.zip">Manuel d'Audacity en français</a> (zip, 700 ko.)
-    <li><a href="../localhelp/fr/audacity-mode-d'emploi.pdf">Mode d'emploi en français</a> (PDF, 700 ko.  Ce fichier peut prendre un bon moment de télécharger.)</li>
     <li><a href="http://www.pearson.fr/livre/?GCOI=27440100057910">Christian Brochec - Audacity 2</a> Une présentation complète d'Audacity, accompagnée d'exercices pour faciliter la prise en main et aller plus loin dans l'utilisation. <b>Attention:</b> En raison du retard pris par le développement d'Audacity 2.0, ce livre est basé sur la version 1.3.11 qui préfigure largement la version finale.</li>
   </ul>
 </div>
@@ -52,13 +49,9 @@
 <div id="de">
   <h3>Hilfe auf Deutsch</h3>
   <ul>
-    <li><a href="../localhelp/de/audacity-1.2-help.htb">Deutsche Hilfedatei (htb)</a> (Ersetzen Sie audacity-1.2-help.htb im Audacity Installation-Heft mit diesem Download.)</li>
-    <li><a href="../localhelp/de/audacity-deutsch-handbuch-23-Jan-2005.pdf">Deutsches Handbuch (PDF)</a></li>
-    <li><a href="http://www.brain-media.de/index.php/catalogsearch/result/?q=audacity">Markus Priemer - Audacity kompakt</a> Das deutschsprachige Anwenderhandbuch zu Audacity. 
-Herausgegeben vom brain-media.de. Mit Unterst&uuml;tzung und einem Vorwort von Audacity-Entwickler Markus Meyer.</li>
+    <li><a href="http://www.brain-media.de/index.php/catalogsearch/result/?q=audacity">Markus Priemer - Audacity kompakt</a> Das deutschsprachige Anwenderhandbuch zu Audacity, basierend auf 1.3.7, die etwas &auml;hnelt Audacity 2.0. Herausgegeben vom brain-media.de. Mit Unterst&uuml;tzung und einem Vorwort von Audacity-Entwickler Markus Meyer.</li>
  </ul>
 </div>
-
 
 
 <?php
