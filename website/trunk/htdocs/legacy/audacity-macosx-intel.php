5a02c5ba3acafe70719e047b034f81d76d195a0f
v.audacity <v.audacity@47890b92-0858-11df-a26f-8b716316a5bc>
2012-04-12 20:18:08 +0000

    Partially revert http://code.google.com/p/audacity/source/detail?r=11682.

--- website/trunk/htdocs/legacy/audacity-macosx-intel.php
+++
@@ -1,9 +0,0 @@
-<?
-
-# perform a redirect to the latest version, so links to it can remain static
-
-include 'mirror.inc.php';
-include 'versions.inc.php';
-header('Location: '.download_url('audacity-macosx-intel-'.macosx_intel_version.'.dmg'));
-
-?>
