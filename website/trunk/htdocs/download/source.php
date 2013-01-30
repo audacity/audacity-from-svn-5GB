52e80c223d6c64d64a0542b579545f6c52de36fe
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2013-01-30 13:28:12 +0000

    tweak to prevent link and text running together in translated versions

--- website/trunk/htdocs/download/source.php
+++ website/trunk/htdocs/download/source.php
@@ -31,7 +31,7 @@
 <ul>
 <!--<li><p><?php printf(_('<a href="%s">Audacity %s source tarball</a> (%s file, %.1lf MB)'), download_url($full_src_url), src_version, src_suffix, full_src_size)?> 
 <?php printf(_('full source tarball: Primarily for use on Windows and Mac, as they typically lack necessary dependencies to compile Audacity. Also needed on a GNU/Linux system configured as an end-user.'))?></p></li>-->
-<li><p><?php printf(_('<a href="%s">SVN Development Code</a> '), "../community/developers#svn")?>
+<li><p><?php printf(_('<a href="%s">SVN Development Code</a>&nbsp;'), "../community/developers#svn")?>
 <?php printf(_('Check out the latest Audacity SVN development code. You can also check out a specific tagged release from SVN, suitable for a Windows, Mac or other system that lacks necessary dependencies to compile the standard source tarball.'))?>
 </p></li> 
 <li><p><?php printf(_('If you have trouble with your download, or need an older version of Audacity, try:')) ?></p>
