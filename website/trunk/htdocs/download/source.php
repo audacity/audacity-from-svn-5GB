87147dad511cd6c374129fe73a1977a2d6cb415b
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2013-07-15 13:22:49 +0000

    Wording tweak. Some users thought HEAD was Linux only, and tagged releases not for a standard Linux system.

--- website/trunk/htdocs/download/source.php
+++ website/trunk/htdocs/download/source.php
@@ -23,7 +23,7 @@
 
 <ul>
 <li><p><?php printf(_('<a href="%s">Audacity %s source tarball</a> (%s file, %.1lf MB)'), download_url($min_src_url), src_version, src_suffix, min_src_size)?> 
-<?php printf(_('standard source tarball, primarily for GNU/Linux. Assumes your system has necessary dependencies/libraries installed (see below).'))?></p></li>
+<?php printf(_('standard source tarball, primarily for GNU/Linux. This assumes your system has necessary dependencies and libraries installed. For Windows and Mac, see SVN (below).'))?></p></li>
 </ul>
 
 <h3><?=_("Optional Downloads")?></h3>
@@ -31,8 +31,8 @@
 <ul>
 <!--<li><p><?php printf(_('<a href="%s">Audacity %s source tarball</a> (%s file, %.1lf MB)'), download_url($full_src_url), src_version, src_suffix, full_src_size)?> 
 <?php printf(_('full source tarball: Primarily for use on Windows and Mac, as they typically lack necessary dependencies to compile Audacity. Also needed on a GNU/Linux system configured as an end-user.'))?></p></li>-->
-<li><p><?php printf(_('<a href="%s">SVN Development Code</a>&nbsp;'), "../community/developers#svn")?>
-<?php printf(_('Check out the latest Audacity SVN development code. You can also check out a specific tagged release from SVN, suitable for a Windows, Mac or other system that lacks necessary dependencies to compile the standard source tarball.'))?>
+<li><p><?php printf(_('<a href="%s">SVN Repository</a>&nbsp;'), "../community/developers#svn")?>
+<?php printf(_('Check out either the latest Audacity development code or a specific tagged release. SVN checkouts are suitable for any system, including those that lack necessary dependencies to compile the standard source tarball.'))?>
 </p></li> 
 <li><p><?php printf(_('If you have trouble with your download, or need an older version of Audacity, try:')) ?></p>
    <ul><li><?php printf(_('<a href="%s">SourceForge</a>: View older versions by clicking to enter the required folder under "Name".'), "http://sourceforge.net/project/showfiles.php?group_id=6235")?></li>
