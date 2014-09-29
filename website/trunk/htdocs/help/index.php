d26a1974936f019f7c80afa566f73b6da6dde975
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2014-09-29 10:16:31 +0000

    Remove link to wiki legacy tutorials.

--- website/trunk/htdocs/help/index.php
+++ website/trunk/htdocs/help/index.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2004 - 2012 Matt Brubeck, Gale Andrews
+ * Copyright 2004 - 2014 Matt Brubeck, Gale Andrews
  * This file is licensed under a Creative Commons license:
  * http://creativecommons.org/licenses/by/3.0/
  */
@@ -19,7 +19,7 @@ echo _('<p><b>Questions?</b> First, check our <a href="faq">Frequently Asked Que
  <ul>
    <li><a href="documentation">Manuals</a> - Read the Audacity Manual (with Tutorials). These Tutorials offer step-by-step help with common tasks in Audacity, such as making ringtones, removing vocals, creating podcasts and transferring tapes and records to computer.</li>
    <li><a href="http://wiki.audacityteam.org/">Audacity Wiki</a> is an extensive user-editable help resource including:
-    <ul><li><a href="http://wiki.audacityteam.org/wiki/Category:Tutorial">Tutorials</a> - More specialised tutorials, including some <a href="http://wiki.audacityteam.org/wiki/Category:Tutorial#legacy">legacy tutorials</a> for old versions of Audacity, links to <a href="http://wiki.audacityteam.org/wiki/Category:Tutorial#external">other tutorials on the web</a> and some tutorials in <a href="http://wiki.audacityteam.org/wiki/MultiLingual">languages other than English</a>.</li>
+    <ul><li><a href="http://wiki.audacityteam.org/wiki/Category:Tutorial">Tutorials</a> - Additional and more specialised tutorials, links to <a href="http://wiki.audacityteam.org/wiki/Category:Tutorial#external">other tutorials on the web</a> and  <a href="http://wiki.audacityteam.org/wiki/MultiLingual">support forums in other than English</a>.</li>
      <li><a href="http://wiki.audacityteam.org/wiki/Category:Tips">Tips</a> - these articles will help you work with and understand Audacity and your audio hardware.</li>
     </ul>
   </li>
